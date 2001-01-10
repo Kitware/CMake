d9a73d8d246b2f091eccd6f27493567c2e31bd8b
Will Schroeder <will.schroeder@kitware.com>
2001-01-10 17:13:32 -0500

    ENH:Beginning clean up; adding documentation

--- Source/cmAuxSourceDirectoryRule.h
+++ Source/cmAuxSourceDirectoryRule.h
@@ -1,35 +1,77 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile$
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
 #ifndef cmAuxSourceDirectoryRule_h
 #define cmAuxSourceDirectoryRule_h
 
 #include "cmStandardIncludes.h"
 #include "cmRuleMaker.h"
 
-
+/** \class cmAuxSourceDirectoryRule
+ * \brief Specify auxiliary source code directories.
+ *
+ * cmAuxSourceDirectoryRule specifies source code directories
+ * that must be built as part of this build process. This directories
+ * are not recursively processed like the SUBDIR rule (cmSubdirRule).
+ * A side effect of this rule is to create a subdirectory in the build
+ * directory structure.
+ */
 class cmAuxSourceDirectoryRule : public cmRuleMaker
 {
 public:
+  /**
+   * This is a virtual constructor for the rule.
+   */
   virtual cmRuleMaker* Clone() 
     {
-      return new cmAuxSourceDirectoryRule;
+    return new cmAuxSourceDirectoryRule;
     }
-  // This is called when the rule is firt encountered in
-  // the input file
+
+  /**
+   * This is called when the rule is first encountered in
+   * the CMakeLists.txt file.
+   */
   virtual bool Invoke(std::vector<std::string>& args);
+
+  /**
+   * This is called at the end after all the information
+   * specified by the rules is accumulated.
+   */
   virtual void FinalPass() { }
   
-  // This is the name used in the input file.
+  /**
+   * The name of the rule as specified in CMakeList.txt.
+   */
   virtual const char* GetName() { return "AUX_SOURCE_DIRECTORY";}
+  
+  /**
+   * Succinct documentation.
+   */
   virtual const char* TerseDocumentation() 
     {
-      return "Add all the source files found in the specified directory to\n"
-        " the build.";
+    return "Add all the source files found in the specified directory to\n"
+           " the build.";
     }
   
-  // Return full documentation for the rule
+  /**
+   * More documentation.
+   */
   virtual const char* FullDocumentation()
     {
-      return
-        "AUX_SOURCE_DIRECTORY(dir)";
+    return
+      "AUX_SOURCE_DIRECTORY(dir)";
     }
 };
 
