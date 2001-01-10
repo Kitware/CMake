be6b895a3a523729a3d497f7c100fb140044b538
Will Schroeder <will.schroeder@kitware.com>
2001-01-10 17:29:46 -0500

    ENH:Beginning clean up; adding documentation

--- Source/cmFindIncludeRule.h
+++ Source/cmFindIncludeRule.h
@@ -1,35 +1,80 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile$
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
 #ifndef cmFindIncludeRule_h
 #define cmFindIncludeRule_h
 
 #include "cmStandardIncludes.h"
 #include "cmRuleMaker.h"
 
-
+/** \class cmFindIncludeRule
+ * \brief Define a rule that searches for an include file.
+ *
+ * cmFindIncludeRule is used to define a variable include
+ * path location by specifying a file and list of directories.
+ */
 class cmFindIncludeRule : public cmRuleMaker
 {
 public:
+  /**
+   * This is a virtual constructor for the rule.
+   */
   virtual cmRuleMaker* Clone() 
     {
-      return new cmFindIncludeRule;
+    return new cmFindIncludeRule;
     }
-  // This is called when the rule is firt encountered in
-  // the input file
-  virtual bool Invoke(std::vector<std::string>& args);
-  virtual void FinalPass() { }
-  virtual bool IsInherited() { return true;  }
 
-  // This is the name used in the input file.
+  /**
+   * This is called when the rule is first encountered in
+   * the CMakeLists.txt file.
+   */
+  virtual bool Invoke(std::vector<std::string>& args);
+  
+  /**
+   * This is called at the end after all the information
+   * specified by the rules is accumulated.
+   */
+  virtual void FinalPass() { }
+
+  /**
+   * This determines if the rule gets propagated down
+   * to makefiles located in subdirectories.
+   */
+  virtual bool IsInherited() 
+    {return true;}
+
+  /**
+   * The name of the rule as specified in CMakeList.txt.
+   */
   virtual const char* GetName() { return "FIND_INCLUDE";}
+
+  /**
+   * Succinct documentation.
+   */
   virtual const char* TerseDocumentation() 
     {
-      return "Find an include path.";
+    return "Find an include path.";
     }
   
-  // Return full documentation for the rule
+  /**
+   * More documentation.
+   */
   virtual const char* FullDocumentation()
     {
-      return
-        "FIND_INCLUDE(DEFINE try1 try2 ...);";
+    return
+      "FIND_INCLUDE(DEFINE try1 try2 ...);";
     }
 };
 
