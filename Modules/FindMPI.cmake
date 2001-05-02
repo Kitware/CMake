0e77477a783804150ed474096ad5d4457082a8f6
Ken Martin <ken.martin@kitware.com>
2001-05-02 17:33:26 -0400

    minor fixes

--- Modules/FindMPI.cmake
+++ Modules/FindMPI.cmake
@@ -7,17 +7,18 @@
 # MPI_LIBRARY      = the library to link against (mpi mpich etc)
 #
 
-FIND_INCLUDE(MPI_INCLUDE_PATH mpi.h /usr/local/include /usr/include)
+FIND_PATH(MPI_INCLUDE_PATH mpi.h /usr/local/include /usr/include /usr/local/mpi/include)
 
 # look for the different MPI libs
 IF (NOT MPI_LIB_PATH)
-  FIND_LIBRARY(MPI_LIB_PATH mpi /usr/lib /usr/local/lib)
+  FIND_LIBRARY(MPI_LIB_PATH mpi /usr/lib /usr/local/lib /usr/local/mpi/lib)
   IF (MPI_LIB_PATH)
     SET (MPI_LIBRARY mpi CACHE)
   ENDIF (MPI_LIB_PATH)
 ENDIF (NOT MPI_LIB_PATH)
+
 IF (NOT MPI_LIB_PATH)
-  FIND_LIBRARY(MPI_LIB_PATH mpich /usr/lib /usr/local/lib)
+  FIND_LIBRARY(MPI_LIB_PATH mpich /usr/lib /usr/local/lib /usr/local/mpi/lib)
   IF (MPI_LIB_PATH)
     SET (MPI_LIBRARY mpich CACHE)
   ENDIF (MPI_LIB_PATH)
