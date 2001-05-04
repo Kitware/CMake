7bf828d132104896a58374b318f36b60dc59444b
Geoffrey Cross <geoff@cross.lu>
2001-05-04 11:35:47 -0400

    INCLUDE these to find the relevant libraries

---
+++ Modules/FindGTK.cmake
@@ -0,0 +1,28 @@
+#
+# try to find GTK
+#
+
+# don't even bother under WIN32
+IF (UNIX)
+
+  FIND_PATH(GTK_INCLUDE_PATH gtk/gtk.h
+  /usr/include
+  /usr/local/include
+  /usr/openwin/share/include
+  )
+
+  FIND_LIBRARY(GTK_LIB_PATH gtk
+  /usr/lib
+  /usr/local/lib
+  /usr/openwin/lib
+  /usr/X11R6/lib
+  )
+
+  FIND_LIBRARY(GTK_GLIB_INCLUDE_PATH glibconfig.h
+  /usr/include
+  /usr/local/include
+  /usr/openwin/share/include
+  )
+
+ENDIF (UNIX)
+
---
+++ Modules/FindJPEG.cmake
@@ -0,0 +1,15 @@
+#
+# Find the native JPEG includes and library
+#
+
+
+FIND_PATH(NATIVE_JPEG_INCLUDE_PATH jpeglib.h
+/usr/local/include
+/usr/include
+)
+
+FIND_LIBRARY(NATIVE_JPEG_LIB_PATH jpeg
+/usr/lib
+/usr/local/lib
+)
+
