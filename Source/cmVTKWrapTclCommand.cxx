67e31b789f0db7431232e499268cd2caed460bf5
Ken Martin <ken.martin@kitware.com>
2001-05-08 16:20:24 -0400

    fixed bug in init code

--- Source/cmVTKWrapTclCommand.cxx
+++ Source/cmVTKWrapTclCommand.cxx
@@ -196,7 +196,7 @@ bool cmVTKWrapTclCommand::WriteInit(const char *kitName,
     fprintf(fout,"ClientData %sNewCommand();\n",classes[i].c_str());
     }
   
-  if (!strcmp(kitName,"Vtkcommon"))
+  if (!strcmp(kitName,"Vtkcommontcl"))
     {
     fprintf(fout,"int vtkCommand(ClientData cd, Tcl_Interp *interp,\n             int argc, char *argv[]);\n");
     fprintf(fout,"\nTcl_HashTable vtkInstanceLookup;\n");
@@ -226,7 +226,7 @@ bool cmVTKWrapTclCommand::WriteInit(const char *kitName,
   
   fprintf(fout,"\n\nint VTK_EXPORT %s_Init(Tcl_Interp *interp)\n{\n",
           kitName);
-  if (!strcmp(kitName,"Vtkcommon"))
+  if (!strcmp(kitName,"Vtkcommontcl"))
     {
     fprintf(fout,
 	    "  vtkTclInterpStruct *info = new vtkTclInterpStruct;\n");
