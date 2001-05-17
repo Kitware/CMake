a0784cc44f63982356ac29164d79d7e72ff2fe21
Ken Martin <ken.martin@kitware.com>
2001-05-17 12:25:33 -0400

    no longer used

--- CMakeWindowsSystemConfig.cmake
+++
@@ -1,9 +0,0 @@
-# 
-# CMakeLocal.make.in should be in the directory where you run configure
-# in, which need not be the source directory
-#
-SET (WORDS_BIGENDIAN )
-SET (HAVE_LIMITS_H   1)
-SET (HAVE_UNISTD_H   1)
-SET (CXX  VC++60)
-
