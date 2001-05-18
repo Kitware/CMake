0df1da0ebaa0c01d75bc8201ba0c5b682f43ff90
Ken Martin <ken.martin@kitware.com>
2001-05-18 15:22:17 -0400

    compiler fix

--- Source/cmMSProjectGenerator.cxx
+++ Source/cmMSProjectGenerator.cxx
@@ -41,6 +41,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "cmMSProjectGenerator.h"
 #include "cmDSWMakefile.h"
 #include "cmDSPMakefile.h"
+#include "cmCacheManager.h"
 
 cmMSProjectGenerator::cmMSProjectGenerator()
 {
