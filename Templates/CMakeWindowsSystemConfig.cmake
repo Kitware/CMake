1a2918040d99a073417b7b230ab7898933300ae9
Sebastien Barre <sebastien.barre@kitware.com>
2001-05-25 15:27:52 -0400

    better help

--- Templates/CMakeWindowsSystemConfig.cmake
+++ Templates/CMakeWindowsSystemConfig.cmake
@@ -7,10 +7,10 @@ SET (HAVE_LIMITS_H   1)
 SET (HAVE_UNISTD_H   1)
 SET (CXX  VC++60 )
 SET (CMAKE_CXX_FLAGS_RELEASE "/MD /O2" CACHE STRING
-        "Flags used by the compiler during release builds")
+        "Flags used by the compiler during release builds (/MD /Ob1 /Oi /Ot /Oy /Gs will produce slightly less optimized but smaller files)")
 SET (CMAKE_CXX_FLAGS_MINSIZEREL "/MD /O1" CACHE STRING
         "Flags used by the compiler during release minsize builds")
 SET (CMAKE_CXX_FLAGS_DEBUG "/MDd /Zi /Od /GZ" CACHE STRING
         "Flags used by the compiler during debug builds")
 SET (CMAKE_CXX_FLAGS "/W3 /Zm1000 /GX /GR" CACHE STRING
-        "Flags used by the compiler during all build types, /GX /GR are for exceptions and rtti in VC++")
+        "Flags used by the compiler during all build types, /GX /GR are for exceptions and rtti in VC++, /Zm1000 increases the compiler's memory allocation to support ANSI C++/stdlib")
