a85e67d937140880d73a9ae7d7cabded366a14a6
Bill Hoffman <bill.hoffman@kitware.com>
2001-05-30 15:56:17 -0400

    BUG: add missing file

---
+++ Source/cmakemain.cxx
@@ -0,0 +1,17 @@
+#include "cmake.h"
+
+int main(int ac, char** av)
+{
+  cmake cm;
+  if(ac < 2)
+    {
+      cm.Usage(av[0]);
+      return -1;
+    }
+  std::vector<std::string> args;
+  for(int i =0; i < ac; ++i)
+    {
+    args.push_back(av[i]);
+    }
+  return cm.Generate(args);
+}
