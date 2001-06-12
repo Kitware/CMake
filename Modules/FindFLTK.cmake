1292d9da3a4dd7d87fa824933b0241a34d77ae3c
Luis Ibanez <luis.ibanez@kitware.com>
2001-06-11 21:50:16 -0400

    ENH: Added the path used in SuSe Linux 7.1

--- Modules/FindFLTK.cmake
+++ Modules/FindFLTK.cmake
@@ -7,10 +7,11 @@ FIND_PATH(FLTK_INCLUDE_PATH FL/FL.H
 /usr/local/include
 /usr/include
 /usr/local/fltk
+/usr/X11R6/include
 H:/usr/local/fltk
 )
 
-FIND_LIBRARY(FLTK_LIB_PATH  fltk
-PATHS /usr/lib /usr/local/lib /usr/local/fltk/lib H:/usr/local/fltk/lib
+FIND_LIBRARY(FLTK_LIBRARY  fltk
+PATHS /usr/lib /usr/local/lib /usr/local/fltk/lib H:/usr/local/fltk/lib /usr/X11R6/lib
 )
 
