f6f8dde5a8cb25ab190987da3c959a58977b2d7d
Ken Martin <ken.martin@kitware.com>
2001-06-13 16:15:27 -0400

    used to find the dart testing system

---
+++ Modules/FindDart.cmake
@@ -0,0 +1,11 @@
+# 
+# this module looks for the dart testing software and sets DART_ROOT
+# to point to where it found it
+#
+
+FIND_PATH(DART_ROOT README.INSTALL 
+    ${PROJECT_SOURCE_DIR}/Dart 
+     /usr/share/Dart 
+    "C:/Program Files/Dart" 
+    [HKEY_LOCAL_MACHINE\SOFTWARE\Dart\InstallPath]
+    )
