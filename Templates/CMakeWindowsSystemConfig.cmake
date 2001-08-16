18165bbe112364dee8a284288352c694b48745e7
Bill Hoffman <bill.hoffman@kitware.com>
2001-08-16 17:42:58 -0400

    ENH: add CMAKE_USE_WIN32_THREADS to the possible thread types

--- Templates/CMakeWindowsSystemConfig.cmake
+++ Templates/CMakeWindowsSystemConfig.cmake
@@ -14,3 +14,4 @@ SET (CMAKE_CXX_FLAGS_DEBUG "/MDd /Zi /Od /GZ" CACHE STRING
         "Flags used by the compiler during debug builds")
 SET (CMAKE_CXX_FLAGS "/W3 /Zm1000 /GX /GR" CACHE STRING
         "Flags used by the compiler during all build types, /GX /GR are for exceptions and rtti in VC++, /Zm1000 increases the compiler's memory allocation to support ANSI C++/stdlib")
+SET (CMAKE_USE_WIN32_THREADS 1 CACHE BOOL "Use the win32 thread library")
