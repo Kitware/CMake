10c54eb2a509e7d8e634c6c38b67f725093413cd
Sebastien Barre <sebastien.barre@kitware.com>
2001-10-23 17:47:32 -0400

    Find Graphivz's dot too

--- Modules/FindDoxygen.cmake
+++ Modules/FindDoxygen.cmake
@@ -1,7 +1,18 @@
 # 
-# this module looks for Doxygen
+# this module looks for Doxygen and the path to Graphiz's dot
 #
 
 FIND_PROGRAM(DOXYGEN
   doxygen
 )
+
+FIND_PROGRAM(DOT
+  dot
+  "C:/Program Files/ATT/Graphviz/bin"
+)
+# HKEY_CURRENT_USER\Software\AT&T\Graphviz
+
+# Since most of the time dot is called by Doxygen, the path to dot is useful too
+GET_FILENAME_COMPONENT(DOT_PATH ${DOT} PATH CACHE)
+
+
