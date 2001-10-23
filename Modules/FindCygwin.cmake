4b7622b9aeda5f1937e13aaad6851234a1e60c92
Sebastien Barre <sebastien.barre@kitware.com>
2001-10-23 18:30:05 -0400

    Cygwin mod

---
+++ Modules/FindCygwin.cmake
@@ -0,0 +1,19 @@
+# 
+# this module looks for Cygwin and some usual commands
+#
+
+FIND_PATH(CYGWIN_INSTALL_PATH
+  cygwin.bat
+  "C:/Cygwin" 
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Cygnus Solutions\\Cygwin\\mounts v2\\/;native]"
+)
+
+FIND_PROGRAM(RM
+   rm
+   ${CYGWIN_INSTALL_PATH}/bin
+)
+
+FIND_PROGRAM(MV
+   mv
+   ${CYGWIN_INSTALL_PATH}/bin
+)
