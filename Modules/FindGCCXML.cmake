0a5fbacfacb1e80abae4e498815b5c67ac5f12b5
Brad King <brad.king@kitware.com>
2001-10-24 09:41:17 -0400

    ENH: Improved FIND_PROGRAM call to find the executable in PREFIX/share/GCC_XML, the new standard install location.

--- Modules/FindGCCXML.cmake
+++ Modules/FindGCCXML.cmake
@@ -1,8 +1,12 @@
-FIND_PROGRAM(GCCXML gccxml
-             /usr/local/bin
-             /usr/bin
-             [HKEY_CURRENT_USER\\Software\\Kitware\\GCC_XML;loc]
-             "C:/Program Files/GCC_XML")
+FIND_PROGRAM(GCCXML
+  NAMES gccxml
+        ../share/GCC_XML/gccxml
+        ../GCC_XML/gccxml
+  PATHS /usr/share/GCC_XML
+        /usr/local/share/GCC_XML
+        [HKEY_CURRENT_USER\\Software\\Kitware\\GCC_XML;loc]
+        "C:/Program Files/GCC_XML"
+)
 
 IF(GCCXML)
   CONFIGURE_GCCXML(${GCCXML} GCCXML_FLAGS)
