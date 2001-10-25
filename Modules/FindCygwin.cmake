4a99c1a98f04323b805f2a5b350c520d98ba8966
Sebastien Barre <sebastien.barre@kitware.com>
2001-10-24 20:33:52 -0400

    Add TAR (path to tar or gtar)

--- Modules/FindCygwin.cmake
+++ Modules/FindCygwin.cmake
@@ -8,12 +8,21 @@ FIND_PATH(CYGWIN_INSTALL_PATH
   "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Cygnus Solutions\\Cygwin\\mounts v2\\/;native]"
 )
 
+FIND_PROGRAM(MV
+   mv
+   ${CYGWIN_INSTALL_PATH}/bin
+)
+
 FIND_PROGRAM(RM
    rm
    ${CYGWIN_INSTALL_PATH}/bin
 )
 
-FIND_PROGRAM(MV
-   mv
+FIND_PROGRAM(TAR
+   NAMES 
+   tar 
+   gtar
+   PATH
    ${CYGWIN_INSTALL_PATH}/bin
 )
+
