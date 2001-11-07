a3f6e2f9c488f3f1e2e12095be0a3e2e9853b50e
Berk Geveci <berk.geveci@kitware.com>
2001-11-07 15:01:59 -0500

    Trying to fix build problems related to curses.

--- Source/CursesDialog/cmCursesStandardIncludes.h
+++ Source/CursesDialog/cmCursesStandardIncludes.h
@@ -1,7 +1,20 @@
 #ifdef __hpux
- #define _XOPEN_SOURCE_EXTENDED
  #define _BOOL_DEFINED
+ #ifndef _XOPEN_SOURCE_EXTENDED
+  #include <sys/time.h>
+  #define _XOPEN_SOURCE_EXTENDED
+  #include <curses.h>
+  #include <form.h>
+  #undef _XOPEN_SOURCE_EXTENDED
+ #else
+  #include <curses.h>
+  #include <form.h>
+ #endif  
+#else  /* __hpux */
+ #include <curses.h>
+ #include <form.h>
 #endif /* __hpux */
 
-#include <curses.h>
-#include <form.h>
+#ifndef getmaxyx
+ #define getmaxyx(w,y,x) ((y) = getmaxy(w), (x) = getmaxx(w))
+#endif
