0e7e1c110c5cb6c0c79804e6d2654331b19db3d0
Ken Martin <ken.martin@kitware.com>
2002-01-04 09:23:27 -0500

    added release version

--- Source/MFCDialog/CMakeSetupDialog.cpp
+++ Source/MFCDialog/CMakeSetupDialog.cpp
@@ -219,8 +219,8 @@ BOOL CMakeSetupDialog::OnInitDialog()
     
   // Set the version number
   char tmp[1024];
-  sprintf(tmp,"Version %d.%d", cmMakefile::GetMajorVersion(),
-          cmMakefile::GetMinorVersion());
+  sprintf(tmp,"Version %d.%d - %s", cmMakefile::GetMajorVersion(),
+          cmMakefile::GetMinorVersion(), cmMakefile::GetReleaseVersion());
   SetDlgItemText(IDC_CMAKE_VERSION, tmp);
   this->UpdateData(FALSE);
   return TRUE;  // return TRUE  unless you set the focus to a control
