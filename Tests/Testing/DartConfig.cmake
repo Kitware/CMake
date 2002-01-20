df5131adc294301b08783a6bc9738afd2049d197
Sebastien Barre <sebastien.barre@kitware.com>
2002-01-20 00:11:59 -0500

    More coverage + include Dart.cmake to maximize chance nslookup/hostname are found

---
+++ Tests/Testing/DartConfig.cmake
@@ -0,0 +1,24 @@
+# Dashboard is opened for submissions for a 24 hour period starting at
+# the specified NIGHLY_START_TIME. Time is specified in 24 hour format.
+SET (NIGHTLY_START_TIME "23:00:00 EST")
+
+# Dart server to submit results (used by client)
+SET (DROP_SITE "")
+SET (DROP_LOCATION "")
+SET (DROP_SITE_USER "")
+SET (DROP_SITE_PASSWORD "")
+SET (TRIGGER_SITE "")
+
+# Dart server configuration 
+# SET (CVS_WEB_URL "")
+# SET (CVS_WEB_CVSROOT "")
+# SET (USE_DOXYGEN "Off")
+# SET (DOXYGEN_URL "")
+# SET (GNATS_WEB_URL "")
+
+# Continuous email delivery variables
+# SET (CONTINUOUS_FROM "")
+# SET (SMTP_MAILHOST "")
+# SET (CONTINUOUS_MONITOR_LIST "")
+# SET (CONTINUOUS_BASE_URL "")
+
