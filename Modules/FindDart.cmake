e2294ae59116b0b7cd90f9648070ba9c2638c83c
Bill Hoffman <bill.hoffman@kitware.com>
2002-02-25 13:20:48 -0500

    ENH: look for Dart in c:

--- Modules/FindDart.cmake
+++ Modules/FindDart.cmake
@@ -7,6 +7,7 @@ FIND_PATH(DART_ROOT README.INSTALL
     $ENV{DART_ROOT}
     ${PROJECT_SOURCE_DIR}/Dart 
      /usr/share/Dart 
+    C:/Dart 
     "C:/Program Files/Dart" 
     ${PROJECT_SOURCE_DIR}/../Dart 
     [HKEY_LOCAL_MACHINE\\SOFTWARE\\Dart\\InstallPath]
