d6c6ab789f51c3c4392d210bdcec03778bdfaa1b
Bill Hoffman <bill.hoffman@kitware.com>
2002-04-09 08:55:26 -0400

    change EST to EDT

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -1,6 +1,6 @@
 # Dashboard is opened for submissions for a 24 hour period starting at
 # the specified NIGHLY_START_TIME. Time is specified in 24 hour format.
-SET (NIGHTLY_START_TIME "22:00:00 EST")
+SET (NIGHTLY_START_TIME "22:00:00 EDT")
 
 # Dart server to submit results (used by client)
 SET (DROP_SITE "public.kitware.com")
