8e7e53960e4e7ecb34f04e7d2679afc05cb81c3e
Bill Hoffman <bill.hoffman@kitware.com>
2002-04-10 11:23:30 -0400

    ENH: only check flag on unix

--- Tests/ComplexRelativePaths/Library/testConly.c
+++ Tests/ComplexRelativePaths/Library/testConly.c
@@ -2,11 +2,13 @@
 
 int CsharedFunction()
 {
+#if !defined(_WIN32) || defined(__CYGWIN__)
 #ifndef TEST_C_FLAGS
   printf("TEST_C_FLAGS failed\n");
   return 0;
 #else
   printf("Passed: TEST_C_FLAGS passed\n");  
 #endif  
+#endif
   return 1;
 }
