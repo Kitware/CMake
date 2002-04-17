c83a8ac2865839f3b49b5ceb4d5d70fcefce3bcd
Brad King <brad.king@kitware.com>
2002-04-17 14:52:28 -0400

    ENH: Updated for latest Cable from CVS.  Old alpha version of Cable is no longer supported.

--- Modules/FindCABLE.cmake
+++ Modules/FindCABLE.cmake
@@ -23,55 +23,46 @@ IF(CABLE_BUILD_DIR)
                EXECUTABLE_OUTPUT_PATH
                MAKECOMMAND
              INCLUDE_INTERNALS
-               CABLE_LIBRARY_OUTPUT_PATH
-               CABLE_EXECUTABLE_OUTPUT_PATH)
+               CABLE_LIBRARY_PATH
+               CABLE_EXECUTABLE_PATH)
 
-  IF(CABLE_LIBRARY_OUTPUT_PATH)
-    FIND_LIBRARY(CABLE_TCL_LIBRARY NAMES WrapTclFacility PATHS
-                 ${CABLE_LIBRARY_OUTPUT_PATH}
-                 ${CABLE_LIBRARY_OUTPUT_PATH}/*)
-  ELSE(CABLE_LIBRARY_OUTPUT_PATH)
-    FIND_LIBRARY(CABLE_TCL_LIBRARY NAMES WrapTclFacility PATHS
-                 ${CABLE_BINARY_DIR}/WrapTclFacility
-                 ${CABLE_BINARY_DIR}/WrapTclFacility/*)
-  ENDIF(CABLE_LIBRARY_OUTPUT_PATH)
+  IF(CABLE_LIBRARY_PATH)
+    FIND_LIBRARY(CABLE_TCL_LIBRARY NAMES CableTclFacility PATHS
+                 ${CABLE_LIBRARY_PATH}
+                 ${CABLE_LIBRARY_PATH}/*)
+  ELSE(CABLE_LIBRARY_PATH)
+    FIND_LIBRARY(CABLE_TCL_LIBRARY NAMES CableTclFacility PATHS
+                 ${CABLE_BINARY_DIR}/CableTclFacility
+                 ${CABLE_BINARY_DIR}/CableTclFacility/*)
+  ENDIF(CABLE_LIBRARY_PATH)
   
-  IF(CABLE_EXECUTABLE_OUTPUT_PATH)
+  IF(CABLE_EXECUTABLE_PATH)
     FIND_PROGRAM(CABLE NAMES cable PATHS
-                 ${CABLE_EXECUTABLE_OUTPUT_PATH}
-                 ${CABLE_EXECUTABLE_OUTPUT_PATH}/*)
-  ELSE(CABLE_EXECUTABLE_OUTPUT_PATH)
+                 ${CABLE_EXECUTABLE_PATH}
+                 ${CABLE_EXECUTABLE_PATH}/*)
+  ELSE(CABLE_EXECUTABLE_PATH)
     FIND_PROGRAM(CABLE NAMES cable PATHS
                  ${CABLE_BINARY_DIR}/Executables
                  ${CABLE_BINARY_DIR}/Executables/*)
-  ENDIF(CABLE_EXECUTABLE_OUTPUT_PATH)
+  ENDIF(CABLE_EXECUTABLE_PATH)
    
-  SET(CABLE_INCLUDE_DIR ${CABLE_SOURCE_DIR}
-      CACHE STRING "Include path for Cable headers.")
+  FIND_PATH(CABLE_INCLUDE_DIR CableTclFacility/ctCalls.h
+            ${CABLE_SOURCE_DIR})
 ELSE(CABLE_BUILD_DIR)
-  # Use some tricks to find the cable executable relative to somewhere
-  # in the system search path.  CMake will collapse the relative paths
-  # automatically.
-  FIND_PROGRAM(CABLE
-    NAMES cable
-          ../share/Cable/bin/cable
-          ../Cable/bin/cable
-    PATHS /usr/share/Cable/bin
-          /usr/local/share/Cable/bin
-          "C:/Program Files/Cable/bin"
-  )
-
+  # Find the cable executable in the path.
+  FIND_PROGRAM(CABLE NAMES cable)
+  
   # Get the path where the executable sits, but without the executable
   # name on it.
   GET_FILENAME_COMPONENT(CABLE_ROOT_BIN ${CABLE} PATH)
   
   # Find the cable include directory in a path relative to the cable
   # executable.
-  FIND_PATH(CABLE_INCLUDE_DIR WrapTclFacility/wrapCalls.h
-            ${CABLE_ROOT_BIN}/../include)
+  FIND_PATH(CABLE_INCLUDE_DIR CableTclFacility/ctCalls.h
+            ${CABLE_ROOT_BIN}/../share/Cable/include)
   
   # Find the WrapTclFacility library in a path relative to the cable
   # executable.
-  FIND_LIBRARY(CABLE_TCL_LIBRARY NAMES WrapTclFacility PATHS
+  FIND_LIBRARY(CABLE_TCL_LIBRARY NAMES CableTclFacility PATHS
                ${CABLE_ROOT_BIN}/../lib)  
 ENDIF(CABLE_BUILD_DIR)
