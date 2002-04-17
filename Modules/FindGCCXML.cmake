11d50284edbf0dffc7435104fcd6ee9a97a67605
Brad King <brad.king@kitware.com>
2002-04-17 14:51:58 -0400

    ENH: Updated for version 0.2 of GCC-XML.

--- Modules/FindGCCXML.cmake
+++ Modules/FindGCCXML.cmake
@@ -1,13 +1,7 @@
+# Find the GCC-XML front-end executable.
 FIND_PROGRAM(GCCXML
   NAMES gccxml
-        ../share/GCC_XML/gccxml
         ../GCC_XML/gccxml
-  PATHS /usr/share/GCC_XML
-        /usr/local/share/GCC_XML
-        [HKEY_CURRENT_USER\\Software\\Kitware\\GCC_XML;loc]
+  PATHS [HKEY_CURRENT_USER\\Software\\Kitware\\GCC_XML;loc]
         "C:/Program Files/GCC_XML"
 )
-
-IF(GCCXML)
-  CONFIGURE_GCCXML(${GCCXML} GCCXML_FLAGS)
-ENDIF(GCCXML)
