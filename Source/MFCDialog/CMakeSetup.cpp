e57d209bdcb092d86bc9e0d2c2d6a0adf229bfc5
Bill Hoffman <bill.hoffman@kitware.com>
2002-06-12 09:46:02 -0400

    fix warning

--- Source/MFCDialog/CMakeSetup.cpp
+++ Source/MFCDialog/CMakeSetup.cpp
@@ -50,13 +50,13 @@ BOOL CMakeSetup::InitInstance()
   // If you are not using these features and wish to reduce the size
   //  of your final executable, you should remove from the following
   //  the specific initialization routines you do not need.
-
+#if _MFC_VER <= 0x421
 #ifdef _AFXDLL
   Enable3dControls();			// Call this when using MFC in a shared DLL
 #else
   Enable3dControlsStatic();	// Call this when linking to MFC statically
 #endif
-
+#endif
   CMakeCommandLineInfo cmdInfo;
   ParseCommandLine(cmdInfo);
 
