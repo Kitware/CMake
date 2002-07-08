ab4e894b92e9fe8687fb2010d8c24c13890881dc
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-07-08 09:30:02 -0400

    Add module that finds java executables. This module should find java, javac, and jar.

---
+++ Modules/FindJava.cmake
@@ -0,0 +1,22 @@
+#
+# This module finds if Java is installed and determines where the
+# include files and libraries are. This code sets the following
+# variables:
+#
+#  JAVA_RUNTIME        = the full path to the Java runtime
+#  JAVA_COMPILE        = the full path to the Java compiler
+#  JAVA_ARCHIVE        = the full path to the Java archiver
+#
+
+FIND_PROGRAM(JAVA_RUNTIME
+  NAMES java
+)
+
+FIND_PROGRAM(JAVA_ARCHIVE
+  NAMES jar
+)
+
+FIND_PROGRAM(JAVA_COMPILE
+  NAMES javac
+)
+
