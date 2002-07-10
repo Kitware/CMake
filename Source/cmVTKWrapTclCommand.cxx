694cff3cf9fd41b176b60fcf9fe868c6819d04fd
Ken Martin <ken.martin@kitware.com>
2002-07-10 16:07:51 -0400

    fix warning on Sun

--- Source/cmVTKWrapTclCommand.cxx
+++ Source/cmVTKWrapTclCommand.cxx
@@ -267,10 +267,10 @@ bool cmVTKWrapTclCommand::WriteInit(const char *kitName,
 
   if (!strcmp(kitName,"Vtkcommontcl"))
     {
-    fprintf(fout,"void vtkCommonDeleteAssocData(ClientData cd)\n");
+    fprintf(fout,"extern \"C\"\n{\nvoid vtkCommonDeleteAssocData(ClientData cd)\n");
     fprintf(fout,"  {\n");
     fprintf(fout,"  vtkTclInterpStruct *tis = static_cast<vtkTclInterpStruct*>(cd);\n");
-    fprintf(fout,"  delete tis;\n  }\n");
+    fprintf(fout,"  delete tis;\n  }\n}\n");
     }
     
   /* the main declaration */
