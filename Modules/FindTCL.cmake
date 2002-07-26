8fc6bce000fffa87fce9f34a5b859c15f8c51827
Sebastien Barre <sebastien.barre@kitware.com>
2002-07-26 14:06:44 -0400

    ENH: clean the module, add debug libs

--- Modules/FindTCL.cmake
+++ Modules/FindTCL.cmake
@@ -3,15 +3,19 @@
 # include files and libraries are. It also determines what the name of
 # the library is. This code sets the following variables:
 #
-#  TCL_LIBRARY      = the full path to the tcl library found (tcl tcl80 etc)
-#  TCL_STUB_LIBRARY  = the full path to the tcl stub library found
-#  TCL_INCLUDE_PATH = the path to where tcl.h can be found
-#  TCL_TCLSH        = the full path to the tclsh binary (tcl tcl80 etc)
-#  TK_LIBRARY       = the full path to the tk library found (tk tk80 etc)
-#  TK_STUB_LIBRARY  = the full path to the tk stub library found
-#  TK_INCLUDE_PATH  = the path to where tk.h can be found
-#  TK_INTERNAL_PATH = the path to where tkWinInt.h can be found
-#  TK_WISH          = the full path to the wish binary (wish wish80 etc)
+#  TCL_LIBRARY            = full path to the Tcl library (tcl tcl80 etc)
+#  TCL_LIBRARY_DEBUG      = full path to the Tcl library (debug)
+#  TCL_STUB_LIBRARY       = full path to the Tcl stub library
+#  TCL_STUB_LIBRARY_DEBUG = full path to the Tcl stub library (debug)
+#  TCL_INCLUDE_PATH       = path to where tcl.h can be found
+#  TCL_TCLSH              = full path to the tclsh binary (tcl tcl80 etc)
+#  TK_LIBRARY             = full path to the Tk library (tk tk80 etc)
+#  TK_LIBRARY_DEBUG       = full path to the Tk library (debug)
+#  TK_STUB_LIBRARY        = full path to the Tk stub library
+#  TK_STUB_LIBRARY_DEBUG  = full path to the Tk stub library (debug)
+#  TK_INCLUDE_PATH        = path to where tk.h can be found
+#  TK_INTERNAL_PATH       = path to where tkWinInt.h can be found
+#  TK_WISH                = full path to the wish binary (wish wish80 etc)
 #
 
 INCLUDE(${CMAKE_ROOT}/Modules/FindTclsh.cmake)
@@ -21,9 +25,7 @@ GET_FILENAME_COMPONENT(TCL_TCLSH_PATH ${TCL_TCLSH} PATH)
 
 GET_FILENAME_COMPONENT(TK_WISH_PATH ${TK_WISH} PATH)
 
-FIND_LIBRARY(TCL_LIBRARY
-  NAMES tcl tcl84 tcl8.4 tcl83 tcl8.3 tcl82 tcl8.2 tcl80 tcl8.0
-  PATHS  
+SET (TCLTK_POSSIBLE_LIB_PATHS
   /usr/lib 
   /usr/local/lib
   "C:/Program Files/Tcl/lib" 
@@ -34,95 +36,76 @@ FIND_LIBRARY(TCL_LIBRARY
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/lib
   "${TCL_TCLSH_PATH}/../lib"
   "${TK_WISH_PATH}/../lib"
+)
+
+FIND_LIBRARY(TCL_LIBRARY
+  NAMES tcl tcl84 tcl8.4 tcl83 tcl8.3 tcl82 tcl8.2 tcl80 tcl8.0
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
+)
+
+FIND_LIBRARY(TCL_LIBRARY_DEBUG
+  NAMES tcld tcl84d tcl8.4d tcl83d tcl8.3d tcl82d tcl8.2d tcl80d tcl8.0d
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
 )
 
 FIND_LIBRARY(TCL_STUB_LIBRARY
   NAMES tclstub tclstub84 tclstub8.4 tclstub83 tclstub8.3 tclstub82 tclstub8.2 tclstub80 tclstub8.0
-  PATHS  
-  /usr/lib 
-  /usr/local/lib
-  "C:/Program Files/Tcl/lib" 
-  "C:/Tcl/lib" 
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.4;Root]/lib
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.3;Root]/lib
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.2;Root]/lib
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/lib
-  "${TCL_TCLSH_PATH}/../lib"
-  "${TK_WISH_PATH}/../lib"
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
+)
+
+FIND_LIBRARY(TCL_STUB_LIBRARY_DEBUG
+  NAMES tclstubd tclstub84d tclstub8.4d tclstub83d tclstub8.3d tclstub82d tclstub8.2d tclstub80d tclstub8.0d
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
 )
 
 FIND_LIBRARY(TK_LIBRARY 
   NAMES tk tk84 tk8.4 tk83 tk8.3 tk82 tk8.2 tk80 tk8.0
-  PATHS 
-  /usr/lib 
-  /usr/local/lib
-  "C:/Program Files/Tcl/lib" 
-  "C:/Tcl/lib" 
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.4;Root]/lib
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.3;Root]/lib
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.2;Root]/lib
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/lib
-  "${TCL_TCLSH_PATH}/../lib"
-  "${TK_WISH_PATH}/../lib"
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
+)
+
+FIND_LIBRARY(TK_LIBRARY_DEBUG
+  NAMES tkd tk84d tk8.4d tk83d tk8.3d tk82d tk8.2d tk80d tk8.0d
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
 )
 
 FIND_LIBRARY(TK_STUB_LIBRARY 
   NAMES tkstub tkstub84 tkstub8.4 tkstub83 tkstub8.3 tkstub82 tkstub8.2 tkstub80 tkstub8.0
-  PATHS 
-  /usr/lib 
-  /usr/local/lib
-  "C:/Program Files/Tcl/lib" 
-  "C:/Tcl/lib" 
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.4;Root]/lib
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.3;Root]/lib
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.2;Root]/lib
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/lib
-  "${TCL_TCLSH_PATH}/../lib"
-  "${TK_WISH_PATH}/../lib"
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
+)
+
+FIND_LIBRARY(TK_STUB_LIBRARY_DEBUG
+  NAMES tkstubd tkstub84d tkstub8.4d tkstub83d tkstub8.3d tkstub82d tkstub8.2d tkstub80d tkstub8.0d
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
+)
+
+SET (TCLTK_POSSIBLE_INCLUDE_PATHS
+  /usr/include 
+  /usr/local/include
+  /usr/include/tcl8.4
+  /usr/include/tcl8.3
+  /usr/include/tcl8.2
+  /usr/include/tcl8.0
+  "C:/Program Files/Tcl/include" 
+  "C:/Tcl/include" 
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.4;Root]/include
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.3;Root]/include
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.2;Root]/include
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/include
+  "${TCL_TCLSH_PATH}/../include"
+  "${TK_WISH_PATH}/../include"
 )
 
 FIND_PATH(TCL_INCLUDE_PATH tcl.h 
-  /usr/include 
-  /usr/local/include
-  /usr/include/tcl8.2
-  /usr/include/tcl8.3
-  "C:/Program Files/Tcl/include" 
-  "C:/Tcl/include" 
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.4;Root]/include
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.3;Root]/include
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.2;Root]/include
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/include
-  "${TCL_TCLSH_PATH}/../include"
-  "${TK_WISH_PATH}/../include"
+  ${TCLTK_POSSIBLE_INCLUDE_PATHS}
 )
 
 FIND_PATH(TK_INCLUDE_PATH tk.h 
-  /usr/include 
-  /usr/local/include
-  /usr/include/tcl8.2
-  /usr/include/tcl8.3
-  "C:/Program Files/Tcl/include" 
-  "C:/Tcl/include" 
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.4;Root]/include
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.3;Root]/include
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.2;Root]/include
-  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/include
-  "${TCL_TCLSH_PATH}/../include"
-  "${TK_WISH_PATH}/../include"
+  ${TCLTK_POSSIBLE_INCLUDE_PATHS}
 )
 
 IF (WIN32)
   FIND_PATH(TK_INTERNAL_PATH tkWinInt.h
-    /usr/include 
-    /usr/local/include
-    "C:/Program Files/Tcl/include" 
-    "C:/Tcl/include" 
-    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.4;Root]/include
-    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.3;Root]/include
-    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.2;Root]/include
-    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/include
-    "${TCL_TCLSH_PATH}/../include"
-    "${TK_WISH_PATH}/../include"
+    ${TCLTK_POSSIBLE_INCLUDE_PATHS}
   )
   MARK_AS_ADVANCED(TK_INTERNAL_PATH)
 
@@ -132,9 +115,13 @@ IF (WIN32)
     TCL_INCLUDE_PATH
     TK_INCLUDE_PATH
     TCL_LIBRARY
+    TCL_LIBRARY_DEBUG
     TCL_STUB_LIBRARY
+    TCL_STUB_LIBRARY_DEBUG
     TK_LIBRARY  
+    TK_LIBRARY_DEBUG
     TK_STUB_LIBRARY  
+    TK_STUB_LIBRARY_DEBUG
     )
 ENDIF(WIN32)
 
