3663167b9e1d2e57998534cfc2b455f676dad838
Ian Scott <ian.m.scott@stud.man.ac.uk>
2002-09-02 12:58:48 -0400

    This Find Module is in the new style

---
+++ Modules/FindTIFF.cmake
@@ -0,0 +1,28 @@
+#
+# Find the native TIFF includes and library
+#
+# This module defines
+# TIFF_INCLUDE_DIR, where to find tiff.h, etc.
+# TIFF_LIBRARIES, the libraries to link against to use TIFF.
+# TIFF_FOUND, If false, do try to use TIFF.
+
+# also defined, but not for general use are
+# TIFF_LIBRARY, where to find the TIFF library.
+
+FIND_PATH(TIFF_INCLUDE_DIR tiff.h
+  /usr/local/include
+  /usr/include
+)
+
+FIND_LIBRARY(TIFF_LIBRARY tiff
+  /usr/lib
+  /usr/local/lib
+)
+
+IF(NATIVE_TIFF_INCLUDE_PATH)
+  IF(NATIVE_TIFF_LIBRARY)
+    SET( TIFF_FOUND "YES" )
+    SET( TIFF_LIBRARIES ${TIFF_LIBRARY} )
+  ENDIF(NATIVE_TIFF_LIBRARY)
+ENDIF(NATIVE_TIFF_INCLUDE_PATH)
+
