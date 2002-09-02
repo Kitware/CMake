c82608591728c8c88d0ef596655808eda93664a2
Ian Scott <ian.m.scott@stud.man.ac.uk>
2002-09-02 15:46:04 -0400

    Copied the X11 PNG and ZLIB functoinality from VXL

--- Modules/FindX11.cmake
+++ Modules/FindX11.cmake
@@ -1,33 +1,52 @@
 #
-# try to find X11 libraries on UNIX systems once done this will define
+# try to find X11 on UNIX systems.
 #
-# X11_INCLUDE_PATH = where the X11 include directory can be found
-# X11_LIBRARY      = the name of the X11 library to link with
-#
-
-IF (WIN32)
-  SET (X11_LIBRARY x11 CACHE STRING "X11 library")
-ELSE (WIN32)
-  FIND_PATH(X11_INCLUDE_PATH X11.h
-  /usr/include 
-  /usr/local/include 
-  /usr/openwin/share/include 
-  /opt/graphics/OpenGL/include 
-  /usr/X11R6/include 
-  )
-  MARK_AS_ADVANCED(
-    X11_INCLUDE_PATH
-  )
-
-  FIND_LIBRARY(X11_LIBRARY libX11.so
-  /usr/lib 
-  /usr/local/lib 
-  /usr/X11R6/lib
-  /usr/openwin/lib 
+# The following values are defined
+# X11_INCLUDE_DIR  - where to find X11.h
+# X11_LIBRARIES    - link against these to use X11
+# X11_FOUND        - True if X11 is available
+# X11_Xext_FOUND   - True if the X11 extensions are available.
+IF (UNIX)
+  
+  FIND_PATH(X11_INCLUDE_DIR X11/X.h
+    /usr/include 
+    /usr/local/include 
+    /usr/openwin/include 
+    /usr/openwin/share/include 
+    /usr/X11R6/include 
+    /usr/include/X11
+    /opt/graphics/OpenGL/include
   )
 
-ENDIF (WIN32)
 
-MARK_AS_ADVANCED(
-  X11_LIBRARY
-)
+  FIND_LIBRARY(X11_X11_LIBRARY X11
+    /usr/lib 
+    /usr/local/lib 
+    /usr/openwin/lib 
+    /usr/X11R6/lib
+  )
+
+  FIND_LIBRARY(X11_Xext_LIBRARY Xext
+    /usr/lib 
+    /usr/local/lib 
+    /usr/openwin/lib 
+    /usr/X11R6/lib
+  )
+
+  IF(X11_INCLUDE_DIR)
+
+    IF(X11_X11_LIBRARY)
+      SET( X11_FOUND "YES" )
+      SET( X11_LIBRARIES ${X11_X11_LIBRARY} )
+    ENDIF(X11_X11_LIBRARY)
+
+    IF(X11_Xext_LIBRARY)
+      SET( X11_LIBRARIES ${X11_LIBRARIES} ${X11_Xext_LIBRARY} )
+      SET( X11_Xext_FOUND "YES")
+    ENDIF(X11_Xext_LIBRARY)
+
+  ENDIF(X11_INCLUDE_PATH)
+
+  # Deprecated variable fro backwards compatibility with CMake 1.4
+  SET (X11_LIBRARY ${X11_X11_LIBRARY})
+ENDIF (UNIX)
