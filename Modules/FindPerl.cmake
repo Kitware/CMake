c5feb8c6c54f13894fd573f0c29d7900487a54d9
Ian Scott <ian.m.scott@stud.man.ac.uk>
2002-09-03 08:33:56 -0400

    Added PERL_FOUND

--- Modules/FindPerl.cmake
+++ Modules/FindPerl.cmake
@@ -1,10 +1,12 @@
 # 
 # this module looks for Perl
 #
+# PERL_EXECUTABLE - the full path to the Perl interpreter
+# PERL_FOUND	  - If false, don't attempt to use perl.
 
 INCLUDE(${CMAKE_ROOT}/Modules/FindCygwin.cmake)
 
-FIND_PROGRAM(PERL
+FIND_PROGRAM(PERL_EXECUTABLE
   perl
   "C:/Perl/bin" 
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\ActiveState\\ActivePerl\\628]/bin
@@ -12,5 +14,15 @@ FIND_PROGRAM(PERL
 )
 
 MARK_AS_ADVANCED(
-  PERL
+  PERL_EXECUTABLE
 )
+
+IF (NOT PERL_EXECUTABLE)
+  SET(PERL_FOUND "NO")
+ELSE (NOT PERL_EXECUTABLE)
+  SET(PERL_FOUND "YES")
+
+# Deprecated settings for compatibility with CMake1.4
+  SET (PERL ${PERL_EXECUTABLE})
+ENDIF (NOT PERL_EXECUTABLE)
+
