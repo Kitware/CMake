d631e114f95865dfd5c59f0fda47ef5a654155f9
Brad King <brad.king@kitware.com>
2002-09-17 15:41:21 -0400

    BUG: Generated CMakeLists.txt file needs to take CMAKE_ANSI_CXXFLAGS into account.

--- Source/cmTryCompileCommand.cxx
+++ Source/cmTryCompileCommand.cxx
@@ -72,6 +72,9 @@ bool cmTryCompileCommand::InitialPass(std::vector<std::string> const& argv)
       return false;
       }
     fprintf(fout,"PROJECT(CMAKE_TRY_COMPILE)\n");
+    fprintf(fout, "IF (CMAKE_ANSI_CXXFLAGS)\n");
+    fprintf(fout, "  SET(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} ${CMAKE_ANSI_CXXFLAGS}\")\n");
+    fprintf(fout, "ENDIF (CMAKE_ANSI_CXXFLAGS)\n");
     fprintf(fout,"ADD_EXECUTABLE(cmTryCompileExec \"%s\")\n",argv[2].c_str());
     fclose(fout);
     projectName = "CMAKE_TRY_COMPILE";
