24e051812287e5d40fd336e1354429844cd00dee
Sebastien Barre <sebastien.barre@kitware.com>
2002-09-18 09:53:11 -0400

    FIX: better support for the Tcl/Tk 8.4 pre-release

--- Source/cmVTKWrapTclCommand.cxx
+++ Source/cmVTKWrapTclCommand.cxx
@@ -232,8 +232,8 @@ bool cmVTKWrapTclCommand::WriteInit(const char *kitName,
   fprintf(fout,
           "extern \"C\"\n"
           "{\n"
-          "#if (TCL_MAJOR_VERSION == 8) && (TCL_MINOR_VERSION >= 4)\n"
-          "  typedef int (*vtkTclCommandType)(ClientData, Tcl_Interp *,int, CONST char *[]);\n"
+          "#if (TCL_MAJOR_VERSION == 8) && (TCL_MINOR_VERSION >= 4) && (TCL_RELEASE_LEVEL >= TCL_FINAL_RELEASE)\n"
+          "  typedef int (*vtkTclCommandType)(ClientData, Tcl_Interp *,int, CONST84 char *[]);\n"
           "#else\n"
           "  typedef int (*vtkTclCommandType)(ClientData, Tcl_Interp *,int, char *[]);\n"
           "#endif\n"
