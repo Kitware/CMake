565adb8115aebc274dcf1a04e0132d5cae3a8595
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-09-23 14:04:02 -0400

    Fix HP build

--- Source/cmTryCompileCommand.cxx
+++ Source/cmTryCompileCommand.cxx
@@ -136,6 +136,7 @@ int cmTryCompileCommand::CoreTryCompileCode(
     fprintf(fout, "PROJECT(CMAKE_TRY_COMPILE)\n");
     fprintf(fout, "IF (CMAKE_ANSI_CXXFLAGS)\n");
     fprintf(fout, "  SET(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} ${CMAKE_ANSI_CXXFLAGS}\")\n");
+    fprintf(fout, "  SET(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} ${CMAKE_ANSI_CFLAGS}\")\n");
     fprintf(fout, "ENDIF (CMAKE_ANSI_CXXFLAGS)\n");
     // handle any compile flags we need to pass on
     if (compileFlags.size())
