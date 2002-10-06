8fd2ec6e91da696202d218df7a3c0834d8c47626
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-10-06 18:56:09 -0400

    Check if directory was actually created

--- Source/cmSystemTools.cxx
+++ Source/cmSystemTools.cxx
@@ -186,6 +186,10 @@ bool cmSystemTools::MakeDirectory(const char* path)
       return false;
       }
     }
+  if(!cmSystemTools::FileExists(path))
+    {
+    return false;
+    }
   return true;
 }
 
