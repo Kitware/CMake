da460f9008f5a949f663e749455e15fb5959d803
Bill Hoffman <bill.hoffman@kitware.com>
2002-10-22 18:17:07 -0400

    test for gnu compiler

--- Modules/CMakeDetermineCCompiler.cmake
+++ Modules/CMakeDetermineCCompiler.cmake
@@ -4,8 +4,12 @@
 
 FIND_PROGRAM(CMAKE_C_COMPILER NAMES $ENV{CC} gcc cc cl bcc PATHS /bin /usr/bin /usr/local/bin )
 
+# test to see if the c compiler is gnu
+EXEC_PROGRAM(${CMAKE_C_COMPILER} ARGS -E ${CMAKE_ROOT}/Modules/CMakeTestGNU.c OUTPUT_VARIABLE CMAKE_COMPILER_OUTPUT RETURN_VALUE CMAKE_COMPILER_RETURN)
 
-# to check for is gnu we need to compile this:
-#  #ifdef __GNUC__
-#  yes;
-#  #endif
+IF(NOT CMAKE_COMPILER_RETURN)
+   IF(${CMAKE_COMPILER_OUTPUT} MATCHES ".*THIS_IS_GNU.*" )
+      SET(CMAKE_COMPILER_IS_GNUGCC 1)
+   ENDIF(${CMAKE_COMPILER_OUTPUT} MATCHES ".*THIS_IS_GNU.*" )
+ENDIF(NOT CMAKE_COMPILER_RETURN)
+
---
+++ Modules/CMakeTestGNU.c
@@ -0,0 +1,3 @@
+#ifdef __GNUC__
+THIS_IS_GNU
+#endif
