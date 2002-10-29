2d970c26f02b56e4d98ab148fbed5d2753427672
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-10-29 15:46:38 -0500

    Add test for X11

---
+++ Tests/X11/X11.c
@@ -0,0 +1,38 @@
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+char hello[]="hello.world";
+char hi[]="Hi";
+
+main(argc, argv)
+int argc;
+char **argv;
+{
+  Display *mydisplay;
+  Window  mywindow;
+  GC mygc;
+
+  XEvent myevent;
+  KeySym mykey;
+
+  XSizeHints myhint;
+  int myscreen;
+  unsigned long myforeground, mybackground;
+  int i;
+  char text[10];
+  int done;
+
+  mydisplay=XOpenDisplay("");
+  myscreen=DefaultScreen(mydisplay);
+  myforeground=BlackPixel(mydisplay, myscreen);
+
+  mywindow=XCreateSimpleWindow(mydisplay,
+                               DefaultRootWindow(mydisplay),
+                               myhint.x, myhint.y, myhint.width, myhint.height, 5,
+                               myforeground, mybackground );
+  XDestroyWindow(mydisplay, mywindow);
+  XCloseDisplay(mydisplay);
+  exit(0);
+}
+
+
