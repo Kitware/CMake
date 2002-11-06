bde5ee3f44d58b6a2c2dc1c61be905cccf51b24f
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-11-06 08:52:25 -0500

    Try to fix test

--- Tests/X11/X11.c
+++ Tests/X11/X11.c
@@ -1,32 +1,16 @@
+#include "stdio.h"
 #ifdef CMAKE_HAS_X
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
-#include <stdlib.h>
 
 int main()
 {
-  Display *mydisplay;
-  char* display = ":0";
-
-
-  if ( getenv("DISPLAY") )
-    {
-    display = getenv("DISPLAY");
-    }
-
-  mydisplay=XOpenDisplay(display);
-  if ( mydisplay )
-    {
-    XCloseDisplay(mydisplay);
-    }
+  printf("There is X on this computer\n");
   return 0;
 }
 
-
 #else
-
-#include "stdio.h"
 
 int main()
 {
