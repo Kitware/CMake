ac481e0ee96554945bca25bde70e3d2e43d0c73b
Bill Hoffman <bill.hoffman@kitware.com>
2002-11-09 13:43:16 -0500

    fix for cxxflags

--- Modules/Platform/Windows-cl.cmake
+++ Modules/Platform/Windows-cl.cmake
@@ -42,12 +42,15 @@ SET (CMAKE_BUILD_TYPE Debug CACHE STRING
 
 SET(CMAKE_CREATE_WIN32_EXE /subsystem:windows)
 
-SET (CMAKE_CXX_FLAGS "/nologo /W3 /Zm1000 /GX /GR")
-SET (CMAKE_CXX_FLAGS_DEBUG "/MDd /Zi /Od /GZ")
-SET (CMAKE_CXX_FLAGS_MINSIZEREL "/MD /O1")
-SET (CMAKE_CXX_FLAGS_RELEASE "/MD /O2")
-SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "/MD /Zi /O2")
-SET (CMAKE_C_FLAGS "/nologo /W3 /Zm1000")
+IF(NOT CMAKE_CXX_FLAGS)
+  SET (CMAKE_CXX_FLAGS "/nologo /W3 /Zm1000 /GX /GR")
+  SET (CMAKE_CXX_FLAGS_DEBUG "/MDd /Zi /Od /GZ")
+  SET (CMAKE_CXX_FLAGS_MINSIZEREL "/MD /O1")
+  SET (CMAKE_CXX_FLAGS_RELEASE "/MD /O2")
+  SET (CMAKE_CXX_FLAGS_RELWITHDEBINFO "/MD /Zi /O2")
+  SET (CMAKE_C_FLAGS "/nologo /W3 /Zm1000")
+ENDIF(NOT CMAKE_CXX_FLAGS)
+
 
 SET (CMAKE_STANDARD_LIBRARIES "kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib  kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib" CACHE STRING 
      "Libraries linked by defalut with all applications.")
