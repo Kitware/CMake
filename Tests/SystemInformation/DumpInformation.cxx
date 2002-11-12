8b689f9a622722d5389d56602387fafe3d398464
Bill Hoffman <bill.hoffman@kitware.com>
2002-11-12 14:18:36 -0500

    clean up test for html output

--- Tests/SystemInformation/DumpInformation.cxx
+++ Tests/SystemInformation/DumpInformation.cxx
@@ -6,17 +6,34 @@ int main(int, char*[])
   FILE* file = fopen(CMAKE_DUMP_FILE, "r");
   if(!file)
     {
-    printf("Error, could not open file %s", CMAKE_DUMP_FILE);
+    printf("Error, could not open file %s\n", CMAKE_DUMP_FILE);
     return -1;
     }
+  printf("#Note less than will show up as { and greater than will be }\n");
   while(!feof(file))
     {
     int ch = fgetc(file);
     if(ch != EOF)
       {
-      putc(ch, stdout);
+      if(ch == '<')
+        {
+        printf("&lt;");
+        }
+      else if(ch == '>')
+        {
+        printf("&gt;");
+        }
+      else if(ch == '&')
+        {
+        printf("&amp;");
+        }
+      else 
+        {
+        putc(ch, stdout);
+        }
       }
     }
+  printf("\n");
   fclose(file);
   return 0;
 }
