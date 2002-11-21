9a6c95fcea52bd1b5b4930fdac0a48088e55273b
Bill Hoffman <bill.hoffman@kitware.com>
2002-11-21 12:52:54 -0500

    add checks for ansi flags and not hard code them

--- Modules/CMakeBackwardCompatibilityCXX.cmake
+++ Modules/CMakeBackwardCompatibilityCXX.cmake
@@ -1,4 +1,27 @@
 #
+
+# check for some ANSI flags in the CXX compiler if it is not gnu
+IF(NOT CMAKE_COMPILER_IS_GNUCXX)
+  INCLUDE(${CMAKE_ROOT}/Modules/TestCXXAcceptFlag.cmake)
+  SET(CMAKE_TRY_ANSI_CXX_FLAGS "")
+  IF(CMAKE_SYSTEM MATCHES "IRIX.*")
+    SET(CMAKE_TRY_ANSI_CXX_FLAGS "-LANG:std")
+  ENDIF(CMAKE_SYSTEM MATCHES "IRIX.*")
+  IF(CMAKE_SYSTEM MATCHES "OSF.*")
+    SET(CMAKE_TRY_ANSI_CXX_FLAGS "-std strict_ansi -nopure_cname")
+  ENDIF(CMAKE_SYSTEM MATCHES "OSF.*")
+  # if CMAKE_TRY_ANSI_CXX_FLAGS has something in it, see
+  # if the compiler accepts it
+  IF( CMAKE_TRY_ANSI_CXX_FLAGS MATCHES ".+")
+    CHECK_CXX_ACCEPTS_FLAG(${CMAKE_TRY_ANSI_CXX_FLAGS} CMAKE_CXX_ACCEPTS_FLAGS)
+    # if the compiler liked the flag then set CMAKE_ANSI_CXXFLAGS
+    # to the flag
+    IF(CMAKE_CXX_ACCEPTS_FLAGS)
+      SET(CMAKE_ANSI_CXXFLAGS ${CMAKE_TRY_ANSI_CXX_FLAGS})
+    ENDIF(CMAKE_CXX_ACCEPTS_FLAGS)
+  ENDIF( CMAKE_TRY_ANSI_CXX_FLAGS MATCHES ".+")
+ENDIF(NOT CMAKE_COMPILER_IS_GNUCXX)
+
 INCLUDE(${CMAKE_ROOT}/Modules/TestForANSIStreamHeaders.cmake)
 INCLUDE(${CMAKE_ROOT}/Modules/CheckIncludeFileCXX.cmake)
 INCLUDE(${CMAKE_ROOT}/Modules/TestForSTDNamespace.cmake)
---
+++ Modules/TestCXXAcceptsFlag.cmake
@@ -0,0 +1,20 @@
+#
+# Check if the CXX compiler accepts a flag
+#
+# CHECK_FUNCTION_EXISTS - macro which checks if the function exists
+# FLAG - the flags to try
+# VARIABLE - variable to store the result
+#
+
+MACRO(CHECK_CXX_ACCEPTS_FLAG FLAGS  VARIABLE)
+   MESSAGE(STATUS "Checking to see if CXX compiler acepts flag ${FLAGS}")
+   TRY_COMPILE(${VARIABLE}
+               ${PROJECT_BINARY_DIR}
+               ${CMAKE_ROOT}/Modules/DummyCXXFile.cxx
+               CMAKE_FLAGS -DCOMPILE_DEFINITIONS:STRING=${FLAGS}
+               OUTPUT_VARIABLE OUTPUT) 
+   IF(NOT ${VARIABLE})
+     WRITE_FILE(${PROJECT_BINARY_DIR}/CMakeError.log
+      "Determining if the CXX compiler accepts the flag ${FLAGS} failed with the following output:\n"      "${OUTPUT}\n" APPEND)
+   ENDIF(NOT ${VARIABLE})
+ENDMACRO(CHECK_CXX_ACCEPTS_FLAG)
