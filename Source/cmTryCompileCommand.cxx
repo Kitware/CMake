fc68dd61c32ec47e6dc19c322ea900092a6e30e2
Bill Hoffman <bill.hoffman@kitware.com>
2002-11-21 16:11:44 -0500

    fix for c flags

--- Source/cmTryCompileCommand.cxx
+++ Source/cmTryCompileCommand.cxx
@@ -153,7 +153,7 @@ int cmTryCompileCommand::CoreTryCompileCode(
       return -1;
       }
 
-    fprintf(fout, "SET(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} ${COMPILE_DEFINITIONS}\"\")\n");
+    fprintf(fout, "SET(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} ${COMPILE_DEFINITIONS}\")\n");
     fprintf(fout, "SET(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} ${COMPILE_DEFINITIONS}\")\n");
     fprintf(fout, "INCLUDE_DIRECTORIES(${INCLUDE_DIRECTORIES})\n");
     fprintf(fout, "LINK_DIRECTORIES(${LINK_DIRECTORIES})\n");
