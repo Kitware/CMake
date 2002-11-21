a8a087b099b2e3dfbba9c8a726208c4e88e98be7
Bill Hoffman <bill.hoffman@kitware.com>
2002-11-21 15:15:49 -0500

    move compile defs to cxx and c flags

--- Source/cmTryCompileCommand.cxx
+++ Source/cmTryCompileCommand.cxx
@@ -153,7 +153,8 @@ int cmTryCompileCommand::CoreTryCompileCode(
       return -1;
       }
 
-    fprintf(fout, "ADD_DEFINITIONS(${COMPILE_DEFINITIONS})\n");
+    fprintf(fout, "SET(CMAKE_C_FLAGS ${CMAKE_C_FLAGS} ${COMPILE_DEFINITIONS})\n");
+    fprintf(fout, "SET(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} ${COMPILE_DEFINITIONS})\n");
     fprintf(fout, "INCLUDE_DIRECTORIES(${INCLUDE_DIRECTORIES})\n");
     fprintf(fout, "LINK_DIRECTORIES(${LINK_DIRECTORIES})\n");
     // handle any compile flags we need to pass on
