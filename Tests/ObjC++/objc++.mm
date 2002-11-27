ad01416a1115ef58a30278d14c49b14bd5f69d95
Yves Starreveld <ystarrev@uwo.ca>
2002-11-26 19:01:51 -0500

    Test for ObjC++ on OSX machines only

---
+++ Tests/ObjC++/objc++.mm
@@ -0,0 +1,22 @@
+#import <iostream.h>
+#import <Cocoa/Cocoa.h>
+
+int main()
+{
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+   
+  NSMutableSet *mySet = [NSMutableSet set];
+  cout<<"Adding values to the set."<<endl;
+  [mySet addObject:[NSNumber numberWithInt:356]];
+  [mySet addObject:[NSNumber numberWithInt:550]];
+  [mySet addObject:[NSNumber numberWithInt:914]];
+
+  cout<<"The set contains "<<[mySet count]<<" objects."<<endl;
+  if ([mySet containsObject:[NSNumber numberWithInt:911]])
+    cout<<"It's there!"<<endl;
+  else
+    cout<<"It's not there."<<endl;
+
+  [pool release];
+  return 0;
+}
