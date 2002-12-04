3a211819410ba0433cf8b713e894683b2f612345
Ken Martin <ken.martin@kitware.com>
2002-12-04 10:44:44 -0500

    added CMAKE_BACKWARDS_COMPATIBILITY entry

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -994,7 +994,23 @@ int cmake::LoadCache()
   if(!this->AddCMakePaths(m_CMakeCommand.c_str()))
     {
     return -3;
+    }  
+
+  // set the default BACKWARDS compatibility to the current version
+  if(!m_CacheManager->GetCacheValue("CMAKE_BACKWARDS_COMPATIBILITY"))
+    {
+    char ver[256];
+    sprintf(ver,"%i.%i",cmMakefile::GetMajorVersion(),
+            cmMakefile::GetMinorVersion());
+    this->m_CacheManager->AddCacheEntry
+      ("CMAKE_BACKWARDS_COMPATIBILITY",ver, 
+       "For backwards compatibility, what version of CMake commands and syntax should this version of CMake allow.",
+       cmCacheManager::STRING);
+    cmCacheManager::CacheIterator it = 
+      this->m_CacheManager->GetCacheIterator("CMAKE_BACKWARDS_COMPATIBILITY");
+    it.SetProperty("ADVANCED", "1");
     }
+  
   return 0;
 }
 
