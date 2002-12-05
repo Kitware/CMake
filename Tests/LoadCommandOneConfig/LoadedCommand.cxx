8d20322160e6e8c7bf9c6d69de9319f57373de5d
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-12-05 11:55:53 -0500

    Speedup the test

--- Tests/LoadCommandOneConfig/LoadedCommand.cxx
+++ Tests/LoadCommandOneConfig/LoadedCommand.cxx
@@ -1,7 +1,18 @@
 #include "LoadedCommand.h"
+#include <stdio.h>
 
 int main ()
 {
+#ifdef HAVE_VSBLABLA
+  printf("Should not be able to find vsblabla\n");
+  return 1;
+#endif
+
+#if !defined( HAVE_PRINTF )
+  printf("Should be able to find printf\n");
+  return 1;
+#endif
+
 #ifdef CMAKE_IS_FUN
   return SIZEOF_CHAR-1;
 #else  
