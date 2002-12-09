c720d473b3fef8c96989ab78cd4eafe82c7e4ed9
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-12-09 16:22:44 -0500

    Better search for python

--- Modules/FindPythonLibs.cmake
+++ Modules/FindPythonLibs.cmake
@@ -10,41 +10,58 @@
 
 IF(WIN32)
   FIND_LIBRARY(PYTHON_DEBUG_LIBRARY 
-    NAMES python python21_d python20_d
+    NAMES python23_d python22_d python21_d python20_d python 
     PATHS
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.3\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.3\\InstallPath]/libs
     [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.2\\InstallPath]/libs/Debug
-    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]/libs/Debug
-    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]/libs/Debug
     [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.2\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]/libs/Debug
     [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]/libs/Debug
     [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.6\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.6\\InstallPath]/libs
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.5\\InstallPath]/libs/Debug
+    [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.5\\InstallPath]/libs
   )
 ENDIF(WIN32)
 
 FIND_LIBRARY(PYTHON_LIBRARY 
-  NAMES python python21 python2.1 python20 python2.0 python1.5 python15 python22 python2.2
+  NAMES python23 python2.3 python22 python2.2 python21 python2.1 
+        python20 python2.0 python16 python1.6 python15 python1.5
   PATHS
-  /usr/lib
+  /usr/lib/python2.3/config
   /usr/lib/python2.2/config
   /usr/lib/python2.1/config
   /usr/lib/python2.0/config
+  /usr/lib/python1.6/config
   /usr/lib/python1.5/config
+  /usr/lib
   /usr/local/lib
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.3\\InstallPath]/libs
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.2\\InstallPath]/libs
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]/libs
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]/libs
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.6\\InstallPath]/libs
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.5\\InstallPath]/libs
 )
 
 FIND_PATH(PYTHON_INCLUDE_PATH Python.h
-  /usr/include
+  /usr/include/python2.3
   /usr/include/python2.2
   /usr/include/python2.1
   /usr/include/python2.0
+  /usr/include/python1.6
   /usr/include/python1.5
+  /usr/include
   /usr/local/include
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.3\\InstallPath]/include
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.2\\InstallPath]/include
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.1\\InstallPath]/include
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\2.0\\InstallPath]/include
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.6\\InstallPath]/include
+  [HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\1.5\\InstallPath]/include
 )
 
 IF (WIN32)
