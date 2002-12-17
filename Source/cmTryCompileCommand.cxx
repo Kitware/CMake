a45217f2a2e76c387eaa02c9a05e03bb924c14c5
Brad King <brad.king@kitware.com>
2002-12-17 11:57:52 -0500

    ENH: TryCompile should produce a verbose makefile.

--- Source/cmTryCompileCommand.cxx
+++ Source/cmTryCompileCommand.cxx
@@ -153,6 +153,7 @@ int cmTryCompileCommand::CoreTryCompileCode(
       return -1;
       }
 
+    fprintf(fout, "SET(CMAKE_VERBOSE_MAKEFILE 1)\n");
     fprintf(fout, "SET(CMAKE_C_FLAGS \"${CMAKE_C_FLAGS} ${COMPILE_DEFINITIONS}\")\n");
     fprintf(fout, "SET(CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} ${COMPILE_DEFINITIONS}\")\n");
     fprintf(fout, "INCLUDE_DIRECTORIES(${INCLUDE_DIRECTORIES})\n");
