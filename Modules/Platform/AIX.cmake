bf6730b03a5ba5cc2f17dab2a83807f8c09bce1c
Bill Hoffman <bill.hoffman@kitware.com>
2002-12-30 11:48:33 -0500

    ENH: fix shared libraries on AIX

--- Modules/Platform/AIX.cmake
+++ Modules/Platform/AIX.cmake
@@ -1,7 +1,8 @@
 SET(CMAKE_SHARED_LIBRARY_PREFIX "lib")          # lib
-SET(CMAKE_SHARED_LIBRARY_SUFFIX "..o")          # .so
+SET(CMAKE_SHARED_LIBRARY_SUFFIX ".so")          # .so
 SET(CMAKE_DL_LIBS "-lld")
-
+SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-G")       # -shared
+SET(CMAKE_SHARED_LIBRARY_LINK_FLAGS "-Wl,-brtl")         # +s, flag for exe link to use shared lib
 IF(CMAKE_COMPILER_IS_GNUCXX) 
   SET(CMAKE_SHARED_LIBRARY_SUFFIX ".so")          # .so
   SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-shared -Wl,-G")       # -shared
