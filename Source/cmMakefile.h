e90d2dd9f8e0e4e16352d8497ea7ea46cddb1140
Ken Martin <ken.martin@kitware.com>
2003-01-06 09:36:27 -0500

    updated version

--- Source/cmMakefile.h
+++ Source/cmMakefile.h
@@ -45,8 +45,8 @@ public:
    * Return major and minor version numbers for cmake.
    */
   static unsigned int GetMajorVersion() { return 1;}
-  static unsigned int GetMinorVersion() { return 5;}
-  static const char *GetReleaseVersion() { return "development";}
+  static unsigned int GetMinorVersion() { return 6;}
+  static const char *GetReleaseVersion() { return "beta 1";}
   
   /**
    * Return the major and minor version of the cmake that
