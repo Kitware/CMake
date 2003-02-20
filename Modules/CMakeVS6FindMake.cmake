02be31b8603b9068ad6c45e55d7e21bf2f002677
Bill Hoffman <bill.hoffman@kitware.com>
2003-02-20 10:44:58 -0500

    BUG: look for msdev in the right place

--- Modules/CMakeVS6FindMake.cmake
+++ Modules/CMakeVS6FindMake.cmake
@@ -1,7 +1,7 @@
 FIND_PROGRAM(CMAKE_MAKE_PROGRAM
         NAMES msdev
         PATHS
-        [HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\6.0\\Setup;VsCommonDir]/MSDev98/Bin/msdev
+        [HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\6.0\\Setup;VsCommonDir]/MSDev98/Bin
         "c:/Program Files/Microsoft Visual Studio/Common/MSDev98/Bin"
         "c:/Program Files/Microsoft Visual Studio/Common/MSDev98/Bin"
         "/Program Files/Microsoft Visual Studio/Common/MSDev98/Bin"
