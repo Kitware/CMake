ab761edaef03ede2e017ea2e7b76f4342fe444a1
Brad King <brad.king@kitware.com>
2003-02-24 11:02:23 -0500

    BUG: Fixed crash when CMAKE_BACKWARDS_COMPATIBILITY is deleted between configures.

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -781,6 +781,18 @@ int cmake::Configure()
                                   cmCacheManager::INTERNAL);
     }
   
+  // set the default BACKWARDS compatibility to the current version
+  if(!m_CacheManager->GetCacheValue("CMAKE_BACKWARDS_COMPATIBILITY"))
+    {
+    char ver[256];
+    sprintf(ver,"%i.%i",cmMakefile::GetMajorVersion(),
+            cmMakefile::GetMinorVersion());
+    this->m_CacheManager->AddCacheEntry
+      ("CMAKE_BACKWARDS_COMPATIBILITY",ver, 
+       "For backwards compatibility, what version of CMake commands and syntax should this version of CMake allow.",
+       cmCacheManager::STRING);
+    }  
+  
   // no generator specified on the command line
   if(!m_GlobalGenerator)
     {
