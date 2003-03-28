4e66ef8fe12793128f24850bc3b6efbb0404b6a6
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-03-28 13:42:34 -0500

    New design of CheckSymbolExists pretty much replaces all other ones.
    For example:
    
    CHECK_HEADER_EXISTS("type.h" HAVE_TYPE_H)
    is:
    CHECK_SYMBOL_EXISTS(main "type.h" HAVE_TYPE_H)
    
    CHECK_LIBRARY_EXISTS("nsl"    gethostname  HAVE_LIBNSL)
    would be
    SET(CMAKE_REQUIRED_LIBRARIES "nsl")
    CHECK_SYMBOL_EXISTS(gethostname "netdb.h" HAVE_LIBNSL)
    
    ...

--- Modules/CheckSymbolExists.cmake
+++ Modules/CheckSymbolExists.cmake
@@ -15,27 +15,38 @@ MACRO(CHECK_SYMBOL_EXISTS SYMBOL FILES VARIABLE)
     STRING(ASCII 40 OPEN_PARENT)
     STRING(ASCII 41 CLOSE_PARENT)
     SET(PARENTS "${OPEN_PARENT}${CLOSE_PARENT}")
+    IF(CMAKE_REQUIRED_LIBRARIES)
+      SET(CHECK_SYMBOL_EXISTS_LIBS 
+        "-DLINK_LIBRARIES:STRING=${CMAKE_REQUIRED_LIBRARIES}")
+    ENDIF(CMAKE_REQUIRED_LIBRARIES)
     FOREACH(FILE ${FILES})
       SET(CHECK_SYMBOL_EXISTS_CONTENT
             "${CHECK_SYMBOL_EXISTS_CONTENT}${POUND}include <${FILE}>\n")
     ENDFOREACH(FILE)
     SET(CHECK_SYMBOL_EXISTS_CONTENT
-        "${CHECK_SYMBOL_EXISTS_CONTENT}\n\nint main${PARENTS}{\n${POUND}ifndef ${SYMBOL}\nchar ${OPEN_PARENT}*f${CLOSE_PARENT} ${PARENTS} = ${SYMBOL};\n${POUND}endif\nreturn 0;\n}\n")
+      "${CHECK_SYMBOL_EXISTS_CONTENT}\nvoid cmakeRequireSymbol${OPEN_PARENT}int dummy,...${CLOSE_PARENT}{${OPEN_PARENT}void${CLOSE_PARENT}dummy;}\nint main${PARENTS}\n{\n${POUND}ifndef ${SYMBOL}\n  cmakeRequireSymbol${OPEN_PARENT}0,&${SYMBOL}${CLOSE_PARENT};\n${POUND}endif\n  return 0;\n}\n")
+
     WRITE_FILE(${CMAKE_BINARY_DIR}/CMakeTmp/CheckSymbolExists.c 
                "${CHECK_SYMBOL_EXISTS_CONTENT}")
     
-    MESSAGE(STATUS "Looking for ${SYMBOL} in ${FILES}")
+    MESSAGE(STATUS "Looking for ${SYMBOL}")
     TRY_COMPILE(${VARIABLE}
                ${CMAKE_BINARY_DIR}
                ${CMAKE_BINARY_DIR}/CMakeTmp/CheckSymbolExists.c
                CMAKE_FLAGS 
                 -DCOMPILE_DEFINITIONS:STRING=${MACRO_CHECK_SYMBOL_EXISTS_FLAGS}
+               "${CHECK_SYMBOL_EXISTS_LIBS}"
                OUTPUT_VARIABLE OUTPUT)
     IF(${VARIABLE})
-      MESSAGE(STATUS "Looking for ${SYMBOL} in ${FILES} - found")
+      MESSAGE(STATUS "Looking for ${SYMBOL} - found")
       SET(${VARIABLE} 1 CACHE INTERNAL "Have symbol ${SYMBOL}")
+      WRITE_FILE(${CMAKE_BINARY_DIR}/CMakeOutput.log 
+        "Determining if the ${SYMBOL} "
+        "exist passed with the following output:\n"
+        "${OUTPUT}\nFile ${CMAKE_BINARY_DIR}/CMakeTmp/CheckSymbolExists.c:\n"
+        "${CHECK_SYMBOL_EXISTS_CONTENT}" APPEND)
     ELSE(${VARIABLE})
-      MESSAGE(STATUS "Looking for ${SYMBOL} in ${FILES} - not found.")
+      MESSAGE(STATUS "Looking for ${SYMBOL} - not found.")
       SET(${VARIABLE} "" CACHE INTERNAL "Have symbol ${SYMBOL}")
       WRITE_FILE(${CMAKE_BINARY_DIR}/CMakeError.log 
         "Determining if the ${SYMBOL} "
