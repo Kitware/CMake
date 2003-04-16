65422964f793ad1004bf07cbc3021e80e7c241cd
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-04-16 17:06:55 -0400

    ENH: cleanup

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -21,12 +21,11 @@ SET (PROJECT_URL "http://www.cmake.org/")
 
 # Dart server configuration 
 SET (ROLLUP_URL "http://${DROP_SITE}/cgi-bin/cmake-rollup-dashboard.sh")
-
 SET (CVS_WEB_URL "http://${DROP_SITE}/cgi-bin/cvsweb.cgi/CMake/")
 SET (CVS_WEB_CVSROOT "CMake")
-
 SET (USE_DOXYGEN "On")
 SET (DOXYGEN_URL "http://www.cmake.org/doc/nightly/html/" )
+
 OPTION(BUILD_DOXYGEN "Build source documentation using doxygen" "Off")
 SET (DOXYGEN_CONFIG "${PROJECT_BINARY_DIR}/doxygen.config" )
 MARK_AS_ADVANCED(BUILD_DOXYGEN)
