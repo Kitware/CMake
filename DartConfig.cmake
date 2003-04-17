6bb35a4d8802d99f708ee894ac0d5babf0043d84
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-04-17 11:17:44 -0400

    More attempt to make continuous email work

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -17,7 +17,7 @@ SET (TRIGGER_SITE
        "http://${DROP_SITE}/cgi-bin/Submit-CMake-TestingResults.pl")
 
 # Project Home Page
-SET (PROJECT_URL "http://www.cmake.org/")
+SET (PROJECT_URL "http://www.cmake.org")
 
 # Dart server configuration 
 SET (ROLLUP_URL "http://${DROP_SITE}/cgi-bin/cmake-rollup-dashboard.sh")
@@ -33,7 +33,7 @@ MARK_AS_ADVANCED(BUILD_DOXYGEN)
 # Continuous email delivery variables
 SET (CONTINUOUS_FROM "cmake-dashboard@public.kitware.com")
 SET (SMTP_MAILHOST "public.kitware.com")
-SET (CONTINUOUS_MONITOR_LIST "cmake-dashboard@public.kitware.com")
+SET (CONTINUOUS_MONITOR_LIST "cmake-dashboard@public.kitware.com andy.cedilnik@kitware.com")
 SET (CONTINUOUS_BASE_URL "${PROJECT_URL}/Testing")
 
 SET (USE_GNATS "Off")
