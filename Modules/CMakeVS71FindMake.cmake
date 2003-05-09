395fae76420230bda51d74fbd45b1eb0f797f854
Bill Hoffman <bill.hoffman@kitware.com>
2003-05-09 15:47:21 -0400

    add find make program for 71

---
+++ Modules/CMakeVS71FindMake.cmake
@@ -0,0 +1,9 @@
+FIND_PROGRAM(CMAKE_MAKE_PROGRAM
+        NAMES devenv
+        PATHS
+        [HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\7.1\\Setup\\VS;EnvironmentDirectory]
+        "c:/Program Files/Microsoft Visual Studio .NET/Common7/IDE"
+        "c:/Program Files/Microsoft Visual Studio.NET/Common7/IDE"
+        "/Program Files/Microsoft Visual Studio .NET/Common7/IDE/"
+)
+MARK_AS_ADVANCED(CMAKE_MAKE_PROGRAM)
