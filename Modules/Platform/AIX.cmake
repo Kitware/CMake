9c3a6eb410fac4bb34b20d7fe7051cc3980188af
Brad King <brad.king@kitware.com>
2003-05-16 15:21:03 -0400

    BUG: Need -brtl when creating shared libraries.  Also added -bexpall (AIX equivalent to Linux's -rdynamic) when building executables.

--- Modules/Platform/AIX.cmake
+++ Modules/Platform/AIX.cmake
@@ -1,8 +1,8 @@
 SET(CMAKE_SHARED_LIBRARY_PREFIX "lib")          # lib
 SET(CMAKE_SHARED_LIBRARY_SUFFIX ".so")          # .so
 SET(CMAKE_DL_LIBS "-lld")
-SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-G")       # -shared
-SET(CMAKE_SHARED_LIBRARY_LINK_FLAGS "-Wl,-brtl")         # +s, flag for exe link to use shared lib
+SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-G -Wl,-brtl")  # -shared
+SET(CMAKE_SHARED_LIBRARY_LINK_FLAGS "-Wl,-brtl,-bexpall")  # +s, flag for exe link to use shared lib
 IF(CMAKE_COMPILER_IS_GNUCXX) 
   SET(CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS "-shared -Wl,-G")       # -shared
 ENDIF(CMAKE_COMPILER_IS_GNUCXX) 
