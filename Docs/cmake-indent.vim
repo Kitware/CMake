06cd152da20de0e7b08332ecefb4d02712b93c43
Brad King <brad.king@kitware.com>
2003-06-19 15:05:07 -0400

    Added copyright.

--- Docs/cmake-indent.vim
+++ Docs/cmake-indent.vim
@@ -1,3 +1,20 @@
+" =============================================================================
+" 
+"   Program:   CMake - Cross-Platform Makefile Generator
+"   Module:    $RCSfile$
+"   Language:  C++
+"   Date:      $Date$
+"   Version:   $Revision$
+" 
+"   Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
+"   See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+" 
+"      This software is distributed WITHOUT ANY WARRANTY"  without even 
+"      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+"      PURPOSE.  See the above copyright notices for more information.
+" 
+" =============================================================================
+
 " Vim indent file
 " Language: CMake (ft=cmake)
 " Author:	Andy Cedilnik <andy.cedilnik@kitware.com>
--- Docs/cmake-mode.el
+++ Docs/cmake-mode.el
@@ -1,5 +1,20 @@
+;=============================================================================
+;
+;  Program:   CMake - Cross-Platform Makefile Generator
+;  Module:    $RCSfile$
+;  Language:  C++
+;  Date:      $Date$
+;  Version:   $Revision$
+;
+;  Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
+;  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+;
+;     This software is distributed WITHOUT ANY WARRANTY; without even 
+;     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+;     PURPOSE.  See the above copyright notices for more information.
+;
+;=============================================================================
 ; cmake-mode.el = Emacs major mode for editing CMake listfiles.
-; Bill Hoffman and Brad King
 
 ; Add cmake listfile names to the mode list.
 (setq auto-mode-alist
