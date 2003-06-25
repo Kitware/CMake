a778cd0f3df4a0fad4d352356793d0beb21c4ece
Brad King <brad.king@kitware.com>
2003-06-25 09:32:38 -0400

    ERR: Fixed use of non-constant initializer.

--- Source/kwsys/testProcess.c
+++ Source/kwsys/testProcess.c
@@ -166,7 +166,11 @@ int main(int argc, const char* argv[])
     int exceptions[4] = {kwsysProcess_Exception_None, kwsysProcess_Exception_None,
                          kwsysProcess_Exception_None, kwsysProcess_Exception_Fault};
     int values[4] = {0, 123, 1, 1};
-    const char* cmd[] = {argv[0], "run", argv[1], 0};
+    const char* cmd[4];
+    cmd[0] = argv[0];
+    cmd[1] = "run";
+    cmd[2] = argv[1];
+    cmd[3] = 0;
     return runChild(cmd, states[n-1], exceptions[n-1], values[n-1]);
     }
   else
