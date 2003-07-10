b75c229444d8072fd63df2d723fb9cbec81f53f4
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-07-10 14:29:30 -0400

    ENH: Add test for string and file commands

---
+++ Tests/StringFileTest/StringFile.cxx
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "OutputFile.h"
+
+int main(int, char*[])
+{
+  int res = 0;
+
+  res += CheckMethod(rmvar, "CMake");
+  res += CheckMethod(rmallvar, "CMake;cmake;CMake");
+  res += CheckMethod(rrepvar, "People should use CMake and CMake");
+  res += CheckMethod(nceqvar, "0");
+  res += CheckMethod(ceqvar, "1");
+  res += CheckMethod(cneqvar, "1");
+  res += CheckMethod(ncneqvar, "0");
+  res += CheckMethod(nclvar, "0");
+  res += CheckMethod(clvar, "1");
+  res += CheckMethod(cgvar, "1");
+  res += CheckMethod(ncgvar, "0");
+  res += CheckMethod(savar, "Cmake");
+  res += CheckMethod(tuvar, "CMAKE");
+  res += CheckMethod(tlvar, "cmake");
+  
+  return res;
+}
