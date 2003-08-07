7e54a53a3da92ea2d16e2396bf7cfc801c3d076d
Bill Hoffman <bill.hoffman@kitware.com>
2003-08-07 16:07:23 -0400

    ENH: improve coverage

--- Source/cmIncludeExternalMSProjectCommand.cxx
+++ Source/cmIncludeExternalMSProjectCommand.cxx
@@ -24,30 +24,33 @@ bool cmIncludeExternalMSProjectCommand::InitialPass(std::vector<std::string> con
     this->SetError("INCLUDE_EXTERNAL_MSPROJECT called with incorrect number of arguments");
     return false;
   }
-
-  
-  if(m_Makefile->GetDefinition("WIN32")) {
-    
+// only compile this for win32 to avoid coverage errors
+#ifdef _WIN32
+  if(m_Makefile->GetDefinition("WIN32"))
+    {
     std::string location = args[1];
-
+    
     std::vector<std::string> name_and_location;
     name_and_location.push_back(args[0]);
     name_and_location.push_back(location);
     
     std::vector<std::string> depends;
-    if (args.size() > 2) {
-      for (unsigned int i=2; i<args.size(); ++i) {
+    if (args.size() > 2)
+      {
+      for (unsigned int i=2; i<args.size(); ++i) 
+        {
         depends.push_back(args[i]); 
+        }
       }
-    }
-
+    
     std::string utility_name("INCLUDE_EXTERNAL_MSPROJECT");
     utility_name += "_";
     utility_name += args[0];
-
+    
     m_Makefile->AddUtilityCommand(utility_name.c_str(), "echo", "\"Include external project\"",
                                   false, name_and_location, depends);
-
-  }
+    
+    }
+#endif
   return true;
 }
