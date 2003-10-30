e92779a657ebcfd19d71b956b5484cf1dfd1f687
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-10-30 10:05:08 -0500

    BUG: Fix Bug #310 - CTest sends wrong time to cvs on Windows

--- Source/cmCTest.cxx
+++ Source/cmCTest.cxx
@@ -536,11 +536,15 @@ int cmCTest::UpdateDirectory()
     {
     struct tm* t = ::GetNightlyTime(m_DartConfiguration["NightlyStartTime"]);
     char current_time[1024];
-    strftime(current_time, 1000, "%Y-%m-%d %H:%M:%S %Z", t);
+    sprintf(current_time, "%04d-%02d-%02d %02d:%02d:%02d UTC",
+      t->tm_year + 1900,
+      t->tm_mon + 1,
+      t->tm_mday,
+      t->tm_hour,
+      t->tm_min,
+      t->tm_sec);
     std::string today_update_date = current_time;
    
-    //std::string today_update_date = current_time + 
-    //  m_DartConfiguration["NightlyStartTime"];
     extra_update_opts += "-D \"" + today_update_date +"\"";
     //std::cout << "Update: " << extra_update_opts << std::endl;
     }
