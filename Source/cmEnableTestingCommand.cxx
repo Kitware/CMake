d18c9559b3a3e1b4652516dc6efa03fb63c046f6
Ken Martin <ken.martin@kitware.com>
2003-12-29 16:27:06 -0500

    sets variable now

--- Source/cmEnableTestingCommand.cxx
+++ Source/cmEnableTestingCommand.cxx
@@ -18,6 +18,12 @@
 
 // we do this in the final pass so that we now the subdirs have all 
 // been defined
+bool cmEnableTestingCommand::InitialPass(std::vector<std::string> const&)
+{
+  m_Makefile->AddDefinition("CMAKE_TESTING_ENABLED","1");
+  return true;
+}
+
 void cmEnableTestingCommand::FinalPass()
 {
   // Create a full path filename for output Testfile
