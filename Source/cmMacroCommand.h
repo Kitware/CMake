bfe234da939d57be7b6d54835a57f0f6fdf413d3
Andy Cedilnik <andy.cedilnik@kitware.com>
2004-02-19 09:35:30 -0500

    ENH: Macro should be scriptable

--- Source/cmMacroCommand.h
+++ Source/cmMacroCommand.h
@@ -68,6 +68,11 @@ public:
   virtual bool IsInherited() {return true;}
 
   /**
+   * This determines if the command is invoked when in script mode.
+   */
+  virtual bool IsScriptable() { return true; }
+
+  /**
    * The name of the command as specified in CMakeList.txt.
    */
   virtual const char* GetName() { return "MACRO";}
