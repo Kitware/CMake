bf699505bc7fc378abf07ec4dfa0f0cc4fb89686
Bill Hoffman <bill.hoffman@kitware.com>
2004-03-09 16:20:41 -0500

    ENH: create new test to test subdir exclude

---
+++ Tests/SubDir/Executable/test.cxx
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include  <io.h>
+#include  <stdio.h>
+#include  <stdlib.h>
+
+
+// return true if the file exists
+int FileExists(const char* filename)
+{
+#ifdef _MSC_VER
+# define access _access
+#endif
+#ifndef F_OK
+#define F_OK 0
+#endif
+  if ( access(filename, F_OK) != 0 )
+    {
+    return false;
+    }
+  else
+    {
+    return true;
+    }
+}
+
+
+int main(int ac, char** av)
+{
+  if(!FileExists(av[1]))
+    {
+    printf("Missing file %s\n", av[1]);
+    return 1;
+    }
+  printf("%s is there!", av[1]);
+  return 0;
+}
