e76b103df7f04de330dec0384743027ded7e82d1
Andy Cedilnik <andy.cedilnik@kitware.com>
2004-04-27 11:08:58 -0400

    ERR: Remove warnings from tests

--- Tests/CustomCommand/wrapper.cxx
+++ Tests/CustomCommand/wrapper.cxx
@@ -2,6 +2,11 @@
 
 int main(int argc, char *argv[])
 {
+  if ( argc < 2 )
+    {
+    fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+    return 1;
+    }
   FILE *fp = fopen(argv[1],"w");
   
   fprintf(fp,"int wrapped() { return 5; }\n");
