26e8d63b809dad775adff0942dfa09b856b93d10
Will Schroeder <will.schroeder@kitware.com>
2004-05-10 16:55:05 -0400

    ENH: Added regex.

--- Source/cmCTest.cxx
+++ Source/cmCTest.cxx
@@ -147,6 +147,7 @@ static const char* cmCTestErrorMatches[] = {
   "ld: 0706-006 Cannot find or open library file: -l ",
   "ild: \\(argument error\\) can't find library argument ::",
   "^could not be found and will not be loaded.",
+  "make: Fatal error: ",
   0
 };
 
