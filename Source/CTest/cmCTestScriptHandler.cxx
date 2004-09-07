3eae8f44059646241482133d98adb32631ab9c14
Ken Martin <ken.martin@kitware.com>
2004-09-07 11:28:49 -0400

    more warnings

--- Source/CTest/cmCTestScriptHandler.cxx
+++ Source/CTest/cmCTestScriptHandler.cxx
@@ -175,30 +175,13 @@ int cmCTestScriptHandler::ExtractVariables()
   // get some info that should be set
   m_Makefile = m_LocalGenerator->GetMakefile();
 
-  m_SourceDir.clear();
-  m_BinaryDir.clear();
-  m_CTestCmd.clear();
-  
-  if (m_Makefile->GetDefinition("CTEST_SOURCE_DIRECTORY"))
-    {
-    m_SourceDir = m_Makefile->GetDefinition("CTEST_SOURCE_DIRECTORY");
-    }
-  if (m_Makefile->GetDefinition("CTEST_BINARY_DIRECTORY"))
-    {
-    m_BinaryDir = m_Makefile->GetDefinition("CTEST_BINARY_DIRECTORY");
-    }
-  if (m_Makefile->GetDefinition("CTEST_COMMAND"))
-    {
-    m_CTestCmd  = m_Makefile->GetDefinition("CTEST_COMMAND");
-    }
+  m_SourceDir = m_Makefile->GetSafeDefinition("CTEST_SOURCE_DIRECTORY");
+  m_BinaryDir = m_Makefile->GetSafeDefinition("CTEST_BINARY_DIRECTORY");
+  m_CTestCmd  = m_Makefile->GetSafeDefinition("CTEST_COMMAND");
   m_Backup    = m_Makefile->IsOn("CTEST_BACKUP_AND_RESTORE");
 
   // in order to backup and restore we also must have the cvs root
-  m_CVSCheckOut.clear();
-  if (m_Makefile->GetDefinition("CTEST_CVS_CHECKOUT"))
-    {
-    m_CVSCheckOut = m_Makefile->GetDefinition("CTEST_CVS_CHECKOUT");
-    }
+  m_CVSCheckOut = m_Makefile->GetSafeDefinition("CTEST_CVS_CHECKOUT");
   if (m_Backup && m_CVSCheckOut.empty())
     {
     cmSystemTools::Error(
@@ -241,11 +224,7 @@ int cmCTestScriptHandler::ExtractVariables()
       atof(m_Makefile->GetDefinition("CTEST_CONTINUOUS_MINIMUM_INTERVAL"));
     }
   
-  m_CVSCmd.clear();
-  if (m_Makefile->GetDefinition("CTEST_CVS_COMMAND"))
-    {
-    m_CVSCmd = m_Makefile->GetDefinition("CTEST_CVS_COMMAND");
-    }
+  m_CVSCmd = m_Makefile->GetSafeDefinition("CTEST_CVS_COMMAND");
   
   return 0;
 }
