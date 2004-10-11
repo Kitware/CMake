e78a57563cf3900c0a42ee580cb3787533a7f67c
Bill Hoffman <bill.hoffman@kitware.com>
2004-10-11 11:55:39 -0400

    ENH: make sure the c++ compiler is a c++ compiler

--- Modules/CMakeTestCXXCompiler.cmake
+++ Modules/CMakeTestCXXCompiler.cmake
@@ -5,7 +5,11 @@
 # any makefiles or projects.
 IF(NOT CMAKE_CXX_COMPILER_WORKS)
   MESSAGE(STATUS "Check for working CXX compiler: ${CMAKE_CXX_COMPILER}")
-  FILE(WRITE ${CMAKE_BINARY_DIR}/CMakeTmp/testCXXCompiler.cxx "int main(){return 0;}\n")
+  FILE(WRITE ${CMAKE_BINARY_DIR}/CMakeTmp/testCXXCompiler.cxx 
+    "#ifndef __cplusplus\n"
+    "# error \"The CMAKE_CXX_COMPILER is set to a C compiler\"\n"
+    "#endif\n"
+    "int main(){return 0;}\n")
   TRY_COMPILE(CMAKE_CXX_COMPILER_WORKS ${CMAKE_BINARY_DIR} 
     ${CMAKE_BINARY_DIR}/CMakeTmp/testCXXCompiler.cxx
     OUTPUT_VARIABLE OUTPUT)
