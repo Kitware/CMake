3dc02020d962807bff2d8e3a44559eadf5e19f3c
Bill Hoffman <bill.hoffman@kitware.com>
2004-10-27 16:00:26 -0400

    DOC: documentation change

--- Source/cmGlobalKdevelopGenerator.cxx
+++ Source/cmGlobalKdevelopGenerator.cxx
@@ -42,11 +42,11 @@ void cmGlobalKdevelopGenerator::GetDocumentation(cmDocumentationEntry& entry) co
   entry.name = this->GetName();
   entry.brief = "Generates KDevelop 3 project files.";
   entry.full =
-    "A hierarchy of UNIX makefiles is generated into the build tree.  Any "
+    "Project files for KDevelop 3 will be created in the top directory and in every subdirectory "
+    "which features a CMakeLists.txt file containing a PROJECT() call. "
+    "If you change the settings using KDevelop cmake will try its best "
+    "to keep your changes when regenerating the project files. "
+    "Additionally a hierarchy of UNIX makefiles is generated into the build tree.  Any "
     "standard UNIX-style make program can build the project through the "
-     "default make target.  A \"make install\" target is also provided."
-     "Additionally in each directory which features executable or library"
-     " targets a KDevelop 3 project file is generated.\n"
-     "If you change the settings using KDevelop your cmake will try its best"
-     "to keep your changes when regenerating the project files.";
+    "default make target.  A \"make install\" target is also provided.";
 }
