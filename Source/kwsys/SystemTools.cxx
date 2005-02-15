fc8e5aeebf8be9e9ee106acd8dceef54e339f684
Brad King <brad.king@kitware.com>
2005-02-15 08:28:19 -0500

    BUG: Adding debugging code to remotely debug a failing dashboard test.

--- Source/kwsys/SystemTools.cxx
+++ Source/kwsys/SystemTools.cxx
@@ -2268,6 +2268,10 @@ void SystemTools::ClassInitialize()
       Realpath(pwd, pwd_path);
       if(cwd == pwd_path && strcmp(cwd, pwd) != 0)
         {
+#if defined(__HP_aCC)
+        fprintf(stderr, "cwd=[%s]\npwd=[%s]\npwd_path=[%s]\n",
+                cwd, pwd, pwd_path.c_str());
+#endif
         // The current working directory is a logical path.  Split
         // both the logical and physical paths into their components.
         kwsys_stl::vector<kwsys_stl::string> cwd_components;
