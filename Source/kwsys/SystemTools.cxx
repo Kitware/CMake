23dfc9440a0a82ec00c48cbd2177b43f2cea76f6
Brad King <brad.king@kitware.com>
2005-02-15 08:40:34 -0500

    BUG: Adding debugging code to remotely debug a failing dashboard test.

--- Source/kwsys/SystemTools.cxx
+++ Source/kwsys/SystemTools.cxx
@@ -2296,6 +2296,10 @@ void SystemTools::ClassInitialize()
         // Add the translation to keep the logical path name.
         if(!cwd_changed.empty() && !pwd_changed.empty())
           {
+#if defined(__HP_aCC)
+          fprintf(stderr, "adding [%s]->[%s]\n", cwd_changed.c_str(),
+                  pwd_changed.c_str());
+#endif
           SystemTools::AddTranslationPath(cwd_changed.c_str(),
                                           pwd_changed.c_str());
           }
