332cd81fdd572f4c9576adb54172a575c21aa69c
Bill Hoffman <bill.hoffman@kitware.com>
2005-03-11 13:12:02 -0500

    ENH: try to see if there is still a problem

--- Tests/Complex/Executable/complex.cxx
+++ Tests/Complex/Executable/complex.cxx
@@ -1011,7 +1011,7 @@ int main()
     }
   else
     {
-    cmPassed("CMake cmOrderLinkDirectories failed to fail when given an impossible set of paths.");
+    cmFailed("CMake cmOrderLinkDirectories failed to fail when given an impossible set of paths.");
     }
   // next run with shouldPass = true, this will 
   // run with B/libA.a removed and should create the order
