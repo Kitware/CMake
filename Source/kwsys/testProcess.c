b1e1d9c0fb732833f71b466b3105b7bb877a9010
Brad King <brad.king@kitware.com>
2005-04-22 09:44:59 -0400

    BUG: Drastically extending test5's timeouts to get it to pass when running on a heavily-loaded machine.

--- Source/kwsys/testProcess.c
+++ Source/kwsys/testProcess.c
@@ -97,7 +97,7 @@ int test5(int argc, const char* argv[])
   fflush(stdout);
   fflush(stderr);
   r = runChild(cmd, kwsysProcess_State_Exception,
-               kwsysProcess_Exception_Fault, 1, 1, 1, 0, 3);
+               kwsysProcess_Exception_Fault, 1, 1, 1, 0, 15);
   fprintf(stdout, "Output on stdout after recursive test.\n");
   fprintf(stderr, "Output on stderr after recursive test.\n");
   fflush(stdout);
@@ -293,7 +293,7 @@ int main(int argc, const char* argv[])
     int values[6] = {0, 123, 1, 1, 0, 0};
     int outputs[6] = {1, 1, 1, 1, 1, 0};
     int delays[6] = {0, 0, 0, 0, 0, 1};
-    double timeouts[6] = {3, 3, 3, 3, 10, 0.1};
+    double timeouts[6] = {3, 3, 3, 3, 30, 0.1};
     int r;
     const char* cmd[4];
     cmd[0] = argv[0];
