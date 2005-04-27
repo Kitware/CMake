cc2cd3bc41358dd585bed1f16411728398829670
Brad King <brad.king@kitware.com>
2005-04-27 10:01:43 -0400

    BUG: Extending timeout of test 6 from 0.1 seconds to 3 seconds.  This should avoid missed signals and intermittent failures.

--- Source/kwsys/testProcess.c
+++ Source/kwsys/testProcess.c
@@ -293,7 +293,7 @@ int main(int argc, const char* argv[])
     int values[6] = {0, 123, 1, 1, 0, 0};
     int outputs[6] = {1, 1, 1, 1, 1, 0};
     int delays[6] = {0, 0, 0, 0, 0, 1};
-    double timeouts[6] = {3, 3, 3, 3, 30, 0.1};
+    double timeouts[6] = {3, 3, 3, 3, 30, 3};
     int r;
     const char* cmd[4];
     cmd[0] = argv[0];
