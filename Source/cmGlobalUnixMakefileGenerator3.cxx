35fcd2715e26eebb99742d5f5d7d5163b08f288d
Ken Martin <ken.martin@kitware.com>
2005-06-01 08:59:18 -0400

    COMP: fix warning

--- Source/cmGlobalUnixMakefileGenerator3.cxx
+++ Source/cmGlobalUnixMakefileGenerator3.cxx
@@ -694,7 +694,6 @@ cmGlobalUnixMakefileGenerator3
 
   std::vector<std::string> depends;  
   std::vector<std::string> commands;
-  std::string localName;
 
   // write the directory level rules for this local gen
   //this->WriteDirectoryRules(ruleFileStream,lg);
