894fa6a8c7404ce324dd8b9ff3de0cab48f20db2
Brad King <brad.king@kitware.com>
2005-06-08 09:59:59 -0400

    ENH: Experimenting with auto-dated copyright.

--- Docs/cmake-mode.el
+++ Docs/cmake-mode.el
@@ -2,11 +2,8 @@
 ;
 ;  Program:   CMake - Cross-Platform Makefile Generator
 ;  Module:    $RCSfile$
-;  Language:  C++
-;  Date:      $Date$
-;  Version:   $Revision$
 ;
-;  Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
+;  Copyright (c) 2000-$Date$ Kitware, Inc., Insight Consortium.  All rights reserved.
 ;  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
 ;
 ;     This software is distributed WITHOUT ANY WARRANTY; without even
