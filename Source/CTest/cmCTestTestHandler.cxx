1823e78d5cd68948bdfec5c18718f47126aabe07
Andy Cedilnik <andy.cedilnik@kitware.com>
2005-06-22 10:54:19 -0400

    BUG: Fix displaying of percentage

--- Source/CTest/cmCTestTestHandler.cxx
+++ Source/CTest/cmCTestTestHandler.cxx
@@ -425,7 +425,7 @@ int cmCTestTestHandler::ProcessHandler()
       {
       percent = 99;
       }
-    cmCTestLog(m_CTest, HANDLER_OUTPUT, std::endl << std::setprecision(0) << percent << "% tests passed, "
+    cmCTestLog(m_CTest, HANDLER_OUTPUT, std::endl << static_cast<int>(percent + .5) << "% tests passed, "
       << failed.size() << " tests failed out of " << total << std::endl);
     //fprintf(stderr,"\n%.0f%% tests passed, %i tests failed out of %i\n",
     //  percent, int(failed.size()), total);
