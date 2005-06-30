6a1de7f5ad2f684fdf8bfdea2a3ad982387e9a27
Jorgen Bodde <jorgb@xs4all.nl>
2005-06-30 15:54:59 -0400

    initial checkin for bin dir

---
+++ Source/WXDialog/bin/FindUPX.cmake
@@ -0,0 +1,21 @@
+#
+# Find the UPX packer program
+#
+# UPX_PROGRAM_PATH      The full path to the UPX command (or UPX_PROGRAM_PATH-NOTFOUND when not found)
+# UPX_FOUND             Is set to 1 when upx is found
+
+FIND_PATH(UPX_PROGRAM_PATH upx.exe
+    "c:\Program Files\upx"
+    "d:\Program Files\upx"
+    "e:\Program Files\upx"
+    "f:\Program Files\upx"
+    "g:\Program Files\upx"
+    "h:\Program Files\upx"
+  )
+  
+# when found, note this as target
+IF(UPX_PROGRAM_PATH)
+  SET(UPX_FOUND 1)
+ELSE(UPX_PROGRAM_PATH)
+  SET(UPX_FOUND 0)
+ENDIF(UPX_PROGRAM_PATH)
