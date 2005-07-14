81211fc427b04d9671cd3780d4ab10b4060e3cc1
Andy Cedilnik <andy.cedilnik@kitware.com>
2005-07-14 09:29:33 -0400

    BUG: Rename tmp files

--- Source/CTest/cmCTestCoverageHandler.cxx
+++ Source/CTest/cmCTestCoverageHandler.cxx
@@ -70,7 +70,7 @@ void cmCTestCoverageHandler::EndCoverageLogFile(cmGeneratedFileStream& ostr, int
   char covLogFilename[1024];
   sprintf(covLogFilename, "CoverageLog-%d.xml", logFileCount);
   cmCTestLog(m_CTest, HANDLER_VERBOSE_OUTPUT, "Close file: " << covLogFilename << std::endl);
-  ostr.close();
+  ostr.Close();
 }
 
 //----------------------------------------------------------------------
