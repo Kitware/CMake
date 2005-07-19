0ba0cb8a1ac6902ae41f4814d041fcec658c1af2
Bill Hoffman <bill.hoffman@kitware.com>
2005-07-19 15:28:41 -0400

    ENH: add new stub generator

---
+++ Source/cmGlobalXCode21Generator.cxx
@@ -0,0 +1,21 @@
+/*=========================================================================
+
+  Program:   CMake - Cross-Platform Makefile Generator
+  Module:    $RCSfile$
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+cmGlobalXCode21Generator::cmGlobalXCode21Generator()
+{
+  this->m_XcodeVersion = 21;
+}
+
---
+++ Source/cmGlobalXCode21Generator.h
@@ -0,0 +1,34 @@
+/*=========================================================================
+
+  Program:   CMake - Cross-Platform Makefile Generator
+  Module:    $RCSfile$
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef cmGlobalXCode21Generator_h
+#define cmGlobalXCode21Generator_h
+
+#include "cmGlobalXCodeGenerator.h"
+
+/** \class cmGlobalXCode21Generator
+ * \brief Write Mac XCode projects
+ *
+ * cmGlobalXCode21Generator manages UNIX build process for a tree
+ */
+class cmGlobalXCode21Generator : public cmGlobalXCodeGenerator
+{
+public:
+  cmGlobalXCode21Generator();
+  static cmGlobalGenerator* New() { return new cmGlobalXCode21Generator; }
+};
+
+#endif
