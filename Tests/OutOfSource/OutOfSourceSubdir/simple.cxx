fd26d148d8b71871bc5abe2f4e48b56d142d6be0
Ken Martin <ken.martin@kitware.com>
2005-09-13 10:40:38 -0400

    ENH: test get directory properties ability to get props from subdirs

--- Tests/OutOfSource/OutOfSourceSubdir/simple.cxx
+++ Tests/OutOfSource/OutOfSourceSubdir/simple.cxx
@@ -1,7 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "testlib.h"
+#include "testdp.h"
 
 int main ()
-{
+{  
+  if (strcmp(animal,"SIZZLING"))
+    {
+    fprintf(stderr,"Get definitions from a subdir did not work\n");
+    return -2;
+    }
   if(TestLib() != 1.0)
     {
     return -1;
