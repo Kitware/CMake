933c97546b41f577c5cd6711d62f91b262542cc4
Andy Cedilnik <andy.cedilnik@kitware.com>
2005-11-09 05:33:01 -0500

    STYLE: Nightly Version update

--- Source/cmVersion.cxx
+++ Source/cmVersion.cxx
@@ -19,7 +19,7 @@
 std::string cmVersion::GetReleaseVersion()
 {
 #if CMake_VERSION_MINOR & 1
-  std::string cver = "Date: 2005-11-08 10:33:37 +0000";
+  std::string cver = "Date: 2005-11-09 10:33:01 +0000";
   std::string res = "";
   std::string::size_type cc, len = cver.size();
   bool aftercol = false;
