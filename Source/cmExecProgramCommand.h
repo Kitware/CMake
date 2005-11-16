f6a6522801a11eab42dd7e1c6ce961be8b6a1aa9
Ken Martin <ken.martin@kitware.com>
2005-11-16 10:40:16 -0500

    STYLE: fix docs

--- Source/cmExecProgramCommand.h
+++ Source/cmExecProgramCommand.h
@@ -72,15 +72,15 @@ public:
       "               [ARGS <arguments to executable>]\n"
       "               [OUTPUT_VARIABLE <var>]\n"
       "               [RETURN_VALUE <var>])\n"
-      "The executable is run in the optionally specified Directory.  The "
+      "The executable is run in the optionally specified directory.  The "
       "executable can include arguments if it is double quoted, but it is "
       "better to use the optional ARGS argument to specify arguments to the "
       "program.   This is because cmake will then be able to escape spaces "
-      "in the Executable path.  An optional argument OUTPUT_VARIABLE "
+      "in the executable path.  An optional argument OUTPUT_VARIABLE "
       "specifies a variable in which to store the output. "
-      "To capture the return value of the execution, use RETURN_VALUE variable. "
-      "If OUTPUT_VARIABLE is specified, then no output will go to the stdout/stderr "
-      "of the console running cmake.";
+      "To capture the return value of the execution, provide a RETURN_VALUE. "
+      "If OUTPUT_VARIABLE is specified, then no output will go to the "
+      "stdout/stderr of the console running cmake.";
     }
   
   cmTypeMacro(cmExecProgramCommand, cmCommand);
