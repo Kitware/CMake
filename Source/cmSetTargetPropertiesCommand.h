590377dcdddeb6b894c9c4e3f152a081ff879ceb
Ken Martin <ken.martin@kitware.com>
2005-11-17 10:41:37 -0500

    STYLE: fix docs

--- Source/cmSetTargetPropertiesCommand.h
+++ Source/cmSetTargetPropertiesCommand.h
@@ -58,15 +58,15 @@ public:
         "Set properties on a target. The syntax for the command is to "
         "list all the files you want "
         "to change, and then provide the values you want to set next.  "
-        "Properties that cmake knows about are PREFIX and SUFFIX for Unix "
+        "Properties that cmake knows about are PREFIX and SUFFIX for UNIX "
         "systems and libraries.   CMake also knows about LINK_FLAGS, which "
         "can be used to add extra flags to the link step of a target. "
         "DEFINE_SYMBOL is a symbol that is defined when compiling C or C++ "
-        "sources.  PRE_INSTALL_SCRIPT specifies CMake script that is run "
+        "sources.  PRE_INSTALL_SCRIPT specifies a CMake script that is run "
         "prior to installing the target. POST_INSTALL_SCRIPT specifies "
-        "CMake script that is run after target is installed. "
+        "a CMake script that is run after target is installed. "
         "For shared libraries VERSION and SOVERSION can be used to specify "
-        "the build version and api version respecitvely. When building or "
+        "the build version and api version respectively. When building or "
         "installing appropriate symlinks are created if the platform "
         "supports symlinks and the linker supports so-names. "
         "If only one of both is specified the missing is assumed to have "
@@ -75,11 +75,11 @@ public:
         "When building or installing appropriate symlinks are created if "
         "the platform supports symlinks. "
         "The OUTPUT_NAME can be used to set an output name that is "
-        "used in place of the target name when creating executables."
+        "used in place of the target name when creating executables. "
         "If not set here then it is set to target_EXPORTS by default "
-        "(with some substitutions if target is not a valid C identifier).  "
-        "You can use and prop value pair you want and extract it later with "
-        "the GET_TARGET_PROPERTY command.";
+        "(with some substitutions if the target is not a valid C "
+        "identifier).  You can use any prop value pair you want and "
+        "extract it later with the GET_TARGET_PROPERTY command.";
     }
   
   cmTypeMacro(cmSetTargetPropertiesCommand, cmCommand);
