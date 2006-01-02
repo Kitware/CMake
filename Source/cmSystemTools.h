8477aa59e5b5f52f81cc140012471faaeba11dbd
Andy Cedilnik <andy.cedilnik@kitware.com>
2006-01-01 23:31:17 -0500

    ENH: Merge from cpack branch

--- Source/cmSystemTools.h
+++ Source/cmSystemTools.h
@@ -297,6 +297,9 @@ public:
       of the form var=value */
   static bool PutEnv(const char* value);
 
+  /** Make string XML safe */
+  static std::string MakeXMLSafe(const char* str);
+
   /** Create tar */
   static bool ListTar(const char* outFileName, std::vector<cmStdString>& files, bool gzip, bool verbose);
   static bool CreateTar(const char* outFileName, const std::vector<cmStdString>& files, bool gzip, bool verbose);
