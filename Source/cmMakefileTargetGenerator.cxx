13661cdd23790dd6d8c118ba7b95b278966e7dae
Ken Martin <ken.martin@kitware.com>
2006-02-16 13:42:58 -0500

    ENH: warning fix

--- Source/cmMakefileTargetGenerator.cxx
+++ Source/cmMakefileTargetGenerator.cxx
@@ -55,7 +55,7 @@ cmMakefileTargetGenerator::New(cmLocalUnixMakefileGenerator3 *lg,
       result = new cmMakefileUtilityTargetGenerator;
       break;
     default:
-      return result
+      return result;
       break;
     }
 
