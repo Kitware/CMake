d41587b67127fb03307ded975a687dde3176ba8e
Alexander Neundorf <neundorf@kde.org>
2006-03-15 13:20:03 -0500

    BUG: change the formatting of the pkgconfig module documentation so that it doesn't crash some versions of konqueror (fixed with current konqy)
    Alex

--- Modules/UsePkgConfig.cmake
+++ Modules/UsePkgConfig.cmake
@@ -1,10 +1,16 @@
 # - pkg-config module for CMake
+#
 # Defines the following macros:
-#   PKGCONFIG(package includedir libdir linkflags cflags)
-#     - Calling PKGCONFIG will fill the desired information into the 4 given arguments,
-#       e.g. PKGCONFIG(libart-2.0 LIBART_INCLUDE_DIR LIBART_LINK_DIR LIBART_LINK_FLAGS LIBART_CFLAGS)
-#      if pkg-config was NOT found or the specified software package doesn't exist, the
-#      variable will be empty when the function returns, otherwise they will contain the respective information
+#
+# PKGCONFIG(package includedir libdir linkflags cflags)
+#
+# Calling PKGCONFIG will fill the desired information into the 4 given arguments,
+# e.g. PKGCONFIG(libart-2.0 LIBART_INCLUDE_DIR LIBART_LINK_DIR LIBART_LINK_FLAGS LIBART_CFLAGS)
+# if pkg-config was NOT found or the specified software package doesn't exist, the
+# variable will be empty when the function returns, otherwise they will contain the respective information
+#
+
+
 
 FIND_PROGRAM(PKGCONFIG_EXECUTABLE NAMES pkg-config PATHS /usr/local/bin )
 
