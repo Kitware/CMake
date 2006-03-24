a0471c3fe2e959728a5fd75fb3583e1c8b55ec44
Bill Hoffman <bill.hoffman@kitware.com>
2006-03-24 16:13:05 -0500

    ENH: allow unset vars to be used in list length

--- Source/cmListCommand.cxx
+++ Source/cmListCommand.cxx
@@ -106,11 +106,10 @@ bool cmListCommand::HandleLengthCommand(std::vector<std::string> const& args)
   const std::string& listName = args[1];
   const std::string& variableName = args[args.size() - 1];
   std::vector<std::string> varArgsExpanded;
-  if ( !this->GetList(varArgsExpanded, listName.c_str()) )
-    {
-    return false;
-    }
-
+  // do not check the return value here
+  // if the list var is not found varArgsExpanded will have size 0
+  // and we will return 0
+  this->GetList(varArgsExpanded, listName.c_str());
   size_t length = varArgsExpanded.size();
   char buffer[1024];
   sprintf(buffer, "%d", static_cast<int>(length));
