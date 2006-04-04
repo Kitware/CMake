58538732be4b78783aac4e45e2ce7bca41355495
Bill Hoffman <bill.hoffman@kitware.com>
2006-04-04 17:27:03 -0400

    ENH: add search directories for 32 bit devenv from a 64bit cmake

--- Modules/CMakeVS8FindMake.cmake
+++ Modules/CMakeVS8FindMake.cmake
@@ -4,8 +4,11 @@ FIND_PROGRAM(CMAKE_MAKE_PROGRAM
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\8.0\\Setup\\VS;EnvironmentDirectory]
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\8.0\\Setup;Dbghelp_path]
   "$ENV{ProgramFiles}/Microsoft Visual Studio .NET/Common7/IDE"
-  "c:/Program Files/Microsoft Visual Studio 8/Common7/IDE"
-  "c:/Program Files/Microsoft Visual Studio8/Common7/IDE"
+  "$ENV{ProgramFiles}/Microsoft Visual Studio 8/Common7/IDE"
+  "$ENV{ProgramFiles}/Microsoft Visual Studio8/Common7/IDE"
+  "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio .NET/Common7/IDE"
+  "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio 8/Common7/IDE"
+  "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio8/Common7/IDE"
   "/Program Files/Microsoft Visual Studio 8/Common7/IDE/"
   )
 MARK_AS_ADVANCED(CMAKE_MAKE_PROGRAM)
