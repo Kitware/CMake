1bca540e524787961c3fbe253113c9c04a828bc7
Ken Martin <ken.martin@kitware.com>
2006-04-12 11:36:55 -0400

    ENH: fix compile issue on HP hopefully

--- Source/cmGlobalGenerator.h
+++ Source/cmGlobalGenerator.h
@@ -175,8 +175,7 @@ public:
       configuration.  This is valid during generation only.  */
   cmTargetManifest const& GetTargetManifest() { return this->TargetManifest; }
 
-  void AddTarget(cmTargets::value_type &v) { 
-    this->TotalTargets.insert(std::pair<cmStdString,cmTarget *>(v.first,&v.second));};
+  void AddTarget(cmTargets::value_type &v) { this->TotalTargets[v.first] = &v.second;};
   
   /** Support for multiple custom command outputs.  */
   virtual void CheckMultipleOutputs(cmMakefile* mf, bool verbose);
