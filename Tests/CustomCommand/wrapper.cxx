58d0c5e17670a61d7fb9b4be5978e1b29f801bb4
Brad King <brad.king@kitware.com>
2006-04-13 15:28:57 -0400

    COMP: Do not use ANSI function prototypes to pacify HP.

--- Tests/CustomCommand/wrapper.cxx
+++ Tests/CustomCommand/wrapper.cxx
@@ -8,11 +8,11 @@ int main(int argc, char *argv[])
     return 1;
     }
   FILE *fp = fopen(argv[1],"w");
-  fprintf(fp,"extern int wrapped_help(void);\n");
-  fprintf(fp,"int wrapped(void) { return wrapped_help(); }\n");
+  fprintf(fp,"extern int wrapped_help();\n");
+  fprintf(fp,"int wrapped() { return wrapped_help(); }\n");
   fclose(fp);
   fp = fopen(argv[2],"w");
-  fprintf(fp,"int wrapped_help(void) { return 5; }\n");
+  fprintf(fp,"int wrapped_help() { return 5; }\n");
   fclose(fp);
   return 0;
 }
