31875743e99f0e10c2a4c5e9c446bd46984dac90
Bill Hoffman <bill.hoffman@kitware.com>
2006-04-20 10:51:16 -0400

    ENH: fix help for global targets

--- Source/cmGlobalUnixMakefileGenerator3.cxx
+++ Source/cmGlobalUnixMakefileGenerator3.cxx
@@ -808,8 +808,6 @@ void cmGlobalUnixMakefileGenerator3::WriteHelpRule
   lg->AppendEcho(commands,"... all (the default if no target is provided)");
   lg->AppendEcho(commands,"... clean");
   lg->AppendEcho(commands,"... depend");
-  lg->AppendEcho(commands,"... install");
-  lg->AppendEcho(commands,"... rebuild_cache");
   
   // Keep track of targets already listed.
   std::set<cmStdString> emittedTargets;
@@ -831,8 +829,8 @@ void cmGlobalUnixMakefileGenerator3::WriteHelpRule
         if((t->second.GetType() == cmTarget::EXECUTABLE) ||
            (t->second.GetType() == cmTarget::STATIC_LIBRARY) ||
            (t->second.GetType() == cmTarget::SHARED_LIBRARY) ||
-           (t->second.GetType() == cmTarget::GLOBAL_TARGET) ||
            (t->second.GetType() == cmTarget::MODULE_LIBRARY) ||
+           (t->second.GetType() == cmTarget::GLOBAL_TARGET) ||
            (t->second.GetType() == cmTarget::UTILITY))
           {
           if(emittedTargets.insert(t->second.GetName()).second)
