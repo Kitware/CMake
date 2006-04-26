c8e355b6a17388b84d4b6b15ce17f94f6ce9ba46
Brad King <brad.king@kitware.com>
2006-04-26 14:28:56 -0400

    BUG: Mode should only be used if CMakeLists.txt is at the end of the buffer name.

--- Docs/cmake-mode.el
+++ Docs/cmake-mode.el
@@ -25,7 +25,7 @@
 ;;  (setq load-path (cons (expand-file-name "/dir/with/cmake-mode") load-path))
 ;;  (require 'cmake-mode)
 ;;  (setq auto-mode-alist
-;;        (append '(("CMakeLists\\.txt" . cmake-mode)
+;;        (append '(("CMakeLists\\.txt$" . cmake-mode)
 ;;                  ("\\.cmake$" . cmake-mode))
 ;;                auto-mode-alist))
 
