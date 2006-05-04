1794836ee3e8727c9eed233f1ebaa0a1dbf76e57
Bill Hoffman <bill.hoffman@kitware.com>
2006-05-04 13:35:56 -0400

    ENH: fix build c stuff with c and c++ with c++

--- Tests/COnly/conly.c
+++ Tests/COnly/conly.c
@@ -7,6 +7,7 @@
 
 int main ()
 {
+  int class = 0;
   if ( LibC1Func() != 2.0 )
     {
     printf("Problem with libc1\n");
@@ -17,6 +18,6 @@ int main ()
     printf("Problem with libc2\n");
     return 1;
     }
-  printf("Foo: %s\n", foo);
+  printf("Foo: %s %d\n", foo, class);
   return 0;
 }
