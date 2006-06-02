e1c110d2448e09ca087b9ac42dac04934bad8de6
Brad King <brad.king@kitware.com>
2006-06-02 12:19:16 -0400

    BUG: Custom commands should actually generate the files they claim to generate.

--- Tests/Wrapping/Wrap.c
+++ Tests/Wrapping/Wrap.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, const char* argv[])
 {
-  printf("Hello\n");
+  FILE* fout = fopen(argv[argc-1], "w");
+  printf("Wrap creating \"%s\"\n", argv[argc-1]);
+  if(fout)
+    {
+    fprintf(fout, "int foo() { return 0; }\n");
+    fclose(fout);
+    }
+  else
+    {
+    return -1;
+    }
   return 0;
 }
