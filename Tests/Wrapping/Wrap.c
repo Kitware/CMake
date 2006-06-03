c32b8c196de9f2121d86c3dafcfdc992f367f492
Brad King <brad.king@kitware.com>
2006-06-03 09:48:39 -0400

    COMP: More fixes for non-ANSI C compilers.

--- Tests/Wrapping/Wrap.c
+++ Tests/Wrapping/Wrap.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char** argv)
+#ifdef __CLASSIC_C__
+int main(argc, argv)
+  int argc;
+  char ** argv;
+#else
+int main(int argc, const char* argv[])
+#endif
 {
   FILE* fout = fopen(argv[argc-1], "w");
   printf("Wrap creating \"%s\"\n", argv[argc-1]);
