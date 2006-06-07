8d604cfad31041d092cd069b25bfc43a33cb1846
Bill Hoffman <bill.hoffman@kitware.com>
2006-06-07 08:47:44 -0400

    ENH: add docs for debug trycompile

--- Source/cmakemain.cxx
+++ Source/cmakemain.cxx
@@ -82,6 +82,9 @@ static const cmDocumentationEntry cmDocumentationOptions[] =
   {"--graphviz=[file]", "Generate graphviz of dependencies.",
    "Generate a graphviz input file that will contain all the library and "
    "executable dependencies in the project."},
+  {"--debug-trycompile", "Do not delete the try compile directories..",
+   "Do not delete the files and directories created for try_compile calls. "
+   "This is useful in debugging failed try_compiles."},
   {"--help-command cmd [file]", "Print help for a single command and exit.",
    "Full documentation specific to the given command is displayed."},
   {"--help-command-list [file]", "List available listfile commands and exit.",
