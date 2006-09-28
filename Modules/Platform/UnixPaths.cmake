506dca3990e56281bd0eecd6c26afe6d36caab6e
Brad King <brad.king@kitware.com>
2006-09-28 11:42:19 -0400

    BUG: Header and library search path ordering should be consistent.

--- Modules/Platform/UnixPaths.cmake
+++ Modules/Platform/UnixPaths.cmake
@@ -1,6 +1,32 @@
-SET(CMAKE_SYSTEM_INCLUDE_PATH ${CMAKE_SYSTEM_INCLUDE_PATH} /usr/include  
-  /usr/local/include /usr/local /opt/local/include /usr/X11R6/include /usr/include/X11 /usr/pkg/include)
-SET(CMAKE_SYSTEM_LIBRARY_PATH ${CMAKE_SYSTEM_LIBRARY_PATH} /lib /usr/lib /usr/local/lib 
-  /usr/lib/w32api /usr/X11R6/lib /opt/local/lib /opt/csw/lib /opt/lib /usr/pkg/lib)
-SET(CMAKE_SYSTEM_PROGRAM_PATH ${CMAKE_SYSTEM_PROGRAM_PATH} /bin /usr/bin /usr/local/bin
-  /usr/pkg/bin /sbin)
+SET(CMAKE_SYSTEM_INCLUDE_PATH ${CMAKE_SYSTEM_INCLUDE_PATH}
+  # Standard
+  /include /usr/include /usr/local/include
+
+  # Windows API on Cygwin
+  /usr/include/w32api
+
+  # X11
+  /usr/X11R6/include /usr/include/X11
+
+  # Other
+  /opt/local/include /usr/pkg/include
+  )
+
+SET(CMAKE_SYSTEM_LIBRARY_PATH ${CMAKE_SYSTEM_LIBRARY_PATH}
+  # Standard
+  /lib     /usr/lib     /usr/local/lib
+
+  # Windows API on Cygwin
+  /usr/lib/w32api
+
+  # X11
+  /usr/X11R6/lib /usr/lib/X11
+
+  # Other
+  /opt/local/lib /usr/pkg/lib
+  /opt/csw/lib /opt/lib
+  )
+
+SET(CMAKE_SYSTEM_PROGRAM_PATH ${CMAKE_SYSTEM_PROGRAM_PATH}
+  /bin /usr/bin /usr/local/bin /usr/pkg/bin /sbin
+  )
