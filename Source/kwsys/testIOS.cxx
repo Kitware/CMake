55af790ebfc4d95c57be817eb1b02fa254220602
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-12-14 11:41:51 -0500

    BUG: Remove stringstream implementation, this was a wrong interface anyway.

--- Source/kwsys/testIOS.cxx
+++ Source/kwsys/testIOS.cxx
@@ -13,37 +13,8 @@
 
 int main()
 {
-  const char refstring[] =  "Hello, World!";
   kwsys_ios::ostringstream ostr;
-  ostr << refstring;
+  ostr << "Hello, World!";
   kwsys_ios::cout << ostr.str() << kwsys_ios::endl;
-  if( ostr.str() != refstring )
-    {
-    return 1;
-    }
-
-
-  kwsys_ios::istringstream istr;
-  istr.str( refstring );
-  kwsys_ios::cout << istr.str() << kwsys_ios::endl;
-  if( istr.str() != refstring )
-    {
-    return 1;
-    }
-
-
-  const int val = 12345;
-  const char valstr[] = "12345";
-  kwsys_ios::stringstream sstr;
-  sstr << val;
-  int v = 0;
-  sstr >> v;
-  kwsys_ios::cout << sstr.str() << kwsys_ios::endl;
-  if( /*v != val ||*/ sstr.str() != valstr)
-    {
-    kwsys_ios::cerr << v << " should be " << val << kwsys_ios::endl;
-    return 1;
-    }
-
   return 0;
 }
