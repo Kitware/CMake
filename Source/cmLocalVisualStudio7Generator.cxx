fd799a5d0e1c5fa62b7f0063854fefca875299f8
Bill Hoffman <bill.hoffman@kitware.com>
2007-02-01 17:06:05 -0500

    STYLE: fix line length

--- Source/cmLocalVisualStudio7Generator.cxx
+++ Source/cmLocalVisualStudio7Generator.cxx
@@ -1661,7 +1661,8 @@ void cmLocalVisualStudio7Generator::ConfigureFinalPass()
       cmCustomCommand cc = l->second.GetPostBuildCommands()[0];
       const cmCustomCommandLines& cmds = cc.GetCommandLines();
       std::string project_name = cmds[0][0];
-      this->ReadAndStoreExternalGUID(project_name.c_str(), cmds[0][1].c_str());
+      this->ReadAndStoreExternalGUID(project_name.c_str(),
+                                     cmds[0][1].c_str());
       }
     else
       {
