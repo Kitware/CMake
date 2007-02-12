c55a3e4d487d38144d24f8e6180d32040acbfa5d
Bill Hoffman <bill.hoffman@kitware.com>
2007-02-12 09:15:25 -0500

    ENH: add cygwin on vogon

---
+++ Utilities/Release/vogon_cygwin.cmake
@@ -0,0 +1,10 @@
+set(CMAKE_RELEASE_DIRECTORY "c:/hoffman/CMakeReleaseCygwin")
+set(PROCESSORS 2)
+set(HOST vogon)
+set(CPACK_BINARY_GENERATORS "CygwinBinary")
+set(CPACK_SOURCE_GENERATORS "CygwinSource")
+set(MAKE_PROGRAM "make")
+set(INITIAL_CACHE "CMAKE_BUILD_TYPE:STRING=Release
+")
+get_filename_component(path "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${path}/release_cmake.cmake)
