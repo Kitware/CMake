2fb0e4ba20ff136cf7944da884c70b513c99670a
Alexander Neundorf <neundorf@kde.org>
2007-02-20 16:05:42 -0500

    STYLE: fix docs for FindKDE4.cmake
    Alex

--- Modules/FindKDE4.cmake
+++ Modules/FindKDE4.cmake
@@ -1,9 +1,8 @@
 # Find KDE4 and provide all necessary variables and macros to compile software for it.
 # It looks for KDE 4 in the following directories in the given order:
-# - CMAKE_INSTALL_PREFIX
-# - KDEDIRS
-# - /opt/kde4
-# - /opt/kde
+#  CMAKE_INSTALL_PREFIX
+#  KDEDIRS
+#  /opt/kde4
 #
 # Please look in FindKDE4Internal.cmake and KDE4Macros.cmake for more information.
 # They are installed with the KDE 4 libraries in $KDEDIRS/share/apps/cmake/modules/.
