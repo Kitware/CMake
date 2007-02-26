20d75ac237db63df453e49cbcfb3fb45c04fab7f
Alexander Neundorf <neundorf@kde.org>
2007-02-25 19:22:59 -0500

    BUG: fix typo reported by Duncan Mac Vicar
    Alex

--- Modules/FindGettext.cmake
+++ Modules/FindGettext.cmake
@@ -43,7 +43,7 @@ MACRO(GETTEXT_CREATE_TRANSLATIONS _potFile _firstPoFile)
          DEPENDS ${_absPotFile} ${_absFile} 
       )
 
-      INSTALL(FILES ${_gmoFile} DESTINATION share/locale/${_lang}/LC_MESSAGES RENAME ${_potBaseName}.mo) 
+      INSTALL(FILES ${_gmoFile} DESTINATION share/locale/${_lang}/LC_MESSAGES RENAME ${_potBasename}.mo) 
       SET(_gmoFiles ${_gmoFiles} ${_gmoFile})
 
    ENDFOREACH (_currentPoFile )
