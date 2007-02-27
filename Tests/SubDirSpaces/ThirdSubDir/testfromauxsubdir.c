73818126f32069337080e7f5ffa727bde48d5ce2
Bill Hoffman <bill.hoffman@kitware.com>
2007-02-27 15:11:47 -0500

    ENH: watcom wmake can not handle () in the path with cd command

--- Tests/SubDirSpaces/ThirdSubDir/testfromauxsubdir.c
+++ Tests/SubDirSpaces/ThirdSubDir/testfromauxsubdir.c
@@ -4,12 +4,16 @@ void secondone();
 void pair_stuff();
 void pair_p_stuff();
 void vcl_stuff();
+#ifdef CMAKE_PAREN
 void testOdd();
+#endif
 int main()
 {
   printf("Hello from subdirectory\n");
   secondone();
+#ifdef CMAKE_PAREN
   testOdd();
+#endif
   pair_stuff();
   pair_p_stuff();
   vcl_stuff();
