f2d3e386f72fb91e5eb1512f95c655366032686d
Bill Hoffman <bill.hoffman@kitware.com>
2007-05-08 12:43:06 -0400

    ENH: make it work on non windows

--- Tests/Framework/bar.cxx
+++ Tests/Framework/bar.cxx
@@ -1,5 +1,7 @@
 #ifdef _WIN32
 #  define CM_TEST_LIB_IMPORT  __declspec( dllimport )
+#else 
+#  define CM_TEST_LIB_IMPORT
 #endif
 CM_TEST_LIB_IMPORT void foo();
 int main()
--- Tests/Framework/foo.cxx
+++ Tests/Framework/foo.cxx
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #ifdef _WIN32
 #  define CM_TEST_LIB_EXPORT  __declspec( dllexport )
+#else 
+#  define CM_TEST_LIB_EXPORT
 #endif
 CM_TEST_LIB_EXPORT void foo()
 {
