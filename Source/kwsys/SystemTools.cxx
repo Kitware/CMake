80de27ace5b86bc169874f1b366c9c0c422ab32a
Bill Hoffman <bill.hoffman@kitware.com>
2007-06-06 10:44:20 -0400

    ENH: fix case problem with drive letters and cmake vs CMakeSetup build.make changing

--- Source/kwsys/SystemTools.cxx
+++ Source/kwsys/SystemTools.cxx
@@ -154,6 +154,11 @@ inline const char* Getcwd(char* buf, unsigned int len)
     fprintf(stderr, "No current working directory.\n");
     abort();
     }
+  // make sure the drive letter is capital
+  if(strlen(buf) > 1 && buf[1] == ':')
+    {
+    buf[0] = toupper(buf[0]);
+    }
   return ret;
 }
 inline int Chdir(const char* dir)
@@ -2874,6 +2879,11 @@ kwsys_stl::string SystemTools::GetActualCaseForPath(const char* p)
   if(len == 0 || len > MAX_PATH+1)
     {
     return p;
+    }
+  // make sure drive letter is always upper case
+  if(longPath.size() > 1 && longPath[1] == ':')
+    {
+    longPath[0] = toupper(longPath[0]);
     }
   return longPath;
 #endif  
