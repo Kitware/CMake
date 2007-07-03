d9ec3bbe43237aed835bf31274e848f5b0c920ae
Alexander Neundorf <neundorf@kde.org>
2007-07-03 11:41:37 -0400

    STYLE: don't test twice for APPLE
    Alex

--- Modules/FindOpenGL.cmake
+++ Modules/FindOpenGL.cmake
@@ -14,11 +14,6 @@
 # On OSX default to using the framework version of opengl
 # People will have to change the cache values of OPENGL_glu_LIBRARY 
 # and OPENGL_gl_LIBRARY to use OpenGL with X11 on OSX
-IF (APPLE)
-  FIND_LIBRARY(OPENGL_gl_LIBRARY OpenGL DOC "OpenGL lib for OSX")
-  FIND_LIBRARY(OPENGL_glu_LIBRARY AGL DOC "AGL lib for OSX")
-  FIND_PATH(OPENGL_INCLUDE_DIR OpenGL/gl.h DOC "Include for OpenGL on OSX")
-ENDIF (APPLE)
 
 IF (WIN32)
   IF (CYGWIN)
@@ -52,72 +47,79 @@ IF (WIN32)
   ENDIF (CYGWIN)
 
 ELSE (WIN32)
-  IF(NOT APPLE)
 
-  # The first line below is to make sure that the proper headers
-  # are used on a Linux machine with the NVidia drivers installed.
-  # They replace Mesa with NVidia's own library but normally do not
-  # install headers and that causes the linking to
-  # fail since the compiler finds the Mesa headers but NVidia's library.
-  # Make sure the NVIDIA directory comes BEFORE the others.
-  #  - Atanas Georgiev <atanas@cs.columbia.edu>
+  IF (APPLE)
 
-  FIND_PATH(OPENGL_INCLUDE_DIR GL/gl.h
-    /usr/share/doc/NVIDIA_GLX-1.0/include
-    /usr/include
-    /usr/local/include
-    /usr/openwin/share/include
-    /opt/graphics/OpenGL/include
-    /usr/X11R6/include
-  )
+    FIND_LIBRARY(OPENGL_gl_LIBRARY OpenGL DOC "OpenGL lib for OSX")
+    FIND_LIBRARY(OPENGL_glu_LIBRARY AGL DOC "AGL lib for OSX")
+    FIND_PATH(OPENGL_INCLUDE_DIR OpenGL/gl.h DOC "Include for OpenGL on OSX")
 
-  FIND_PATH(OPENGL_xmesa_INCLUDE_DIR GL/xmesa.h
-    /usr/share/doc/NVIDIA_GLX-1.0/include
-    /usr/include
-    /usr/local/include
-    /usr/openwin/share/include
-    /opt/graphics/OpenGL/include
-    /usr/X11R6/include
-  )
+  ELSE(APPLE)
 
-  FIND_LIBRARY(OPENGL_gl_LIBRARY
-    NAMES GL MesaGL
-    PATHS /usr/lib
-          /usr/local/lib
-          /opt/graphics/OpenGL/lib
-          /usr/openwin/lib
-          /usr/X11R6/lib
-          /usr/shlib
-  )
+    # The first line below is to make sure that the proper headers
+    # are used on a Linux machine with the NVidia drivers installed.
+    # They replace Mesa with NVidia's own library but normally do not
+    # install headers and that causes the linking to
+    # fail since the compiler finds the Mesa headers but NVidia's library.
+    # Make sure the NVIDIA directory comes BEFORE the others.
+    #  - Atanas Georgiev <atanas@cs.columbia.edu>
 
-  # On Unix OpenGL most certainly always requires X11.
-  # Feel free to tighten up these conditions if you don't 
-  # think this is always true.
-  # It's not true on OSX.
+    FIND_PATH(OPENGL_INCLUDE_DIR GL/gl.h
+      /usr/share/doc/NVIDIA_GLX-1.0/include
+      /usr/include
+      /usr/local/include
+      /usr/openwin/share/include
+      /opt/graphics/OpenGL/include
+      /usr/X11R6/include
+    )
 
-  IF (OPENGL_gl_LIBRARY)
-    IF(NOT X11_FOUND)
-      INCLUDE(FindX11)
-    ENDIF(NOT X11_FOUND)
-    IF (X11_FOUND)
-      IF (NOT APPLE)
-        SET (OPENGL_LIBRARIES ${X11_LIBRARIES})
-      ENDIF (NOT APPLE)
-    ENDIF (X11_FOUND)
-  ENDIF (OPENGL_gl_LIBRARY)
+    FIND_PATH(OPENGL_xmesa_INCLUDE_DIR GL/xmesa.h
+      /usr/share/doc/NVIDIA_GLX-1.0/include
+      /usr/include
+      /usr/local/include
+      /usr/openwin/share/include
+      /opt/graphics/OpenGL/include
+      /usr/X11R6/include
+    )
 
-  FIND_LIBRARY(OPENGL_glu_LIBRARY
-    NAMES GLU MesaGLU
-    PATHS ${OPENGL_gl_LIBRARY}
-          /usr/lib
-          /usr/local/lib
-          /opt/graphics/OpenGL/lib
-          /usr/openwin/lib
-          /usr/X11R6/lib
-          /usr/shlib
-  )
+    FIND_LIBRARY(OPENGL_gl_LIBRARY
+      NAMES GL MesaGL
+      PATHS /usr/lib
+            /usr/local/lib
+            /opt/graphics/OpenGL/lib
+            /usr/openwin/lib
+            /usr/X11R6/lib
+            /usr/shlib
+    )
 
-  ENDIF(NOT APPLE)
+    # On Unix OpenGL most certainly always requires X11.
+    # Feel free to tighten up these conditions if you don't 
+    # think this is always true.
+    # It's not true on OSX.
+
+    IF (OPENGL_gl_LIBRARY)
+      IF(NOT X11_FOUND)
+        INCLUDE(FindX11)
+      ENDIF(NOT X11_FOUND)
+      IF (X11_FOUND)
+        IF (NOT APPLE)
+          SET (OPENGL_LIBRARIES ${X11_LIBRARIES})
+        ENDIF (NOT APPLE)
+      ENDIF (X11_FOUND)
+    ENDIF (OPENGL_gl_LIBRARY)
+
+    FIND_LIBRARY(OPENGL_glu_LIBRARY
+      NAMES GLU MesaGLU
+      PATHS ${OPENGL_gl_LIBRARY}
+            /usr/lib
+            /usr/local/lib
+            /opt/graphics/OpenGL/lib
+            /usr/openwin/lib
+            /usr/X11R6/lib
+            /usr/shlib
+    )
+
+  ENDIF(APPLE)
 ENDIF (WIN32)
 
 SET( OPENGL_FOUND "NO" )
