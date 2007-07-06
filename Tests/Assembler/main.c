ef7b647b3b11d6526439d3ececed254c237b3eac
Alexander Neundorf <neundorf@kde.org>
2007-07-06 08:53:16 -0400

    COMP: OPTIONAL was missing in ENABLE_LANGUAGE() -the assembler file seems to work for Linux and FreeBSD -try to fix main() for HP-UX compiler
    Alex

--- Tests/Assembler/main.c
+++ Tests/Assembler/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(int argc, char** argv)
+#ifdef __CLASSIC_C__
+int main(){
+  int ac;
+  char*av[];
+#else
+int main(int ac, char*av[]){
+#endif
 {
   printf("hello assembler world, %d arguments  given\n", argc);
   return 0;
