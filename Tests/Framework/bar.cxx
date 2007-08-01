64f40a2816de1dcb2e064d961fc0ac4e3c52d551
David Cole <david.cole@kitware.com>
2007-08-01 16:15:54 -0400

    BUG: Fix test that broke on Windows - sharing sources between SHARED and STATIC libraries requires correct export and import decorations in the source code...

--- Tests/Framework/bar.cxx
+++ Tests/Framework/bar.cxx
@@ -1,9 +1,4 @@
-#ifdef _WIN32
-#  define CM_TEST_LIB_IMPORT  __declspec( dllimport )
-#else 
-#  define CM_TEST_LIB_IMPORT
-#endif
-CM_TEST_LIB_IMPORT void foo();
+void foo();
 int main()
 {
   foo();
--- Tests/Framework/foo.cxx
+++ Tests/Framework/foo.cxx
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#ifdef _WIN32
+#if defined(_WIN32) && defined(foo_EXPORTS)
 #  define CM_TEST_LIB_EXPORT  __declspec( dllexport )
 #else 
 #  define CM_TEST_LIB_EXPORT
