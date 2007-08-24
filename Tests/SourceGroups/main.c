90aabf14f93de211229bd701c9a4c3ec98e31fdb
Alexander Neundorf <neundorf@kde.org>
2007-08-24 14:39:13 -0400

    BUG: demo (not really test) for the source_group() command
    Alex

--- Tests/SourceGroups/main.c
+++ Tests/SourceGroups/main.c
@@ -4,9 +4,10 @@ extern int foo(void);
 extern int bar(void);
 extern int foobar(void);
 extern int barbar(void);
+extern int baz(void);
 
 int main()
 {
-   printf("foo: %d bar: %d foobar: %d barbar: %d\n", foo(), bar(), foobar(), barbar());
+   printf("foo: %d bar: %d foobar: %d barbar: %d baz: %d\n", foo(), bar(), foobar(), barbar(), baz());
    return 0;
 }
