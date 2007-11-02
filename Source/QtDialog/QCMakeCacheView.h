6921981452052190ae420868b666a732ab1cab26
Clinton Stimpson <clinton@elemtech.com>
2007-11-02 11:55:57 -0400

    STYLE:  Add license info to code.

--- Source/QtDialog/QCMakeCacheView.h
+++ Source/QtDialog/QCMakeCacheView.h
@@ -1,3 +1,19 @@
+/*=========================================================================
+
+  Program:   CMake - Cross-Platform Makefile Generator
+  Module:    $RCSfile$
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Kitware, Inc., Insight Consortium.  All rights reserved.
+  See Copyright.txt or http://www.cmake.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 
 #ifndef QCMakeCacheView_h
 #define QCMakeCacheView_h
