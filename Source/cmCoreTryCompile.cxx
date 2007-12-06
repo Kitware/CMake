3944e268f430e6e2e9dbf837d875d609b8bf3234
Bill Hoffman <bill.hoffman@kitware.com>
2007-12-06 09:56:02 -0500

    ENH: for try compile do not put the rules to rebuild the project with cmake inside it.  This has caused infinite loops of builds in some cases, and it is just a waste of time anyway.

--- Source/cmCoreTryCompile.cxx
+++ Source/cmCoreTryCompile.cxx
@@ -214,6 +214,7 @@ int cmCoreTryCompile::TryCompileCode(std::vector<std::string> const& argv)
       }
     fprintf(fout, " ${COMPILE_DEFINITIONS}\")\n");
     fprintf(fout, "INCLUDE_DIRECTORIES(${INCLUDE_DIRECTORIES})\n");
+    fprintf(fout, "SET(CMAKE_SUPPRESS_REGENERATION 1)\n");
     fprintf(fout, "LINK_DIRECTORIES(${LINK_DIRECTORIES})\n");
     // handle any compile flags we need to pass on
     if (compileFlags.size())
