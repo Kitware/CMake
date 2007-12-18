1cfe812f119b2ba6b7fcd82cab7dea36f2eae021
Bill Hoffman <bill.hoffman@kitware.com>
2007-12-17 21:37:16 -0500

    BUG: fix for 5722

--- Modules/FindPkgConfig.cmake
+++ Modules/FindPkgConfig.cmake
@@ -22,10 +22,10 @@
 #   <XPREFIX>_LIBRARIES      ... only the libraries (w/o the '-l')
 #   <XPREFIX>_LIBRARY_DIRS   ... the paths of the libraries (w/o the '-L')
 #   <XPREFIX>_LDFLAGS        ... all required linker flags
-#   <XPREFIX>_LDFLAGS_OTHERS ... all other linker flags
+#   <XPREFIX>_LDFLAGS_OTHER  ... all other linker flags
 #   <XPREFIX>_INCLUDE_DIRS   ... the '-I' preprocessor flags (w/o the '-I')
 #   <XPREFIX>_CFLAGS         ... all required cflags
-#   <XPREFIX>_CFLAGS_OTHERS  ... the other compiler flags
+#   <XPREFIX>_CFLAGS_OTHER   ... the other compiler flags
 #
 #   <XPREFIX> = <PREFIX>        for common case
 #   <XPREFIX> = <PREFIX>_STATIC for static linking
