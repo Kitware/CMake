611bff2c1b8a0ce533d92662504a391c9e3d494b
Brad King <brad.king@kitware.com>
2008-01-28 13:06:17 -0500

    ENH: Added framework to ExportImport test.

--- Tests/ExportImport/Import/imp_testExe1.c
+++ Tests/ExportImport/Import/imp_testExe1.c
@@ -1,8 +1,9 @@
 extern int generated_by_testExe1();
 extern int testLib2();
 extern int testLib3();
+extern int testLib4();
 
 int main()
 {
-  return testLib2() + generated_by_testExe1() + testLib3();
+  return testLib2() + generated_by_testExe1() + testLib3() + testLib4();
 }
