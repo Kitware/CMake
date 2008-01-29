6e70424fc85ad81f60c589dd01cc127af1be0fad
Sebastien Barre <sebastien.barre@kitware.com>
2008-01-29 15:10:37 -0500

    ENH: need quotes

--- Modules/FindHTMLHelp.cmake
+++ Modules/FindHTMLHelp.cmake
@@ -5,18 +5,18 @@
 #   HTML_HELP_LIBRARY      : full path to the library (htmlhelp.lib)
 #
 
-IF (WIN32)
+IF(WIN32)
 
-  FIND_PROGRAM (HTML_HELP_COMPILER
+  FIND_PROGRAM(HTML_HELP_COMPILER
     hhc
     "[HKEY_CURRENT_USER\\Software\\Microsoft\\HTML Help Workshop;InstallDir]"
     "$ENV{ProgramFiles}/HTML Help Workshop"
     "C:/Program Files/HTML Help Workshop" 
     )
 
-  GET_FILENAME_COMPONENT (HTML_HELP_COMPILER_PATH ${HTML_HELP_COMPILER} PATH)
+  GET_FILENAME_COMPONENT(HTML_HELP_COMPILER_PATH "${HTML_HELP_COMPILER}" PATH)
 
-  FIND_PATH (HTML_HELP_INCLUDE_PATH 
+  FIND_PATH(HTML_HELP_INCLUDE_PATH 
     htmlhelp.h 
     "${HTML_HELP_COMPILER_PATH}/include"
     "[HKEY_CURRENT_USER\\Software\\Microsoft\\HTML Help Workshop;InstallDir]/include"
@@ -24,7 +24,7 @@ IF (WIN32)
     "C:/Program Files/HTML Help Workshop/include" 
     )
 
-  FIND_LIBRARY (HTML_HELP_LIBRARY 
+  FIND_LIBRARY(HTML_HELP_LIBRARY 
     htmlhelp
     "${HTML_HELP_COMPILER_PATH}/lib"
     "[HKEY_CURRENT_USER\\Software\\Microsoft\\HTML Help Workshop;InstallDir]/lib"
@@ -32,10 +32,10 @@ IF (WIN32)
     "C:/Program Files/HTML Help Workshop/lib"
     )
 
-  MARK_AS_ADVANCED (
+  MARK_AS_ADVANCED(
     HTML_HELP_COMPILER
     HTML_HELP_INCLUDE_PATH
     HTML_HELP_LIBRARY
     )
 
-ENDIF (WIN32)
+ENDIF(WIN32)
