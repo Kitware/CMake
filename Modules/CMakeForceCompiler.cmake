5f8feedbace2f8410d70951b8f705cff86ff719e
Brad King <brad.king@kitware.com>
2008-03-03 11:18:49 -0500

    STYLE: Fixed docs of new CMakeForceCompiler

--- Modules/CMakeForceCompiler.cmake
+++ Modules/CMakeForceCompiler.cmake
@@ -6,13 +6,15 @@
 #   CMAKE_FORCE_C_COMPILER(<compiler> <compiler-id>)
 # It sets CMAKE_C_COMPILER to the given compiler and the cmake
 # internal variable CMAKE_C_COMPILER_ID to the given compiler-id.
-# It also bypasses ???
+# It also bypasses the check for working compiler and basic compiler
+# information tests.
 #
 # Macro CMAKE_FORCE_CXX_COMPILER has the following signature:
 #   CMAKE_FORCE_CXX_COMPILER(<compiler> <compiler-id>)
 # It sets CMAKE_CXX_COMPILER to the given compiler and the cmake
 # internal variable CMAKE_CXX_COMPILER_ID to the given compiler-id.
-# It also bypasses ???
+# It also bypasses the check for working compiler and basic compiler
+# information tests.
 #
 # So a simple toolchain file could look like this:
 #   INCLUDE (CMakeForceCompiler)
