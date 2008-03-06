afd8448d0d5a7c351a223c5f01bef92dd3f0baf6
Ken Martin <ken.martin@kitware.com>
2008-03-06 15:51:51 -0500

    BUG: make default CMAKE_BACKWARDS_COMPATIBILITY 2.5

--- Source/cmListFileCache.cxx
+++ Source/cmListFileCache.cxx
@@ -143,7 +143,7 @@ bool cmListFile::ParseFile(const char* filename,
           GetCacheValue("CMAKE_BACKWARDS_COMPATIBILITY"))
       {
         mf->AddCacheDefinition
-          ("CMAKE_BACKWARDS_COMPATIBILITY", "2.4",
+          ("CMAKE_BACKWARDS_COMPATIBILITY", "2.5",
            "For backwards compatibility, what version of CMake commands and "
            "syntax should this version of CMake try to support.",
            cmCacheManager::STRING);
