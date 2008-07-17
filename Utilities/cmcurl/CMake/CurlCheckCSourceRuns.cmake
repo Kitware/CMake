eb8567692872e14ff865b7bb060250f955a2dd03
Brad King <brad.king@kitware.com>
2008-07-17 15:19:17 -0400

    ENH: Avoid cmcurl CMake macro name conflicts
    Utilities/cmcurl/CMake provides macros with the same file names and
    macro names as others in Modules, but with different interfaces.  We
    rename the curl ones to avoid conflict.

--- Utilities/cmcurl/CMake/CurlCheckCSourceRuns.cmake
+++ Utilities/cmcurl/CMake/CurlCheckCSourceRuns.cmake
@@ -1,8 +1,8 @@
-# - Check if the source code provided in the SOURCE argument compiles.
-# CHECK_C_SOURCE_COMPILES(SOURCE VAR)
-# - macro which checks if the source code compiles
+# - Check if the source code provided in the SOURCE argument compiles and runs.
+# CURL_CHECK_C_SOURCE_RUNS(SOURCE VAR)
+# - macro which checks if the source code runs
 #  SOURCE   - source code to try to compile
-#  VAR      - variable to store whether the source code compiled
+#  VAR - variable to store size if the type exists.
 #
 # The following variables may be set before calling this macro to
 # modify the way the check is run:
@@ -12,7 +12,7 @@
 #  CMAKE_REQUIRED_INCLUDES = list of include directories
 #  CMAKE_REQUIRED_LIBRARIES = list of libraries to link
 
-MACRO(CHECK_C_SOURCE_COMPILES SOURCE VAR)
+MACRO(CURL_CHECK_C_SOURCE_RUNS SOURCE VAR)
   IF("${VAR}" MATCHES "^${VAR}$" OR "${VAR}" MATCHES "UNKNOWN")
     SET(message "${VAR}")
     # If the number of arguments is greater than 2 (SOURCE VAR)
@@ -23,16 +23,16 @@ MACRO(CHECK_C_SOURCE_COMPILES SOURCE VAR)
     SET(MACRO_CHECK_FUNCTION_DEFINITIONS
       "-D${VAR} ${CMAKE_REQUIRED_FLAGS}")
     IF(CMAKE_REQUIRED_LIBRARIES)
-      SET(CHECK_C_SOURCE_COMPILES_ADD_LIBRARIES
+      SET(CURL_CHECK_C_SOURCE_COMPILES_ADD_LIBRARIES
         "-DLINK_LIBRARIES:STRING=${CMAKE_REQUIRED_LIBRARIES}")
     ELSE(CMAKE_REQUIRED_LIBRARIES)
-      SET(CHECK_C_SOURCE_COMPILES_ADD_LIBRARIES)
+      SET(CURL_CHECK_C_SOURCE_COMPILES_ADD_LIBRARIES)
     ENDIF(CMAKE_REQUIRED_LIBRARIES)
     IF(CMAKE_REQUIRED_INCLUDES)
-      SET(CHECK_C_SOURCE_COMPILES_ADD_INCLUDES
+      SET(CURL_CHECK_C_SOURCE_COMPILES_ADD_INCLUDES
         "-DINCLUDE_DIRECTORIES:STRING=${CMAKE_REQUIRED_INCLUDES}")
     ELSE(CMAKE_REQUIRED_INCLUDES)
-      SET(CHECK_C_SOURCE_COMPILES_ADD_INCLUDES)
+      SET(CURL_CHECK_C_SOURCE_COMPILES_ADD_INCLUDES)
     ENDIF(CMAKE_REQUIRED_INCLUDES)
     SET(src "")
     FOREACH(def ${EXTRA_DEFINES})
@@ -48,28 +48,36 @@ MACRO(CHECK_C_SOURCE_COMPILES SOURCE VAR)
       "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.c"
       IMMEDIATE)
     MESSAGE(STATUS "Performing Test ${message}")
-    TRY_COMPILE(${VAR}
+    TRY_RUN(${VAR} ${VAR}_COMPILED
       ${CMAKE_BINARY_DIR}
       ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src.c
       COMPILE_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS}
       CMAKE_FLAGS -DCOMPILE_DEFINITIONS:STRING=${MACRO_CHECK_FUNCTION_DEFINITIONS}
-      "${CHECK_C_SOURCE_COMPILES_ADD_LIBRARIES}"
-      "${CHECK_C_SOURCE_COMPILES_ADD_INCLUDES}"
+      "${CURL_CHECK_C_SOURCE_COMPILES_ADD_LIBRARIES}"
+      "${CURL_CHECK_C_SOURCE_COMPILES_ADD_INCLUDES}"
       OUTPUT_VARIABLE OUTPUT)
-    IF(${VAR})
+    # if it did not compile make the return value fail code of 1
+    IF(NOT ${VAR}_COMPILED)
+      SET(${VAR} 1)
+    ENDIF(NOT ${VAR}_COMPILED)
+    # if the return value was 0 then it worked
+    SET(result_var ${${VAR}})
+    IF("${result_var}" EQUAL 0)
       SET(${VAR} 1 CACHE INTERNAL "Test ${message}")
       MESSAGE(STATUS "Performing Test ${message} - Success")
       FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeOutput.log
         "Performing C SOURCE FILE Test ${message} succeded with the following output:\n"
         "${OUTPUT}\n"
+        "Return value: ${${VAR}}\n"
         "Source file was:\n${src}\n")
-    ELSE(${VAR})
+    ELSE("${result_var}" EQUAL 0)
       MESSAGE(STATUS "Performing Test ${message} - Failed")
       SET(${VAR} "" CACHE INTERNAL "Test ${message}")
       FILE(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log
         "Performing C SOURCE FILE Test ${message} failed with the following output:\n"
         "${OUTPUT}\n"
+        "Return value: ${result_var}\n"
         "Source file was:\n${src}\n")
-    ENDIF(${VAR})
+    ENDIF("${result_var}" EQUAL 0)
   ENDIF("${VAR}" MATCHES "^${VAR}$" OR "${VAR}" MATCHES "UNKNOWN")
-ENDMACRO(CHECK_C_SOURCE_COMPILES)
+ENDMACRO(CURL_CHECK_C_SOURCE_RUNS)
