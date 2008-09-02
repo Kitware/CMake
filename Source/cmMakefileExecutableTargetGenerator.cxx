d797592726a48b0bbb555eeccf627481af548a76
David Cole <david.cole@kitware.com>
2008-09-02 12:43:45 -0400

    BUG: Fix issue #3648 - make sure CMake reruns if a Bundle application's directory is removed or if it's Info.plist file disappears since those elements are put in place at CMake configure time.

--- Source/cmMakefileExecutableTargetGenerator.cxx
+++ Source/cmMakefileExecutableTargetGenerator.cxx
@@ -504,6 +504,7 @@ cmMakefileExecutableTargetGenerator::CreateAppBundle(std::string& targetName,
   outpath += "MacOS";
   cmSystemTools::MakeDirectory(outpath.c_str());
   outpath += "/";
+  this->Makefile->AddCMakeDependFile(outpath.c_str());
 
   // Configure the Info.plist file.  Note that it needs the executable name
   // to be set.
@@ -511,4 +512,5 @@ cmMakefileExecutableTargetGenerator::CreateAppBundle(std::string& targetName,
   this->LocalGenerator->GenerateAppleInfoPList(this->Target,
                                                targetName.c_str(),
                                                plist.c_str());
+  this->Makefile->AddCMakeDependFile(plist.c_str());
 }
