4d3ae30bee240d270582ad5528b1b404c3b98517
Bill Hoffman <bill.hoffman@kitware.com>
2008-09-09 13:01:46 -0400

    ENH: support old cmake for dashboards

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,3 +5,8 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "www.cdash.org")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=CMake")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
+# use old trigger stuff so that cmake 2.4 and below will not 
+# get errors on trigger
+SET (TRIGGER_SITE 
+  "http://public.kitware.com/cgi-bin/Submit-CMake-TestingResults.cgi")
