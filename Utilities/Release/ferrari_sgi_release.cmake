4c17c9b87b24cc2239486a55c1d552a484c65a22
Bill Hoffman <bill.hoffman@kitware.com>
2008-10-01 13:13:23 -0400

    ENH: add new sgi release scripts

---
+++ Utilities/Release/ferrari_sgi_release.cmake
@@ -0,0 +1,12 @@
+set(CVS_COMMAND "/home/whoffman/bin/cvs")
+set(CMAKE_RELEASE_DIRECTORY "/home/whoffman/CMakeReleaseDirectory")
+set(PROCESSORS 2)
+set(HOST sgi)
+set(MAKE_PROGRAM "make")
+set(MAKE "${MAKE_PROGRAM} -P")
+set(INITIAL_CACHE "CMAKE_BUILD_TYPE:STRING=Release
+CMAKE_SKIP_BOOTSTRAP_TEST:STRING=TRUE
+CPACK_SYSTEM_NAME:STRING=IRIX64-n32
+")
+get_filename_component(path "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${path}/release_cmake.cmake)
