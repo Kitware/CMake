4cb360afd25dd2967b3fc273e174e13ac11b4aeb
Bill Hoffman <bill.hoffman@kitware.com>
2008-10-29 11:50:20 -0400

    ENH: fix for xlf module linkage

--- Modules/FortranCInterface.cmake
+++ Modules/FortranCInterface.cmake
@@ -87,7 +87,7 @@ function(discover_fortran_module_mangling prefix suffix found)
   
   foreach(interface 
       "test_interface$" 
-      ".__test_interface_NMOD_" 
+      "__test_interface_NMOD_" 
       "__test_interface_MOD_")
     test_fortran_mangling("${CODE}" "${interface}"
       ${FORTRAN_C_MANGLING_UPPERCASE} "" "module" "sub" worked)
