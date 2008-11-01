16b4dcbe502a157b44db0412255c275afb941888
Brad King <brad.king@kitware.com>
2008-11-01 00:03:01 -0400

    STYLE: Nightly Date Stamp

--- Source/kwsys/kwsysDateStamp.cmake
+++ Source/kwsys/kwsysDateStamp.cmake
@@ -4,7 +4,7 @@
 SET(KWSYS_DATE_STAMP_YEAR  2008)
 
 # KWSys version date month component.  Format is MM.
-SET(KWSYS_DATE_STAMP_MONTH 10)
+SET(KWSYS_DATE_STAMP_MONTH 11)
 
 # KWSys version date day component.  Format is DD.
-SET(KWSYS_DATE_STAMP_DAY   31)
+SET(KWSYS_DATE_STAMP_DAY   01)
