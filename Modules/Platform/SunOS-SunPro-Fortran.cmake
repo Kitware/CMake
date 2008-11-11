a4f04d581705b5a33a2801ed62300db24b82bd22
Bill Hoffman <bill.hoffman@kitware.com>
2008-11-11 13:58:31 -0500

    ENH: fix fortran flags on sun

--- Modules/Platform/SunOS-SunPro-Fortran.cmake
+++ Modules/Platform/SunOS-SunPro-Fortran.cmake
@@ -1,2 +1,5 @@
-SET(CMAKE_Fortran_MODDIR_FLAG "-moddir=")
-SET(CMAKE_Fortran_MODPATH_FLAG "-M")
+set(CMAKE_Fortran_MODDIR_FLAG "-moddir=")
+set(CMAKE_Fortran_MODPATH_FLAG "-M")
+set(CMAKE_EXECUTABLE_RUNTIME_Fortran_FLAG "-R")
+set(CMAKE_SHARED_LIBRARY_Fortran_FLAGS "-KPIC")
+
