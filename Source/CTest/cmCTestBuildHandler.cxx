046449e2f71e22a2cf46b9774665250ffa9da27c
Bill Hoffman <bill.hoffman@kitware.com>
2009-01-14 13:48:03 -0500

    ENH: fix return value to ctest_build and remove debug print in cmProcess

--- Source/CTest/cmCTestBuildHandler.cxx
+++ Source/CTest/cmCTestBuildHandler.cxx
@@ -475,7 +475,7 @@ int cmCTestBuildHandler::ProcessHandler()
     }
   this->GenerateDartBuildOutput(
     xofs, this->ErrorsAndWarnings, elapsed_build_time);
-  return res;
+  return retVal;
 }
 
 //----------------------------------------------------------------------
@@ -724,7 +724,6 @@ int cmCTestBuildHandler::RunMakeCommand(const char* command,
         errorwarning.Error       = false;
         this->ErrorsAndWarnings.push_back(errorwarning);
         this->TotalWarnings ++;
-        return *retVal;  // return the program return value
         }
       }
     }
@@ -735,13 +734,11 @@ int cmCTestBuildHandler::RunMakeCommand(const char* command,
       *retVal = cmsysProcess_GetExitException(cp);
       cmCTestLog(this->CTest, WARNING, "There was an exception: " << *retVal
                  << std::endl);
-      return *retVal;
       }
     }
   else if(result == cmsysProcess_State_Expired)
     {
     cmCTestLog(this->CTest, WARNING, "There was a timeout" << std::endl);
-    return -1;
     }
   else if(result == cmsysProcess_State_Error)
     {
@@ -757,12 +754,10 @@ int cmCTestBuildHandler::RunMakeCommand(const char* command,
     this->TotalErrors ++;
     cmCTestLog(this->CTest, ERROR_MESSAGE, "There was an error: "
       << cmsysProcess_GetErrorString(cp) << std::endl);
-    return -1;
     }
 
   cmsysProcess_Delete(cp);
-
-  return 0;
+  return result;
 }
 
 //######################################################################
