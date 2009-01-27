ef5b9d00a29bc55788c76dedfaa00571522f4ce2
Bill Hoffman <bill.hoffman@kitware.com>
2009-01-27 10:34:14 -0500

    ENH: change to use CMAKE_CREATE_VERSION from CMAKE_VERSION as CMAKE_VERSION is auto-defined now

--- Utilities/Release/create-cmake-release.cmake
+++ Utilities/Release/create-cmake-release.cmake
@@ -1,6 +1,6 @@
-if(NOT DEFINED CMAKE_VERSION)
-  message(FATAL_ERROR "CMAKE_VERSION not defined")
-endif(NOT DEFINED CMAKE_VERSION)
+if(NOT DEFINED CMAKE_CREATE_VERSION)
+  message(FATAL_ERROR "CMAKE_CREATE_VERSION not defined")
+endif(NOT DEFINED CMAKE_CREATE_VERSION)
 
 set(RELEASE_SCRIPTS
   dashmacmini2_release.cmake  # Mac Darwin universal
@@ -18,16 +18,16 @@ set(RELEASE_SCRIPTS
 #  r15n65_aix_release.cmake    # AIX 5.2
 )
 
-file(WRITE create-${CMAKE_VERSION}.sh "#!/bin/sh")
+file(WRITE create-${CMAKE_CREATE_VERSION}.sh "#!/bin/sh")
 make_directory(${CMAKE_CURRENT_SOURCE_DIR}/logs)
 
 foreach(f ${RELEASE_SCRIPTS})
-  file(APPEND create-${CMAKE_VERSION}.sh
+  file(APPEND create-${CMAKE_CREATE_VERSION}.sh
     "
-${CMAKE_COMMAND} -DCMAKE_VERSION=${CMAKE_VERSION} -P ${CMAKE_ROOT}/Utilities/Release/${f} < /dev/null >& ${CMAKE_CURRENT_SOURCE_DIR}/logs/${f}-${CMAKE_VERSION}.log &
- xterm -geometry 80x10 -sb -sl 2000 -T ${f}-${CMAKE_VERSION}.log -e tail -f  ${CMAKE_CURRENT_SOURCE_DIR}/logs/${f}-${CMAKE_VERSION}.log&")
+${CMAKE_COMMAND} -DCMAKE_CREATE_VERSION=${CMAKE_CREATE_VERSION} -P ${CMAKE_ROOT}/Utilities/Release/${f} < /dev/null >& ${CMAKE_CURRENT_SOURCE_DIR}/logs/${f}-${CMAKE_CREATE_VERSION}.log &
+ xterm -geometry 80x10 -sb -sl 2000 -T ${f}-${CMAKE_CREATE_VERSION}.log -e tail -f  ${CMAKE_CURRENT_SOURCE_DIR}/logs/${f}-${CMAKE_CREATE_VERSION}.log&")
 endforeach(f)
-execute_process(COMMAND chmod a+x create-${CMAKE_VERSION}.sh)
-message("Run ./create-${CMAKE_VERSION}.sh")
+execute_process(COMMAND chmod a+x create-${CMAKE_CREATE_VERSION}.sh)
+message("Run ./create-${CMAKE_CREATE_VERSION}.sh")
 
 
