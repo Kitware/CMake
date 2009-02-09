e18a33e49150cd71164475a472484c15aec4f05f
Christopher Harvey <chris@basementcode.com>
2009-02-09 16:25:48 -0500

    ENH: Made the documentation for FindDevIL.cmake cleaner. Changed the XXX_LIBRARYs to XXX_LIBRARIES.

--- Modules/FindDevIL.cmake
+++ Modules/FindDevIL.cmake
@@ -2,11 +2,11 @@
 # http://openil.sourceforge.net/
 #
 # This module sets:
-# IL_LIBRARY the name of the IL library.
-# ILU_LIBRARY the name of the ILU library.
-# ILUT_LIBRARY the name of the ILUT library.
+# IL_LIBRARIES the name of the IL library. These include the full path to the core DevIL library. This one has to be linked into the application.
+# ILU_LIBRARIES the name of the ILU library. Again, the full path. This library is for filters and effects, not actual loading. It doesn't have to be linked if the functionality it provides is not used.
+# ILUT_LIBRARIES the name of the ILUT library. Full path. This part of the library interfaces with OpenGL. It is not strictly needed in applications.
 # IL_INCLUDE_DIR where to find the il.h, ilu.h and ilut.h files.
-# IL_FOUND this is set to TRUE if all the above variables were set.
+# IL_FOUND this is set to TRUE if all the above variables were set. This will be set to false if ILU or ILUT are not found, even if they are not needed. In most systems, if one library is found all the others are as well. That's the way the DevIL developers release it.
 
 # Original file by: Christopher Harvey
 # TODO: Add version support.
@@ -21,30 +21,30 @@ FIND_PATH(IL_INCLUDE_DIR il.h
 
 #MESSAGE("IL_INCLUDE_DIR is ${IL_INCLUDE_DIR}")
 
-FIND_LIBRARY(IL_LIBRARY
+FIND_LIBRARY(IL_LIBRARIES
   NAMES IL
   PATH_SUFFIXES lib64 lib lib32
   DOC "The file that corresponds to the base il library."
 )
 
-#MESSAGE("IL_LIBRARY is ${IL_LIBRARY}")
+#MESSAGE("IL_LIBRARIES is ${IL_LIBRARIES}")
 
-FIND_LIBRARY(ILUT_LIBRARY
+FIND_LIBRARY(ILUT_LIBRARIES
   NAMES ILUT
   PATH_SUFFIXES lib64 lib lib32
   DOC "The file that corresponds to the il (system?) utility library."
 )
 
-#MESSAGE("ILUT_LIBRARY is ${ILUT_LIBRARY}")
+#MESSAGE("ILUT_LIBRARIES is ${ILUT_LIBRARIES}")
 
-FIND_LIBRARY(ILU_LIBRARY
+FIND_LIBRARY(ILU_LIBRARIES
   NAMES ILU
   PATH_SUFFIXES lib64 lib lib32
   DOC "The file that corresponds to the il utility library."
 )
 
-#MESSAGE("ILU_LIBRARY is ${ILU_LIBRARY}")
+#MESSAGE("ILU_LIBRARIES is ${ILU_LIBRARIES}")
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(IL DEFAULT_MSG 
-                                  IL_LIBRARY ILU_LIBRARY 
-                                  ILUT_LIBRARY IL_INCLUDE_DIR)
+                                  IL_LIBRARIES ILU_LIBRARIES 
+                                  ILUT_LIBRARIES IL_INCLUDE_DIR)
