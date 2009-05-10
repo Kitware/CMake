352fb9ff66787f6eaffdfa964794de62877bd965
Alexander Neundorf <neundorf@kde.org>
2009-05-10 06:00:27 -0400

    ENH: move the code which queries gcc for the system include dirs from CMakeSystemSpecificInformation.cmake into a separate file, CMakeEclipseCDT4.cmake -if CMAKE_EXTRA_GENERATOR is set, i.e. either CodeBlocks or KDevelop3 or EclipseCDT4, load a matching cmake script file, which can do things specific for this generator - added such files for Eclipse, KDevelop and CodeBlocks, one thing they all do is they try to find the respective IDE and store it in the CMAKE_(KDEVELOP3|CODEBLOCKS|ECLIPSE)_EXECUTABLE variable. This could be used by cmake-gui to open the project it just generated with the gui (not sure this is possible with eclipse).
    Alex

---
+++ Modules/CMakeCodeBlocks.cmake
@@ -0,0 +1,5 @@
+# This file is included in CMakeSystemSpecificInformation.cmake if
+# the CodeBlocks extra generator has been selected.
+
+FIND_PROGRAM(CMAKE_CODEBLOCKS_EXECUTABLE NAMES codeblocks DOC "The CodeBlocks executable")
+
---
+++ Modules/CMakeKDevelop3.cmake
@@ -0,0 +1,5 @@
+# This file is included in CMakeSystemSpecificInformation.cmake if
+# the KDevelop3 extra generator has been selected.
+
+FIND_PROGRAM(CMAKE_KDEVELOP3_EXECUTABLE NAMES kdevelop DOC "The KDevelop3 executable")
+
