a36465bc36d17b63b32fc1041fa816f552795219
Alexander Neundorf <neundorf@kde.org>
2009-05-12 15:17:10 -0400

    STYLE: remove these files now that I added them with a more consistent name as CMakeFind<GENERATOR>.cmake (should have been in the same commit...)
    Alex

--- Modules/CMakeCodeBlocks.cmake
+++
@@ -1,5 +0,0 @@
-# This file is included in CMakeSystemSpecificInformation.cmake if
-# the CodeBlocks extra generator has been selected.
-
-FIND_PROGRAM(CMAKE_CODEBLOCKS_EXECUTABLE NAMES codeblocks DOC "The CodeBlocks executable")
-
--- Modules/CMakeKDevelop3.cmake
+++
@@ -1,5 +0,0 @@
-# This file is included in CMakeSystemSpecificInformation.cmake if
-# the KDevelop3 extra generator has been selected.
-
-FIND_PROGRAM(CMAKE_KDEVELOP3_EXECUTABLE NAMES kdevelop DOC "The KDevelop3 executable")
-
