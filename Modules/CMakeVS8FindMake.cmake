08817ac400b5aab95439202b0fbe0caa442f21f7
David Cole <david.cole@kitware.com>
2009-05-26 09:55:10 -0400

    BUG: Rearrange paths to find correct installations of Visual Studio. Patch devenv.modified_search_order.patch came from issue #7919.

--- Modules/CMakeVS8FindMake.cmake
+++ Modules/CMakeVS8FindMake.cmake
@@ -7,16 +7,17 @@ ENDIF(NOT CMAKE_CROSSCOMPILING)
 
 FIND_PROGRAM(CMAKE_MAKE_PROGRAM
   NAMES ${_CMAKE_MAKE_PROGRAM_NAMES}
-  PATHS
+  HINTS
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\8.0\\Setup\\VS;EnvironmentDirectory]
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\8.0\\Setup;Dbghelp_path]
-  "$ENV{ProgramFiles}/Microsoft Visual Studio .NET/Common7/IDE"
   "$ENV{ProgramFiles}/Microsoft Visual Studio 8/Common7/IDE"
   "$ENV{ProgramFiles}/Microsoft Visual Studio8/Common7/IDE"
-  "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio .NET/Common7/IDE"
   "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio 8/Common7/IDE"
   "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio8/Common7/IDE"
   "/Program Files/Microsoft Visual Studio 8/Common7/IDE/"
+  PATHS
+  "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio .NET/Common7/IDE"
+  "$ENV{ProgramFiles}/Microsoft Visual Studio .NET/Common7/IDE"
   )
 MARK_AS_ADVANCED(CMAKE_MAKE_PROGRAM)
 SET(MSVC80 1)
--- Modules/CMakeVS9FindMake.cmake
+++ Modules/CMakeVS9FindMake.cmake
@@ -7,16 +7,23 @@ ENDIF(NOT CMAKE_CROSSCOMPILING)
 
 FIND_PROGRAM(CMAKE_MAKE_PROGRAM
   NAMES ${_CMAKE_MAKE_PROGRAM_NAMES}
-  PATHS
+  HINTS
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\9.0\\Setup\\VS;EnvironmentDirectory]
   [HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\VisualStudio\\9.0\\Setup;Dbghelp_path]
-  "$ENV{ProgramFiles}/Microsoft Visual Studio .NET/Common7/IDE"
+  "$ENV{ProgramFiles}/Microsoft Visual Studio 9.0/Common7/IDE"
+  "$ENV{ProgramFiles}/Microsoft Visual Studio9.0/Common7/IDE"
   "$ENV{ProgramFiles}/Microsoft Visual Studio 9/Common7/IDE"
   "$ENV{ProgramFiles}/Microsoft Visual Studio9/Common7/IDE"
-  "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio .NET/Common7/IDE"
+  "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio 9.0/Common7/IDE"
+  "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio9.0/Common7/IDE"
   "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio 9/Common7/IDE"
   "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio9/Common7/IDE"
+  "/Program Files/Microsoft Visual Studio 9.0/Common7/IDE/"
   "/Program Files/Microsoft Visual Studio 9/Common7/IDE/"
+  PATHS
+  "$ENV{ProgramFiles} (x86)/Microsoft Visual Studio .NET/Common7/IDE"
+  "$ENV{ProgramFiles}/Microsoft Visual Studio .NET/Common7/IDE"
+  
   )
 MARK_AS_ADVANCED(CMAKE_MAKE_PROGRAM)
 SET(MSVC90 1)
