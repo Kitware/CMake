a32754093dfbccbcb05873984ce21711fe1eacdf
Brad King <brad.king@kitware.com>
2009-06-10 13:07:48 -0400

    ENH: Remove VMSbuild directory
    This directory was a temporary solution for bootstrapping CMake on
    OpenVMS.  It is not necessary now that the bootstrap script works.

--- VMSbuild/stdstring.cxx
+++
@@ -1,8 +0,0 @@
-// This file is a hack to get the VMS CC to define the templates for basic_string
-
-#include <string>
-
-#pragma define_template std::basic_string<char, std::char_traits<char >, std::allocator<char > >
-
-
-
