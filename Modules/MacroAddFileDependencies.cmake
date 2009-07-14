1e20be319f75dd936b9e057c4de7f3dc5526cee9
Alexander Neundorf <neundorf@kde.org>
2009-07-14 14:44:27 -0400

    STYLE: add documentation for MACRO_ADD_FILE_DEPENDENCIES()
    Alex

--- Modules/MacroAddFileDependencies.cmake
+++ Modules/MacroAddFileDependencies.cmake
@@ -1,14 +1,20 @@
 # - MACRO_ADD_FILE_DEPENDENCIES(<_file> depend_files...)
-# MACRO_OPTIONAL_FIND_PACKAGE( <name> [QUIT] )
+# Using the macro MACRO_ADD_FILE_DEPENDENCIES() is discouraged. There are usually
+# better ways to specifiy the correct dependencies.
+#
+# MACRO_ADD_FILE_DEPENDENCIES(<_file> depend_files...) is just a convenience 
+# wrapper around the OBJECT_DEPENDS source file property. You can just
+# use SET_PROPERTY(SOURCE <file> APPEND PROPERTY OBJECT_DEPENDS depend_files) instead.
+
 
 MACRO (MACRO_ADD_FILE_DEPENDENCIES _file)
 
    GET_SOURCE_FILE_PROPERTY(_deps ${_file} OBJECT_DEPENDS)
-   if (_deps)
-      set(_deps ${_deps} ${ARGN})
-   else (_deps)
-      set(_deps ${ARGN})
-   endif (_deps)
+   IF (_deps)
+      SET(_deps ${_deps} ${ARGN})
+   ELSE (_deps)
+      SET(_deps ${ARGN})
+   ENDIF (_deps)
 
    SET_SOURCE_FILES_PROPERTIES(${_file} PROPERTIES OBJECT_DEPENDS "${_deps}")
 
