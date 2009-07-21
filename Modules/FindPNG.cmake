699a059768220fbea7d7a7598fa1ec4a8e5bdc69
Ken Martin <ken.martin@kitware.com>
2009-07-21 12:45:37 -0400

    ENH: just converted case to lower for the book

--- Modules/FindPNG.cmake
+++ Modules/FindPNG.cmake
@@ -3,42 +3,42 @@
 # This module defines
 #  PNG_INCLUDE_DIR, where to find png.h, etc.
 #  PNG_LIBRARIES, the libraries to link against to use PNG.
-#  PNG_DEFINITIONS - You should ADD_DEFINITONS(${PNG_DEFINITIONS}) before compiling code that includes png library files.
+#  PNG_DEFINITIONS - You should add_definitons(${PNG_DEFINITIONS}) before compiling code that includes png library files.
 #  PNG_FOUND, If false, do not try to use PNG.
 # also defined, but not for general use are
 #  PNG_LIBRARY, where to find the PNG library.
 # None of the above will be defined unles zlib can be found.
 # PNG depends on Zlib
-INCLUDE(FindZLIB)
+include(FindZLIB)
 
-IF(ZLIB_FOUND)
-  FIND_PATH(PNG_PNG_INCLUDE_DIR png.h
+if(ZLIB_FOUND)
+  find_path(PNG_PNG_INCLUDE_DIR png.h
   /usr/local/include/libpng             # OpenBSD
   )
 
-  SET(PNG_NAMES ${PNG_NAMES} png libpng png12 libpng12)
-  FIND_LIBRARY(PNG_LIBRARY NAMES ${PNG_NAMES} )
+  set(PNG_NAMES ${PNG_NAMES} png libpng png12 libpng12)
+  find_library(PNG_LIBRARY NAMES ${PNG_NAMES} )
 
-  IF (PNG_LIBRARY AND PNG_PNG_INCLUDE_DIR)
+  if (PNG_LIBRARY AND PNG_PNG_INCLUDE_DIR)
       # png.h includes zlib.h. Sigh.
       SET(PNG_INCLUDE_DIR ${PNG_PNG_INCLUDE_DIR} ${ZLIB_INCLUDE_DIR} )
       SET(PNG_LIBRARIES ${PNG_LIBRARY} ${ZLIB_LIBRARY})
 
-      IF (CYGWIN)
-        IF(BUILD_SHARED_LIBS)
+      if (CYGWIN)
+        if(BUILD_SHARED_LIBS)
            # No need to define PNG_USE_DLL here, because it's default for Cygwin.
-        ELSE(BUILD_SHARED_LIBS)
+        else(BUILD_SHARED_LIBS)
           SET (PNG_DEFINITIONS -DPNG_STATIC)
-        ENDIF(BUILD_SHARED_LIBS)
-      ENDIF (CYGWIN)
+        endif(BUILD_SHARED_LIBS)
+      endif (CYGWIN)
 
-  ENDIF (PNG_LIBRARY AND PNG_PNG_INCLUDE_DIR)
+  endif (PNG_LIBRARY AND PNG_PNG_INCLUDE_DIR)
 
-ENDIF(ZLIB_FOUND)
+endif(ZLIB_FOUND)
 
-# handle the QUIETLY and REQUIRED arguments and set PNG_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set PNG_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
+include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(PNG DEFAULT_MSG PNG_LIBRARY PNG_PNG_INCLUDE_DIR)
 
-MARK_AS_ADVANCED(PNG_PNG_INCLUDE_DIR PNG_LIBRARY )
+mark_as_advanced(PNG_PNG_INCLUDE_DIR PNG_LIBRARY )
