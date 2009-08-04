f3cd1e06f5a5fb092f249de3e1b582125d067daa
Brad King <brad.king@kitware.com>
2009-08-04 14:06:45 -0400

    Test C, C++, Fortran interface combinations
    Previously the Fortran test created a single executable containing C,
    C++, and Fortran sources.  This commit divides the executable into three
    libraries corresponding to each language, and two executables testing
    Fortran/C only and Fortran/C/C++ together.  The result tests more
    combinations of using the languages together, and that language
    requirements propagate through linking.

--- Tests/Fortran/foo.cxx
+++
@@ -1,6 +0,0 @@
-extern "C" int foo(void);
-int main()
-{
-  delete new int;
-  return foo();
-}
--- Tests/Fortran/myc.c
+++ Tests/Fortran/myc.c
@@ -1,11 +1,12 @@
 #include "foo.h"
-extern F_test_mod_sub();
-extern F_mysub();
-int foo()
+extern F_test_mod_sub(void);
+extern F_mysub(void);
+int myc(void)
 {
   F_mysub();
   F_my_sub();
 #ifdef F_test_mod_sub
   F_test_mod_sub();
 #endif
+  return 0;
 }
