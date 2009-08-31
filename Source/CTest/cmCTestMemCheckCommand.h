6d0b6f2b9e959be073a91d6040810b3057f2b1b1
Zach Mullen <zach.mullen@kitware.com>
2009-08-31 11:32:39 -0400

    Fixed ctest_memcheck docs (http://www.cmake.org/Bug/view.php?id=9242)

--- Source/CTest/cmCTestMemCheckCommand.h
+++ Source/CTest/cmCTestMemCheckCommand.h
@@ -62,10 +62,20 @@ public:
   virtual const char* GetFullDocumentation()
     {
     return
-      "  ctest_memcheck([BUILD build_dir] [RETURN_VALUE res] [APPEND])\n"
-      "Performs a memory checking of tests in the given build directory and "
-      "stores results in MemCheck.xml. The second argument is a variable "
-      "that will hold value."
+      "  ctest_memcheck([BUILD build_dir] [RETURN_VALUE res] [APPEND]\n"
+      "             [START start number] [END end number]\n"
+      "             [STRIDE stride number] [EXCLUDE exclude regex ]\n"
+      "             [INCLUDE include regex] \n" 
+      "             [EXCLUDE_LABEL exclude regex] \n"
+      "             [INCLUDE_LABEL label regex] )\n"
+      "Tests the given build directory and stores results in MemCheck.xml. "
+      "The second argument is a variable that will hold value. Optionally, "
+      "you can specify the starting test number START, the ending test number "
+      "END, the number of tests to skip between each test STRIDE, a regular "
+      "expression for tests to run INCLUDE, or a regular expression for tests "
+      "not to run EXCLUDE. EXCLUDE_LABEL and INCLUDE_LABEL are regular "
+      "expressions for tests to be included or excluded by the test "
+      "property LABEL."
       "\n"
       CTEST_COMMAND_APPEND_OPTION_DOCS;
     }
