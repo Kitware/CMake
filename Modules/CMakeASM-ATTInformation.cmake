c6a10370bb021f9123f7fdb9fa24dcb4fdd2679b
Alexander Neundorf <neundorf@kde.org>
2009-09-12 02:15:30 -0400

    Don't pass *.S files to the assembler, they must go through gcc, because they must be preprocessed
    Alex

--- Modules/CMakeASM-ATTInformation.cmake
+++ Modules/CMakeASM-ATTInformation.cmake
@@ -1,6 +1,8 @@
 # support for AT&T syntax assemblers, e.g. GNU as
 
 SET(ASM_DIALECT "-ATT")
-SET(CMAKE_ASM${ASM_DIALECT}_SOURCE_FILE_EXTENSIONS s;S;asm)
+# *.S files are supposed to be preprocessed, so they should not be passed to
+# assembler but should be processed by gcc
+SET(CMAKE_ASM${ASM_DIALECT}_SOURCE_FILE_EXTENSIONS s;asm)
 INCLUDE(CMakeASMInformation)
 SET(ASM_DIALECT)
