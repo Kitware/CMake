83957d9e0defcff8ae497078a47f77f183674f16
Brad King <brad.king@kitware.com>
2009-09-18 09:49:44 -0400

    Fix CHECK_(C|CXX)_COMPILER_FLAG for XL and SunPro
    These compilers warn and return 0 for unrecognized flags.  We fix the
    compiler flag check macros by looking for a warning in the output.  We
    also update the regex for GNU on older Macs.  See issue #9516.

--- Modules/CheckCXXCompilerFlag.cmake
+++ Modules/CheckCXXCompilerFlag.cmake
@@ -19,9 +19,11 @@ MACRO (CHECK_CXX_COMPILER_FLAG _FLAG _RESULT)
    SET(CMAKE_REQUIRED_DEFINITIONS "${_FLAG}")
    CHECK_CXX_SOURCE_COMPILES("int main() { return 0;}" ${_RESULT}
      # Some compilers do not fail with a bad flag
-     FAIL_REGEX "unrecognized option"                       # GNU
+     FAIL_REGEX "unrecognized .*option"                     # GNU
      FAIL_REGEX "ignoring unknown option"                   # MSVC
      FAIL_REGEX "[Uu]nknown option"                         # HP
+     FAIL_REGEX "[Ww]arning: [Oo]ption"                     # SunPro
+     FAIL_REGEX "command option .* is not recognized"       # XL
      )
    SET (CMAKE_REQUIRED_DEFINITIONS "${SAFE_CMAKE_REQUIRED_DEFINITIONS}")
 ENDMACRO (CHECK_CXX_COMPILER_FLAG)
