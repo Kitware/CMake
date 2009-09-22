d1162b6b59576f4119a8f4a0bc3f7d7daa5f4d16
Bill Hoffman <bill.hoffman@kitware.com>
2009-09-22 17:07:43 -0400

    disable svn

--- Utilities/Release/vogon_cygwin.cmake
+++ Utilities/Release/vogon_cygwin.cmake
@@ -5,6 +5,7 @@ set(CPACK_BINARY_GENERATORS "CygwinBinary")
 set(CPACK_SOURCE_GENERATORS "CygwinSource")
 set(MAKE_PROGRAM "make")
 set(INITIAL_CACHE "CMAKE_BUILD_TYPE:STRING=Release
+Subversion_SVNADMIN_EXECUTABLE:STRING=FALSE
 ")
 get_filename_component(path "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${path}/release_cmake.cmake)
