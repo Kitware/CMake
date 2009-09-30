6d434ee6ccc250bfc653281643d70a3d1f8938cc
Brad King <brad.king@kitware.com>
2009-09-30 09:37:35 -0400

    Split XL compiler information files
    This moves platform-independent XL compiler flags into separate
    "Compiler/XL-<lang>.cmake" modules.  Platform-specific flags go in
    "Platform/<os>-XL-<lang>.cmake" modules.

--- Modules/Platform/Darwin-XL-C.cmake
+++ Modules/Platform/Darwin-XL-C.cmake
@@ -3,4 +3,3 @@ SET(CMAKE_SHARED_MODULE_CREATE_C_FLAGS "-bundle")
 
 # Enable shared library versioning.
 SET(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG "-Wl,-install_name")
-SET(CMAKE_SHARED_LIBRARY_SONAME_CXX_FLAG "-Wl,-install_name")
