07c3597a86223048a61118619be39f9bf47270e4
Bill Hoffman <bill.hoffman@kitware.com>
2009-10-02 08:44:51 -0400

    Fix the documentation to say what it really does. Bug #9638

--- Modules/TestForANSIStreamHeaders.cmake
+++ Modules/TestForANSIStreamHeaders.cmake
@@ -1,5 +1,5 @@
 # - Test for compiler support of ANSI stream headers iostream, etc.
-# check if we they have the standard ansi stream files (without the .h)
+# check if the compiler supports the standard ANSI iostream header (without the .h)
 #  CMAKE_NO_ANSI_STREAM_HEADERS - defined by the results
 #
 
