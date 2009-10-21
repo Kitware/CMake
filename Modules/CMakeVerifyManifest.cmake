b8464a066ecf8b8b0c346f0230d28d85d5924458
Bill Hoffman <bill.hoffman@kitware.com>
2009-10-21 13:44:33 -0400

    Add license.

--- Modules/CMakeVerifyManifest.cmake
+++ Modules/CMakeVerifyManifest.cmake
@@ -16,6 +16,20 @@
 # of CRT are compared to those found in the .manifest files
 # from the first glob.
 
+#=============================================================================
+# Copyright 2008-2009 Kitware, Inc.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distributed this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+
 # crt_version:
 # function to extract the CRT version from a file
 # this can be passed a .exe, .dll, or a .manifest file
