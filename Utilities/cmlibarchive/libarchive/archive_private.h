ccd831c559b0dfbd94a0ce28c409c4dd97dbf29d
Brad King <brad.king@kitware.com>
2009-11-04 13:09:41 -0500

    libarchive: Disable MSVC warnings
    We disable warnings to silence them while making minimal changes to
    third-party code.

--- Utilities/cmlibarchive/libarchive/archive_private.h
+++ Utilities/cmlibarchive/libarchive/archive_private.h
@@ -35,6 +35,10 @@
 #include "archive.h"
 #include "archive_string.h"
 
+#if defined(_MSC_VER)
+# pragma warning (push,1)
+#endif
+
 #if defined(__GNUC__) && (__GNUC__ > 2 || \
               (__GNUC__ == 2 && __GNUC_MINOR__ >= 5))
 #define __LA_DEAD   __attribute__((__noreturn__))
