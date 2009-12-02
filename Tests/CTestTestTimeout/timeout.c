b14734b9b65db0abbc21a3e339a4ce6df77a4f6d
Brad King <brad.king@kitware.com>
2009-12-02 09:31:49 -0500

    Make CTestTestTimeout time configurable
    In this test we start up a cmake script that runs a process that sleeps,
    and the timeout for the script is shorter than the sleep time.  However,
    in order to properly detect that the sleeping grandchild is killed when
    the script times out we need to give sufficient time for the script to
    start the grandchild.  Otherwise the log file for the grandchild is not
    available.
    
    On some (cygwin) builds our previous 1 second timeout for the script was
    not long enough to let the interpreter load and start the grandchild.
    We make the timeout time configurable by setting CTestTestTimeout_TIME
    in the cache for CMake itself.  It tells the test how long to let the
    script run.  The grandchild always sleeps for 4 seconds longer to ensure
    a comfortable window during which the process tree can be killed.

--- Tests/CTestTestTimeout/timeout.c
+++ Tests/CTestTestTimeout/timeout.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 #if defined(_WIN32)
-  Sleep(5000);
+  Sleep((TIMEOUT+4)*1000);
 #else
-  sleep(5);
+  sleep((TIMEOUT+4));
 #endif
   printf("timeout process finished sleeping!\n");
   return -1;
