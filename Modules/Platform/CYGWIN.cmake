e46e8fb937a4d4d5e2ce7018d73cde7d85ea3142
Brad King <brad.king@kitware.com>
2010-01-13 14:12:29 -0500

    Do not find cyg*.dll on Cygwin
    While Cygwin supports linking directly to .dll files, the behavior is
    now discouraged.  All Cygwin packages now provide import libraries of
    the form lib*.dll.a and CMake has built the import libraries for years.
    
    We believe it is now safe to stop explicitly searching for .dll files
    because their import libraries will always be available when the
    corresponding header files are available.  Users can always set
    find_library cache entries to point at a .dll file by hand if they
    really must use one.
    
    Change based on patch from issue #10122.

--- Modules/Platform/CYGWIN.cmake
+++ Modules/Platform/CYGWIN.cmake
@@ -11,8 +11,8 @@ SET(CMAKE_EXECUTABLE_SUFFIX ".exe")          # .exe
 # Modules have a different default prefix that shared libs.
 SET(CMAKE_MODULE_EXISTS 1)
 
-SET(CMAKE_FIND_LIBRARY_PREFIXES "cyg" "lib")
-SET(CMAKE_FIND_LIBRARY_SUFFIXES ".dll" ".dll.a" ".a")
+SET(CMAKE_FIND_LIBRARY_PREFIXES "lib")
+SET(CMAKE_FIND_LIBRARY_SUFFIXES ".dll.a" ".a")
 
 # Shared libraries on cygwin can be named with their version number.
 SET(CMAKE_SHARED_LIBRARY_NAME_WITH_VERSION 1)
