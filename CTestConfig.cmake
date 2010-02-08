41fcf6e1558cb3ef4639928f87d58531b45fe237
Zach Mullen <zach.mullen@kitware.com>
2010-02-08 09:47:39 -0500

    Moved call to cache CDash version information to cmCTest::initialize.  Also added a 3 second timeout when requesting the version from the server.  Added an option to the CTestConfiguration that will be used to determine whether to query the version information at all.  (Behavior for this setting is not yet defined.)  Updated CMake's local CDash version setting to 1.6.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -16,7 +16,8 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "www.cdash.org")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=CMake")
 set(CTEST_DROP_SITE_CDASH TRUE)
-set(CTEST_CDASH_VERSION "1.4")
+set(CTEST_CDASH_VERSION "1.6")
+set(CTEST_CDASH_QUERY_VERSION TRUE)
 
 # use old trigger stuff so that cmake 2.4 and below will not 
 # get errors on trigger
