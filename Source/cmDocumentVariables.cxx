0328379411386abc89ba07991f858cdacca926b2
Brad King <brad.king@kitware.com>
2010-04-23 10:01:49 -0400

    Report commit hash in CMake development versions
    For builds from Git repositories, add "-g<commit>" to the end of the
    version number.  If the source tree is modified, append "-dirty".
    For builds from CVS checkouts, add "-cvs-<branch>".

--- Source/cmDocumentVariables.cxx
+++ Source/cmDocumentVariables.cxx
@@ -259,12 +259,14 @@ void cmDocumentVariables::DefineVariables(cmake* cm)
      ,false, "Variables that Provide Information");
   cm->DefineProperty
     ("CMAKE_VERSION", cmProperty::VARIABLE,
-     "The full version of cmake in major.minor.patch[.tweak] format.",
+     "The full version of cmake in major.minor.patch[.tweak[-id]] format.",
      "This specifies the full version of the CMake executable being run.  "
      "This variable is defined by versions 2.6.3 and higher.  "
      "See variables CMAKE_MAJOR_VERSION, CMAKE_MINOR_VERSION, "
      "CMAKE_PATCH_VERSION, and CMAKE_TWEAK_VERSION "
-     "for individual version components.", false,
+     "for individual version components.  "
+     "The [-id] component appears in non-release versions "
+     "and may be arbitrary text.", false,
      "Variables that Provide Information");
 
   cm->DefineProperty
