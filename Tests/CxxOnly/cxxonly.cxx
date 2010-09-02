ced61f5722d65525fd4b8dbf96d5ea8f254099c2
Bill Hoffman <bill.hoffman@kitware.com>
2010-09-02 11:56:40 -0400

    Let CMake recognize .CPP .CXX and .C++ as c++ files.

--- Tests/CxxOnly/cxxonly.cxx
+++ Tests/CxxOnly/cxxonly.cxx
@@ -1,10 +1,14 @@
 #include "libcxx1.h"
 #include "libcxx2.h"
+extern int testCPP;
+extern int testCXX;
+extern int testCplusplus;
 
 #include <stdio.h>
 
 int main ()
 {
+  testCPP = testCXX= testCplusplus = 1;
   if ( LibCxx1Class::Method() != 2.0 )
     {
     printf("Problem with libcxx1\n");
