4a67481ed6045e8aebb2b5e169fcb55c953432be
David Cole <david.cole@kitware.com>
2010-09-15 18:17:29 -0400

    Update release scripts.
    Remove scripts for machines that no longer build
    releases. Update comments in README.

--- Utilities/Release/create-cmake-release.cmake
+++ Utilities/Release/create-cmake-release.cmake
@@ -5,17 +5,14 @@ endif(NOT DEFINED CMAKE_CREATE_VERSION)
 set(RELEASE_SCRIPTS
   dashmacmini2_release.cmake  # Mac Darwin universal
   dashsun1_release.cmake      # SunOS
-  destiny_release.cmake       # HPUX
+#  destiny_release.cmake       # HPUX -- destiny is dead; long live destiny
   magrathea_release.cmake     # Linux
   dash2win64_release.cmake    # Windows
 #  dash2win64_cygwin.cmake     # Cygwin
 #  blight_cygwin.cmake     # Cygwin
   v20n250_aix_release.cmake    # AIX 5.3
-#  vogon_cygwin.cmake          # Cygwin
   ferrari_sgi64_release.cmake # IRIX 64
   ferrari_sgi_release.cmake   # IRIX 64
-#  r36n11_aix_release.cmake    # AIX 5.3
-#  r15n65_aix_release.cmake    # AIX 5.2
 )
 
 file(WRITE create-${CMAKE_CREATE_VERSION}.sh "#!/bin/bash")
@@ -29,5 +26,3 @@ ${CMAKE_COMMAND} -DCMAKE_CREATE_VERSION=${CMAKE_CREATE_VERSION} -P ${CMAKE_ROOT}
 endforeach(f)
 execute_process(COMMAND chmod a+x create-${CMAKE_CREATE_VERSION}.sh)
 message("Run ./create-${CMAKE_CREATE_VERSION}.sh")
-
-
