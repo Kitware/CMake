ffeca06a93eca70ef7505ac087cbb246e5191ed2
Alex Neundorf <neundorf@kde.org>
2010-09-27 18:04:34 +0200

    Add missing copyright headers
    Alex

--- Modules/CMakeASM_NASMInformation.cmake
+++ Modules/CMakeASM_NASMInformation.cmake
@@ -1,3 +1,17 @@
+
+#=============================================================================
+# Copyright 2008-2009 Kitware, Inc.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
 # support for the nasm assembler
 
 set(CMAKE_ASM_NASM_SOURCE_FILE_EXTENSIONS nasm)
--- Modules/CMakeTestASM_NASMCompiler.cmake
+++ Modules/CMakeTestASM_NASMCompiler.cmake
@@ -1,3 +1,17 @@
+
+#=============================================================================
+# Copyright 2008-2009 Kitware, Inc.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
 # This file is used by EnableLanguage in cmGlobalGenerator to
 # determine that the selected ASM_NASM "compiler" works.
 # For assembler this can only check whether the compiler has been found,
