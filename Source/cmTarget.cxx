abb6e755eb8387622710e547f2b1d131f96ca990
Brad King <brad.king@kitware.com>
2010-10-11 08:35:19 -0400

    Document ENABLE_EXPORTS behavior on Mac (#11295)
    Mention that it is implemented with -bundle-loader on platforms that
    need link-time symbol resolution but that do not use DLL import libs.

--- Source/cmTarget.cxx
+++ Source/cmTarget.cxx
@@ -834,12 +834,16 @@ void cmTarget::DefineProperties(cmake *cm)
      "executable with the TARGET_LINK_LIBRARIES command.  "
      "On all platforms a target-level dependency on the executable is "
      "created for targets that link to it.  "
-     "For non-DLL platforms the link rule is simply ignored since "
-     "the dynamic loader will automatically bind symbols when the "
-     "module is loaded.  "
      "For DLL platforms an import library will be created for the "
      "exported symbols and then used for linking.  "
-     "All Windows-based systems including Cygwin are DLL platforms.");
+     "All Windows-based systems including Cygwin are DLL platforms.  "
+     "For non-DLL platforms that require all symbols to be resolved at "
+     "link time, such as Mac OS X, the module will \"link\" to the "
+     "executable using a flag like \"-bundle_loader\".  "
+     "For other non-DLL platforms the link rule is simply ignored since "
+     "the dynamic loader will automatically bind symbols when the "
+     "module is loaded.  "
+      );
 
   cm->DefineProperty
     ("Fortran_MODULE_DIRECTORY", cmProperty::TARGET,
