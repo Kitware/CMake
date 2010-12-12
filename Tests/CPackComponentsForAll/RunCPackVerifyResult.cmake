cbdc46356dacb75f1901da44e00ac96c1af24408
Eric NOULARD <eric.noulard@gmail.com>
2010-12-12 12:19:50 +0100

    CPackTest spit out more output in case of failure

--- Tests/CPackComponentsForAll/RunCPackVerifyResult.cmake
+++ Tests/CPackComponentsForAll/RunCPackVerifyResult.cmake
@@ -20,13 +20,22 @@ endif(NOT CPackComponentWay)
 
 set(expected_file_mask "")
 # The usual default behavior is to expect a single file
+# Then some specific generators (Archive, RPM, ...)
+# May produce several numbers of files depending on
+# CPACK_COMPONENT_xxx values
 set(expected_count 1)
 
 execute_process(COMMAND ${CPackCommand} -G ${CPackGen}
-    RESULT_VARIABLE result
-    OUTPUT_VARIABLE stdout
-    ERROR_VARIABLE stderr
+    RESULT_VARIABLE CPack_result
+    OUTPUT_VARIABLE CPack_output
+    ERROR_VARIABLE CPack_error
     WORKING_DIRECTORY ${CPackComponentsForAll_BINARY_DIR})
+
+if (CPack_result)
+  message(SEND_ERROR "CPack_output=${CPack_output}")
+  message(SEND_ERROR "CPack_error=${CPack_error}")
+  message(FATAL_ERROR "error: CPack execution went wrong!")
+endif(CPack_result)
 
 if(CPackGen MATCHES "ZIP")
     set(expected_file_mask "${CPackComponentsForAll_BINARY_DIR}/MyLib-*.zip")
@@ -46,12 +55,16 @@ if(expected_file_mask)
   message(STATUS "expected_file_mask='${expected_file_mask}'")
 
   if(NOT expected_file)
+    message(SEND_ERROR "CPack_output=${CPack_output}")
+    message(SEND_ERROR "CPack_error=${CPack_error}")
     message(FATAL_ERROR "error: expected_file=${expected_file} does not exist: CPackComponentsForAll test fails.")
   endif(NOT expected_file)
 
   list(LENGTH expected_file actual_count)
   message(STATUS "actual_count='${actual_count}'")
   if(NOT actual_count EQUAL expected_count)
+    message(SEND_ERROR "CPack_output=${CPack_output}")
+    message(SEND_ERROR "CPack_error=${CPack_error}")
     message(FATAL_ERROR "error: expected_count=${expected_count} does not match actual_count=${actual_count}: CPackComponents test fails.")
   endif(NOT actual_count EQUAL expected_count)
 endif(expected_file_mask)
