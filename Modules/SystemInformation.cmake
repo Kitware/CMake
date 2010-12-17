a6cb1d46535b083a74be6c6517acf343294105a7
Brad King <brad.king@kitware.com>
2010-12-17 09:39:30 -0500

    Declare min CMake version in --system-information project
    The --system-information flag's project triggered a CMP0000 warning
    because the CMakeLists.txt it generates needs cmake_minimum_required.

--- Modules/SystemInformation.cmake
+++ Modules/SystemInformation.cmake
@@ -1,6 +1,6 @@
 
 #=============================================================================
-# Copyright 2007-2009 Kitware, Inc.
+# Copyright 2007-2010 Kitware, Inc.
 #
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file Copyright.txt for details.
@@ -12,6 +12,7 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
+CMAKE_MINIMUM_REQUIRED(VERSION ${CMAKE_VERSION})
 PROJECT(DumpInformation)
 
 # first get the standard information for th platform
