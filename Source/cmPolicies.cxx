528eec02c93452882ae609debd3c3773f37ca8fa
Brad King <brad.king@kitware.com>
2011-01-04 07:58:29 -0500

    Revert "Prefer files from CMAKE_ROOT when including from CMAKE_ROOT."
    This reverts commit 07410f329d8a219fc8c6153212cdcdb6034ab27c.

--- Source/cmPolicies.cxx
+++ Source/cmPolicies.cxx
@@ -446,22 +446,6 @@ cmPolicies::cmPolicies()
     "wasn't a valid target. "
     "In CMake 2.8.3 and above it reports an error in this case.",
     2,8,3,0, cmPolicies::WARN);
-
-    this->DefinePolicy(
-    CMP0017, "CMP0017",
-    "Prefer files from CMAKE_ROOT/ when including from CMAKE_ROOT.",
-    "Starting with CMake 2.8.3, if a cmake-module shipped with CMake (i.e. "
-    "located in CMAKE_ROOT/Modules/) calls include() or find_package(), "
-    "the files located in CMAKE_ROOT/Modules/ are prefered over the files "
-    "in CMAKE_MODULE_PATH.  This makes sure that the modules belonging to "
-    "CMake always get those files included which they expect, and against "
-    "which they were developed and tested.  "
-    "In call other cases, the files found in "
-    "CMAKE_MODULE_PATH still take precedence over the ones in "
-    "CMAKE_ROOT/Modules/.  "
-    "The OLD behaviour is to always prefer files from CMAKE_MODULE_PATH over "
-    "files from CMAKE_ROOT/Modules/.",
-    2,8,3,0, cmPolicies::WARN);
 }
 
 cmPolicies::~cmPolicies()
