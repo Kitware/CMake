fd63219557c4acc6e474263302ab9c78985f6d91
Nicolas Despres <nicolas.despres@gmail.com>
2011-01-07 14:48:35 +0100

    ccmake: Align 'g' and 'q' key instructions.
    They were miss-aligned with the 'enter' key instruction.
    
    Before:
    ----
    Press [enter] to edit option           CMake Version 2.8.3.20110107-g4b05a-dirty
    Press [c] to configure
    Press [h] for help         Press [q] to quit without generating
    ----
    
    After:
    ----
    Press [enter] to edit option           CMake Version 2.8.3.20110107-g4b05a-dirty
    Press [c] to configure
    Press [h] for help           Press [q] to quit without generating
    ----

--- Source/CursesDialog/cmCursesMainForm.cxx
+++ Source/CursesDialog/cmCursesMainForm.cxx
@@ -421,7 +421,7 @@ void cmCursesMainForm::PrintKeys(int process /* = 0 */)
     if (this->OkToGenerate)
       {
       sprintf(firstLine,
-              "Press [c] to configure     Press [g] to generate and exit");
+              "Press [c] to configure       Press [g] to generate and exit");
       }
     else
       {
@@ -437,7 +437,8 @@ void cmCursesMainForm::PrintKeys(int process /* = 0 */)
       }
 
     sprintf(secondLine,
-            "Press [h] for help         Press [q] to quit without generating");
+            "Press [h] for help           "
+            "Press [q] to quit without generating");
     }
 
   curses_move(y-4,0);
