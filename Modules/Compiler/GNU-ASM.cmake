4b40d4297aa7b984e9b5fa905cdee21960ec4f8a
Alex Neundorf <neundorf@kde.org>
2011-01-30 21:03:37 +0100

    Rework the way assembler is handled, use the C/CXX compiler by default
    This commit changes the way how the assembler support works in cmake.
    The language "ASM" now always uses the C/Cxx compiler instead
    of the assembler directly. This fixes #8392, assembler files are
    not preprocessed.
    If one wants to use the assembler directly, the specific
    assembler "dialect" has to be enabled. I.e. to get as/gas,
    you have to use now ASM-ATT, the same way for ASM_MASM and ASM_NASM.
    
    Implemented this now for gcc.
    SunStudio, IBM, HP and Intel still todo.
    
    Alex

---
+++ Modules/Compiler/GNU-ASM.cmake
@@ -0,0 +1,8 @@
+# This file is loaded when gcc/g++ is used for assembler files (the "ASM" cmake language)
+include(Compiler/GNU)
+
+set(CMAKE_ASM_SOURCE_FILE_EXTENSIONS s;asm)
+
+set(CMAKE_ASM_COMPILE_OBJECT "<CMAKE_C_COMPILER> <DEFINES> <FLAGS> -o <OBJECT>   -c <SOURCE>")
+
+__compiler_gnu(ASM)
