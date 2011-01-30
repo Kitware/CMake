38f92bfbe19afa18f92d336b69576bcba5d1cc35
Alex Neundorf <neundorf@kde.org>
2011-01-30 21:14:53 +0100

    Add ASM support for the Intel compiler
    Alex

--- Modules/Compiler/GNU-ASM.cmake
+++ Modules/Compiler/GNU-ASM.cmake
@@ -1,7 +1,7 @@
 # This file is loaded when gcc/g++ is used for assembler files (the "ASM" cmake language)
 include(Compiler/GNU)
 
-set(CMAKE_ASM_SOURCE_FILE_EXTENSIONS s;asm)
+set(CMAKE_ASM_SOURCE_FILE_EXTENSIONS s;S;asm)
 
 set(CMAKE_ASM_COMPILE_OBJECT "<CMAKE_C_COMPILER> <DEFINES> <FLAGS> -o <OBJECT>   -c <SOURCE>")
 
