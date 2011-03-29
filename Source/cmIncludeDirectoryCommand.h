Kitware/CMake

commit 8d32d229a3b77f7cb0dd708c6a492fa33797b9f0

ENH: make commands lower case by default

Source/cmIncludeDirectoryCommand.h
@@ -45,7 +45,7 @@ public:
   /**
    * The name of the command as specified in CMakeList.txt.
    */
-  virtual const char* GetName() { return "INCLUDE_DIRECTORIES";}
+  virtual const char* GetName() { return "include_directories";}
 
   /**
    * Succinct documentation.
@@ -61,11 +61,11 @@ public:
   virtual const char* GetFullDocumentation()
     {
     return
-      "  INCLUDE_DIRECTORIES([AFTER|BEFORE] [SYSTEM] dir1 dir2 ...)\n"
+      "  include_directories([AFTER|BEFORE] [SYSTEM] dir1 dir2 ...)\n"
       "Add the given directories to those searched by the compiler for "
       "include files. By default the directories are appended onto "
       "the current list of directories. This default behavior can be "
-      "changed by setting CMAKE_INCLUDE_DIRECTORIES_BEFORE to ON. "
+      "changed by setting CMAKE_include_directories_BEFORE to ON. "
       "By using BEFORE or AFTER you can select between appending and "
       "prepending, independent from the default. "
       "If the SYSTEM option is given the compiler will be told that the "

commit 84b7d928fee378c812d3a15536a3530e26cf3ad2

Fix typo in include_directories documentation (#12020)
A search-and-replace in commit 8d32d229 (make commands lower case by
default, 2007-10-10) accidentally changed the variable reference
CMAKE_INCLUDE_DIRECTORIES_BEFORE to CMAKE_include_directories_BEFORE.
Change it back.

Source/cmIncludeDirectoryCommand.h
@@ -61,7 +61,7 @@ public:
       "Add the given directories to those searched by the compiler for "
       "include files. By default the directories are appended onto "
       "the current list of directories. This default behavior can be "
-      "changed by setting CMAKE_include_directories_BEFORE to ON. "
+      "changed by setting CMAKE_INCLUDE_DIRECTORIES_BEFORE to ON. "
       "By using BEFORE or AFTER you can select between appending and "
       "prepending, independent from the default. "
       "If the SYSTEM option is given the compiler will be told that the "