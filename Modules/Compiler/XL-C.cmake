2f3eee74908669e612eec74db2c2b4791d543db4
Brad King <brad.king@kitware.com>
2011-04-07 17:09:05 -0400

    XL: Consolidate compiler flag information
    Factor duplicate information out of Compiler/XL-<lang>.cmake modules
    into a macro in a new Compiler/XL.cmake module.  Invoke it from the
    per-language files to produce the original settings.

--- Modules/Compiler/XL-C.cmake
+++ Modules/Compiler/XL-C.cmake
@@ -1,14 +1,9 @@
-SET(CMAKE_C_VERBOSE_FLAG "-V")
+include(Compiler/XL)
+__compiler_xl(C)
+set(CMAKE_C_FLAGS_RELEASE_INIT "${CMAKE_C_FLAGS_RELEASE_INIT} -DNDEBUG")
+set(CMAKE_C_FLAGS_MINSIZEREL_INIT "${CMAKE_C_FLAGS_MINSIZEREL_INIT} -DNDEBUG")
 
 # -qthreaded     = Ensures that all optimizations will be thread-safe
 # -qalias=noansi = Turns off type-based aliasing completely (safer optimizer)
 # -qhalt=e       = Halt on error messages (rather than just severe errors)
 SET(CMAKE_C_FLAGS_INIT "-qthreaded -qalias=noansi -qhalt=e")
-
-SET(CMAKE_C_FLAGS_DEBUG_INIT "-g")
-SET(CMAKE_C_FLAGS_RELEASE_INIT "-O -DNDEBUG")
-SET(CMAKE_C_FLAGS_MINSIZEREL_INIT "-O -DNDEBUG")
-SET(CMAKE_C_FLAGS_RELWITHDEBINFO_INIT "-g")
-
-SET(CMAKE_C_CREATE_PREPROCESSED_SOURCE "<CMAKE_C_COMPILER> <DEFINES> <FLAGS> -E <SOURCE> > <PREPROCESSED_SOURCE>")
-SET(CMAKE_C_CREATE_ASSEMBLY_SOURCE "<CMAKE_C_COMPILER> <DEFINES> <FLAGS> -S <SOURCE> -o <ASSEMBLY_SOURCE>")
