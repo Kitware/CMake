Kitware/CMake

commit e28c16b482ace7a27ddd9c3680ce942ca83a9f38

Split GNU compiler information files
This moves GNU compiler flags into new-style modules

  Compiler/GNU-<lang>.cmake
  Platform/<os>-GNU-<lang>.cmake

We use language-independent helper modules

  Compiler/GNU.cmake
  Platform/<os>-GNU.cmake

to define macros consolidating the information.

Modules/Compiler/GNU.cmake
@@ -1,0 +1,38 @@
+
+#=============================================================================
+# Copyright 2002-2009 Kitware, Inc.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distributed this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+# This module is shared by multiple languages; use include blocker.
+if(__COMPILER_GNU)
+  return()
+endif()
+set(__COMPILER_GNU 1)
+
+macro(__compiler_gnu lang)
+  # Feature flags.
+  set(CMAKE_${lang}_VERBOSE_FLAG "-v")
+  set(CMAKE_SHARED_LIBRARY_${lang}_FLAGS "-fPIC")
+  set(CMAKE_SHARED_LIBRARY_CREATE_${lang}_FLAGS "-shared")
+
+  # Initial configuration flags.
+  set(CMAKE_${lang}_FLAGS_INIT "")
+  set(CMAKE_${lang}_FLAGS_DEBUG_INIT "-g")
+  set(CMAKE_${lang}_FLAGS_MINSIZEREL_INIT "-Os -DNDEBUG")
+  set(CMAKE_${lang}_FLAGS_RELEASE_INIT "-O3 -DNDEBUG")
+  set(CMAKE_${lang}_FLAGS_RELWITHDEBINFO_INIT "-O2 -g")
+  set(CMAKE_${lang}_CREATE_PREPROCESSED_SOURCE "<CMAKE_${lang}_COMPILER> <DEFINES> <FLAGS> -E <SOURCE> > <PREPROCESSED_SOURCE>")
+  set(CMAKE_${lang}_CREATE_ASSEMBLY_SOURCE "<CMAKE_${lang}_COMPILER> <DEFINES> <FLAGS> -S <SOURCE> -o <ASSEMBLY_SOURCE>")
+  if(NOT APPLE)
+    set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem ")
+  endif(NOT APPLE)
+endmacro()

Modules/CMakeGenericSystem.cmake
@@ -88,6 +88,3 @@ MARK_AS_ADVANCED(
   CMAKE_SKIP_RPATH
   CMAKE_VERBOSE_MAKEFILE
 )
-
-# always include the gcc compiler information
-INCLUDE(Platform/gcc)

Modules/Compiler/GNU-C.cmake
@@ -1,1 +1,2 @@
-SET(CMAKE_C_VERBOSE_FLAG "-v")
+include(Compiler/GNU)
+__compiler_gnu(C)

Modules/Compiler/GNU-CXX.cmake
@@ -1,1 +1,2 @@
-SET(CMAKE_CXX_VERBOSE_FLAG "-v")
+include(Compiler/GNU)
+__compiler_gnu(CXX)

Modules/Compiler/GNU-Fortran.cmake
@@ -1,1 +1,14 @@
-SET(CMAKE_Fortran_VERBOSE_FLAG "-v")
+include(Compiler/GNU)
+__compiler_gnu(Fortran)
+
+# No -DNDEBUG for Fortran.
+SET(CMAKE_Fortran_FLAGS_MINSIZEREL_INIT "-Os")
+SET(CMAKE_Fortran_FLAGS_RELEASE_INIT "-O3")
+
+# We require updates to CMake C++ code to support preprocessing rules
+# for Fortran.
+SET(CMAKE_Fortran_CREATE_PREPROCESSED_SOURCE)
+SET(CMAKE_Fortran_CREATE_ASSEMBLY_SOURCE)
+
+# Fortran-specific feature flags.
+SET(CMAKE_Fortran_MODDIR_FLAG -J)

Modules/Platform/AIX-GNU-C.cmake
@@ -1,0 +1,2 @@
+include(Platform/AIX-GNU)
+__aix_compiler_gnu(C)

Modules/Platform/AIX-GNU-CXX.cmake
@@ -1,0 +1,2 @@
+include(Platform/AIX-GNU)
+__aix_compiler_gnu(CXX)

Modules/Platform/AIX-GNU-Fortran.cmake
@@ -1,0 +1,2 @@
+include(Platform/AIX-GNU)
+__aix_compiler_gnu(Fortran)

commit 33f5a83a5d7aa4c5a021c33a3fa55069da5eeed8

GNU: Fix CMAKE_INCLUDE_SYSTEM_FLAG_<lang> value (#12258)
Fix typo introduced by commit e28c16b4 (Split GNU compiler information
files, 2009-12-02).

Reported-by: Campbell Barton <ideasman42@gmail.com>
Suggested-by: Michael Hertling <mhertling@online.de>

Modules/Compiler/GNU.cmake
@@ -33,6 +33,6 @@ macro(__compiler_gnu lang)
   set(CMAKE_${lang}_CREATE_PREPROCESSED_SOURCE "<CMAKE_${lang}_COMPILER> <DEFINES> <FLAGS> -E <SOURCE> > <PREPROCESSED_SOURCE>")
   set(CMAKE_${lang}_CREATE_ASSEMBLY_SOURCE "<CMAKE_${lang}_COMPILER> <DEFINES> <FLAGS> -S <SOURCE> -o <ASSEMBLY_SOURCE>")
   if(NOT APPLE)
-    set(CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem ")
+    set(CMAKE_INCLUDE_SYSTEM_FLAG_${lang} "-isystem ")
   endif(NOT APPLE)
 endmacro()