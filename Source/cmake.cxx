ea1696e9ab941cf74e9df8ce25de7c771af03adb
Johan Björk <phb@spotify.com>
2011-07-26 09:36:40 +0200

    RunSingleCommand: Add a OUTPUT_NORMAL flag.
    OUTPUT_NORMAL does no processing of the output streams, it just passes
    them through the same streams as they were received on.

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -4296,7 +4296,8 @@ int cmake::Build(const std::string& dir,
                  const std::string& target,
                  const std::string& config,
                  const std::vector<std::string>& nativeOptions,
-                 bool clean)
+                 bool clean,
+                 cmSystemTools::OutputOption outputflag)
 {
   if(!cmSystemTools::FileIsDirectory(dir.c_str()))
     {
@@ -4338,8 +4339,7 @@ int cmake::Build(const std::string& dir,
                     projName.c_str(), target.c_str(),
                     &output,
                     makeProgram.c_str(),
-                    config.c_str(), clean, false, 0,
-                    cmSystemTools::OUTPUT_MERGE,
+                    config.c_str(), clean, false, 0, outputflag,
                     0, nativeOptions);
 }
 
