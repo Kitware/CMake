67fcc838d9e857de2697c2fbe34e40ac095973dc
Brad King <brad.king@kitware.com>
2011-09-14 13:59:23 -0400

    Simplify IntelVSImplicitPath detection project
    Use the ENV{LIB} variable directly instead of parsing the output of the
    whole environment from "set".  Store the output in a .cmake script and
    include it from CMakeDetermineCompilerABI instead of using file(READ).

--- Modules/CMakeDetermineCompilerABI.cmake
+++ Modules/CMakeDetermineCompilerABI.cmake
@@ -98,13 +98,10 @@ FUNCTION(CMAKE_DETERMINE_COMPILER_ABI lang src)
           CMAKE_FLAGS
           "-DCMAKE_Fortran_FLAGS:STRING=${CMAKE_Fortran_FLAGS}"
           OUTPUT_VARIABLE _output)
-        FILE(READ
-          ${CMAKE_BINARY_DIR}/CMakeFiles/IntelVSImplicitPath/implict_link.txt
-          dir)
-        LIST(APPEND implicit_dirs "${dir}")
         FILE(WRITE
           "${CMAKE_BINARY_DIR}/CMakeFiles/IntelVSImplicitPath/output.txt"
           "${_output}")
+        INCLUDE(${CMAKE_BINARY_DIR}/CMakeFiles/IntelVSImplicitPath/output.cmake OPTIONAL)
         SET(_desc "Determine Intel Fortran Compiler Implicit Link Path -- done")
         MESSAGE(STATUS "${_desc}")
       ENDIF()
