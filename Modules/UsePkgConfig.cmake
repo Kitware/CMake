59ccf385540709c5c3e9352120f55b8ca90aaf6e
Alex Neundorf <neundorf@kde.org>
2011-09-22 14:47:18 +0200

    Minor improvements to the UsePkgConfig.cmake docs
    Alex

--- Modules/UsePkgConfig.cmake
+++ Modules/UsePkgConfig.cmake
@@ -1,13 +1,14 @@
-# - obsolete pkg-config module for CMake
+# - Osolete pkg-config module for CMake, use FindPkgConfig instead.
 #
-# Defines the following macros:
+# This module defines the following macro:
 #
 # PKGCONFIG(package includedir libdir linkflags cflags)
 #
 # Calling PKGCONFIG will fill the desired information into the 4 given arguments,
 # e.g. PKGCONFIG(libart-2.0 LIBART_INCLUDE_DIR LIBART_LINK_DIR LIBART_LINK_FLAGS LIBART_CFLAGS)
 # if pkg-config was NOT found or the specified software package doesn't exist, the
-# variable will be empty when the function returns, otherwise they will contain the respective information
+# variable will be empty when the function returns, otherwise they will contain
+# the respective information
 #
 
 #=============================================================================
@@ -27,7 +28,7 @@ FIND_PROGRAM(PKGCONFIG_EXECUTABLE NAMES pkg-config )
 
 MACRO(PKGCONFIG _package _include_DIR _link_DIR _link_FLAGS _cflags)
   MESSAGE(STATUS
-    "WARNING: you are using the obsolete 'PKGCONFIG' macro use FindPkgConfig")
+    "WARNING: you are using the obsolete 'PKGCONFIG' macro, use FindPkgConfig")
 # reset the variables at the beginning
   SET(${_include_DIR})
   SET(${_link_DIR})
