ac4dd41bcc3818f010fc19e28b2e76e2407d2a09
Stephen Kelly <steveire@gmail.com>
2011-10-01 12:23:56 +0200

    Make it possible to clear LINK_INTERFACE_LIBRARIES for all targets.

---
+++ Tests/CMakeCommands/target_link_libraries/src/main.cpp
@@ -0,0 +1,15 @@
+
+#include "classA.h"
+#include "classB.h"
+#include "classC.h"
+#include "classD.h"
+
+int main(int, char **) {
+
+  classA a;
+  classB b;
+  classC c;
+  classD d;
+
+  return 0;
+}
