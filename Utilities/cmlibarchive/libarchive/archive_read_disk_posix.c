02d5e40572408efc3733e796dd6a199f4d9ae7f2
Brad King <brad.king@kitware.com>
2011-12-22 16:59:14 -0500

    libarchive: Check for 'struct statvfs' member 'f_iosize'
    Configure the result as definition HAVE_STRUCT_STATVFS_F_IOSIZE and use
    the member only if it exists.  At least one platform (IRIX) provides
    struct statvfs without this member.

--- Utilities/cmlibarchive/libarchive/archive_read_disk_posix.c
+++ Utilities/cmlibarchive/libarchive/archive_read_disk_posix.c
@@ -1382,8 +1382,13 @@ setup_current_filesystem(struct archive_read_disk *a)
 		 * for pathconf() function. */
 		t->current_filesystem->xfer_align = sfs.f_frsize;
 		t->current_filesystem->max_xfer_size = -1;
+#if defined(HAVE_STRUCT_STATVFS_F_IOSIZE)
 		t->current_filesystem->min_xfer_size = sfs.f_iosize;
 		t->current_filesystem->incr_xfer_size = sfs.f_iosize;
+#else
+		t->current_filesystem->min_xfer_size = sfs.f_bsize;
+		t->current_filesystem->incr_xfer_size = sfs.f_bsize;
+#endif
 	}
 	if (sfs.f_flag & ST_LOCAL)
 		t->current_filesystem->remote = 0;
