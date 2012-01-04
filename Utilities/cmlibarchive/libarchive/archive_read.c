65b6e19a35c1bde5ee3c132ade076ee5f42e6559
Brad King <brad.king@kitware.com>
2012-01-04 11:54:51 -0500

    libarchive: Avoid bogus conversion warning from PGI compiler
    We cannot suppress PGI compiler warnings completely because even with
    the "-w" flag the compiler still writes a message containing "compilation
    completed with warnings" to stderr.
    
    A warning is triggered by expressions like
    
      test ? NULL : ptr_to_const_char
      test ? ".." : ptr_to_const_char
    
    that the PGI compiler handles incorrectly.  It chooses the pointer type
    of the first option (either void* or char*) and warns about conversion
    of the second without a cast.  Flip the expression logic to
    
      !test ? ptr_to_const_char : NULL
      !test ? ptr_to_const_char : ".."
    
    to help the compiler choose the proper result type.

--- Utilities/cmlibarchive/libarchive/archive_read.c
+++ Utilities/cmlibarchive/libarchive/archive_read.c
@@ -902,7 +902,7 @@ static const char *
 _archive_filter_name(struct archive *_a, int n)
 {
 	struct archive_read_filter *f = get_filter(_a, n);
-	return f == NULL ? NULL : f->name;
+	return f != NULL ? f->name : NULL;
 }
 
 static int64_t
