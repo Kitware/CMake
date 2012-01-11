c43a18e018f15e5cbb8dcddb8726ec76a5c8ace3
Eric NOULARD <eric.noulard@gmail.com>
2012-01-11 14:55:09 +0100

    Fix indentation style

--- Source/cmArchiveWrite.cxx
+++ Source/cmArchiveWrite.cxx
@@ -250,11 +250,11 @@ bool cmArchiveWrite::AddFile(const char* file,
   // do not copy content of symlink
   if (!(archive_entry_filetype(e) & AE_IFLNK))
     {
-      // Content.
-      if(size_t size = static_cast<size_t>(archive_entry_size(e)))
-        {
-          return this->AddData(file, size);
-        }
+    // Content.
+    if(size_t size = static_cast<size_t>(archive_entry_size(e)))
+      {
+      return this->AddData(file, size);
+      }
     }
   return true;
 }
