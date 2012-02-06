Kitware/CMake

commit fd63219557c4acc6e474263302ab9c78985f6d91

ccmake: Align 'g' and 'q' key instructions.
They were miss-aligned with the 'enter' key instruction.

Before:
----
Press [enter] to edit option           CMake Version 2.8.3.20110107-g4b05a-dirty
Press [c] to configure
Press [h] for help         Press [q] to quit without generating
----

After:
----
Press [enter] to edit option           CMake Version 2.8.3.20110107-g4b05a-dirty
Press [c] to configure
Press [h] for help           Press [q] to quit without generating
----

Source/CursesDialog/cmCursesMainForm.cxx
@@ -421,7 +421,7 @@ void cmCursesMainForm::PrintKeys(int process /* = 0 */)
     if (this->OkToGenerate)
       {
       sprintf(firstLine,
-              "Press [c] to configure     Press [g] to generate and exit");
+              "Press [c] to configure       Press [g] to generate and exit");
       }
     else
       {
@@ -437,7 +437,8 @@ void cmCursesMainForm::PrintKeys(int process /* = 0 */)
       }
 
     sprintf(secondLine,
-            "Press [h] for help         Press [q] to quit without generating");
+            "Press [h] for help           "
+            "Press [q] to quit without generating");
     }
 
   curses_move(y-4,0);

commit 415ffda7a8ca0dfea2e00353a65fb31d8fadfb75

ccmake: Extend clear line.
When configuring or generating the 'ng' of the end of the second line
is not cleared.  This patch fixes this.

The problem was introduced by commit fd632195 (ccmake: Align 'g' and 'q'
key instructions, 2011-01-07) which adjusted the length of lines that
need clearing.

Source/CursesDialog/cmCursesMainForm.cxx
@@ -410,7 +410,7 @@ void cmCursesMainForm::PrintKeys(int process /* = 0 */)
   if (process)
     {
     const char* clearLine =
-      "                                                               ";
+      "                                                                    ";
     strcpy(firstLine, clearLine);
     strcpy(secondLine, clearLine);
     strcpy(thirdLine, clearLine);