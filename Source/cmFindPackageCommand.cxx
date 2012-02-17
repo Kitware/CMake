2a48331e5d1ba855429b8c35e832143f18764e1b
Alex Neundorf <neundorf@kde.org>
2012-02-17 11:26:03 +0100

    find_package(): improved error message mentioning CMAKE_PREFIX_PATH
    Now CMAKE_PREFIX_PATH is mentioned for searching the config files.
    Also the possible names of the config files are now closer to the top.
    
    Before it said:
    
    CMake Error at CMakeLists.txt:4 (find_package):
      Could not find module Findecm.cmake or a configuration file
      for package ecm.
    
      Adjust CMAKE_MODULE_PATH to find Findecm.cmake or set
      ecm_DIR to the directory containing a CMake configuration
      file for ecm.  The file will have one of the following
      names:
    
        ecmConfig.cmake
        ecm-config.cmake
    
    Now it says:
    
    CMake Error at CMakeLists.txt:7 (find_package):
      Could not find module Findecm.cmake or a configuration file
      for package ecm with one of the following names:
    
        ecmConfig.cmake
        ecm-config.cmake
    
      Adjust CMAKE_MODULE_PATH to find Findecm.cmake.  To find
      the configuration file, set CMAKE_PREFIX_PATH to the installation prefix of
      ecm, or set ecm_DIR to the directory
      containing a CMake configuration file for ecm.

--- Source/cmFindPackageCommand.cxx
+++ Source/cmFindPackageCommand.cxx
@@ -916,31 +916,31 @@ bool cmFindPackageCommand::HandlePackageMode()
         {
         e << "module Find" << this->Name << ".cmake or ";
         }
-      e << "a configuration file for package " << this->Name << ".\n";
-      if(!this->NoModule)
-        {
-        e << "Adjust CMAKE_MODULE_PATH to find Find"
-          << this->Name << ".cmake or set ";
-        }
-      else
-        {
-        e << "Set ";
-        }
-      e << this->Variable << " to the directory containing a CMake "
-        << "configuration file for " << this->Name << ".  ";
       if(this->Configs.size() == 1)
         {
-        e << "The file will be called " << this->Configs[0];
+        e << "a configuration file named " << this->Configs[0]
+          << " for package " << this->Name << ".\n";
         }
       else
         {
-        e << "The file will have one of the following names:\n";
+        e << "a configuration file for package " << this->Name
+          << " with one of the following names:\n";
         for(std::vector<std::string>::const_iterator ci=this->Configs.begin();
             ci != this->Configs.end(); ++ci)
           {
           e << "  " << *ci << "\n";
           }
         }
+
+      if(!this->NoModule)
+        {
+        e << "Adjust CMAKE_MODULE_PATH to find Find"
+          << this->Name << ".cmake. ";
+        }
+      e << "To find the configuration file, set CMAKE_PREFIX_PATH to the "
+        << "installation prefix of " << this->Name
+        << ", or set " << this->Variable << " to the directory containing a "
+        << "CMake configuration file for " << this->Name << ".  ";
       }
 
 
