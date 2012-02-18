d36d29f1f079c58bf7d5993da442e4296c7f82a8
Modestas Vainius <modax@debian.org>
2012-02-18 16:11:18 +0100

    various typo and formatting fixes in manual pages (#12975)
    The patch fixes the following lintian warnings:
    
    W: cmake-data: manpage-has-errors-from-man usr/share/man/man1/cmakemodules.1.gz 2728: warning: macro `..' not defined
    I: cmake-data: spelling-error-in-manpage usr/share/man/man1/cmakemodules.1.gz overriden overridden
    I: cmake-data: spelling-error-in-manpage usr/share/man/man1/cmakemodules.1.gz overriden overridden
    I: cmake-data: spelling-error-in-manpage usr/share/man/man1/cmakemodules.1.gz explicitely explicitly
    I: cmake-data: spelling-error-in-manpage usr/share/man/man1/cmakemodules.1.gz jave java
    W: cmake-data: manpage-has-errors-from-man usr/share/man/man1/cmakeprops.1.gz 1040: warning [p 25, 3.7i]: can't break line
    W: cmake: manpage-has-errors-from-man usr/share/man/man1/cmake.1.gz 4233: warning [p 85, 1.3i]: can't break line
    I: cmake: spelling-error-in-manpage usr/share/man/man1/cmake.1.gz overriden overridden
    I: cmake: spelling-error-in-manpage usr/share/man/man1/cmake.1.gz overriden overridden
    I: cmake: spelling-error-in-manpage usr/share/man/man1/cmake.1.gz explicitely explicitly
    I: cmake: spelling-error-in-manpage usr/share/man/man1/cmake.1.gz jave java

--- Source/cmTarget.cxx
+++ Source/cmTarget.cxx
@@ -1047,10 +1047,10 @@ void cmTarget::DefineProperties(cmake *cm)
      "Can be set to one or more UUIDs recognized by Visual Studio "
      "to indicate the type of project. This value is copied "
      "verbatim into the generated project file. Example for a "
-     "managed C++ unit testing project: \""
-     "{3AC096D0-A1C2-E12C-1390-A8335801FDAB};"
-     "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}\". UUIDs are "
-     "semicolon-delimited.");
+     "managed C++ unit testing project:\n"
+     " {3AC096D0-A1C2-E12C-1390-A8335801FDAB};"
+     "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}\n"
+     "UUIDs are semicolon-delimited.");
   cm->DefineProperty
     ("VS_GLOBAL_KEYWORD", cmProperty::TARGET,
      "Visual Studio project keyword.",
