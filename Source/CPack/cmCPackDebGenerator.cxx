805c1b21d63ccc6b02b1a43f438a3b409dd12056
Eric NOULARD <eric.noulard@gmail.com>
2012-03-18 20:21:35 +0100

    Handle CPACK_MONOLITHIC_INSTALL in some rare use cases.
    For example, when CPACK_<GEN>_COMPONENT_INSTALL and
    CPACK_MONOLITHIC_INSTALL are both set. Previously, this
    combination of variable settings produced an error without
    any explanation. Now, in this case CPACK_MONOLITHIC_INSTALL wins
    without trouble.
    This is useful for when e.g. CPACK_ARCHIVE_COMPONENT_INSTALL is
    globally on and one wants MONOLITHIC install for STGZ (but not
    other generators). The same behavior may be obtained by re-setting
    CPACK_ARCHIVE_COMPONENT_INSTALL to 0 but in any case the
    'both set' case should have been handled without error.

--- Source/CPack/cmCPackDebGenerator.cxx
+++ Source/CPack/cmCPackDebGenerator.cxx
@@ -236,7 +236,7 @@ int cmCPackDebGenerator::PackageFiles()
   int retval = -1;
 
   /* Are we in the component packaging case */
-  if (SupportsComponentInstallation()) {
+  if (WantsComponentInstallation()) {
     // CASE 1 : COMPONENT ALL-IN-ONE package
     // If ALL GROUPS or ALL COMPONENTS in ONE package has been requested
     // then the package file is unique and should be open here.
