89987c21027af53a97db07babf91d718e52676c3
Brad King <brad.king@kitware.com>
2012-04-02 10:40:13 -0400

    Test generated module .def files
    Teach the ModuleDefinition test to cover the case that a .def file is
    generated by a custom command.

--- Tests/ModuleDefinition/example_exe.c
+++ Tests/ModuleDefinition/example_exe.c
@@ -1,4 +1,5 @@
 extern int __declspec(dllimport) example_dll_function(void);
+extern int __declspec(dllimport) example_dll_gen_function(void);
 #ifdef EXAMPLE_DLL_2
 extern int __declspec(dllimport) example_dll_2_function(void);
 #endif
@@ -7,6 +8,7 @@ int main(void)
 {
   return
     example_dll_function() +
+    example_dll_gen_function() +
 #ifdef EXAMPLE_DLL_2
     example_dll_2_function() +
 #endif
