77cb7b502f3a90c75e92df655f2d37132ee5627f
Peter Kuemmel <syntheticpp@gmx.net>
2012-06-14 09:52:39 +0200

    Ninja: suppress startup logos
    Having Ninja's smart printing we are more allergic on
    unneeded tool output.

--- Source/cmcldeps.cxx
+++ Source/cmcldeps.cxx
@@ -694,20 +694,22 @@ int main() {
   fprintf(stderr, "\n\n");
 #endif
 
+  std::string showflag = " /showIncludes /nologo ";
   if (lang != "RC") {
     return process(srcfile, dfile, objfile, prefix,
-                   binpath + " /showIncludes " + rest);
+                   binpath + showflag + rest);
   } else {
     // "misuse" cl.exe to get headers from .rc files
     // rc: /fo  x\CMakeFiles\x.dir\x.rc.res              src\x\x.rc
     // cl: /out:x\CMakeFiles\x.dir\x.rc.res.dep.obj  /Tc src\x\x.rc
 
-    cl = "\"" + cl + "\" /P /DRC_INVOKED /showIncludes " +
+    cl = "\"" + cl + "\" /P /DRC_INVOKED " + showflag +
          replace(rest, "/fo" + objfile, "/out:" + objfile + ".dep.obj /Tc ");
 
     int ret;
     ret = process(srcfile, dfile, objfile, prefix, cl, true);
-    ret = process(srcfile, ""   , objfile, prefix, binpath + " " + rest);
+    ret = process(srcfile, ""   , objfile, prefix,
+                  binpath + " /nologo "  + rest);
     return ret;
   }
 
