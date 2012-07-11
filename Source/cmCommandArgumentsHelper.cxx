Kitware/CMake

commit 43cad3e4a54542dce758e2c0f3ea4aef7356ac14

BUG: fix the default "Unspecified" component when only the generic (i.e. not RUNTIME, ARCHIVE, LIBRARY, etc.) arguments are given.
If the component of a part of a target is queried, return the specific one,
if a specific one hasn't been set, return the generic one, if that hasn't
been set, return "Unspecified".

Alex

Source/cmInstallCommandArguments.cxx
@@ -40,9 +40,7 @@ cmInstallCommandArguments::cmInstallCommandArguments()
 ,NamelinkOnly  (&Parser, "NAMELINK_ONLY" , &ArgumentGroup)
 ,NamelinkSkip  (&Parser, "NAMELINK_SKIP" , &ArgumentGroup)
 ,GenericArguments(0)
-{
-  this->Component.SetDefaultString("Unspecified");
-}
+{}
 
 const std::string& cmInstallCommandArguments::GetDestination() const
 {
@@ -67,7 +65,9 @@ const std::string& cmInstallCommandArguments::GetComponent() const
     {
     return this->GenericArguments->GetComponent();
     }
-  return this->EmptyString;
+
+  static std::string unspecifiedComponent = "Unspecified";
+  return unspecifiedComponent;
 }
 
 const std::string& cmInstallCommandArguments::GetRename() const

commit 8720aa0496bad1381a6636a3c4a6d5d550381960

install: Fix COMPONENT option
Since commit 7ced0732 (make default install component name configurable,
2012-05-13) the COMPONENT option has not worked in all cases.  The
change restored use of the broken SetDefaultString previously removed by
commit 43cad3e4 (fix the default "Unspecified" component, 2008-03-26).

Remove SetDefaultString and use the default component name only when an
invocation does not specify a non-empty COMPONENT option value.

Bug-Debian: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=680825

Source/cmCommandArgumentsHelper.cxx
@@ -178,7 +178,7 @@ bool cmCAString::DoConsume(const std::string& arg, unsigned int index)
 
 void cmCAString::DoReset()
 {
-  this->String = this->DefaultString;
+  this->String = "";
 }
 
 cmCAEnabler::cmCAEnabler(cmCommandArgumentsHelper* args, 

Source/cmCommandArgumentsHelper.h
@@ -125,11 +125,8 @@ class cmCAString : public cmCommandArgument
     /// Return the string
     const std::string& GetString() const                 {return this->String;}
     const char* GetCString() const               {return this->String.c_str();}
-    void SetDefaultString(const char* text)
-                                    {this->DefaultString = (text ? text : "");}
   private:
     std::string String;
-    std::string DefaultString;
     unsigned int DataStart;
     virtual bool DoConsume(const std::string& arg, unsigned int index);
     virtual void DoReset();

Source/cmInstallCommandArguments.cxx
@@ -36,8 +36,8 @@ cmInstallCommandArguments::cmInstallCommandArguments(
 ,NamelinkOnly  (&Parser, "NAMELINK_ONLY" , &ArgumentGroup)
 ,NamelinkSkip  (&Parser, "NAMELINK_SKIP" , &ArgumentGroup)
 ,GenericArguments(0)
+,DefaultComponentName(defaultComponent)
 {
-  this->Component.SetDefaultString(defaultComponent.c_str());
 }
 
 const std::string& cmInstallCommandArguments::GetDestination() const
@@ -63,7 +63,10 @@ const std::string& cmInstallCommandArguments::GetComponent() const
     {
     return this->GenericArguments->GetComponent();
     }
-
+  if (!this->DefaultComponentName.empty())
+    {
+    return this->DefaultComponentName;
+    }
   static std::string unspecifiedComponent = "Unspecified";
   return unspecifiedComponent;
 }

Source/cmInstallCommandArguments.h
@@ -61,6 +61,7 @@ class cmInstallCommandArguments
     cmInstallCommandArguments* GenericArguments;
     static const char* PermissionsTable[];
     static const std::string EmptyString;
+    std::string DefaultComponentName;
     bool CheckPermissions();
 };
 