f3155cd62ab669afc9d21bc877d5eb452ed94a7d
Brad King <brad.king@kitware.com>
2012-08-06 09:29:20 -0400

    Add RunCMake.Syntax test to cover argument parsing
    Test basic unquoted and quoted argument parsing cases including failure
    on an unterminated string and an unterminated command invocation.  Also
    cover arguments not separated by any spaces, which is accidentally
    allowed by the current parser.

---
+++ Tests/RunCMake/Syntax/RunCMakeTest.cmake
@@ -0,0 +1,9 @@
+include(RunCMake)
+
+run_cmake(String0)
+run_cmake(String1)
+run_cmake(StringNoSpace)
+run_cmake(Unquoted0)
+run_cmake(Unquoted1)
+run_cmake(UnterminatedCall1)
+run_cmake(UnterminatedString)
