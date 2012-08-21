733deb66c63dbbd8caa84f7630318c6f76da2d9f
Stephen Kelly <steveire@gmail.com>
2012-08-21 11:04:09 +0200

    Fix CompileDefinitions test on Visual Studio.
    Avoid testing unsupported spaces in define values.

--- Tests/CompileDefinitions/main.cpp
+++ Tests/CompileDefinitions/main.cpp
@@ -15,11 +15,16 @@ template<>
 struct CMakeStaticAssert<true> {};
 
 static const char fun_string[] = CMAKE_IS_;
+#ifndef NO_SPACES_IN_DEFINE_VALUES
 static const char very_fun_string[] = CMAKE_IS_REALLY;
+#endif
 
 enum {
-  StringLiteralTest1 = sizeof(CMakeStaticAssert<sizeof(CMAKE_IS_) == sizeof("Fun")>),
+  StringLiteralTest1 = sizeof(CMakeStaticAssert<sizeof(CMAKE_IS_) == sizeof("Fun")>)
+#ifndef NO_SPACES_IN_DEFINE_VALUES
+  ,
   StringLiteralTest2 = sizeof(CMakeStaticAssert<sizeof(CMAKE_IS_REALLY) == sizeof("Very Fun")>)
+#endif
 };
 
 int main(int argc, char **argv)
