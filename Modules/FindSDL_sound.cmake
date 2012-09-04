03dd6cc4ffb2ac00f774fe24c5b0721b371d3c91
Benjamin Eikel <cmake@eikel.org>
2012-09-04 16:00:04 +0200

    FindSDL: Pass SDL_SOUND_LIBRARY to FIND_PACKAGE_HANDLE_STANDARD_ARGS

--- Modules/FindSDL_sound.cmake
+++ Modules/FindSDL_sound.cmake
@@ -395,5 +395,5 @@ endif()
 include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(SDL_sound
-                                  REQUIRED_VARS SDL_SOUND_LIBRARIES SDL_SOUND_INCLUDE_DIR
+                                  REQUIRED_VARS SDL_SOUND_LIBRARY SDL_SOUND_INCLUDE_DIR
                                   VERSION_VAR SDL_SOUND_VERSION_STRING)
