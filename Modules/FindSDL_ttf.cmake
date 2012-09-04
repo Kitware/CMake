22154c7fc23e32c97952e368f7437999576ea082
Benjamin Eikel <cmake@eikel.org>
2012-09-04 16:12:13 +0200

    FindSDL: Update documentation

--- Modules/FindSDL_ttf.cmake
+++ Modules/FindSDL_ttf.cmake
@@ -1,8 +1,14 @@
-# Locate SDL_ttf library
-# This module defines
-# SDLTTF_LIBRARY, the name of the library to link against
-# SDLTTF_FOUND, if false, do not try to link to SDL
-# SDLTTF_INCLUDE_DIR, where to find SDL/SDL.h
+# - Locate SDL_ttf library
+# This module defines:
+#  SDL_TTF_LIBRARIES, the name of the library to link against
+#  SDL_TTF_INCLUDE_DIRS, where to find the headers
+#  SDL_TTF_FOUND, if false, do not try to link against
+#  SDL_TTF_VERSION_STRING - human-readable string containing the version of SDL_ttf
+#
+# For backward compatiblity the following variables are also set:
+#  SDLTTF_LIBRARY (same value as SDL_TTF_LIBRARIES)
+#  SDLTTF_INCLUDE_DIR (same value as SDL_TTF_INCLUDE_DIRS)
+#  SDLTTF_FOUND (same value as SDL_TTF_FOUND)
 #
 # $SDLDIR is an environment variable that would
 # correspond to the ./configure --prefix=$SDLDIR
