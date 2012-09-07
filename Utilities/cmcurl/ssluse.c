8b51762426ff6e3efbe61ee49f99755c24b4772c
Brad King <brad.king@kitware.com>
2012-09-07 13:23:45 -0400

    curl: Honor OPENSSL_NO_SSL2
    Some OpenSSL distributions have dropped support for the ancient SSLv2
    protocol completely.  Port changes from upstream curl to recognize this
    case and avoid using it.

--- Utilities/cmcurl/ssluse.c
+++ Utilities/cmcurl/ssluse.c
@@ -1285,8 +1285,13 @@ Curl_ossl_connect_step1(struct connectdata *conn,
     req_method = TLSv1_client_method();
     break;
   case CURL_SSLVERSION_SSLv2:
+#ifdef OPENSSL_NO_SSL2
+    failf(data, "OpenSSL was built without SSLv2 support");
+    return CURLE_NOT_BUILT_IN;
+#else
     req_method = SSLv2_client_method();
     break;
+#endif
   case CURL_SSLVERSION_SSLv3:
     req_method = SSLv3_client_method();
     break;
