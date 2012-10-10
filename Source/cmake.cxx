b8457ad18b7eac2998fdd29fbc295c1dadbbba42
Morné Chamberlain <thefreeman.za@gmail.com>
2012-10-11 00:01:48 +0200

    Added a generator for Sublime Text 2 project files.
    The generator adds all list and source files for each defined
    project. The generated project files also include build_system
    entries that run the platform-specific make on the generated
    Makefiles. A build_system entry is created for each target
    that was defined in the CMakeLists.txt files.
    
    At the moment this has only been tested with C/C++ projects.

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -94,6 +94,7 @@
 #if !defined(CMAKE_BOOT_MINGW)
 # include "cmExtraCodeBlocksGenerator.h"
 #endif
+#include "cmExtraSublimeTextGenerator.h"
 
 #ifdef CMAKE_USE_KDEVELOP
 # include "cmGlobalKdevelopGenerator.h"
@@ -1847,6 +1848,8 @@ void cmake::AddDefaultExtraGenerators()
 
   this->AddExtraGenerator(cmExtraCodeBlocksGenerator::GetActualName(),
                           &cmExtraCodeBlocksGenerator::New);
+  this->AddExtraGenerator(cmExtraSublimeTextGenerator::GetActualName(),
+                          &cmExtraSublimeTextGenerator::New);
 
 #ifdef CMAKE_USE_ECLIPSE
   this->AddExtraGenerator(cmExtraEclipseCDT4Generator::GetActualName(),
