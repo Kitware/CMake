cd76ec3e22f299f03e34db3deee5e835613af315
Morné Chamberlain <thefreeman.za@gmail.com>
2012-10-13 12:54:51 +0200

    Added and cleaned up some comments.

--- Source/cmExtraSublimeTextGenerator.h
+++ Source/cmExtraSublimeTextGenerator.h
@@ -45,8 +45,15 @@ private:
 
   void CreateNewProjectFile(const std::vector<cmLocalGenerator*>& lgs,
                                 const std::string& filename);
+  /** Populates allFiles with the full paths to all of the source files
+   *  from the local generators in lgs.
+   */
   void GetFileList(const std::vector<cmLocalGenerator*>& lgs,
                    std::vector<std::string>& allFiles);
+  /** Sends comma seperated source files paths to fileIncludePatternsStream
+   *  and builds a set of all directories and subdirectories containing
+   *  source files.
+   */
   void GetFileStringAndFolderSet(const std::vector<cmLocalGenerator*>& lgs,
                                  const cmMakefile* mf,
                                  const std::vector<std::string>& allFiles,
@@ -61,8 +68,14 @@ private:
                         cmGeneratedFileStream& fout,
                         std::set<std::string>& includeDirs,
                         std::set<std::string>& defines);
+  /** Returns the build command that needs to be executed to build the
+   *  specified target.
+   */
   std::string BuildMakeCommand(const std::string& make, const char* makefile,
                                const char* target);
+  /** Appends the specified target to the generated project file as a Sublime
+   *  Text build system.
+   */
   void AppendTarget(cmGeneratedFileStream& fout,
                     const char* targetName,
                     cmTarget* target,
