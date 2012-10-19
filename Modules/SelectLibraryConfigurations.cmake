Kitware/CMake

commit 5797512cec6ce106cf3277c59abd2ccb86ce2846

SelectLibraryConfiguration: generate correct output when input vars are lists
In case that any of the input variables that hold the library names contains
more than just a single library the "debug" or "optimized" keywords were only
prepended to the first item, making all other libs appear in all
configurations. Just treat both input variables as lists.

Thanks to Philipp Berger <newsletters@philippberger.de> for pointing me at
this.

Modules/SelectLibraryConfigurations.cmake
@@ -48,17 +48,19 @@ macro( select_library_configurations basename )
     # if only the debug version was found, set the release value to be the
     # debug value.
     _set_library_name( ${basename} DEBUG RELEASE )
-    if (${basename}_LIBRARY_DEBUG AND ${basename}_LIBRARY_RELEASE AND
-           NOT ${basename}_LIBRARY_DEBUG STREQUAL ${basename}_LIBRARY_RELEASE)
+    if( ${basename}_LIBRARY_DEBUG AND ${basename}_LIBRARY_RELEASE AND
+           NOT ${basename}_LIBRARY_DEBUG STREQUAL ${basename}_LIBRARY_RELEASE )
         # if the generator supports configuration types or CMAKE_BUILD_TYPE
         # is set, then set optimized and debug options.
         if( CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE )
-            set( ${basename}_LIBRARY 
-                optimized ${${basename}_LIBRARY_RELEASE}
-                debug ${${basename}_LIBRARY_DEBUG} )
-            set( ${basename}_LIBRARIES 
-                optimized ${${basename}_LIBRARY_RELEASE}
-                debug ${${basename}_LIBRARY_DEBUG} )
+            set( ${basename}_LIBRARY )
+            foreach( _libname LISTS ${basename}_LIBRARY_RELEASE )
+                list( APPEND ${basename}_LIBRARY optimized "${_libname}" )
+            endforeach()
+            foreach( _libname LISTS ${basename}_LIBRARY_DEBUG )
+                list( APPEND ${basename}_LIBRARY debug "${_libname}" )
+            endforeach()
+            set( ${basename}_LIBRARIES "${${basename}_LIBRARY}" )
         else( CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE )
             # If there are no configuration types or build type, just use
             # the release version

commit 476017275a1ffe1735a57ca59ed1e92ef24e175b

fix regression in SelectLibraryConfigurations
The bug was introduced in 5797512cec6ce106cf3277c59abd2ccb86ce2846
(SelectLibraryConfiguration: generate correct output when input vars are lists).

Modules/SelectLibraryConfigurations.cmake
@@ -54,10 +54,10 @@ macro( select_library_configurations basename )
         # is set, then set optimized and debug options.
         if( CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE )
             set( ${basename}_LIBRARY )
-            foreach( _libname LISTS ${basename}_LIBRARY_RELEASE )
+            foreach( _libname IN LISTS ${basename}_LIBRARY_RELEASE )
                 list( APPEND ${basename}_LIBRARY optimized "${_libname}" )
             endforeach()
-            foreach( _libname LISTS ${basename}_LIBRARY_DEBUG )
+            foreach( _libname IN LISTS ${basename}_LIBRARY_DEBUG )
                 list( APPEND ${basename}_LIBRARY debug "${_libname}" )
             endforeach()
             set( ${basename}_LIBRARIES "${${basename}_LIBRARY}" )