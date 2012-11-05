dc4152ad8506a495c354e158620f62978fd0b2b6
Stephen Kelly <steveire@gmail.com>
2012-11-05 12:46:26 +0100

    Process the INTERFACE_PIC property from linked dependencies
    This allows a dependee to inform a target that it should have its
    POSITION_INDEPENDENT_CODE property set to ON, or OFF. The value of
    the POSITION_INDEPENDENT_CODE property, if set, must be consistent
    with any INTERFACE_POSITION_INDEPENDENT_CODE properties on dependees.

--- Source/cmTarget.cxx
+++ Source/cmTarget.cxx
@@ -878,6 +878,20 @@ void cmTarget::DefineProperties(cmake *cm)
      "created.");
 
   cm->DefineProperty
+    ("INTERFACE_POSITION_INDEPENDENT_CODE", cmProperty::TARGET,
+     "Whether consumers need to create a position-independent target",
+     "The INTERFACE_POSITION_INDEPENDENT_CODE property informs consumers of "
+     "this target whether they must set their POSITION_INDEPENDENT_CODE "
+     "property to ON.  If this property is set to ON, then the "
+     "POSITION_INDEPENDENT_CODE property on all consumers will be set to "
+     "ON.  Similarly, if this property is set to OFF, then the "
+     "POSITION_INDEPENDENT_CODE property on all consumers will be set to "
+     "OFF.  If this property is undefined, then consumers will determine "
+     "their POSITION_INDEPENDENT_CODE property by other means.  Consumers "
+     "must ensure that the targets that they link to have a consistent "
+     "requirement for their INTERFACE_POSITION_INDEPENDENT_CODE property.");
+
+  cm->DefineProperty
     ("POST_INSTALL_SCRIPT", cmProperty::TARGET,
      "Deprecated install support.",
      "The PRE_INSTALL_SCRIPT and POST_INSTALL_SCRIPT properties are the "
