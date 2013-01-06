40c26579f971dd64aaabc88d6ae65afe527a17e3
Stephen Kelly <steveire@gmail.com>
2013-01-06 13:49:20 +0100

    Add the COMPATIBLE_INTERFACE_STRING property.

--- Tests/CompatibleInterface/main.cpp
+++ Tests/CompatibleInterface/main.cpp
@@ -11,6 +11,18 @@
 #error Expected BOOL_PROP3
 #endif
 
+#ifndef STRING_PROP1
+#error Expected STRING_PROP1
+#endif
+
+#ifndef STRING_PROP2
+#error Expected STRING_PROP2
+#endif
+
+#ifndef STRING_PROP3
+#error Expected STRING_PROP3
+#endif
+
 #include "iface2.h"
 
 int main(int argc, char **argv)
