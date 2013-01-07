495fa24d70ce4c5b594e15cae464af243a127fb8
KWSys Robot <kwrobot@kitware.com>
2013-01-07 10:20:26 -0500

    KWSys 2013-01-07 (fc60c8b8)
    Extract upstream KWSys using the following shell commands.
    
    $ git archive --prefix=upstream-kwsys/ fc60c8b8 | tar x
    $ git shortlog --no-merges --abbrev=8 --format='%h %s' 8ce09af5..fc60c8b8
    Brad King (3):
          cb5f835f SystemTools: Fix MakeDirectory with colon in path
          1643507a IOStream: Fix check for compiler 'long long' support
          34177aec SystemTools: Fix MakeDirectory after recent cleanup
    
    Rolf Eike Beer (24):
          4da9894d SystemInformation: Clean up QuerySolarisInfo()
          01392358 SystemInformation: don't attribute i386 Solaris systems to Sun
          3db65ac1 SystemInformation: wrap the call to CPUID in a function
          61bd9b42 SystemInformation: use the __cpuid compiler intrinsic if present
          5932e7c0 SystemInformation: query memory size on Cygwin using sysconf()
          ea5612ed SystemInformation: count memory with _SC_PAGESIZE and _SC_PHYS_PAGES
          3aca6642 SystemInformation: query memory size, CPU count, and CPU speed on BSD
          3572c54d SystemInformation: count CPUs on HP-UX
          e6771b34 SystemInformation: determine CPU type on HP-UX
          640210e5 SystemInformation: use /proc/cpuinfo only when present
          5bdcfd10 SystemInformation: query total memory on AIX
          de69d547 SystemInformation: use intrinsic for RDTSC if supported
          9808d4e7 SystemInformation: get CPU speed on Windows when RDTSC fails
          f4c625b5 tests: avoid truncation of testSystemInformation output in CDash
          03d6fbe5 SystemInformation: get x86 CPU features from /proc/cpuinfo
          f8e917c1 SystemInformation: remove the #define CPUID_INSTRUCTION
          59c4b5c5 SystemInformation: split Windows code out of QueryMemory()
          200ee91f SystemInformation: split HP-UX code out of QueryMemory()
          88217703 SystemInformation: split Linux code out of QueryMemory()
          9e317872 SystemInformation: split AIX code out of QueryMemory()
          349cee5b SystemInformation: remove useless zeroing from QueryMemory()
          7271926e SystemInformation: split Cygwin code out of QueryMemory()
          6da78ad1 SystemInformation: make QueryMemory() return bool
          182179e9 Haiku no longer defines __BEOS__
    
    Stephen Kelly (1):
          247b8a3c SystemTools: Rename MakeCnidentifier() to MakeCidentifier()
    
    Change-Id: Ib95b5bddab7ecc0a4025ab29792426acf57e5623

--- testProcess.c
+++ testProcess.c
@@ -32,7 +32,7 @@
 # pragma warn -8060 /* possibly incorrect assignment */
 #endif
 
-#if defined(__BEOS__) && !defined(__ZETA__) && !defined(__HAIKU__)
+#if defined(__BEOS__) && !defined(__ZETA__)
 /* BeOS 5 doesn't have usleep(), but it has snooze(), which is identical. */
 # include <be/kernel/OS.h>
 static inline void testProcess_usleep(unsigned int msec)
