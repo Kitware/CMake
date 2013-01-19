e98799105bdb296e8d9f5b3ef5cf99bcebcafc40
Stephen Kelly <steveire@gmail.com>
2013-01-19 11:21:14 +0100

    Make INTERFACE determined properties readable in generator expressions.
    The properties are evaluated as link-dependent interface properties when
    evaluating the generator expressions.

---
+++ Tests/CompatibleInterface/main.cpp
@@ -0,0 +1,17 @@
+
+#ifndef BOOL_PROP1
+#error Expected BOOL_PROP1
+#endif
+
+#ifndef BOOL_PROP2
+#error Expected BOOL_PROP2
+#endif
+
+#ifndef BOOL_PROP3
+#error Expected BOOL_PROP3
+#endif
+
+int main(int argc, char **argv)
+{
+  return 0;
+}
---
+++ Tests/RunCMake/CompatibleInterface/RunCMakeTest.cmake
@@ -0,0 +1,6 @@
+include(RunCMake)
+
+run_cmake(InterfaceBool-mismatch-depends)
+run_cmake(InterfaceBool-mismatch-depend-self)
+run_cmake(InterfaceBool-mismatched-use)
+run_cmake(InterfaceBool-builtin-prop)
