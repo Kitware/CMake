d936eae8c1784ac3257dd5031defb4e2cad11924
Stephen Kelly <steveire@gmail.com>
2013-01-20 19:29:43 +0100

    Fix use of target_link_libraries with an IMPORTED target.
    cmTarget::MaybeInvalidatePropertyCache only invalidates the cache
    for properties matching "IMPORTED_*".
    
    We need to append in the tll implementation to
    the IMPORTED_LINK_INTERFACE_LIBRARIES_NOCONFIG property, because
    that is what is read by cmTarget::ComputeImportInfo in the case
    of no config.

---
+++ Tests/ExportImport/Export/testSharedLibImportDepend.h
@@ -0,0 +1,12 @@
+
+#ifndef TESTSHAREDLIBIMPORTDEPEND_H
+#define TESTSHAREDLIBIMPORTDEPEND_H
+
+#include "testsharedlibimportdepend_export.h"
+
+struct TESTSHAREDLIBIMPORTDEPEND_EXPORT TestSharedLibImportDepend
+{
+  int foo();
+};
+
+#endif
--- Tests/ExportImport/Import/A/deps_shared_iface.cpp
+++ Tests/ExportImport/Import/A/deps_shared_iface.cpp
@@ -2,6 +2,8 @@
 
 #include "testSharedLibDepends.h"
 
+#include "testSharedLibImportDepend.h"
+
 #ifdef CHECK_PIC_WORKS
 #if defined(__ELF__) && !defined(__PIC__) && !defined(__PIE__)
 #error Expected by INTERFACE_POSITION_INDEPENDENT_CODE property of dependency
@@ -16,12 +18,13 @@ int main(int,char **)
 {
   TestSharedLibDepends dep;
   TestSharedLibRequired req;
+  TestSharedLibImportDepend imp;
 
 #ifdef TEST_SUBDIR_LIB
   SubDirObject sdo;
 #endif
 
-  return dep.foo() + req.foo()
+  return dep.foo() + req.foo() + imp.foo()
 #ifdef TEST_SUBDIR_LIB
                    + sdo.foo()
 #endif
