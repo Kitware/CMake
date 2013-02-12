567c8d103ed3c3cb7303877d89e8508dd3f8d0ca
Stephen Kelly <steveire@gmail.com>
2013-02-12 11:53:36 +0100

    Revert "Don't allow utility or global targets in the LINKED expression."
    This reverts commit 9712362b4580fb92394ecf8ff57be186571f4319.

--- Tests/RunCMake/TargetPropertyGeneratorExpressions/BadLinked-UtilityTarget.cmake
+++
@@ -1,7 +0,0 @@
-
-add_custom_target(check ALL
-  COMMAND ${CMAKE_COMMAND} -E echo check
-)
-
-add_library(foo STATIC empty.cpp)
-set_property(TARGET foo PROPERTY INCLUDE_DIRECTORIES $<LINKED:check>)
