675f03050da0982c16741a3887f73da21983333f
Stephen Kelly <steveire@gmail.com>
2013-02-26 00:06:05 +0100

    Revert "Don't relink shared libraries by default if dependencies files change."
    This reverts commit 6e954854c1afeefcf8621f213dda55e66cea42d1.

--- Source/cmTarget.cxx
+++ Source/cmTarget.cxx
@@ -706,19 +706,19 @@ void cmTarget::DefineProperties(cmake *cm)
      "custom Makefile link rules.");
 
   cm->DefineProperty
-    ("LINK_DEPENDS_SHARED_LIBRARIES", cmProperty::TARGET,
-     "Depend on linked shared library files.",
-     "Set this property to true to tell CMake generators to add "
+    ("LINK_DEPENDS_NO_SHARED", cmProperty::TARGET,
+     "Do not depend on linked shared library files.",
+     "Set this property to true to tell CMake generators not to add "
      "file-level dependencies on the shared library files linked by "
      "this target.  "
-     "Modification to the shared libraries will then be sufficient to "
+     "Modification to the shared libraries will not be sufficient to "
      "re-link this target.  "
      "Logical target-level dependencies will not be affected so the "
      "linked shared libraries will still be brought up to date before "
      "this target is built."
      "\n"
      "This property is initialized by the value of the variable "
-     "CMAKE_LINK_DEPENDS_SHARED_LIBRARIES if it is set when a target is "
+     "CMAKE_LINK_DEPENDS_NO_SHARED if it is set when a target is "
      "created.");
 
   cm->DefineProperty
@@ -1451,7 +1451,7 @@ void cmTarget::SetMakefile(cmMakefile* mf)
   this->SetPropertyDefault("OSX_ARCHITECTURES", 0);
   this->SetPropertyDefault("AUTOMOC", 0);
   this->SetPropertyDefault("AUTOMOC_MOC_OPTIONS", 0);
-  this->SetPropertyDefault("LINK_DEPENDS_SHARED_LIBRARIES", 0);
+  this->SetPropertyDefault("LINK_DEPENDS_NO_SHARED", 0);
   this->SetPropertyDefault("LINK_INTERFACE_LIBRARIES", 0);
   this->SetPropertyDefault("WIN32_EXECUTABLE", 0);
   this->SetPropertyDefault("MACOSX_BUNDLE", 0);
