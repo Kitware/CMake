2e1c2bd2dd97e426944d6ac18e4073ec26ddf64c
Brad King <brad.king@kitware.com>
2013-03-12 18:12:52 -0400

    build_command: Fail early without CMAKE_MAKE_PROGRAM (#14005)
    If CMAKE_MAKE_PROGRAM is not set fail with an error message instead of
    crashing.  Suggest calling project() or enable_language() first to
    ensure that CMAKE_MAKE_PROGRAM is set.

---
+++ Tests/RunCMake/build_command/BeforeProject.cmake
@@ -0,0 +1,2 @@
+build_command(MAKECOMMAND_DEFAULT_VALUE)
+project(${RunCMake_TEST} NONE)
--- Tests/RunCMake/build_command/RunCMakeTest.cmake
+++ Tests/RunCMake/build_command/RunCMakeTest.cmake
@@ -2,3 +2,7 @@ include(RunCMake)
 
 run_cmake(ErrorsOFF)
 run_cmake(ErrorsON)
+
+set(RunCMake_TEST_OPTIONS -DNoProject=1)
+run_cmake(BeforeProject)
+unset(RunCMake_TEST_OPTIONS)
