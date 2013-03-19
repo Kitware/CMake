fc43477de01e4d95a2c68a401afa2fc7f5f4d255
Brad King <brad.king@kitware.com>
2013-03-19 16:40:06 -0400

    Rename variable for including current directory in interfaces
    Rename the variable added by commit 9ce1b9ef (Add
    CMAKE_BUILD_INTERFACE_INCLUDES build-variable, 2012-11-25) to
    CMAKE_INCLUDE_CURRENT_DIR_IN_INTERFACE to be more consistent with the
    existing CMAKE_INCLUDE_CURRENT_DIR variable.
    
    Suggested-by: Alex Neundorf <neundorf@kde.org>

--- Source/cmDocumentVariables.cxx
+++ Source/cmDocumentVariables.cxx
@@ -1164,14 +1164,14 @@ void cmDocumentVariables::DefineVariables(cmake* cm)
      "Variables that Control the Build");
 
   cm->DefineProperty
-    ("CMAKE_BUILD_INTERFACE_INCLUDES", cmProperty::VARIABLE,
+    ("CMAKE_INCLUDE_CURRENT_DIR_IN_INTERFACE", cmProperty::VARIABLE,
      "Automatically add the current source- and build directories "
      "to the INTERFACE_INCLUDE_DIRECTORIES.",
      "If this variable is enabled, CMake automatically adds for each shared "
      "library target, static library target, module target and executable "
      "target, ${CMAKE_CURRENT_SOURCE_DIR} and ${CMAKE_CURRENT_BINARY_DIR} to "
      "the INTERFACE_INCLUDE_DIRECTORIES."
-     "By default CMAKE_BUILD_INTERFACE_INCLUDES is OFF.",
+     "By default CMAKE_INCLUDE_CURRENT_DIR_IN_INTERFACE is OFF.",
      false,
      "Variables that Control the Build");
 
