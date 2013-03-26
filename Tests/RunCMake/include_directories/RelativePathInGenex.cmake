634bb33f3aa3b7f2181a896c025976b52171524a
Stephen Kelly <steveire@gmail.com>
2013-03-26 18:08:26 +0100

    Error if linked target has relative paths in INTERFACE_INCLUDE_DIRECTORIES
    We can do this check only if the TargetName is non-empty, which means
    that we're evaluating INTERFACE_INCLUDE_DIRECTORIES from a linked
    dependency which was set using target_link_libraries.
    
    It is possible to have relative paths in INCLUDE_DIRECTORIES already
    in CMake 2.8.10.2, so that part will require a policy to fix.

---
+++ Tests/RunCMake/include_directories/RelativePathInGenex.cmake
@@ -0,0 +1,8 @@
+
+project(RelativePathInInterface)
+
+add_library(testTarget "${CMAKE_CURRENT_SOURCE_DIR}/empty.cpp")
+set_property(TARGET testTarget PROPERTY INTERFACE_INCLUDE_DIRECTORIES "$<1:foo>")
+
+add_library(userTarget "${CMAKE_CURRENT_SOURCE_DIR}/empty.cpp")
+target_link_libraries(userTarget testTarget)
