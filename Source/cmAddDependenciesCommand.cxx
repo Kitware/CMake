de13d68d11b9f8f08c67b8cbcdd0f957d5bae6e1
Brad King <brad.king@kitware.com>
2013-03-29 14:53:36 -0400

    add_dependencies: Distinguish target v. file dependencies in error (#14050)
    When called with a non-existent LHS target name the user may be trying
    to add file-level dependencies.  Clarify the error message to explain
    the difference between target-level and file-level dependencies.  Point
    the reader at the commands and options needed for the latter.

--- Source/cmAddDependenciesCommand.cxx
+++ Source/cmAddDependenciesCommand.cxx
@@ -35,10 +35,14 @@ bool cmAddDependenciesCommand
     }
   else
     {
-    std::string error = "Adding dependency to non-existent target: ";
-    error += target_name;
-    this->SetError(error.c_str());
-    return false;
+    cmOStringStream e;
+    e << "Cannot add target-level dependencies to non-existent target \""
+      << target_name << "\".\n"
+      << "The add_dependencies works for top-level logical targets created "
+      << "by the add_executable, add_library, or add_custom_target commands.  "
+      << "If you want to add file-level dependencies see the DEPENDS option "
+      << "of the add_custom_target and add_custom_command commands.";
+    this->Makefile->IssueMessage(cmake::FATAL_ERROR, e.str());
     }
 
   return true;
