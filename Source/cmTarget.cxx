59ef6f3b59c60db8d7d37e000416e40739218420
Stephen Kelly <steveire@gmail.com>
2013-04-29 12:00:53 +0200

    Ensure that empty strings are not passed to the genex TARGET_PROPERTY
    Ensure that a link entry is a non-empty string before processing
    includes from it.
    
    Include directories are processed from the link interface of
    dependent targets since commit a1c4905f (Use the link information
    as a source of compile definitions and includes., 2013-02-12). That
    commit included an optimization of skipping an entry from the link
    interface if it is known not to be a target after evaluation as a
    generator expression.
    
    Commit 42ebb188 (Memoize includes and defines from interface
    libraries., 2013-02-22) introduced caching of the includes. Part of
    the problem is that the cache for the includes is not config
    sensitive because the content can contain $<CONFIG> generator
    expressions which will be evaluated anyway.
    
    When generating the buildsystem with a multi-config generator,
    the GetIncludeDirectories method happens to be called first with
    the DEBUG configuration, followed by the others. So, in the
    testcase:
    
     add_library(B SHARED a.c)
     target_link_libraries(B debug A)
    
    the $<$<CONFIG:Debug>:A> is evaluated to 'A', and the generator
    expression is cached. When the same genex is evaluated with
    non-debug configurations, the evaluated result is the empty string,
    which does not validate for the the TARGET_PROPERTY generator
    expression.
    
    If the 'debug' is replaced by 'optimized' in the testcase, the
    bug does not occur. That is because the first call to
    GetIncludeDirectories will evaluate $<NOT:$<CONFIG:Debug>:A>, get
    an empty string (because config == Debug), and continue before
    caching the result. The next call to GetIncludeDirectories will
    be a non-empty string.

--- Source/cmTarget.cxx
+++ Source/cmTarget.cxx
@@ -2944,10 +2944,18 @@ std::vector<std::string> cmTarget::GetIncludeDirectories(const char *config)
         continue;
         }
       }
+      std::string includeGenex = "$<TARGET_PROPERTY:" +
+                              it->Value + ",INTERFACE_INCLUDE_DIRECTORIES>";
+      if (cmGeneratorExpression::Find(it->Value) != std::string::npos)
+        {
+        // Because it->Value is a generator expression, ensure that it
+        // evaluates to the non-empty string before being used in the
+        // TARGET_PROPERTY expression.
+        includeGenex = "$<$<BOOL:" + it->Value + ">:" + includeGenex + ">";
+        }
       cmGeneratorExpression ge(it->Backtrace);
       cmsys::auto_ptr<cmCompiledGeneratorExpression> cge = ge.Parse(
-        "$<$<BOOL:"+it->Value+">:"
-          "$<TARGET_PROPERTY:"+it->Value+",INTERFACE_INCLUDE_DIRECTORIES>>");
+                                                              includeGenex);
 
       this->Internal->CachedLinkInterfaceIncludeDirectoriesEntries.push_back(
                         new cmTargetInternals::IncludeDirectoriesEntry(cge,
