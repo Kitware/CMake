b9c1f3b78afe63a172f641365559b15e8be831b8
Jack O'Connor <oconnor663@gmail.com>
2013-04-30 17:21:50 -0700

    Eclipse: Add a missing space in the documentation

--- Source/cmExtraEclipseCDT4Generator.cxx
+++ Source/cmExtraEclipseCDT4Generator.cxx
@@ -50,7 +50,7 @@ void cmExtraEclipseCDT4Generator
   entry.Full =
     "Project files for Eclipse will be created in the top directory. "
     "In out of source builds, a linked resource to the top level source "
-    "directory will be created."
+    "directory will be created. "
     "Additionally a hierarchy of makefiles is generated into the "
     "build tree. The appropriate make program can build the project through "
     "the default make target. A \"make install\" target is also provided.";
