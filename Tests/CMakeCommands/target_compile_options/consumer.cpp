b05cf492527b98546bd72556dda2e836307f3dfa
Stephen Kelly <steveire@gmail.com>
2013-05-16 15:20:16 +0200

    Add target_compile_options command.
    This command populates the COMPILE_OPTIONS target property.

---
+++ Tests/CMakeCommands/target_compile_options/consumer.cpp
@@ -0,0 +1,14 @@
+
+#ifdef MY_PRIVATE_DEFINE
+#error Unexpected MY_PRIVATE_DEFINE
+#endif
+
+#ifndef MY_PUBLIC_DEFINE
+#error Expected MY_PUBLIC_DEFINE
+#endif
+
+#ifndef MY_INTERFACE_DEFINE
+#error Expected MY_INTERFACE_DEFINE
+#endif
+
+int main() { return 0; }
