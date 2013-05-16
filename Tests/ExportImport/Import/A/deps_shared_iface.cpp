a0886e8bba17d05b9ca503050bd01b8a7af060b9
Stephen Kelly <steveire@gmail.com>
2013-05-16 23:10:01 +0200

    Revert "Add EXPORT_NAME property."
    This reverts commit 1cda7beb05a4ff719b44533a8b46ba29f4e2bd19.

--- Tests/ExportImport/Import/A/deps_shared_iface.cpp
+++ Tests/ExportImport/Import/A/deps_shared_iface.cpp
@@ -22,7 +22,6 @@
 
 #ifdef TEST_SUBDIR_LIB
 #include "subdir.h"
-#include "renamed.h"
 #endif
 
 int main(int,char **)
@@ -32,12 +31,11 @@ int main(int,char **)
 
 #ifdef TEST_SUBDIR_LIB
   SubDirObject sdo;
-  Renamed ren;
 #endif
 
   return dep.foo() + req.foo()
 #ifdef TEST_SUBDIR_LIB
-                   + sdo.foo() + ren.foo()
+                   + sdo.foo()
 #endif
                               ;
 }
