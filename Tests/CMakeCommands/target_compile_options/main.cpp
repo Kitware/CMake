632912c3d7d8f59f8bd729d75976eaa18f3615d1
Stephen Kelly <steveire@gmail.com>
2013-05-28 18:29:23 +0200

    Only run the GNU compiler tests when using that compiler.

--- Tests/CMakeCommands/target_compile_options/main.cpp
+++ Tests/CMakeCommands/target_compile_options/main.cpp
@@ -1,14 +1,18 @@
 
-#ifndef MY_PRIVATE_DEFINE
-#error Expected MY_PRIVATE_DEFINE
-#endif
+#ifdef DO_GNU_TESTS
 
-#ifndef MY_PUBLIC_DEFINE
-#error Expected MY_PUBLIC_DEFINE
-#endif
+#  ifndef MY_PRIVATE_DEFINE
+#  error Expected MY_PRIVATE_DEFINE
+#  endif
 
-#ifdef MY_INTERFACE_DEFINE
-#error Unexpected MY_INTERFACE_DEFINE
+#  ifndef MY_PUBLIC_DEFINE
+#  error Expected MY_PUBLIC_DEFINE
+#  endif
+
+#  ifdef MY_INTERFACE_DEFINE
+#  error Unexpected MY_INTERFACE_DEFINE
+#  endif
+
 #endif
 
 int main() { return 0; }
