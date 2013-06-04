48c3e48a053dcc5c6eb2203713c7f0e0b2a9d797
Stephen Kelly <steveire@gmail.com>
2013-06-04 16:25:47 +0200

    Introduce the INTERFACE_LINK_LIBRARIES property.
    This property replaces the properties which
    match (IMPORTED_)?LINK_INTERFACE_LIBRARIES(_<CONFIG>)?, and is enabled
    for IMPORTED targets, and for non-IMPORTED targets only with a policy.
    
    For static libraries, the INTERFACE_LINK_LIBRARIES property is
    also used as the source of transitive usage requirements content.
    Static libraries still require users to link to all entries in
    their LINK_LIBRARIES, but usage requirements such as INCLUDE_DIRECTORIES
    COMPILE_DEFINITIONS and COMPILE_OPTIONS can be restricted to only
    certain interface libraries.
    
    Because the INTERFACE_LINK_LIBRARIES property is populated unconditionally,
    we need to compare the evaluated result of it with the link implementation
    to determine whether to issue the policy warning for static libraries. For
    shared libraries, the policy warning is issued if the contents of
    the INTERFACE_LINK_LIBRARIES property differs from the contents of the
    relevant config-specific old LINK_INTERFACE_LIBRARIES property.

--- Source/cmPolicies.cxx
+++ Source/cmPolicies.cxx
@@ -544,6 +544,28 @@ cmPolicies::cmPolicies()
     "The NEW behavior for this policy is to issue a FATAL_ERROR if "
     "INCLUDE_DIRECTORIES contains a relative path.",
     2,8,11,20130516, cmPolicies::WARN);
+
+  this->DefinePolicy(
+    CMP0022, "CMP0022",
+    "INTERFACE_LINK_LIBRARIES defines the link interface.",
+    "CMake 2.8.11 constructed the 'link interface' of a target from "
+    "properties matching (IMPORTED_)?LINK_INTERFACE_LIBRARIES(_<CONFIG>)?.  "
+    "The modern way to specify config-sensitive content is to use generator "
+    "expressions and the IMPORTED_ prefix makes uniform processing of the "
+    "link interface with generator expressions impossible.  The "
+    "INTERFACE_LINK_LIBRARIES target property was introduced as a "
+    "replacement in CMake 2.8.12. This new property is named consistently "
+    "with the INTERFACE_COMPILE_DEFINITIONS, INTERFACE_INCLUDE_DIRECTORIES "
+    "and INTERFACE_COMPILE_OPTIONS properties.  For in-build targets, CMake "
+    "will use the INTERFACE_LINK_LIBRARIES property as the source of the "
+    "link interface only if policy CMP0022 is NEW.  "
+    "\n"
+    "The OLD behavior for this policy is to ignore the "
+    "INTERFACE_LINK_LIBRARIES property for in-build targets.  "
+    "The NEW behavior for this policy is to use the INTERFACE_LINK_LIBRARIES "
+    "property for in-build targets, and ignore the old properties matching "
+    "(IMPORTED_)?LINK_INTERFACE_LIBRARIES(_<CONFIG>)?.",
+    2,8,11,20130516, cmPolicies::WARN);
 }
 
 cmPolicies::~cmPolicies()
