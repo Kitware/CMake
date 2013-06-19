6f2275f8ded53ac237bae39292b05afe9240796c
Stephen Kelly <steveire@gmail.com>
2013-06-19 09:58:33 +0200

    Revert "Add generator expressions for compiler versions."
    This reverts commit e5df6630d75ca6003897490426b2a6d2e61ed73c.

--- Tests/CompileOptions/main.cpp
+++ Tests/CompileOptions/main.cpp
@@ -5,15 +5,7 @@
 #  endif
 #endif
 
-#include <cstring>
-
 int main(int argc, char **argv)
 {
-#ifdef DO_GNU_TESTS
-  return strcmp(EXPECTED_C_COMPILER_VERSION, TEST_C_COMPILER_VERSION)
-      && strcmp(EXPECTED_CXX_COMPILER_VERSION, TEST_CXX_COMPILER_VERSION)
-      && TEST_C_COMPILER_VERSION_EQUALITY
-      && TEST_CXX_COMPILER_VERSION_EQUALITY;
-#endif
   return 0;
 }
