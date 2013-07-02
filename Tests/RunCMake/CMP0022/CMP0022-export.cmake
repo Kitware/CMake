0984e10212b514c248b4911b6ec530ec51f45dac
Stephen Kelly <steveire@gmail.com>
2013-07-02 16:06:08 +0200

    Rename test files to avoid VS6 compatibility issues.

--- Tests/RunCMake/CMP0022/CMP0022-export.cmake
+++ Tests/RunCMake/CMP0022/CMP0022-export.cmake
@@ -3,8 +3,8 @@ project(cmp0022NEW)
 
 cmake_policy(SET CMP0022 NEW)
 
-add_library(cmp0022NEW SHARED empty.cpp)
-add_library(testLib SHARED empty.cpp)
+add_library(cmp0022NEW SHARED empty_vs6_1.cpp)
+add_library(testLib SHARED empty_vs6_2.cpp)
 
 set_property(TARGET cmp0022NEW APPEND PROPERTY LINK_INTERFACE_LIBRARIES testLib)
 
