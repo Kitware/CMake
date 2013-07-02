d54d364ec9e2aa19225dbf7d8a5512c26d21f831
Stephen Kelly <steveire@gmail.com>
2013-07-02 15:58:52 +0200

    Rename test files to avoid VS6 compatibility issues.

--- Tests/RunCMake/CMP0022/CMP0022-WARN-static.cmake
+++ Tests/RunCMake/CMP0022/CMP0022-WARN-static.cmake
@@ -1,9 +1,9 @@
 
 project(CMP0022-WARN)
 
-add_library(foo STATIC empty.cpp)
-add_library(bar STATIC empty.cpp)
-add_library(bat STATIC empty.cpp)
+add_library(foo STATIC empty_vs6_1.cpp)
+add_library(bar STATIC empty_vs6_2.cpp)
+add_library(bat STATIC empty_vs6_3.cpp)
 set_property(TARGET bar PROPERTY INTERFACE_LINK_LIBRARIES foo)
 set_property(TARGET bar PROPERTY LINK_LIBRARIES bat)
 
--- Tests/RunCMake/CMP0022/CMP0022-WARN.cmake
+++ Tests/RunCMake/CMP0022/CMP0022-WARN.cmake
@@ -1,9 +1,9 @@
 
 project(CMP0022-WARN)
 
-add_library(foo SHARED empty.cpp)
-add_library(bar SHARED empty.cpp)
-add_library(bat SHARED empty.cpp)
+add_library(foo SHARED empty_vs6_1.cpp)
+add_library(bar SHARED empty_vs6_2.cpp)
+add_library(bat SHARED empty_vs6_3.cpp)
 set_property(TARGET bar PROPERTY INTERFACE_LINK_LIBRARIES foo)
 set_property(TARGET bar PROPERTY LINK_INTERFACE_LIBRARIES bat)
 
