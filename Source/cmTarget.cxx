16af722cf42fcd91f9d0bb48eeb3035a49dad809
Stephen Kelly <steveire@gmail.com>
2013-07-11 17:28:33 +0200

    Revert "Use linked frameworks as a source of include directories."
    This reverts commit 4fb8ace00d835a81d9279818c62b02eaa2cf59b2.

--- Source/cmTarget.cxx
+++ Source/cmTarget.cxx
@@ -3206,29 +3206,6 @@ std::vector<std::string> cmTarget::GetIncludeDirectories(const char *config)
                         new cmTargetInternals::TargetPropertyEntry(cge,
                                                               it->Value));
       }
-
-    if(this->Makefile->IsOn("APPLE"))
-      {
-      LinkImplementation const* impl = this->GetLinkImplementation(config,
-                                                                   this);
-      for(std::vector<std::string>::const_iterator
-          it = impl->Libraries.begin();
-          it != impl->Libraries.end(); ++it)
-        {
-        std::string libDir = *it;
-        libDir += "/../";
-        libDir = cmSystemTools::CollapseFullPath(libDir.c_str());
-        if(cmSystemTools::IsPathToFramework(libDir.c_str()))
-          {
-          cmGeneratorExpression ge(lfbt);
-          cmsys::auto_ptr<cmCompiledGeneratorExpression> cge =
-                                                            ge.Parse(libDir);
-          this->Internal
-                  ->CachedLinkInterfaceIncludeDirectoriesEntries[configString]
-                  .push_back(new cmTargetInternals::TargetPropertyEntry(cge));
-          }
-        }
-      }
     }
 
   processIncludeDirectories(this,
