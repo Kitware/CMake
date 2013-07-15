2ea0d613b5428154c6fe446e4e2cde1b3983827a
Brad King <brad.king@kitware.com>
2013-07-15 09:36:43 -0400

    bootstrap: Skip VS_UNICODE_OUTPUT handling
    We do not need this during MSys bootstrap anyway.

--- Source/cmSystemTools.cxx
+++ Source/cmSystemTools.cxx
@@ -1706,11 +1706,13 @@ void cmSystemTools::EnableVSConsoleOutput()
   // output and allow it to be captured on the fly.
   cmSystemTools::PutEnv("vsconsoleoutput=1");
 
+# ifdef CMAKE_BUILD_WITH_CMAKE
   // VS sets an environment variable to tell MS tools like "cl" to report
   // output through a backdoor pipe instead of stdout/stderr.  Unset the
   // environment variable to close this backdoor for any path of process
   // invocations that passes through CMake so we can capture the output.
   cmSystemTools::UnsetEnv("VS_UNICODE_OUTPUT");
+# endif
 #endif
 }
 
