2cc9f1fd5cece37efe00dde8a70a632eeb113112
Brad King <brad.king@kitware.com>
2013-07-16 09:21:35 -0400

    OS X: Add copyright notices to Darwin-*-Fortran.cmake

--- Modules/Platform/Darwin-NAG-Fortran.cmake
+++ Modules/Platform/Darwin-NAG-Fortran.cmake
@@ -1,3 +1,16 @@
+#=============================================================================
+# Copyright 2010 Kitware, Inc.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
 set(CMAKE_Fortran_VERBOSE_FLAG "-Wl,-v") # Runs gcc under the hood.
 
 # Need -fpp explicitly on case-insensitive filesystem.
