4b7dcfb9745644711709ed41daebc5626375b61b
Stephen Kelly <steveire@gmail.com>
2013-07-19 08:59:46 +0200

    Handle NoConfig case.

--- Tests/GeneratorExpression/check-part3.cmake
+++ Tests/GeneratorExpression/check-part3.cmake
@@ -9,7 +9,7 @@ check(test_version_equal_1 "0")
 check(test_version_equal_2 "1")
 
 foreach(c debug release relwithdebinfo minsizerel)
-  if(config)
+  if(config AND NOT config STREQUAL NoConfig)
     if(NOT "${test_imported_${c}}" MATCHES "^;/imported2/include$"
         AND NOT "${test_imported_${c}}" MATCHES "^/imported1/include;$")
       message(SEND_ERROR "test_imported_${c} is not correct: ${test_imported_${c}}")
