c0756b6dd67b7f2e113dafde398cec597537068c
Rolf Eike Beer <eike@sf-mail.de>
2013-07-27 21:40:25 +0200

    Revert "CTest: fix pre and post test commands with spaces" (#13887)
    This reverts commit 6187876dea89618044e200808bcae75a18bd4043.
    
    It was actually possible before to have paths with spaces in them, the spaces
    just need to be quoted. This way spaces will work as argument separators.

--- Source/CTest/cmCTestTestHandler.cxx
+++ Source/CTest/cmCTestTestHandler.cxx
@@ -1302,10 +1302,9 @@ int cmCTestTestHandler::ExecuteCommands(std::vector<cmStdString>& vec)
   for ( it = vec.begin(); it != vec.end(); ++it )
     {
     int retVal = 0;
-    std::string cmd = cmSystemTools::ConvertToOutputPath(it->c_str());
-    cmCTestLog(this->CTest, HANDLER_VERBOSE_OUTPUT, "Run command: " << cmd
+    cmCTestLog(this->CTest, HANDLER_VERBOSE_OUTPUT, "Run command: " << *it
       << std::endl);
-    if ( !cmSystemTools::RunSingleCommand(cmd.c_str(), 0, &retVal, 0,
+    if ( !cmSystemTools::RunSingleCommand(it->c_str(), 0, &retVal, 0,
                                           cmSystemTools::OUTPUT_MERGE
         /*this->Verbose*/) || retVal != 0 )
       {
