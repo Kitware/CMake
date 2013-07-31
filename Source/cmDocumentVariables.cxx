Kitware/CMake

commit f10e64804e13c7e5505ee2a2da5b4384f8616d7a

Docs: Document existing target property debugging options.

Source/cmDocumentVariables.cxx
@@ -963,9 +963,11 @@ void cmDocumentVariables::DefineVariables(cmake* cm)
      "Enables tracing output for target properties.",
      "This variable can be populated with a list of properties to generate "
      "debug output for when evaluating target properties.  Currently it can "
-     "only be used when evaluating the INCLUDE_DIRECTORIES target property.  "
-     "In that case, it outputs a backtrace for each include directory in "
-     "the build.  Default is unset.",false,"Variables That Change Behavior");
+     "only be used when evaluating the INCLUDE_DIRECTORIES, "
+     "COMPILE_DEFINITIONS and COMPILE_OPTIONS target properties.  "
+     "In that case, it outputs a backtrace for each entry in the target "
+     "propertythe build.  Default is unset.",
+     false,"Variables That Change Behavior");
 
   // Variables defined by CMake that describe the system
 

commit 742994125417b30443e8e58a72a2839c31d97c23

Docs: Fix typo in CMAKE_DEBUG_TARGET_PROPERTIES
Introduced in commit f10e6480 (Docs: Document existing target
property debugging options., 2013-07-26)

Source/cmDocumentVariables.cxx
@@ -966,8 +966,7 @@ void cmDocumentVariables::DefineVariables(cmake* cm)
      "only be used when evaluating the INCLUDE_DIRECTORIES, "
      "COMPILE_DEFINITIONS and COMPILE_OPTIONS target properties.  "
      "In that case, it outputs a backtrace for each entry in the target "
-     "propertythe build.  Default is unset.",
-     false,"Variables That Change Behavior");
+     "property.  Default is unset.", false, "Variables That Change Behavior");
 
   // Variables defined by CMake that describe the system
 