7ca5958820b7741b251081af66e1cb67af243fd5
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-07-31 17:40:07 +0200

    FindGTK2: Detect gmodule library

--- Modules/FindGTK2.cmake
+++ Modules/FindGTK2.cmake
@@ -77,6 +77,7 @@
 #     appropriate. A new set of _RELEASE variables was also added.
 #   * Remove GTK2_SKIP_MARK_AS_ADVANCED option, as now the variables are
 #     marked as advanced by SelectLibraryConfigurations
+#   * Detect gmodule library
 # Version 1.4 (10/4/2012) (CMake 2.8.10)
 #   * 12596: Missing paths for FindGTK2 on NetBSD
 #   * 12049: Fixed detection of GTK include files in the lib folder on
@@ -481,6 +482,8 @@ foreach(_GTK2_component ${GTK2_FIND_COMPONENTS})
         _GTK2_FIND_LIBRARY    (GTK2_GDK_PIXBUF gdk_pixbuf false true)
 
         _GTK2_FIND_LIBRARY    (GTK2_GTHREAD gthread false true)
+
+        _GTK2_FIND_LIBRARY    (GTK2_GMODULE gmodule false true)
 
         _GTK2_FIND_LIBRARY    (GTK2_GIO gio false true)
 
