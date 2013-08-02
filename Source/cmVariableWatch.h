0d6acb1df8c20bb21f4d328cf2c35d0cbb6d7ea3
Ben Boeckel <mathstuf@gmail.com>
2013-08-02 15:44:15 -0400

    variable_watch: Add a deleter for the client data
    The client data is arbitrary and the callback may be called an
    unspecified number of times, so the cmVariableWatch must be the one to
    delete the client data in the end (if it is needed at all).

--- Source/cmVariableWatch.h
+++ Source/cmVariableWatch.h
@@ -26,6 +26,7 @@ class cmVariableWatch
 public:
   typedef void (*WatchMethod)(const std::string& variable, int access_type,
     void* client_data, const char* newValue, const cmMakefile* mf);
+  typedef void (*DeleteData)(void* client_data);
 
   cmVariableWatch();
   ~cmVariableWatch();
@@ -34,7 +35,7 @@ public:
    * Add watch to the variable
    */
   void AddWatch(const std::string& variable, WatchMethod method,
-                void* client_data=0);
+                void* client_data=0, DeleteData delete_data=0);
   void RemoveWatch(const std::string& variable, WatchMethod method);
 
   /**
@@ -67,7 +68,15 @@ protected:
   {
     WatchMethod Method;
     void*        ClientData;
-    Pair() : Method(0), ClientData(0) {}
+    DeleteData   DeleteDataCall;
+    Pair() : Method(0), ClientData(0), DeleteDataCall(0) {}
+    ~Pair()
+      {
+      if (this->DeleteDataCall && this->ClientData)
+        {
+        this->DeleteDataCall(this->ClientData);
+        }
+      }
   };
 
   typedef std::vector< Pair* > VectorOfPairs;
