e9f46dfeb27109fa555b75e5c308c6782cc2855d
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-08-14 10:48:44 +0200

    FindGTK2: Add config directories only if different from include ones

--- Modules/FindGTK2.cmake
+++ Modules/FindGTK2.cmake
@@ -414,7 +414,7 @@ function(_GTK2_ADJUST_LIB_VARS _var)
             set_property(TARGET GTK2::${_basename} APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${GTK2_${_var}_INCLUDE_DIR}")
         endif()
 
-        if(GTK2_${_var}CONFIG_INCLUDE_DIR)
+        if(GTK2_${_var}CONFIG_INCLUDE_DIR AND NOT "${GTK2_${_var}CONFIG_INCLUDE_DIR}" STREQUAL "GTK2_${_var}_INCLUDE_DIR")
             set_property(TARGET GTK2::${_basename} APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${GTK2_${_var}CONFIG_INCLUDE_DIR}")
         endif()
 
