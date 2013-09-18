2b63734b7f078281540adaaf75b5a25ed897106d
Alex Neundorf <neundorf@kde.org>
2013-09-19 00:53:38 +0200

    Eclipse: print different message if CMAKE_ECLIPSE_VERSION is already set
    This fixes #14422.
    
    Alex

--- Modules/CMakeFindEclipseCDT4.cmake
+++ Modules/CMakeFindEclipseCDT4.cmake
@@ -19,18 +19,6 @@ find_program(CMAKE_ECLIPSE_EXECUTABLE NAMES eclipse DOC "The Eclipse executable"
 
 function(_FIND_ECLIPSE_VERSION)
   # This code is in a function so the variables used here have only local scope
-  if(CMAKE_ECLIPSE_EXECUTABLE)
-    # use REALPATH to resolve symlinks (http://public.kitware.com/Bug/view.php?id=13036)
-    get_filename_component(_REALPATH_CMAKE_ECLIPSE_EXECUTABLE "${CMAKE_ECLIPSE_EXECUTABLE}" REALPATH)
-    get_filename_component(_ECLIPSE_DIR "${_REALPATH_CMAKE_ECLIPSE_EXECUTABLE}" PATH)
-    file(GLOB _ECLIPSE_FEATURE_DIR "${_ECLIPSE_DIR}/features/org.eclipse.platform*")
-    if(APPLE AND NOT _ECLIPSE_FEATURE_DIR)
-      file(GLOB _ECLIPSE_FEATURE_DIR "${_ECLIPSE_DIR}/../../../features/org.eclipse.platform*")
-    endif()
-    if("${_ECLIPSE_FEATURE_DIR}" MATCHES ".+org.eclipse.platform_([0-9]+\\.[0-9]+).+")
-      set(_ECLIPSE_VERSION ${CMAKE_MATCH_1})
-    endif()
-  endif()
 
   # Set up a map with the names of the Eclipse releases:
   set(_ECLIPSE_VERSION_NAME_    "Unknown" )
@@ -42,14 +30,32 @@ function(_FIND_ECLIPSE_VERSION)
   set(_ECLIPSE_VERSION_NAME_3.7 "Indigo" )
   set(_ECLIPSE_VERSION_NAME_4.2 "Juno" )
 
-  if(_ECLIPSE_VERSION)
-    message(STATUS "Found Eclipse version ${_ECLIPSE_VERSION} (${_ECLIPSE_VERSION_NAME_${_ECLIPSE_VERSION}})")
+  if(NOT DEFINED CMAKE_ECLIPSE_VERSION)
+    if(CMAKE_ECLIPSE_EXECUTABLE)
+      # use REALPATH to resolve symlinks (http://public.kitware.com/Bug/view.php?id=13036)
+      get_filename_component(_REALPATH_CMAKE_ECLIPSE_EXECUTABLE "${CMAKE_ECLIPSE_EXECUTABLE}" REALPATH)
+      get_filename_component(_ECLIPSE_DIR "${_REALPATH_CMAKE_ECLIPSE_EXECUTABLE}" PATH)
+      file(GLOB _ECLIPSE_FEATURE_DIR "${_ECLIPSE_DIR}/features/org.eclipse.platform*")
+      if(APPLE AND NOT _ECLIPSE_FEATURE_DIR)
+        file(GLOB _ECLIPSE_FEATURE_DIR "${_ECLIPSE_DIR}/../../../features/org.eclipse.platform*")
+      endif()
+      if("${_ECLIPSE_FEATURE_DIR}" MATCHES ".+org.eclipse.platform_([0-9]+\\.[0-9]+).+")
+        set(_ECLIPSE_VERSION ${CMAKE_MATCH_1})
+      endif()
+    endif()
+
+    if(_ECLIPSE_VERSION)
+      message(STATUS "Found Eclipse version ${_ECLIPSE_VERSION} (${_ECLIPSE_VERSION_NAME_${_ECLIPSE_VERSION}})")
+    else()
+      set(_ECLIPSE_VERSION "3.6" )
+      message(STATUS "Could not determine Eclipse version, assuming at least ${_ECLIPSE_VERSION} (${_ECLIPSE_VERSION_NAME_${_ECLIPSE_VERSION}}). Adjust CMAKE_ECLIPSE_VERSION if this is wrong.")
+    endif()
+
+    set(CMAKE_ECLIPSE_VERSION "${_ECLIPSE_VERSION} (${_ECLIPSE_VERSION_NAME_${_ECLIPSE_VERSION}})" CACHE STRING "The version of Eclipse. If Eclipse has not been found, 3.6 (Helios) is assumed.")
   else()
-    set(_ECLIPSE_VERSION "3.6" )
-    message(STATUS "Could not determine Eclipse version, assuming at least ${_ECLIPSE_VERSION} (${_ECLIPSE_VERSION_NAME_${_ECLIPSE_VERSION}}). Adjust CMAKE_ECLIPSE_VERSION if this is wrong.")
+    message(STATUS "Eclipse version is set to ${CMAKE_ECLIPSE_VERSION}. Adjust CMAKE_ECLIPSE_VERSION if this is wrong.")
   endif()
 
-  set(CMAKE_ECLIPSE_VERSION "${_ECLIPSE_VERSION} (${_ECLIPSE_VERSION_NAME_${_ECLIPSE_VERSION}})" CACHE STRING "The version of Eclipse. If Eclipse has not been found, 3.6 (Helios) is assumed.")
   set_property(CACHE CMAKE_ECLIPSE_VERSION PROPERTY STRINGS "3.2 (${_ECLIPSE_VERSION_NAME_3.2})"
                                                             "3.3 (${_ECLIPSE_VERSION_NAME_3.3})"
                                                             "3.4 (${_ECLIPSE_VERSION_NAME_3.4})"
