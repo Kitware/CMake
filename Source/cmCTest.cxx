54ef2bea379ca47d961c1bb0fd0ccb026b482810
Adrien Destugues <pulkomandy@pulkomandy.tk>
2013-10-05 16:59:25 +0200

    Haiku: Include files cleanup in cmCTest
    * No need to use a different path from the BeOS one, which still works.
    
    Applied-by: Rolf Eike Beer <eike@sf-mail.de>

--- Source/cmCTest.cxx
+++ Source/cmCTest.cxx
@@ -53,12 +53,8 @@
 #include <cm_zlib.h>
 #include <cmsys/Base64.h>
 
-#if defined(__BEOS__)
+#if defined(__BEOS__) || defined(__HAIKU__)
 #include <be/kernel/OS.h>   /* disable_debugger() API. */
-#endif
-
-#if defined(__HAIKU__)
-#include <os/kernel/OS.h>   /* disable_debugger() API. */
 #endif
 
 
