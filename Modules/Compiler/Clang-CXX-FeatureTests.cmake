9355c7d3d7be9d059c1ce66172d373e4d1c793c0
Stephen Kelly <steveire@gmail.com>
2013-10-17 11:47:29 +0200

    Record the compilers capable of the cxx_delegating_constructors feature.
    Add a feature test using the compiler macros and the preprocessor to
    determine available features. Add a unit test which outputs the
    feature test file compiled. Record the list of features in
    the SystemInformation unit test.
    
    Add a CMAKE_CXX_COMPILE_FEATURES variable which contains all features
    known to the loaded compiler, and a CMAKE_CXX_KNOWN_FEATURES variable
    containing all features known to CMake. Add language standard specific
    variables for internal use to determine the standard-specific compile
    flags to use. Add internal variables matching CMAKE_PP_NAME_<feature>
    for each feature. Those variables are used as preprocessor defines.

---
+++ Modules/Compiler/Clang-CXX-FeatureTests.cmake
@@ -0,0 +1,9 @@
+
+set(testable_features
+  cxx_delegating_constructors
+)
+foreach(feature ${testable_features})
+  set(_cmake_feature_test_${feature} "__has_extension(${feature})")
+endforeach()
+
+unset(testable_features)
