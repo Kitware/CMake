51f394244e6408ea37e7e83b2dcfc32aaf2b4448
Stephen Kelly <steveire@gmail.com>
2013-10-17 11:47:29 +0200

    Record the compilers capable of the cxx_delegating_constructors feature.
    Add a feature test using the compiler macros and the preprocessor to
    determine available features. Add a unit test which outputs the
    feature test file compiled. Record the list of features in
    the SystemInformation unit test.
    
    Add a CMAKE_CXX_COMPILE_FEATURES variable which contains all features
    known to the loaded compiler, and a CMAKE_CXX_KNOWN_FEATURES variable
    containing all features known to CMake. Add language standard specific
    variables for internal use to determine the standard-specific compile
    flags to use. Add internal variables matching CMAKE_PP_NAME_<feature>
    for each feature. Those variables are used as preprocessor defines.

---
+++ Modules/CMakeDetermineCompileFeatures.cmake
@@ -0,0 +1,43 @@
+
+#=============================================================================
+# Copyright 2013 Stephen Kelly <steveire@gmail.com>
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+function(cmake_determine_compile_features lang)
+
+  if(lang STREQUAL CXX AND COMMAND cmake_record_cxx_compile_features)
+    message(STATUS "Detecting ${lang} compile features")
+
+    set(CMAKE_CXX11_COMPILE_FEATURES)
+
+    include("${CMAKE_ROOT}/Modules/Internal/FeatureTesting.cmake")
+
+    cmake_record_cxx_compile_features()
+
+    if(NOT _result EQUAL 0)
+      message(STATUS "Detecting ${lang} compile features - failed")
+      return()
+    endif()
+
+    if(NOT CMAKE_CXX_COMPILE_FEATURES)
+      set(CMAKE_CXX_COMPILE_FEATURES
+        ${CMAKE_CXX11_COMPILE_FEATURES}
+      )
+    endif()
+
+    set(CMAKE_CXX_COMPILE_FEATURES ${CMAKE_CXX_COMPILE_FEATURES} PARENT_SCOPE)
+    set(CMAKE_CXX11_COMPILE_FEATURES ${CMAKE_CXX11_COMPILE_FEATURES} PARENT_SCOPE)
+
+    message(STATUS "Detecting ${lang} compile features - done")
+  endif()
+
+endfunction()
