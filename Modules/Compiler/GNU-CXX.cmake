f1a88194cc380989e778a7e52daf85fa4d208760
Stephen Kelly <steveire@gmail.com>
2013-10-17 11:47:29 +0200

    Record the compilers capable of the cxx_delegating_constructors feature.

--- Modules/Compiler/GNU-CXX.cmake
+++ Modules/Compiler/GNU-CXX.cmake
@@ -11,6 +11,14 @@ else()
   endif()
 endif()
 
+set(CMAKE_CXX11_COMPILER_FEATURES)
+
+include("${CMAKE_ROOT}/Modules/Internal/FeatureTesting.cmake")
+
+macro(_get_gcc_features std_version list)
+  record_compiler_features(CXX "-std=${std_version}" ${list})
+endmacro()
+
 # Appropriate version?
 if(NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.3)
   set(CMAKE_CXX98_STANDARD_COMPILE_OPTION "-std=c++98")
@@ -20,7 +28,15 @@ endif()
 if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.7)
   set(CMAKE_CXX11_STANDARD_COMPILE_OPTION "-std=c++11")
   set(CMAKE_CXX11_EXTENSION_COMPILE_OPTION "-std=gnu++11")
+
+  _get_gcc_features(c++11 CMAKE_CXX11_COMPILER_FEATURES)
 elseif(NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.3)
   set(CMAKE_CXX11_STANDARD_COMPILE_OPTION "-std=c++0x")
   set(CMAKE_CXX11_EXTENSION_COMPILE_OPTION "-std=gnu++0x")
+
+  _get_gcc_features(c++0x CMAKE_CXX11_COMPILER_FEATURES)
 endif()
+
+set(CMAKE_CXX_COMPILER_FEATURES
+  ${CMAKE_CXX11_COMPILER_FEATURES}
+)
