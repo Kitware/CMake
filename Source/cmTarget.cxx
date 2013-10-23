dd6e36a0e2ef78e32f6e13dabf2d3d50c76c2aa2
Stephen Kelly <steveire@gmail.com>
2013-10-23 15:03:01 +0200

    Remove unused variable.

--- Source/cmTarget.cxx
+++ Source/cmTarget.cxx
@@ -4482,7 +4482,6 @@ const char * consistentNumberProperty(const char *lhs, const char *rhs,
 {
   double lnum;
   double rnum;
-  bool result;
   if(sscanf(lhs, "%lg", &lnum) != 1 ||
       sscanf(rhs, "%lg", &rnum) != 1)
     {
