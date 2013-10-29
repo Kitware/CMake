144068e270a5e99fb6c3e5dbea0eb94d0bd8fd79
Brad King <brad.king@kitware.com>
2013-10-29 09:55:43 -0400

    Add to RunCMake.Syntax test comments not separated by spaces
    Extend the BracketComment5 test case to cover both bracket comments and
    line comments starting immediately after a bracket comment closes.

--- Tests/RunCMake/Syntax/BracketComment5.cmake
+++ Tests/RunCMake/Syntax/BracketComment5.cmake
@@ -2,5 +2,10 @@
 message(FATAL_ERROR "This is commented out.")
 #]] #[[
 message(FATAL_ERROR "This is commented out.")
+#]]#[[
+message(FATAL_ERROR "This is commented out.")
 #]] #message(FATAL_ERROR "This is commented out.")
+#[[
+message(FATAL_ERROR "This is commented out.")
+#]]#message(FATAL_ERROR "This is commented out.")
 message(STATUS "The above FATAL_ERROR did not occur.")
