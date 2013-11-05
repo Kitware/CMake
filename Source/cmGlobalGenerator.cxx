7b29f18472faea8300b92463cc4f046f71d475e3
Peter Kümmel <syntheticpp@gmx.net>
2013-11-05 10:34:23 +0100

    Revert "Ninja: run cmake-gui for target edit_cache"
    This reverts commit 4dc41c1865c74bd0bc191ab07488c5fac479802c.

--- Source/cmGlobalGenerator.cxx
+++ Source/cmGlobalGenerator.cxx
@@ -2152,7 +2152,7 @@ void cmGlobalGenerator::CreateDefaultGlobalTargets(cmTargets* targets)
       cpackCommandLines.push_back(singleLine);
       (*targets)[editCacheTargetName] =
         this->CreateGlobalTarget(
-          editCacheTargetName, "Running cmake-gui ...",
+          editCacheTargetName, "Running CMake cache editor...",
           &cpackCommandLines, depends, 0);
       }
     else
