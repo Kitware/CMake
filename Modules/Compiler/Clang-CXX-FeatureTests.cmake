872ae8e96e3dd490f75203d23e5912b93baaebe9
Stephen Kelly <steveire@gmail.com>
2013-11-09 09:18:32 +0100

    Record compilers capable of the gnuxx_typeof feature.

--- Modules/Compiler/Clang-CXX-FeatureTests.cmake
+++ Modules/Compiler/Clang-CXX-FeatureTests.cmake
@@ -5,3 +5,5 @@ set(testable_features
 foreach(feature ${testable_features})
   set(_cmake_feature_test_${feature} "__has_extension(${feature})")
 endforeach()
+
+set(_cmake_feature_test_gnuxx_typeof "!defined(__STRICT_ANSI__)")
--- Source/cmMakefile.cxx
+++ Source/cmMakefile.cxx
@@ -42,7 +42,8 @@
 #include <assert.h>
 
 #define FOR_EACH_CXX_FEATURE(F) \
-  F(cxx_delegating_constructors)
+  F(cxx_delegating_constructors) \
+  F(gnuxx_typeof)
 
 class cmMakefile::Internals
 {
