74742c504e92febbd36923686ddf36fc08477262
Stephen Kelly <steveire@gmail.com>
2013-11-09 14:37:25 +0100

    Update unit test.

--- Tests/RunCMake/target_compile_features/RunCMakeTest.cmake
+++ Tests/RunCMake/target_compile_features/RunCMakeTest.cmake
@@ -8,4 +8,9 @@ run_cmake(invalid_args_on_interface)
 run_cmake(imported_target)
 run_cmake(no_target)
 run_cmake(not_a_cxx_feature)
-run_cmake(no_matching_cxx_feature)
+
+string(REPLACE "-" ";" CMAKE_CXX_COMPILE_FEATURES "${CMAKE_CXX_COMPILE_FEATURES}")
+if (";${CMAKE_CXX_COMPILE_FEATURES};" MATCHES ";gnuxx_typeof;"
+    OR ";${CMAKE_CXX_COMPILE_FEATURES};" MATCHES ";msvcxx_sealed;" )
+  run_cmake(no_matching_cxx_feature)
+endif()
