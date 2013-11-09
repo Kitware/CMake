a868d813aa4537262260a43f79ee004aeb267575
Stephen Kelly <steveire@gmail.com>
2013-11-09 09:18:32 +0100

    Record compilers capable of the gnuxx_typeof feature.

--- Modules/Compiler/Clang-CXX-FeatureTests.cmake
+++ Modules/Compiler/Clang-CXX-FeatureTests.cmake
@@ -1,4 +1,9 @@
 
+if(CMAKE_COMPILER_VERSION VERSION_LESS 2.1)
+  # This file is only loaded if Clang >= 2.1
+  message(FATAL_ERROR "This file should not be included for Clang < 2.1.")
+endif()
+
 set(testable_features
   cxx_delegating_constructors
 )
@@ -7,3 +12,5 @@ foreach(feature ${testable_features})
 endforeach()
 
 unset(testable_features)
+
+set(_cmake_feature_test_gnuxx_typeof "!defined(__STRICT_ANSI__)")
