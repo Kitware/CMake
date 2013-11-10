5eba3c33282e675c7bead0604e9580c041997d05
Stephen Kelly <steveire@gmail.com>
2013-11-10 09:01:32 +0100

    Fix the recording of MSVC features.

--- Modules/Platform/Windows-MSVC-CXX.cmake
+++ Modules/Platform/Windows-MSVC-CXX.cmake
@@ -5,4 +5,6 @@ if(NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 18.0)
 endif()
 __windows_compiler_msvc(CXX)
 
-set(_cmake_feature_test_msvcxx_sealed "_MSC_VER >= 1400")
+include("${CMAKE_ROOT}/Modules/Internal/FeatureTesting.cmake")
+
+record_compiler_features(CXX "" ${CMAKE_CXX_COMPILE_FEATURES})
