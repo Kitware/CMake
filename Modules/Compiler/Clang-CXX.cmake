59b5fdd37c87dd27205e3756d17a186e02e0d326
Stephen Kelly <steveire@gmail.com>
2013-11-11 20:35:52 +0100

    Don't load Clang-CXX from AppleClang-CXX.
    The Clang-CXX module is going to get version-specific checks, and
    the version system for AppleClang is not the same as Clang.

--- Modules/Compiler/Clang-CXX.cmake
+++ Modules/Compiler/Clang-CXX.cmake
@@ -4,3 +4,8 @@ __compiler_clang(CXX)
 if(NOT CMAKE_CXX_SIMULATE_ID STREQUAL "MSVC")
   set(CMAKE_CXX_COMPILE_OPTIONS_VISIBILITY_INLINES_HIDDEN "-fvisibility-inlines-hidden")
 endif()
+
+cmake_policy(GET CMP0025 appleClangPolicy)
+if(NOT appleClangPolicy STREQUAL NEW)
+  return()
+endif()
