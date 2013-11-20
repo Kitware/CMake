2cd559789b1315d85ae1221941b5a4359e36d552
Arnaud Desmier <arnaud.desmier@free.fr>
2013-11-20 16:43:24 +0100

    FindwxWidgets: Add support for wxWidgets 3.0.0 (#14587)
    Add the "wxWidgets-3.0.0" path suffix and the "30" library names.
    While at it, add the "wxWidgets-2.9.5" suffix too.

--- Modules/FindwxWidgets.cmake
+++ Modules/FindwxWidgets.cmake
@@ -303,6 +303,7 @@ if(wxWidgets_FIND_STYLE STREQUAL "win32")
     # Find wxWidgets multilib base libraries.
     find_library(WX_base${_DBG}
       NAMES
+      wxbase30${_UCD}${_DBG}
       wxbase29${_UCD}${_DBG}
       wxbase28${_UCD}${_DBG}
       wxbase27${_UCD}${_DBG}
@@ -315,6 +316,7 @@ if(wxWidgets_FIND_STYLE STREQUAL "win32")
     foreach(LIB net odbc xml)
       find_library(WX_${LIB}${_DBG}
         NAMES
+        wxbase30${_UCD}${_DBG}_${LIB}
         wxbase29${_UCD}${_DBG}_${LIB}
         wxbase28${_UCD}${_DBG}_${LIB}
         wxbase27${_UCD}${_DBG}_${LIB}
@@ -329,6 +331,7 @@ if(wxWidgets_FIND_STYLE STREQUAL "win32")
     # Find wxWidgets monolithic library.
     find_library(WX_mono${_DBG}
       NAMES
+      wxmsw${_UNV}30${_UCD}${_DBG}
       wxmsw${_UNV}29${_UCD}${_DBG}
       wxmsw${_UNV}28${_UCD}${_DBG}
       wxmsw${_UNV}27${_UCD}${_DBG}
@@ -344,6 +347,7 @@ if(wxWidgets_FIND_STYLE STREQUAL "win32")
                 stc ribbon propgrid webview)
       find_library(WX_${LIB}${_DBG}
         NAMES
+        wxmsw${_UNV}30${_UCD}${_DBG}_${LIB}
         wxmsw${_UNV}29${_UCD}${_DBG}_${LIB}
         wxmsw${_UNV}28${_UCD}${_DBG}_${LIB}
         wxmsw${_UNV}27${_UCD}${_DBG}_${LIB}
@@ -457,6 +461,8 @@ if(wxWidgets_FIND_STYLE STREQUAL "win32")
       D:/
       ENV ProgramFiles
     PATH_SUFFIXES
+      wxWidgets-3.0.0
+      wxWidgets-2.9.5
       wxWidgets-2.9.4
       wxWidgets-2.9.3
       wxWidgets-2.9.2
