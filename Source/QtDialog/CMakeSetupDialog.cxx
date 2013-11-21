b56698737463b2cfcc282c2ef412e89fb28d2c9e
Clinton Stimpson <clinton@elemtech.com>
2013-11-20 21:24:54 -0700

    cmake-gui: Configure again if a variable is changed before Generate is pushed.

--- Source/QtDialog/CMakeSetupDialog.cxx
+++ Source/QtDialog/CMakeSetupDialog.cxx
@@ -946,6 +946,7 @@ void CMakeSetupDialog::saveBuildPaths(const QStringList& paths)
 void CMakeSetupDialog::setCacheModified()
 {
   this->CacheModified = true;
+  this->ConfigureNeeded = true;
   this->enterState(ReadyConfigure);
 }
 
