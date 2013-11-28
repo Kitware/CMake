f2c14bd14ff399df4a3374eb210bd74649cd1e5f
Stephen Kelly <steveire@gmail.com>
2013-11-28 13:21:25 +0100

    Remove version checks.
    These files can't have version checks as they can be read by a
    CMake which has a different compiler loaded.

--- Modules/Compiler/Clang-CXX-FeatureTests.cmake
+++ Modules/Compiler/Clang-CXX-FeatureTests.cmake
@@ -1,9 +1,4 @@
 
-if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 2.1)
-  # This file is only loaded if Clang >= 2.1
-  message(FATAL_ERROR "This file should not be included for Clang < 2.1.")
-endif()
-
 set(testable_features
   cxx_delegating_constructors
 )
--- Modules/Compiler/GNU-CXX-FeatureTests.cmake
+++ Modules/Compiler/GNU-CXX-FeatureTests.cmake
@@ -1,11 +1,6 @@
 
 set(_cmake_feature_test_gnuxx_typeof "!defined(__STRICT_ANSI__)")
 
-if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.3)
-  # This file is only loaded if GCC >= 4.3
-  message(FATAL_ERROR "This file should not be included for GCC < 4.3.")
-endif()
-
 # For GCC 4.7, we can test the standard __cplusplus macro, ...
 # set(isCxx98Mode "__cplusplus >= 199711L")
 # set(isCxx11Mode "__cplusplus >= 201103L")
