fa99c950a6436b4500c3e83aa55a08c2dd257cb3
Stephen Kelly <steveire@gmail.com>
2013-11-29 09:40:09 +0100

    Clang always supports binary literals.

--- Modules/Compiler/Clang-CXX-FeatureTests.cmake
+++ Modules/Compiler/Clang-CXX-FeatureTests.cmake
@@ -4,7 +4,6 @@ set(_cmake_compiler_test_macro __clang__)
 set(testable_features
   cxx_delegating_constructors
   cxx_variadic_templates
-  cxx_binary_literals
 )
 foreach(feature ${testable_features})
   set(_cmake_feature_test_${feature} "__has_extension(${feature})")
@@ -13,3 +12,4 @@ endforeach()
 unset(testable_features)
 
 set(_cmake_feature_test_gnuxx_typeof "!defined(__STRICT_ANSI__)")
+set(_cmake_feature_test_cxx_binary_literals "1")
