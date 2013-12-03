591b4aa3bdbfa8ce8223b8b1e7b3b3c64199269b
Brad King <brad.king@kitware.com>
2013-12-03 09:39:16 -0500

    Revert topic 'CMakeParseArguments_EmptyArgs'
    It will be revised after working out macro _-rename conflicts.

--- Tests/RunCMake/CMakeParseArguments/RunCMakeTest.cmake
+++
@@ -1,9 +0,0 @@
-include(RunCMake)
-
-run_cmake(Example)
-run_cmake(VERSION-SKIP)
-# run_cmake(VERSION-KEEP) # Enable when 3.0.0 is released
-run_cmake(PROPERTY-SKIP)
-run_cmake(PROPERTY-KEEP)
-run_cmake(ARGUMENT-SKIP)
-run_cmake(ARGUMENT-KEEP)
