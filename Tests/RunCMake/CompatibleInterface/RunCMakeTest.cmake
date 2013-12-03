3cda6c47f795281a4f1bb8d2c4c5b58d9ea7f901
Stephen Kelly <steveire@gmail.com>
2013-12-03 13:26:51 +0100

    cmTarget: Require a compatible INTERFACE_AUTOUIC_OPTIONS from dependencies.
    Revert the origin-tracking infrastructure from commit 98093c45 (QtAutoUic:
    Add INTERFACE_AUTOUIC_OPTIONS target property., 2013-11-20). Use the
    compatibility-tracking for compatible strings instead.
    
    If two different dependencies require different AUTOUIC_OPTIONS,
    cmake will now appropriately issue an error.

--- Tests/RunCMake/CompatibleInterface/RunCMakeTest.cmake
+++ Tests/RunCMake/CompatibleInterface/RunCMakeTest.cmake
@@ -10,3 +10,7 @@ run_cmake(InterfaceString-mismatched-use)
 run_cmake(InterfaceString-builtin-prop)
 run_cmake(InterfaceString-Bool-Conflict)
 run_cmake(DebugProperties)
+
+set(RunCMake_TEST_OPTIONS -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE})
+
+run_cmake(AutoUic)
