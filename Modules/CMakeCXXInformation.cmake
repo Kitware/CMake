5d2452d89ac3508ce3de404952b601d43ac5200f
Brad King <brad.king@kitware.com>
2013-12-03 09:20:11 -0500

    Fix CMAKE_(C|CXX|Fortran)_STANDARD_LIBRARIES helpstrings (#14622)
    s/defalut/default/
    
    Reported-by: Stephan Tolksdorf <st@quanttec.com>

--- Modules/CMakeCXXInformation.cmake
+++ Modules/CMakeCXXInformation.cmake
@@ -219,7 +219,7 @@ endif()
 
 if(CMAKE_CXX_STANDARD_LIBRARIES_INIT)
   set(CMAKE_CXX_STANDARD_LIBRARIES "${CMAKE_CXX_STANDARD_LIBRARIES_INIT}"
-    CACHE STRING "Libraries linked by defalut with all C++ applications.")
+    CACHE STRING "Libraries linked by default with all C++ applications.")
   mark_as_advanced(CMAKE_CXX_STANDARD_LIBRARIES)
 endif()
 
