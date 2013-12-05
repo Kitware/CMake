de294e6007f2eb086c63c31c2da6ba08d1fdf60f
Stephen Kelly <steveire@gmail.com>
2013-12-05 18:23:03 +0100

    Try to fix the test.

--- Tests/RunCMake/CompatibleInterface/RunCMakeTest.cmake
+++ Tests/RunCMake/CompatibleInterface/RunCMakeTest.cmake
@@ -11,6 +11,7 @@ run_cmake(InterfaceString-builtin-prop)
 run_cmake(InterfaceString-Bool-Conflict)
 run_cmake(DebugProperties)
 
-set(RunCMake_TEST_OPTIONS -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE})
-
-run_cmake(AutoUic)
+if (QT_QMAKE_EXECUTABLE})
+  set(RunCMake_TEST_OPTIONS -DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE})
+  run_cmake(AutoUic)
+endif()
