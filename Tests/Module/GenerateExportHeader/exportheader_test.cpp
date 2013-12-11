0f9e5dea2e83ea3aa6c258efdb800ea3b1dfae73
Stephen Kelly <steveire@gmail.com>
2013-12-11 01:01:50 +0100

    Speed up the GenerateExportHeader unit test (#14453).
    Instead of running many small tests with many cmake projects, simply
    compare the generated export header against a reference.

--- Tests/Module/GenerateExportHeader/exportheader_test.cpp
+++ Tests/Module/GenerateExportHeader/exportheader_test.cpp
@@ -11,6 +11,52 @@
 #define DOES_NOT_BUILD(function) function
 #endif
 
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+void compare(const char* refName, const char* testName)
+{
+  std::ifstream ref;
+  ref.open(refName);
+  if (!ref.is_open())
+    {
+    std::cout << "Could not open \"" << refName << "\"." << std::endl;
+    exit(1);
+    }
+  std::ifstream test;
+  test.open(testName);
+  if (!test.is_open())
+    {
+    std::cout << "Could not open \"" << testName << "\"." << std::endl;
+    exit(1);
+    }
+
+  while (!ref.eof() && !test.eof())
+    {
+    std::string refLine;
+    std::string testLine;
+    std::getline(ref, refLine);
+    std::getline(test, testLine);
+    if (testLine.size() && testLine[testLine.size()-1] == ' ')
+      {
+      testLine = testLine.substr(0, testLine.size() - 1);
+      }
+    if (refLine != testLine)
+      {
+      std::cout << "Ref and test are not the same:\n  Ref:  \""
+                          << refLine << "\"\n  Test: \"" << testLine << "\"\n";
+      exit(1);
+      }
+    }
+  if (!ref.eof() || !test.eof())
+    {
+    std::cout << "Ref and test have differing numbers of lines.";
+    exit(1);
+    }
+}
+
 int main()
 {
   {
@@ -77,6 +123,14 @@ int main()
   libstatic_deprecated();
   libstatic_not_exported();
   libstatic_excluded();
+
+#define STRINGIFY_IMPL(A) #A
+#define STRINGIFY(A) STRINGIFY_IMPL(A)
+
+  compare(STRINGIFY(SRC_DIR) "/libshared_export.h",
+          STRINGIFY(BIN_DIR) "/libshared/libshared_export.h");
+  compare(STRINGIFY(SRC_DIR) "/libstatic_export.h",
+          STRINGIFY(BIN_DIR) "/libstatic/libstatic_export.h");
 
   return 0;
 }
