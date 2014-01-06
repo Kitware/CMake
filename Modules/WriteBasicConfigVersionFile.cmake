41d2f2c4cbf41bfe3ba7118c1fea0ed5e2f5f683
Alex Neundorf <neundorf@kde.org>
2014-01-06 21:20:44 +0100

    write_basic_package_version_file: use PROJECT_VERSION
    In the write_basic_package_version_file(), the VERSION argument
    is now optional. If none is given, it falls back to ${PROJECT_VERSION}.
    
    Alex

--- Modules/WriteBasicConfigVersionFile.cmake
+++ Modules/WriteBasicConfigVersionFile.cmake
@@ -6,7 +6,7 @@
 #
 # ::
 #
-#   WRITE_BASIC_CONFIG_VERSION_FILE( filename VERSION major.minor.patch COMPATIBILITY (AnyNewerVersion|SameMajorVersion) )
+#   WRITE_BASIC_CONFIG_VERSION_FILE( filename [VERSION major.minor.patch] COMPATIBILITY (AnyNewerVersion|SameMajorVersion) )
 #
 #
 #
@@ -46,7 +46,11 @@ function(WRITE_BASIC_CONFIG_VERSION_FILE _filename)
   endif()
 
   if("${CVF_VERSION}" STREQUAL "")
-    message(FATAL_ERROR "No VERSION specified for WRITE_BASIC_CONFIG_VERSION_FILE()")
+    if ("${PROJECT_VERSION}" STREQUAL "")
+      message(FATAL_ERROR "No VERSION specified for WRITE_BASIC_CONFIG_VERSION_FILE()")
+    else()
+      set(CVF_VERSION "${PROJECT_VERSION}")
+    endif()
   endif()
 
   configure_file("${versionTemplateFile}" "${_filename}" @ONLY)
