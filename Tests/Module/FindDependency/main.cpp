0bba365ff5423fc51d574d17b7934e3a0148ac2e
Stephen Kelly <steveire@gmail.com>
2014-02-26 21:30:18 +0100

    Add test for find_dependency macro.

---
+++ Tests/Module/FindDependency/main.cpp
@@ -0,0 +1,17 @@
+
+#ifndef HAVE_PACK1
+#error Expected HAVE_PACK1
+#endif
+
+#ifndef HAVE_PACK2
+#error Expected HAVE_PACK2
+#endif
+
+#ifndef HAVE_PACK3
+#error Expected HAVE_PACK3
+#endif
+
+int main(int argc, char** argv)
+{
+  return 0;
+}
