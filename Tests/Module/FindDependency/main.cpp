45abfb0c4d622c86880b2b32619f79417410a6c8
Stephen Kelly <steveire@gmail.com>
2014-03-06 15:06:22 +0100

    find_dependency: Extend the tests for use of EXACT macro parameter.

--- Tests/Module/FindDependency/main.cpp
+++ Tests/Module/FindDependency/main.cpp
@@ -11,6 +11,18 @@
 #error Expected HAVE_PACK3
 #endif
 
+#ifndef HAVE_PACK4
+#error Expected HAVE_PACK4
+#endif
+
+#ifndef HAVE_PACK5
+#error Expected HAVE_PACK5
+#endif
+
+#ifndef HAVE_PACK6
+#error Expected HAVE_PACK6
+#endif
+
 int main(int argc, char** argv)
 {
   return 0;
