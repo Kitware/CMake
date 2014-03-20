f26147b8716adfbc46d93df0b05c7cde0c9413f4
Julien Schueller <schueller@phimeca.com>
2014-03-20 09:57:38 +0100

    UseSWIG: Enable NO_SONAME on swig modules (#14815)
    Swig modules are plugins and do not need the SONAME field set.

--- Modules/UseSWIG.cmake
+++ Modules/UseSWIG.cmake
@@ -201,6 +201,7 @@ macro(SWIG_ADD_MODULE name language)
     MODULE
     ${swig_generated_sources}
     ${swig_other_sources})
+  set_target_properties(${SWIG_MODULE_${name}_REAL_NAME} PROPERTIES NO_SONAME ON)
   string(TOLOWER "${language}" swig_lowercase_language)
   if ("${swig_lowercase_language}" STREQUAL "octave")
     set_target_properties(${SWIG_MODULE_${name}_REAL_NAME} PROPERTIES PREFIX "")
