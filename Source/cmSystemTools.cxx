1de086855caa63063380387d70811e8acf1f3d05
Brad King <brad.king@kitware.com>
2014-04-02 09:35:36 -0400

    cmSystemTools: Restore unreachable return to silence warnings
    Some compilers do not recognize that the end of copy_data cannot
    be reached and complain that there is no return statement.  Clang
    warns that there is an unreachable return statement if it appears.
    Conditionally add the return statement when not building with Clang.

--- Source/cmSystemTools.cxx
+++ Source/cmSystemTools.cxx
@@ -1662,6 +1662,9 @@ long copy_data(struct archive *ar, struct archive *aw)
       return (r);
       }
     }
+#if !defined(__clang__)
+  return r; /* this should not happen but it silences a warning */
+#endif
 }
 
 bool extract_tar(const char* outFileName, bool verbose,
