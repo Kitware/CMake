8d0b1ccac13389255a318422d38b246cf47d9ace
Stephen Kelly <steveire@gmail.com>
2014-04-15 18:09:21 +0200

    Features: FATAL_ERROR on compilers with no recorded features.
    Users of the new target_compile_features command are expected to
    check the existence of the CMAKE_CXX_COMPILE_FEATURES variable before
    attempting to use it to require features.

--- Tests/RunCMake/CompileFeatures/RunCMakeTest.cmake
+++ Tests/RunCMake/CompileFeatures/RunCMakeTest.cmake
@@ -7,3 +7,14 @@ run_cmake(NotAFeature_OriginDebug)
 run_cmake(NotAFeature_OriginDebugGenex)
 run_cmake(NotAFeature_OriginDebugTransitive)
 run_cmake(NotAFeature_OriginDebug_target_compile_features)
+
+run_cmake(generate_feature_list)
+file(READ
+  "${RunCMake_BINARY_DIR}/generate_feature_list-build/features.txt"
+  FEATURES
+)
+
+if (NOT FEATURES)
+  run_cmake(NoSupportedCxxFeatures)
+  run_cmake(NoSupportedCxxFeaturesGenex)
+endif()
