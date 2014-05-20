0645eedb7150aa28f52161abc3f8ef2256aafb54
Rolf Eike Beer <eike@sf-mail.de>
2014-05-20 17:47:54 +0200

    FindQt[34]: Prefer matching versioned tool names
    After finding qmake we search in QT_BINARY_DIR for the other Qt tools.
    Try all versioned executable names before trying the plain executable name.
    This makes it much more likely that if 2 different major versions are installed
    in the same prefix CMake will be able to detect a proper set of tools.

--- Modules/FindQt3.cmake
+++ Modules/FindQt3.cmake
@@ -161,7 +161,7 @@ find_library(QT_QASSISTANTCLIENT_LIBRARY
 
 # Qt 3 should prefer QTDIR over the PATH
 find_program(QT_MOC_EXECUTABLE
-  NAMES moc-qt3 moc moc3 moc3-mt
+  NAMES moc-qt3 moc3 moc3-mt moc
   HINTS
     ENV QTDIR
   PATHS
@@ -186,7 +186,7 @@ endif()
 
 # Qt 3 should prefer QTDIR over the PATH
 find_program(QT_UIC_EXECUTABLE
-  NAMES uic-qt3 uic uic3 uic3-mt
+  NAMES uic-qt3 uic3 uic3-mt uic
   HINTS
     ENV QTDIR
   PATHS
