6650b0fa43ef55e8bad9188c584f40d515f38bd6
Brad King <brad.king@kitware.com>
2014-06-25 13:34:15 -0400

    bootstrap: Fix "make test" and "make package" targets (#14989)
    Since commit v3.0.0-rc1~374^2 (Refactor internal resource location APIs
    and initialization, 2013-11-07) a bootstrap-built "cmake" tries to
    reference "ctest" and "cpack" executables next to itself, which never
    exist.  Teach cmSystemTools::FindCMakeResources, when bootstrap-built,
    to refer to the "ctest" and "cpack" executables in the location where
    they will be built after "make".

--- Source/cmSystemTools.cxx
+++ Source/cmSystemTools.cxx
@@ -2163,6 +2163,11 @@ void cmSystemTools::FindCMakeResources(const char* argv0)
   cmSystemToolsCMakeCommand = exe_dir;
   cmSystemToolsCMakeCommand += "/cmake";
   cmSystemToolsCMakeCommand += cmSystemTools::GetExecutableExtension();
+#ifndef CMAKE_BUILD_WITH_CMAKE
+  // The bootstrap cmake does not provide the other tools,
+  // so use the directory where they are about to be built.
+  exe_dir = CMAKE_BOOTSTRAP_BINARY_DIR "/bin";
+#endif
   cmSystemToolsCTestCommand = exe_dir;
   cmSystemToolsCTestCommand += "/ctest";
   cmSystemToolsCTestCommand += cmSystemTools::GetExecutableExtension();
