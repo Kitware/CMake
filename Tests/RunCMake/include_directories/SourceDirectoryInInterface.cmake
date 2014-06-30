9998d78d630dec9532a40d4423e4deabcd3252b2
Brad King <brad.king@kitware.com>
2014-06-30 14:37:38 -0400

    Tests: Speed up RunCMake.include_directories test
    Remove unnecessary language initializations.  Change the main
    CMakeLists.txt project() call to specify NONE.  Use enable_language(CXX)
    instead of project() in cases that need it to avoid enabling C too.

--- Tests/RunCMake/include_directories/SourceDirectoryInInterface.cmake
+++ Tests/RunCMake/include_directories/SourceDirectoryInInterface.cmake
@@ -1,5 +1,5 @@
 
-project(SourceDirectoryInInterface)
+enable_language(CXX)
 
 add_library(testTarget "${CMAKE_CURRENT_SOURCE_DIR}/empty.cpp")
 target_include_directories(testTarget INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/foo")
