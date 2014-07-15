3151024d241c581463d20bf0ce3d1060bdd22de4
Stephen Kelly <steveire@gmail.com>
2014-07-15 11:01:36 +0200

    QNX: Add missing flags for configurations and artifact creation.
    Commit v3.0.0-rc1~111^2 (QNX: Introduce QCC compiler id for that
    QNX platform compiler., 2014-01-20) split handling of the QNX QCC
    compiler into a separate compiler-id.  That refactoring results in
    the QCC compiler not using the CMake-compiler-id "GNU", which means
    that the __compiler_gnu macro is no longer executed for it.
    
    Add Compiler/QCC*.cmake modules to define and call the __compiler_qcc
    macro and teach it to call __compiler_gnu internally.  Remove the
    corresponding pieces from the Platform/QNX*.cmake modules.
    
    It is also necessary to change the language conditional to dereference
    the lang macro parameter, which is another bug introduced by the
    same commit.  The extra -lang-c++ flag is only necessary when the CXX
    compiler is specified as 'qcc' instead of 'QCC' in the toolchain file,
    which is why this bug was not noticed before.  The flag is also necessary
    in that case when linking in order to find the appropriate standard
    libraries.  The flag was not previously added when linking executables,
    so linking failed even with CMake 2.8.12 with the lower-case compiler-id.
    
    Co-Author: Brad King <brad.king@kitware.com>

--- Modules/Platform/QNX.cmake
+++ Modules/Platform/QNX.cmake
@@ -1,5 +1,8 @@
 set(QNXNTO 1)
 
+include(Platform/GNU)
+unset(CMAKE_LIBRARY_ARCHITECTURE_REGEX)
+
 set(CMAKE_DL_LIBS "")
 
 # Shared libraries with no builtin soname may not be linked safely by
@@ -14,22 +17,3 @@ foreach(type SHARED_LIBRARY SHARED_MODULE EXE)
   set(CMAKE_${type}_LINK_STATIC_C_FLAGS "-Wl,-Bstatic")
   set(CMAKE_${type}_LINK_DYNAMIC_C_FLAGS "-Wl,-Bdynamic")
 endforeach()
-
-include(Platform/GNU)
-unset(CMAKE_LIBRARY_ARCHITECTURE_REGEX)
-
-macro(__compiler_qcc lang)
-  # http://www.qnx.com/developers/docs/6.4.0/neutrino/utilities/q/qcc.html#examples
-  set(CMAKE_${lang}_COMPILE_OPTIONS_TARGET "-V")
-
-  set(CMAKE_INCLUDE_SYSTEM_FLAG_${lang} "-Wp,-isystem,")
-  set(CMAKE_DEPFILE_FLAGS_${lang} "-Wc,-MMD,<DEPFILE>,-MT,<OBJECT>,-MF,<DEPFILE>")
-
-  if (lang STREQUAL CXX)
-    # If the toolchain uses qcc for CMAKE_CXX_COMPILER instead of QCC, the
-    # default for the driver is not c++.
-    set(CMAKE_CXX_COMPILE_OBJECT
-    "<CMAKE_CXX_COMPILER> -lang-c++ <DEFINES> <FLAGS> -o <OBJECT> -c <SOURCE>")
-  endif()
-
-endmacro()
