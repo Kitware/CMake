fd63722dc86ec30e8a6f543881d56b3c473da34d
Chuck Atkins <chuck.atkins@kitware.com>
2014-07-16 09:04:42 -0500

    cmcurl: Fix a build failure with the Cray compiler on Linux (#15026)
    The error is caused by a workaround for UNICOS.  The workaround
    incorrectly uses the _CRAYC macro to determine if building for
    UNICOS.  This macro will always be defined for the Cray C and
    C++ compiler regardless of the target platform, even when
    building for Linux.  The correct macro should be _CRAY, which as
    per Cray documentation is only defined for UNICOS targeted
    builds.

--- Utilities/cmcurl/hostip.c
+++ Utilities/cmcurl/hostip.c
@@ -609,7 +609,7 @@ Curl_addrinfo *Curl_ip2addr(in_addr_t num, const char *hostname, int port)
   h = &buf->hostentry;
   h->h_addr_list = &buf->h_addr_list[0];
   addrentry = &buf->addrentry;
-#ifdef _CRAYC
+#ifdef _CRAY
   /* On UNICOS, s_addr is a bit field and for some reason assigning to it
    * doesn't work.  There must be a better fix than this ugly hack.
    */
