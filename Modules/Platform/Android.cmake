178f56a57996651a4f8c2a20271f1e2c999279d2
Brad King <brad.king@kitware.com>
2014-09-19 10:11:36 -0400

    VS: Fix Tegra-Android platform linking of libraries by name
    Nsight Tegra Visual Studio Edition handles prefixing of library names
    with '-l' automatically, so teach the generator not to do so.
    
    Reported-by: Mourad Boufarguine <mourad@boufarguine.name>

--- Modules/Platform/Android.cmake
+++ Modules/Platform/Android.cmake
@@ -7,3 +7,9 @@ set(CMAKE_PLATFORM_NO_VERSIONED_SONAME 1)
 # Android reportedly ignores RPATH, and we cannot predict the install
 # location anyway.
 set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "")
+
+# Nsight Tegra Visual Studio Edition takes care of
+# prefixing library names with '-l'.
+if(CMAKE_VS_PLATFORM_NAME STREQUAL "Tegra-Android")
+  set(CMAKE_LINK_LIBRARY_FLAG "")
+endif()
