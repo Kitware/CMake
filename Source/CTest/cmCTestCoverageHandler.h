5c31c3e4eb36cccaaf72d0f0582beed98f6665e0
Joseph Snyder <joe.snyder@kitware.com>
2014-09-29 14:52:10 -0400

    CTest: Add code coverage parser for Pascal/Delphi
    Add a class to parse the HTML output of the Delphi-code-coverage tool
    
     http://code.google.com/p/delphi-code-coverage/
    
    Add a test for the new parser.

--- Source/CTest/cmCTestCoverageHandler.h
+++ Source/CTest/cmCTestCoverageHandler.h
@@ -84,6 +84,9 @@ private:
   //! Handle coverage for Jacoco
   int HandleJacocoCoverage(cmCTestCoverageHandlerContainer* cont);
 
+  //! Handle coverage for Delphi (Pascal)
+  int HandleDelphiCoverage(cmCTestCoverageHandlerContainer* cont);
+
 //! Handle coverage using Bullseye
   int HandleBullseyeCoverage(cmCTestCoverageHandlerContainer* cont);
   int RunBullseyeSourceSummary(cmCTestCoverageHandlerContainer* cont);
