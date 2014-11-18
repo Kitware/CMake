7a68729223c05d779188de89066de414d131dbcb
Ådne Hovda <ahovda@openit.com>
2014-11-18 11:40:05 +0100

    ccmake: Cleanup and simplify conditional blocks for HP-UX

--- Source/CursesDialog/cmCursesStandardIncludes.h
+++ Source/CursesDialog/cmCursesStandardIncludes.h
@@ -15,29 +15,12 @@
  #define _MSE_INT_H
 #endif
 
-#include <cmFormConfigure.h>
-
 #if defined(__hpux)
 # define _BOOL_DEFINED
 # include <sys/time.h>
-# define _XOPEN_SOURCE_EXTENDED
-# include <curses.h>
-# include <form.h>
-# undef _XOPEN_SOURCE_EXTENDED
-#else
-/* figure out which curses.h to include */
-# if defined(CURSES_HAVE_NCURSES_H)
-#  include <ncurses.h>
-# elif defined(CURSES_HAVE_NCURSES_NCURSES_H)
-#  include <ncurses/ncurses.h>
-# elif defined(CURSES_HAVE_NCURSES_CURSES_H)
-#  include <ncurses/curses.h>
-# else
-#  include <curses.h>
-# endif
-
-# include <form.h>
 #endif
+
+#include <form.h>
 
 // This is a hack to prevent warnings about these functions being
 // declared but not referenced.
@@ -55,10 +38,6 @@ public:
     Ref6 = sizeof(cfsetispeed(0,0))
   };
 };
-#endif
-
-#ifndef getmaxyx
- #define getmaxyx(w,y,x) ((y) = getmaxy(w), (x) = getmaxx(w))
 #endif
 
 
