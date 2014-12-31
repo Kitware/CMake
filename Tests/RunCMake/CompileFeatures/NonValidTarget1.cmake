07d1f6fca1d6ea21dea64b32639c44d5eecf98a6
Robert Maynard <robert.maynard@kitware.com>
2014-12-31 10:25:33 -0500

    Features: Properly evaluate if the compiler supports cxx_final.
    Previously we expanded HAVE_FINAL to determine what the copied_file number
    would be, but when we don't have cxx_final than HAVE_FINAL is not defined.
    What we really want is to use expected_result.

--- Tests/RunCMake/CompileFeatures/NonValidTarget1.cmake
+++ Tests/RunCMake/CompileFeatures/NonValidTarget1.cmake
@@ -7,11 +7,11 @@ else()
   set(expected_result 0)
 endif()
 
-add_custom_command(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/copied_file${HAVE_FINAL}.cpp"
+add_custom_command(OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/copied_file${expected_result}.cpp"
   COMMAND "${CMAKE_COMMAND}" -E copy "${CMAKE_CURRENT_SOURCE_DIR}/empty.cpp" "${CMAKE_CURRENT_BINARY_DIR}/copied_file${genexvar}.cpp"
 )
 
-add_library(empty "${CMAKE_CURRENT_BINARY_DIR}/copied_file${genexvar}.cpp")
+add_library(empty "${CMAKE_CURRENT_BINARY_DIR}/copied_file${expected_result}.cpp")
 if (HAVE_FINAL)
   target_compile_features(empty PRIVATE cxx_final)
 endif()
