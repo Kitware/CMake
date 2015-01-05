837a8a6312d543f1c46efa532f7d5b5dfb334bc9
Stephen Kelly <steveire@gmail.com>
2015-01-05 19:53:30 +0100

    cmStandardIncludes: Drop Comeau-related workaround.
    The compiler is not tested on the dashboard, and on Windows there
    are more-natural compilers to use to build CMake.

--- Source/cmStandardIncludes.h
+++ Source/cmStandardIncludes.h
@@ -88,19 +88,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#if !defined(_WIN32) && defined(__COMO__)
-// Hack for como strict mode to avoid defining _SVID_SOURCE or _BSD_SOURCE.
-extern "C"
-{
-extern FILE *popen (__const char *__command, __const char *__modes) __THROW;
-extern int pclose (FILE *__stream) __THROW;
-extern char *realpath (__const char *__restrict __name,
-                       char *__restrict __resolved) __THROW;
-extern char *strdup (__const char *__s) __THROW;
-extern int putenv (char *__string) __THROW;
-}
-#endif
-
 // if std:: is not supported, then just #define it away
 #ifdef CMAKE_NO_STD_NAMESPACE
 #define std
