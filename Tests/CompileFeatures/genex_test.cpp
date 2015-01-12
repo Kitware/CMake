06ff525492b32cd3182c185c908c526379766912
Stephen Kelly <steveire@gmail.com>
2015-01-12 20:02:17 +0100

    Features: Extend the tests for the COMPILE_FEATURES genex.

--- Tests/CompileFeatures/genex_test.cpp
+++ Tests/CompileFeatures/genex_test.cpp
@@ -21,6 +21,36 @@ struct B final : A
 #error "Expect nullptr feature"
 #else
 
+#if !HAVE_INHERITING_CONSTRUCTORS
+#  if EXPECT_INHERITING_CONSTRUCTORS
+#    error Expect cxx_inheriting_constructors support
+#  endif
+#else
+#  if !EXPECT_INHERITING_CONSTRUCTORS
+#    error Expect no cxx_inheriting_constructors support
+#  endif
+#endif
+
+#if !HAVE_FINAL
+#  if EXPECT_FINAL
+#    error Expect cxx_final support
+#  endif
+#else
+#  if !EXPECT_FINAL
+#    error Expect no cxx_final support
+#  endif
+#endif
+
+#if !HAVE_INHERITING_CONSTRUCTORS_AND_FINAL
+#  if EXPECT_INHERITING_CONSTRUCTORS_AND_FINAL
+#    error Expect cxx_inheriting_constructors and cxx_final support
+#  endif
+#else
+#  if !EXPECT_INHERITING_CONSTRUCTORS_AND_FINAL
+#    error Expect no combined cxx_inheriting_constructors and cxx_final support
+#  endif
+#endif
+
 const char* getString()
 {
   return nullptr;
