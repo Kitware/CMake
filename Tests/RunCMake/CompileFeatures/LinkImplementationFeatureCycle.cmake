f13a2eb118d8b152a897e09fdcf43793f8e1bcca
Stephen Kelly <steveire@gmail.com>
2015-01-15 23:53:23 +0100

    Features: Adjust the RunCMake test to use more-common features.

--- Tests/RunCMake/CompileFeatures/LinkImplementationFeatureCycle.cmake
+++ Tests/RunCMake/CompileFeatures/LinkImplementationFeatureCycle.cmake
@@ -3,12 +3,12 @@ add_library(empty1 empty.cpp)
 
 add_library(empty2 INTERFACE)
 add_library(empty3 INTERFACE)
-target_compile_features(empty3 INTERFACE cxx_constexpr)
+target_compile_features(empty3 INTERFACE cxx_static_assert)
 
 target_link_libraries(empty1
-  # When starting, $<COMPILE_FEATURES:cxx_final> is '0', so 'freeze' the
+  # When starting, $<COMPILE_FEATURES:cxx_nullptr> is '0', so 'freeze' the
   # CXX_STANDARD at 98 during computation.
-  $<$<COMPILE_FEATURES:cxx_final>:empty2>
+  $<$<COMPILE_FEATURES:cxx_nullptr>:empty2>
   # This would add cxx_constexpr, but that would require CXX_STANDARD = 11,
   # which is not allowed after freeze.  Report an error.
   empty3
