c66e3317492709ab3b830024f67021dde67a7fe5
Stephen Kelly <steveire@gmail.com>
2015-01-18 14:37:34 +0100

    Features: Use a more-common feature in cycle-test.
    We require that the $<$<COMPILE_FEATURES:cxx_feature>:empty2> generates
    a '1' when CXX11 is enabled.  GNU 4.4 does not support cxx_nullptr, but
    does support cxx_auto_type.

--- Tests/RunCMake/CompileFeatures/LinkImplementationFeatureCycle.cmake
+++ Tests/RunCMake/CompileFeatures/LinkImplementationFeatureCycle.cmake
@@ -6,9 +6,9 @@ add_library(empty3 INTERFACE)
 target_compile_features(empty3 INTERFACE cxx_static_assert)
 
 target_link_libraries(empty1
-  # When starting, $<COMPILE_FEATURES:cxx_nullptr> is '0', so 'freeze' the
+  # When starting, $<COMPILE_FEATURES:cxx_auto_type> is '0', so 'freeze' the
   # CXX_STANDARD at 98 during computation.
-  $<$<COMPILE_FEATURES:cxx_nullptr>:empty2>
+  $<$<COMPILE_FEATURES:cxx_auto_type>:empty2>
   # This would add cxx_static_assert, but that would require CXX_STANDARD = 11,
   # which is not allowed after freeze.  Report an error.
   empty3
