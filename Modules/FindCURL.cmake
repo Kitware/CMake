aa64c62f8f3b405403bc09109f3012d2822ee7b7
OWenT <owt5008137@live.com>
2015-02-17 10:46:17 +0800

    curl ext

--- Modules/FindCURL.cmake
+++ Modules/FindCURL.cmake
@@ -9,6 +9,7 @@
 # ::
 #
 #   CURL_INCLUDE_DIRS   - where to find curl/curl.h, etc.
+#   CURL_EXECUTABLE     - path of curl executable
 #   CURL_LIBRARIES      - List of libraries when using curl.
 #   CURL_FOUND          - True if curl found.
 #   CURL_VERSION_STRING - the version of curl found (since CMake 2.8.8)
@@ -16,6 +17,7 @@
 #=============================================================================
 # Copyright 2006-2009 Kitware, Inc.
 # Copyright 2012 Rolf Eike Beer <eike@sf-mail.de>
+# Copyright 2015 OWenT <admin@owent.net>
 #
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file Copyright.txt for details.
@@ -27,8 +29,18 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
+unset(CURL_SEARCH_INCLUDE)
+unset(CURL_SEARCH_LIBRARY)
+unset(CURL_SEARCH_EXECUTE)
+if (CURL_ROOT)
+  set(CURL_SEARCH_INCLUDE PATHS ${CURL_ROOT} NO_DEFAULT_PATH PATH_SUFFIXES include)
+  set(CURL_SEARCH_LIBRARY PATHS ${CURL_ROOT} NO_DEFAULT_PATH PATH_SUFFIXES lib)
+  set(CURL_SEARCH_EXECUTE PATHS ${CURL_ROOT} NO_DEFAULT_PATH PATH_SUFFIXES bin)
+  unset(CURL_EXECUTABLE CACHE)
+endif()
+
 # Look for the header file.
-find_path(CURL_INCLUDE_DIR NAMES curl/curl.h)
+find_path(CURL_INCLUDE_DIR NAMES curl/curl.h ${CURL_SEARCH_INCLUDE})
 mark_as_advanced(CURL_INCLUDE_DIR)
 
 # Look for the library (sorted from most current/relevant entry to least).
@@ -40,8 +52,14 @@ find_library(CURL_LIBRARY NAMES
     curllib_static
   # Windows older "Win32 - MSVC" prebuilts (libcurl.lib, e.g. libcurl-7.15.5-win32-msvc.zip):
     libcurl
+    ${CURL_SEARCH_LIBRARY}
 )
+
 mark_as_advanced(CURL_LIBRARY)
+
+# executable is optional
+find_program(CURL_EXECUTABLE NAMES curl curl.exe ${CURL_SEARCH_EXECUTE})
+mark_as_advanced(CURL_EXECUTABLE)
 
 if(CURL_INCLUDE_DIR)
   foreach(_curl_version_header curlver.h curl.h)
