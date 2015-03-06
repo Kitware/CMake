b0852ebc09eec44041e8aa624ec4bb17bda14dac
Nils Gladitz <nilsgladitz@gmail.com>
2015-03-06 20:40:53 +0100

    CPackWIX: Support patching of root <Feature> elements.

--- Source/CPack/WiX/cmCPackWIXGenerator.cxx
+++ Source/CPack/WiX/cmCPackWIXGenerator.cxx
@@ -474,6 +474,7 @@ bool cmCPackWIXGenerator::CreateWiXSourceFiles()
 
   featureDefinitions.AddAttribute("Title", cpackPackageName);
   featureDefinitions.AddAttribute("Level", "1");
+  this->Patch->ApplyFragment("#PRODUCTFEATURE", featureDefinitions);
 
   const char* package = GetOption("CPACK_WIX_CMAKE_PACKAGE_REGISTRY");
   if(package)
