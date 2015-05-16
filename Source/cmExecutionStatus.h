c8c341690f62b01392c487ec68ca39c578be170b
Stephen Kelly <steveire@gmail.com>
2015-05-16 07:06:39 +0200

    cmExecutionStatus: De-virtualize API.
    It is clearly not required.

--- Source/cmExecutionStatus.h
+++ Source/cmExecutionStatus.h
@@ -26,33 +26,32 @@ public:
 
   cmExecutionStatus() { this->Clear();}
 
-  virtual void SetReturnInvoked(bool val)
+  void SetReturnInvoked(bool val)
   { this->ReturnInvoked = val; }
-  virtual bool GetReturnInvoked()
+  bool GetReturnInvoked()
   { return this->ReturnInvoked; }
 
-  virtual void SetBreakInvoked(bool val)
+  void SetBreakInvoked(bool val)
   { this->BreakInvoked = val; }
-  virtual bool GetBreakInvoked()
+  bool GetBreakInvoked()
   { return this->BreakInvoked; }
 
-  virtual void SetContinueInvoked(bool val)
+  void SetContinueInvoked(bool val)
   { this->ContinueInvoked = val; }
-  virtual bool GetContinueInvoked()
+  bool GetContinueInvoked()
   { return this->ContinueInvoked; }
 
-  virtual void Clear()
+  void Clear()
     {
     this->ReturnInvoked = false;
     this->BreakInvoked = false;
     this->ContinueInvoked = false;
     this->NestedError = false;
     }
-  virtual void SetNestedError(bool val) { this->NestedError = val; }
-  virtual bool GetNestedError() { return this->NestedError; }
+  void SetNestedError(bool val) { this->NestedError = val; }
+  bool GetNestedError() { return this->NestedError; }
 
-
-protected:
+private:
   bool ReturnInvoked;
   bool BreakInvoked;
   bool ContinueInvoked;
