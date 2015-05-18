d412f20c19b32e7993489e68ab2075223899826a
Tamar Kranenburg <info@takar.nl>
2015-05-18 22:10:44 +0200

    Revert "Make module FindPostgreSQL relocatable"
    This reverts commit 90e8bbd479efd673f346d98ec1d874b93ad84d9c.

--- Modules/FindPostgreSQL.cmake
+++ Modules/FindPostgreSQL.cmake
@@ -170,7 +170,7 @@ if (PostgreSQL_INCLUDE_DIR)
 endif()
 
 # Did we find anything?
-include(FindPackageHandleStandardArgs)
+include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
 find_package_handle_standard_args(PostgreSQL
                                   REQUIRED_VARS PostgreSQL_LIBRARY PostgreSQL_INCLUDE_DIR PostgreSQL_TYPE_INCLUDE_DIR
                                   VERSION_VAR PostgreSQL_VERSION_STRING)
