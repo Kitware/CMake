8b4b2ca686abfbc4f74496c2c6a17c1fe3f646c2
Domen Vrankar <domen.vrankar@gmail.com>
2015-06-05 21:12:08 +0200

    CPack RPM PARTIALLY_RELOCATABLE_WARNING test move
    Moved PARTIALLY_RELOCATABLE_WARNING test
    to new common CPack test script structure
    to have all tests in one place as well
    as additional benefit of having some more
    checks done during test execution.

--- Tests/RunCMake/CPack/RunCMakeTest.cmake
+++ Tests/RunCMake/CPack/RunCMakeTest.cmake
@@ -3,3 +3,4 @@ include("${RunCMake_SOURCE_DIR}/CPackTestHelpers.cmake")
 
 # args: TEST_NAME "GENERATORS" RUN_CMAKE_BUILD_STEP
 run_cpack_test(MINIMAL "RPM;DEB" false)
+run_cpack_test(PARTIALLY_RELOCATABLE_WARNING "RPM" false)
