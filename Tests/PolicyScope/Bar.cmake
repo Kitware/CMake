8fd90bb8c5609308ab60acc48a9674dcd47b42a2
Stephen Kelly <steveire@gmail.com>
2015-06-10 00:08:51 +0200

    Tests: Implement PolicyScope tests in terms of newer policies.
    Keep the spirit of the tests.

--- Tests/PolicyScope/Bar.cmake
+++ Tests/PolicyScope/Bar.cmake
@@ -1,8 +1,5 @@
-cmake_minimum_required(VERSION 2.6.3)
+cmake_minimum_required(VERSION 3.2)
 
 # Make sure a policy set differently by our includer is now correct.
-cmake_policy(GET CMP0003 cmp)
-check(CMP0003 "NEW" "${cmp}")
-
-# Test allowing the top-level file to not have cmake_minimum_required.
-cmake_policy(SET CMP0000 OLD)
+cmake_policy(GET CMP0056 cmp)
+check(CMP0056 "NEW" "${cmp}")
