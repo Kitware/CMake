22d09ecf33f3d07925003995b76b7e8aa788c04f
Stephen Kelly <steveire@gmail.com>
2015-06-09 22:27:35 +0200

    cmLocalGenerator: Remove ConfigureFinalPass.
    Call the cmMakefile implementation directly.  This is a configure-time
    construct.

--- Source/cmLocalGenerator.cxx
+++ Source/cmLocalGenerator.cxx
@@ -113,11 +113,6 @@ void cmLocalGenerator::ComputeObjectMaxPath()
   this->ObjectMaxPathViolations.clear();
 }
 
-void cmLocalGenerator::ConfigureFinalPass()
-{
-  this->Makefile->ConfigureFinalPass();
-}
-
 void cmLocalGenerator::TraceDependencies()
 {
   std::vector<std::string> configs;
