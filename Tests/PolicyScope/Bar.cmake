7a57d3615f4020783e44f06c3aad4b24dbc342d3
Stephen Kelly <steveire@gmail.com>
2015-06-13 08:25:04 +0200

    Revert topic.

--- Tests/PolicyScope/Bar.cmake
+++ Tests/PolicyScope/Bar.cmake
@@ -1,5 +1,8 @@
-cmake_minimum_required(VERSION 3.2)
+cmake_minimum_required(VERSION 2.6.3)
 
 # Make sure a policy set differently by our includer is now correct.
-cmake_policy(GET CMP0056 cmp)
-check(CMP0056 "NEW" "${cmp}")
+cmake_policy(GET CMP0003 cmp)
+check(CMP0003 "NEW" "${cmp}")
+
+# Test allowing the top-level file to not have cmake_minimum_required.
+cmake_policy(SET CMP0000 OLD)
