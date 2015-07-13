81895aa46f06d1fea6c5a2a1faaa8dea935eb651
Domen Vrankar <domen.vrankar@gmail.com>
2015-07-13 22:23:22 +0200

    CTest/RunCMake/CPack/DEB: added verifyDebControl
    Added verifyDebControl helper function for checking
    of package control files such as preinstall scripts.

--- Tests/RunCMake/CPack/DEB/Helpers.cmake
+++ Tests/RunCMake/CPack/DEB/Helpers.cmake
@@ -8,3 +8,21 @@ function(getPackageContent FILE RESULT_VAR)
 
   set(${RESULT_VAR} "${package_content_}" PARENT_SCOPE)
 endfunction()
+
+function(verifyDebControl FILE PREFIX VERIFY_FILES)
+  execute_process(COMMAND ${DPKG_EXECUTABLE} --control ${FILE} control_${PREFIX}
+          ERROR_VARIABLE err_)
+
+  if(err_)
+    message(FATAL_ERROR "Debian controll verification failed for file: "
+        "'${FILE}'; error output: '${err_}'")
+  endif()
+
+  foreach(FILE_ IN LISTS VERIFY_FILES)
+    file(READ "${CMAKE_CURRENT_BINARY_DIR}/control_${PREFIX}/${FILE_}" content_)
+    if(NOT content_ MATCHES "${${PREFIX}_${FILE_}}")
+      message(FATAL_ERROR "Unexpected content in for '${PREFIX}_${FILE_}'!"
+          " Content: '${content_}'")
+    endif()
+  endforeach()
+endfunction()
