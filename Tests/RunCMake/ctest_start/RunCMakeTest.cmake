0699e0d3e4305fc4e615c36b831d0171c2f8c757
Brad King <brad.king@kitware.com>
2015-07-14 09:29:10 -0400

    Tests: Move CTestTestConfigFileInBuildDir into RunCMake.ctest_start
    Subsume the CTestTestConfigFileInBuildDir test cases into the
    RunCMake.ctest_start test as new cases to avoid infrastructure
    duplication.  This also avoids using REGEX_ESCAPE_STRING to try to
    regex-match full paths.

--- Tests/RunCMake/ctest_start/RunCMakeTest.cmake
+++ Tests/RunCMake/ctest_start/RunCMakeTest.cmake
@@ -8,3 +8,16 @@ function(run_ctest_start CASE_NAME)
 endfunction()
 
 run_ctest_start(StartQuiet Experimental QUIET)
+
+run_ctest_start(ConfigInSource Experimental)
+
+function(run_ConfigInBuild)
+  set(RunCMake_TEST_BINARY_DIR ${RunCMake_BINARY_DIR}/ConfigInBuild-build)
+  set(RunCMake_TEST_NO_CLEAN 1)
+  file(REMOVE_RECURSE "${RunCMake_TEST_BINARY_DIR}")
+  file(MAKE_DIRECTORY "${RunCMake_TEST_BINARY_DIR}")
+  configure_file(${RunCMake_SOURCE_DIR}/CTestConfig.cmake.in
+                 ${RunCMake_BINARY_DIR}/ConfigInBuild-build/CTestConfig.cmake @ONLY)
+  run_ctest_start(ConfigInBuild Experimental)
+endfunction()
+run_ConfigInBuild()
