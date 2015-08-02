6ce940ac9701c93be01ed169c6bd23b22e04782f
Stephen Kelly <steveire@gmail.com>
2015-08-02 10:57:31 +0200

    cmMakefile: Use std::string in ProjectName API.

--- Source/cmProjectCommand.cxx
+++ Source/cmProjectCommand.cxx
@@ -20,7 +20,7 @@ bool cmProjectCommand
     this->SetError("PROJECT called with incorrect number of arguments");
     return false;
     }
-  this->Makefile->SetProjectName(args[0].c_str());
+  this->Makefile->SetProjectName(args[0]);
 
   std::string bindir = args[0];
   bindir += "_BINARY_DIR";
