821468acc73560aabb6fa9904db511b5d992450e
Stephen Kelly <steveire@gmail.com>
2015-08-29 09:21:32 +0200

    cmGlobalGenerator: Do more computation at compute time.

--- Source/cmGlobalGenerator.cxx
+++ Source/cmGlobalGenerator.cxx
@@ -1308,13 +1308,6 @@ bool cmGlobalGenerator::Compute()
       }
     }
 
-  return true;
-}
-
-void cmGlobalGenerator::Generate()
-{
-  unsigned int i;
-
   // Trace the dependencies, after that no custom commands should be added
   // because their dependencies might not be handled correctly
   for (i = 0; i < this->LocalGenerators.size(); ++i)
@@ -1333,8 +1326,15 @@ void cmGlobalGenerator::Generate()
   // Compute the inter-target dependencies.
   if(!this->ComputeTargetDepends())
     {
-    return;
+    return false;
     }
+
+  return true;
+}
+
+void cmGlobalGenerator::Generate()
+{
+  unsigned int i;
 
   // Create a map from local generator to the complete set of targets
   // it builds by default.
