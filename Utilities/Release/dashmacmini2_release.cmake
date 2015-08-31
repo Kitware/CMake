f4b633a3612d290d6986bbfc56ff2d69807a134b
Brad King <brad.king@kitware.com>
2015-08-31 16:22:46 -0400

    Utilities/Release: Install docs in unversioned dir on OS X (#15692)
    The CMake.app directory will never overlap with another version of CMake
    so we do not need to have a version number on the documentation
    directory.  Configure our release binaries on OS X to place the
    documentation in an unversioned directory under CMake.app so that
    one can always use a path like
    
     /Applications/CMake.app/Contents/doc/cmake/html/index.html
    
    to visit the documentation even after updating CMake.

--- Utilities/Release/dashmacmini2_release.cmake
+++ Utilities/Release/dashmacmini2_release.cmake
@@ -9,6 +9,7 @@ set(CPACK_BINARY_GENERATORS "DragNDrop TGZ TZ")
 set(CPACK_DMG_FORMAT "UDBZ") #build using bzip2 for smaller package size
 set(INITIAL_CACHE "
 CMAKE_BUILD_TYPE:STRING=Release
+CMAKE_DOC_DIR:PATH=doc/cmake
 CMAKE_OSX_ARCHITECTURES:STRING=ppc;i386
 CMAKE_USE_OPENSSL:BOOL=ON
 OPENSSL_CRYPTO_LIBRARY:FILEPATH=/Users/kitware/openssl-1.0.1g-install/lib/libcrypto.a
