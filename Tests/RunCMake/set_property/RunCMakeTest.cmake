c7c0d1a6057d7b4bcffe94032844cb3b852d51fd
Brad King <brad.king@kitware.com>
2015-09-02 15:57:41 -0400

    Tests: Cover set_property for buildsystem target properties
    Extend the RunCMake.set_property test with cases covering buildsystem
    target properties:
    
    * COMPILE_DEFINITIONS
    * COMPILE_FEATURES
    * COMPILE_OPTIONS
    * INCLUDE_DIRECTORIES
    * LINK_LIBRARIES
    * SOURCES
    
    Also test a non-buildsystem property to document the current difference
    in behavior.  Refactor the existing LINK_LIBRARIES case to the same
    more-extensive test as the rest.  Use the output generated by CMake 3.3
    as the expected output for each test case.

--- Tests/RunCMake/set_property/RunCMakeTest.cmake
+++ Tests/RunCMake/set_property/RunCMakeTest.cmake
@@ -1,3 +1,9 @@
 include(RunCMake)
 
+run_cmake(COMPILE_DEFINITIONS)
+run_cmake(COMPILE_FEATURES)
+run_cmake(COMPILE_OPTIONS)
+run_cmake(INCLUDE_DIRECTORIES)
 run_cmake(LINK_LIBRARIES)
+run_cmake(SOURCES)
+run_cmake(USER_PROP)
