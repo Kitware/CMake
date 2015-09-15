da00be6359055ffdb2067a9ec1e817eb782ad145
Brad King <brad.king@kitware.com>
2015-09-15 15:51:19 -0400

    MSVC: Rewrite manifest file handling with Makefile and Ninja
    Add a helper class private to "cmcmd.cxx" to contain the implementation.
    Update the link logic to use the intermediate files directory for each
    target to hold manifest and resource files before embedding into the
    binary.  Preserve the old behavior of placing the .manifest file next
    to the binary when not linking incrementally even though it will be
    embedded.

--- Source/cmcmd.h
+++ Source/cmcmd.h
@@ -35,20 +35,6 @@ protected:
   static int WindowsCEEnvironment(const char* version,
                                   const std::string& name);
   static int VisualStudioLink(std::vector<std::string>& args, int type);
-  static int VisualStudioLinkIncremental(std::vector<std::string>& args,
-                                         int type,
-                                         bool verbose);
-  static int VisualStudioLinkNonIncremental(std::vector<std::string>& args,
-                                            int type,
-                                            bool hasManifest,
-                                            bool verbose);
-  static int ParseVisualStudioLinkCommand(std::vector<std::string>& args,
-                                          std::vector<std::string>& command,
-                                          std::string& targetName);
-  static bool RunCommand(const char* comment,
-                         std::vector<std::string>& command,
-                         bool verbose,
-                         int* retCodeOut = 0);
 };
 
 #endif
