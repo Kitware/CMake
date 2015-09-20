f8e759449c838dda772f8661aef526ae972f4717
Stephen Kelly <steveire@gmail.com>
2015-09-20 16:07:34 +0200

    Add conflict hunk.

--- Source/cmMakefile.cxx
+++ Source/cmMakefile.cxx
@@ -1777,6 +1777,11 @@ void cmMakefile::AddSubDirectory(const std::string& srcPath,
     {
     this->UnConfiguredDirectories.push_back(subMf);
     }
+
+  // create a new local generator and set its parent
+  cmLocalGenerator *lg2 = this->GetGlobalGenerator()
+        ->CreateLocalGenerator(subMf);
+  this->GetGlobalGenerator()->AddLocalGenerator(lg2);
 }
 
 void cmMakefile::SetCurrentSourceDirectory(const std::string& dir)
