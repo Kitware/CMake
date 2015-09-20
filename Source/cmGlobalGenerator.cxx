4679cbe9aa905d891ab07dc70eaacbaa50b5bc5e
Stephen Kelly <steveire@gmail.com>
2015-09-20 14:16:01 +0200

    Revert "cmGlobalGenerator: Create all local generators after Configure()."
    This reverts commit e66e92256a93c69c45e0207e3cb349f7d71bf245.

--- Source/cmGlobalGenerator.cxx
+++ Source/cmGlobalGenerator.cxx
@@ -1123,6 +1123,8 @@ void cmGlobalGenerator::Configure()
   dirMf->Configure();
   dirMf->EnforceDirectoryLevelRules();
 
+  this->CreateLocalGenerators();
+
   // Put a copy of each global target in every directory.
   cmTargets globalTargets;
   this->CreateDefaultGlobalTargets(&globalTargets);
@@ -1180,7 +1182,6 @@ void cmGlobalGenerator::Configure()
 
 void cmGlobalGenerator::CreateGenerationObjects(TargetTypes targetTypes)
 {
-  this->CreateLocalGenerators();
   cmDeleteAll(this->GeneratorTargets);
   this->GeneratorTargets.clear();
   this->CreateGeneratorTargets(targetTypes);
