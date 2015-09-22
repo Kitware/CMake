bc3e1e4b5586c4404591e5f01461376eb51391d1
Michael Scott <michael.scott250@gmail.com>
2015-09-22 22:43:24 +0100

    Tests: Add case for -Wdev and -Wno-dev with non-message() warnings (#15747)

--- Tests/RunCMake/CommandLine/Wno-dev.cmake
+++ Tests/RunCMake/CommandLine/Wno-dev.cmake
@@ -1,1 +1,6 @@
 message(AUTHOR_WARNING "Some Author Warning")
+
+# without -Wno-dev this will also cause an AUTHOR_WARNING message, checks that
+# messages issued outside of the message command, by other CMake commands, also
+# are affected by -Wno-dev
+include("")
