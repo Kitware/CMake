b58de9fe2ba4ac3e194403ecd118fd54681ffae8
Domen Vrankar <domen.vrankar@gmail.com>
2015-09-22 02:08:26 +0200

    CPack: allow packaging of empty directories

---
+++ Tests/RunCMake/CPack/DEB/COMPONENTS_EMPTY_DIR-ExpectedFiles.cmake
@@ -0,0 +1,5 @@
+set(whitespaces_ "[\t\n\r ]*")
+
+set(EXPECTED_FILES_COUNT "1")
+set(EXPECTED_FILE_1 "components_empty_dir*.deb")
+set(EXPECTED_FILE_CONTENT_1 "^.*/usr/${whitespaces_}.*/usr/empty/$")
