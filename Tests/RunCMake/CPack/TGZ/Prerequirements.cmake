c326dc92fc6677df1d8e7ed345356a5001accd0e
Brad King <brad.king@kitware.com>
2015-10-01 09:25:25 -0400

    fixup! CPack: fix TGZ test missing compiler errors
    We know the CMAKE_COMMAND is always available.

--- Tests/RunCMake/CPack/TGZ/Prerequirements.cmake
+++ Tests/RunCMake/CPack/TGZ/Prerequirements.cmake
@@ -1,4 +1,4 @@
 function(get_test_prerequirements found_var config_file)
-  file(WRITE "${config_file}" "set(TAR_EXECUTABLE \"${CMAKE_COMMAND}\" -E tar)")
+  file(WRITE "${config_file}" "")
   set(${found_var} true PARENT_SCOPE)
 endfunction()
