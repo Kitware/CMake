832fe4b133f47781f4e08df1f83fdc4e3e7aa06b
Brad King <brad.king@kitware.com>
2015-10-21 11:32:11 -0400

    libarchive: Fix VS 7.1 Debug build
    This version of the MS C runtime library forgets to export
    _byteswap_ushort.

--- Utilities/cmlibarchive/libarchive/archive_read_support_format_lha.c
+++ Utilities/cmlibarchive/libarchive/archive_read_support_format_lha.c
@@ -1712,7 +1712,7 @@ lha_crc16(uint16_t crc, const void *pp, size_t len)
 	for (;len >= 8; len -= 8) {
 		/* This if statement expects compiler optimization will
 		 * remove the stament which will not be executed. */
-#ifdef _MSC_VER  /* Visual Studio */
+#if defined(_MSC_VER) && _MSC_VER >= 1400  /* Visual Studio */
 #  define bswap16(x) _byteswap_ushort(x)
 #elif (defined(__GNUC__) && __GNUC__ >= 4 && __GNUC_MINOR__ >= 8) \
       || defined(__clang__)
