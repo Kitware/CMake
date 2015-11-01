aa427a4239eb691d4129ebc383ab7b0d61b5b94e
Michael Scott <michael.scott250@gmail.com>
2015-11-01 16:05:15 +0000

    Tests: Revise message in RunCMake.CommandLine -Wdev case
    Use more prose-like capitalization.

--- Tests/RunCMake/CommandLine/Wno-dev.cmake
+++ Tests/RunCMake/CommandLine/Wno-dev.cmake
@@ -1,4 +1,4 @@
-message(AUTHOR_WARNING "Some Author Warning")
+message(AUTHOR_WARNING "Some author warning")
 
 # without -Wno-dev this will also cause an AUTHOR_WARNING message, checks that
 # messages issued outside of the message command, by other CMake commands, also
