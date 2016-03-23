38caf1bb7459d17339d06516eeb4e21787c14bd9
Kitware Robot <kwrobot@kitware.com>
2016-03-23 00:01:06 -0400

    CMake Nightly Date Stamp

--- Source/CMakeVersion.cmake
+++ Source/CMakeVersion.cmake
@@ -1,5 +1,5 @@
 # CMake version number components.
 set(CMake_VERSION_MAJOR 3)
 set(CMake_VERSION_MINOR 5)
-set(CMake_VERSION_PATCH 20160322)
+set(CMake_VERSION_PATCH 20160323)
 #set(CMake_VERSION_RC 1)
