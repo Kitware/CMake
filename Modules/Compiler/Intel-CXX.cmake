9addce99c2b38a20c86815adb13d7cf450a72512
Brad King <brad.king@kitware.com>
2016-04-18 14:20:32 -0400

    Features: Record standard flags for Intel C/C++ on Windows
    Select the `-std=` or `-Qstd=` flag based on whether Intel is GNU-like
    or MSVC-like, respectively.

--- Modules/Compiler/Intel-CXX.cmake
+++ Modules/Compiler/Intel-CXX.cmake
@@ -8,29 +8,33 @@ set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-O2 -g -DNDEBUG")
 
 set(CMAKE_DEPFILE_FLAGS_CXX "-MD -MT <OBJECT> -MF <DEPFILE>")
 
-if(NOT "x${CMAKE_CXX_SIMULATE_ID}" STREQUAL "xMSVC")
-  if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 15.0.2)
-    set(CMAKE_CXX14_STANDARD_COMPILE_OPTION "-std=c++14")
-    # todo: there is no gnu++14 value supported; figure out what to do
-    set(CMAKE_CXX14_EXTENSION_COMPILE_OPTION "-std=c++14")
-  elseif (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 15.0.0)
-    set(CMAKE_CXX14_STANDARD_COMPILE_OPTION "-std=c++1y")
-    # todo: there is no gnu++14 value supported; figure out what to do
-    set(CMAKE_CXX14_EXTENSION_COMPILE_OPTION "-std=c++1y")
-  endif()
+if("x${CMAKE_CXX_SIMULATE_ID}" STREQUAL "xMSVC")
+  set(_std -Qstd)
+else()
+  set(_std -std)
+endif()
 
-  if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 13.0)
-    set(CMAKE_CXX11_STANDARD_COMPILE_OPTION "-std=c++11")
-    set(CMAKE_CXX11_EXTENSION_COMPILE_OPTION "-std=gnu++11")
-  elseif (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.1)
-    set(CMAKE_CXX11_STANDARD_COMPILE_OPTION "-std=c++0x")
-    set(CMAKE_CXX11_EXTENSION_COMPILE_OPTION "-std=gnu++0x")
-  endif()
+if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 15.0.2)
+  set(CMAKE_CXX14_STANDARD_COMPILE_OPTION "${_std}=c++14")
+  # todo: there is no gnu++14 value supported; figure out what to do
+  set(CMAKE_CXX14_EXTENSION_COMPILE_OPTION "${_std}=c++14")
+elseif (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 15.0.0)
+  set(CMAKE_CXX14_STANDARD_COMPILE_OPTION "${_std}=c++1y")
+  # todo: there is no gnu++14 value supported; figure out what to do
+  set(CMAKE_CXX14_EXTENSION_COMPILE_OPTION "${_std}=c++1y")
+endif()
 
-  if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.1)
-    set(CMAKE_CXX98_STANDARD_COMPILE_OPTION "-std=c++98")
-    set(CMAKE_CXX98_EXTENSION_COMPILE_OPTION "-std=gnu++98")
-  endif()
+if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 13.0)
+  set(CMAKE_CXX11_STANDARD_COMPILE_OPTION "${_std}=c++11")
+  set(CMAKE_CXX11_EXTENSION_COMPILE_OPTION "${_std}=gnu++11")
+elseif (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.1)
+  set(CMAKE_CXX11_STANDARD_COMPILE_OPTION "${_std}=c++0x")
+  set(CMAKE_CXX11_EXTENSION_COMPILE_OPTION "${_std}=gnu++0x")
+endif()
+
+if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.1)
+  set(CMAKE_CXX98_STANDARD_COMPILE_OPTION "${_std}=c++98")
+  set(CMAKE_CXX98_EXTENSION_COMPILE_OPTION "${_std}=gnu++98")
 endif()
 
 if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.1)
@@ -45,6 +49,8 @@ if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12.1)
     set(CMAKE_CXX_STANDARD_DEFAULT 98)
   endif()
 endif()
+
+unset(_std)
 
 macro(cmake_record_cxx_compile_features)
   macro(_get_intel_features std_version list)
