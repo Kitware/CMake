4544c68a6ed18a42edb10903b9e0d4983a0e2f5c
Brad King <brad.king@kitware.com>
2016-05-05 15:10:01 -0400

    expat: Hard-code configuration options for our needs
    Also include our configured header on Windows too to get the same
    configuration.

--- Utilities/cmexpat/expat_config.h.cmake
+++ Utilities/cmexpat/expat_config.h.cmake
@@ -56,13 +56,13 @@
 
 /* Define to specify how much context to retain around the current parse
    point. */
-#cmakedefine XML_CONTEXT_BYTES @XML_CONTEXT_BYTES@
+#define XML_CONTEXT_BYTES 1024
 
 /* Define to make parameter entity parsing functionality available. */
-#cmakedefine XML_DTD
+/* #undef XML_DTD */
 
 /* Define to make XML Namespaces functionality available. */
-#cmakedefine XML_NS
+/* #undef XML_NS */
 
 /* Define to __FUNCTION__ or "" if `__func__' does not conform to ANSI C. */
 #ifdef _MSC_VER
--- Utilities/cmexpat/lib/winconfig.h
+++ Utilities/cmexpat/lib/winconfig.h
@@ -17,14 +17,6 @@
 #include <memory.h>
 #include <string.h>
 
-#define XML_NS 1
-#define XML_DTD 1
-#define XML_CONTEXT_BYTES 1024
-
-/* we will assume all Windows platforms are little endian */
-#define BYTEORDER 1234
-
-/* Windows has memmove() available. */
-#define HAVE_MEMMOVE
+#include "expat_config.h"
 
 #endif /* ndef WINCONFIG_H */
