9f7de3960cbf3e36ce3a83b57d48bef7116693de
Brad King <brad.king@kitware.com>
2016-05-05 15:06:15 -0400

    expat: Drop upstream test/install/packaging code

--- Utilities/cmexpat/expat_config.h.cmake
+++ Utilities/cmexpat/expat_config.h.cmake
@@ -48,21 +48,6 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #cmakedefine HAVE_UNISTD_H
 
-/* Define to the address where bug reports for this package should be sent. */
-#cmakedefine PACKAGE_BUGREPORT
-
-/* Define to the full name of this package. */
-#cmakedefine PACKAGE_NAME
-
-/* Define to the full name and version of this package. */
-#cmakedefine PACKAGE_STRING
-
-/* Define to the one symbol short name of this package. */
-#cmakedefine PACKAGE_TARNAME
-
-/* Define to the version of this package. */
-#cmakedefine PACKAGE_VERSION
-
 /* Define to 1 if you have the ANSI C header files. */
 #cmakedefine STDC_HEADERS
 
