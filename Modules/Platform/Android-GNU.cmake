47866770cee381851ccc8070f64459909a838288
Brad King <brad.king@kitware.com>
2016-06-02 10:16:11 -0400

    Android: Add placeholders for platform-specific compiler settings
    Add infrastructure modules to be loaded for Clang and GNU compilers
    when targeting Android platforms.

---
+++ Modules/Platform/Android-GNU.cmake
@@ -0,0 +1,24 @@
+#=============================================================================
+# Copyright 2015-2016 Kitware, Inc.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+# This module is shared by multiple languages; use include blocker.
+if(__ANDROID_COMPILER_GNU)
+  return()
+endif()
+set(__ANDROID_COMPILER_GNU 1)
+
+include(Platform/Android-Common)
+
+macro(__android_compiler_gnu lang)
+  __android_compiler_common(${lang})
+endmacro()
