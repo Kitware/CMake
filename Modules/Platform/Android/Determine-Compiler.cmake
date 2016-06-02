64be1ae4a3ae98e63cf35d495f0ca06c77cdf923
Brad King <brad.king@kitware.com>
2016-06-02 10:19:59 -0400

    Android: Add placeholders for platform-specific compiler selection
    Add infrastructure modules to be loaded for determining a compiler
    selection when targeting Android platforms.

---
+++ Modules/Platform/Android/Determine-Compiler.cmake
@@ -0,0 +1,21 @@
+#=============================================================================
+# Copyright 2015-2016 Kitware, Inc.
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+# This module is shared by multiple languages; use include blocker.
+if(__ANDROID_DETERMINE_COMPILER)
+  return()
+endif()
+set(__ANDROID_DETERMINE_COMPILER 1)
+
+macro(__android_determine_compiler lang)
+endmacro()
