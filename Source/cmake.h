7066218e792927cb6494ce73e834b8ddb3d275e6
Tobias Hunger <tobias.hunger@qt.io>
2016-06-10 09:54:07 +0200

    cmake: Kill cmake::CacheManager and its getter
    This member variable is never initialized and has apparently moved
    to cmState.

--- Source/cmake.h
+++ Source/cmake.h
@@ -209,9 +209,6 @@ public:
     return this->GeneratorToolset;
   }
 
-  ///! get the cmCachemManager used by this invocation of cmake
-  cmCacheManager* GetCacheManager() { return this->CacheManager; }
-
   const std::vector<std::string>& GetSourceExtensions() const
   {
     return this->SourceFileExtensions;
@@ -421,7 +418,6 @@ protected:
                          CreateExtraGeneratorFunctionType newFunction);
 
   cmGlobalGenerator* GlobalGenerator;
-  cmCacheManager* CacheManager;
   std::map<std::string, DiagLevel> DiagLevels;
   std::string GeneratorPlatform;
   std::string GeneratorToolset;
