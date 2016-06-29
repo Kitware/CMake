3f77655d060ae48fabd9dc5c445cad813a70db42
Daniel Pfeifer <daniel@pfeifer-mail.de>
2016-06-29 11:33:31 +0200

    CM_OVERRIDE: fix feature test for clang
    Clang refuses to default initialize an instance of a class that does not
    have a default constructor.  Fix the check by adding default
    constructors.  Don't use brace initialization like it is proposed in the
    error message.  We want to test the override support independent from
    the support for brace initialization.

--- Source/Checks/cm_cxx_override.cxx
+++ Source/Checks/cm_cxx_override.cxx
@@ -1,11 +1,13 @@
 struct Foo
 {
+  Foo() {}
   virtual ~Foo() {}
   virtual int test() const = 0;
 };
 
 struct Bar : Foo
 {
+  Bar() {}
   ~Bar() override {}
   int test() const override { return 0; }
 };
