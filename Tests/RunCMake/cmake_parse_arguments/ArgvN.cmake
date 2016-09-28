66c70cd9f1eb69b03cefe7fbe8e238aaa4630f47
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-09-28 15:20:42 -0400

    cmake_parse_arguments: Add additional unit tests
    Add additional unit tests for some corner cases in argument splitting.

--- Tests/RunCMake/cmake_parse_arguments/ArgvN.cmake
+++ Tests/RunCMake/cmake_parse_arguments/ArgvN.cmake
@@ -1,15 +1,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/test_utils.cmake)
 
-function(test_multi list)
-  set(i 0)
-  foreach(value IN LISTS ${list})
-    math(EXPR j "${i} + 1")
-    set(${list}[${i}] "${value}")
-    TEST(${list}[${i}] "${ARGV${j}}")
-    set(i ${j})
-  endforeach()
-endfunction()
-
 function(test1)
   cmake_parse_arguments(PARSE_ARGV 0
     pref "OPT1;OPT2" "SINGLE1;SINGLE2" "MULTI1;MULTI2")
@@ -33,7 +23,7 @@ function(test2 arg1)
   TEST(pref_OPT2 FALSE)
   TEST(pref_SINGLE1 "foo;bar")
   TEST(pref_SINGLE2 UNDEFINED)
-  test_multi(pref_MULTI1 bar "foo;bar")
+  TEST(pref_MULTI1 bar "foo;bar")
   TEST(pref_MULTI2 UNDEFINED)
   TEST(pref_UNPARSED_ARGUMENTS UNDEFINED)
 endfunction()
@@ -43,6 +33,6 @@ function(test3 arg1)
   cmake_parse_arguments(PARSE_ARGV 0
     pref "" "" "")
 
-  test_multi(pref_UNPARSED_ARGUMENTS "foo;bar" dog cat)
+  TEST(pref_UNPARSED_ARGUMENTS "foo;bar" dog cat)
 endfunction()
 test3("foo;bar" dog cat)
