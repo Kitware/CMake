af35ddc80821d6fff54c5e9ede9dc24653f0253f
Brad King <brad.king@kitware.com>
2016-09-28 09:07:42 -0400

    Tests: Add newline to RunCMake.CPack_RPM SOURCE_PACKAGE case
    The `main.cpp` that this test generates needs a newline so that
    compilers do not warn about a missing newline.  Otherwise the warning
    causes RunCMake infrastructure to detect unexpected output.

--- Tests/RunCMake/CPack/SOURCE_PACKAGE.cmake
+++ Tests/RunCMake/CPack/SOURCE_PACKAGE.cmake
@@ -1,5 +1,5 @@
 file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/main.cpp"
-    "int main() {return 0;}")
+    "int main() {return 0;}\n")
 add_executable(test_prog "${CMAKE_CURRENT_BINARY_DIR}/main.cpp")
 
 install(TARGETS test_prog DESTINATION foo COMPONENT applications)
