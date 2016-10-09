378849f41e574e72c577b549dfc2d5333ca8f9b5
Stephen Kelly <steveire@gmail.com>
2016-10-09 10:47:43 +0200

    Makefiles: Replace array access with local variable

--- Source/cmLocalUnixMakefileGenerator3.cxx
+++ Source/cmLocalUnixMakefileGenerator3.cxx
@@ -995,14 +995,15 @@ void cmLocalUnixMakefileGenerator3::AppendCustomCommand(
         std::string output;
         const std::vector<std::string>& outputs = ccg.GetOutputs();
         if (!outputs.empty()) {
+          output = outputs[0];
           if (workingDir.empty()) {
             output = this->MaybeConvertToRelativePath(
-              this->GetCurrentBinaryDirectory(), outputs[0]);
-            output = this->ConvertToOutputFormat(output,
-              cmOutputConverter::SHELL);
+              this->GetCurrentBinaryDirectory(), output);
+            output =
+              this->ConvertToOutputFormat(output, cmOutputConverter::SHELL);
           } else {
-            output = this->ConvertToOutputFormat(outputs[0],
-                                                 cmOutputConverter::SHELL);
+            output =
+              this->ConvertToOutputFormat(output, cmOutputConverter::SHELL);
           }
         }
         vars.Output = output.c_str();
