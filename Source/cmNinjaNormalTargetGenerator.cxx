20278872e3d8145f4aa81ea6cde01144daa3ca8f
Brad King <brad.king@kitware.com>
2016-10-13 13:51:10 -0400

    Ninja: Fix POST_BUILD commands on macOS Frameworks
    The condition added by commit v2.8.12~179^2 (OS X: Fix regression
    handling frameworks for Ninja, 2013-07-16) for excluding use of
    versioning symlinks on macOS Frameworks must match that used for
    POST_BUILD selection.  Otherwise we place the POST_BUILD commands after
    a symlink step that is never added.
    
    Closes: #16363

--- Source/cmNinjaNormalTargetGenerator.cxx
+++ Source/cmNinjaNormalTargetGenerator.cxx
@@ -645,7 +645,9 @@ void cmNinjaNormalTargetGenerator::WriteLinkStatement()
   std::string postBuildCmdLine = localGen.BuildCommandLine(postBuildCmdLines);
 
   cmNinjaVars symlinkVars;
-  if (targetOutput == targetOutputReal) {
+  bool const symlinkNeeded =
+    (targetOutput != targetOutputReal && !gt.IsFrameworkOnApple());
+  if (!symlinkNeeded) {
     vars["POST_BUILD"] = postBuildCmdLine;
   } else {
     vars["POST_BUILD"] = ":";
@@ -687,7 +689,7 @@ void cmNinjaNormalTargetGenerator::WriteLinkStatement()
                        commandLineLengthLimit, &usedResponseFile);
   this->WriteLinkRule(usedResponseFile);
 
-  if (targetOutput != targetOutputReal && !gt.IsFrameworkOnApple()) {
+  if (symlinkNeeded) {
     if (targetType == cmState::EXECUTABLE) {
       globalGen.WriteBuild(
         this->GetBuildFileStream(),
