e3770c56a8ef6b20f473556a1ca29ea6e0f51f7f
Daniel Pfeifer <daniel@pfeifer-mail.de>
2016-10-21 18:18:18 +0200

    BootstrapTest: Use --parallel=... argument.
    Determine the number of processors and provide it to the bootstrap
    script.  Also make sure this test does not run parallel with other
    tests.

--- Tests/BootstrapTest.cmake
+++ Tests/BootstrapTest.cmake
@@ -1,7 +1,12 @@
 file(MAKE_DIRECTORY "${bin_dir}")
-message(STATUS "running bootstrap: ${bootstrap}")
+include(ProcessorCount)
+ProcessorCount(nproc)
+if(NOT nproc EQUAL 0)
+  set(parallel_arg --parallel=${nproc})
+endif()
+message(STATUS "running bootstrap: ${bootstrap} ${parallel_arg}")
 execute_process(
-  COMMAND ${bootstrap}
+  COMMAND ${bootstrap} ${parallel_arg}
   WORKING_DIRECTORY "${bin_dir}"
   RESULT_VARIABLE result
   )
