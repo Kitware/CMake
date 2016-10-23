c401f95888938562881cd5d7c21ccea5974fe130
Daniel Pfeifer <daniel@pfeifer-mail.de>
2016-10-23 08:55:19 +0200

    Remove cmCommand::Enabled and all related accessors
    Enabled is never set to false.  Remove the member variable and all
    related getters and setters.

--- Source/cmCommand.h
+++ Source/cmCommand.h
@@ -23,11 +23,10 @@ class cmCommand
 {
 public:
   /**
-   * Construct the command. By default it is enabled with no makefile.
+   * Construct the command. By default it has no makefile.
    */
   cmCommand()
     : Makefile(CM_NULLPTR)
-    , Enabled(true)
   {
   }
 
@@ -92,26 +91,6 @@ public:
   virtual std::string GetName() const = 0;
 
   /**
-   * Enable the command.
-   */
-  void EnabledOn() { this->Enabled = true; }
-
-  /**
-   * Disable the command.
-   */
-  void EnabledOff() { this->Enabled = false; }
-
-  /**
-   * Query whether the command is enabled.
-   */
-  bool GetEnabled() const { return this->Enabled; }
-
-  /**
-   * Disable or enable the command.
-   */
-  void SetEnabled(bool enabled) { this->Enabled = enabled; }
-
-  /**
    * Return the last error string.
    */
   const char* GetError();
@@ -129,7 +108,6 @@ protected:
   cmCommandArgumentsHelper Helper;
 
 private:
-  bool Enabled;
   std::string Error;
 };
 
--- Source/cmMakefile.cxx
+++ Source/cmMakefile.cxx
@@ -266,7 +266,7 @@ bool cmMakefile::ExecuteCommand(const cmListFileFunction& lff,
     pcmd->SetMakefile(this);
 
     // Decide whether to invoke the command.
-    if (pcmd->GetEnabled() && !cmSystemTools::GetFatalErrorOccured() &&
+    if (!cmSystemTools::GetFatalErrorOccured() &&
         (this->GetCMakeInstance()->GetWorkingMode() != cmake::SCRIPT_MODE ||
          pcmd->IsScriptable()))
 
