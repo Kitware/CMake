465a85fb4615f4cfa20058c534bf3bfe71df1732
Brad King <brad.king@kitware.com>
2016-11-09 11:43:34 -0500

    librhash: Avoid signed left-shift overflow
    Fix `rhash_md5_final` to use unsigned integers for left shifting to
    avoid the possibility of undefined overflow behavior.

--- Utilities/cmlibrhash/librhash/md5.c
+++ Utilities/cmlibrhash/librhash/md5.c
@@ -213,8 +213,8 @@ void rhash_md5_final(md5_ctx *ctx, unsigned char* result)
 	/* pad message and run for last block */
 
 	/* append the byte 0x80 to the message */
-	ctx->message[index]   &= ~(0xFFFFFFFF << shift);
-	ctx->message[index++] ^= 0x80 << shift;
+	ctx->message[index]   &= ~(0xFFFFFFFFu << shift);
+	ctx->message[index++] ^= 0x80u << shift;
 
 	/* if no room left in the message to store 64-bit message length */
 	if (index > 14) {
