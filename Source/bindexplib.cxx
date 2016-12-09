845c482448e5b60f4934a48eea9c8c1e466f8fea
Bertrand Bellenot <Bertrand.Bellenot@cern.ch>
2016-12-09 11:22:23 +0000

    bindexplib: Add method for parsing and integrating `.def` files

--- Source/bindexplib.cxx
+++ Source/bindexplib.cxx
@@ -64,7 +64,7 @@
 #include "bindexplib.h"
 
 #include <cmsys/Encoding.hxx>
-#include <fstream>
+#include <cmsys/FStream.hxx>
 #include <iostream>
 #include <windows.h>
 
@@ -427,6 +427,34 @@ DumpFile(const char* filename,
 bool bindexplib::AddObjectFile(const char* filename)
 {
   return DumpFile(filename, this->Symbols, this->DataSymbols);
+}
+
+bool bindexplib::AddDefinitionFile(const char* filename)
+{
+   cmsys::ifstream infile(filename);
+   if (!infile) {
+     fprintf(stderr, "Couldn't open definition file '%s'\n", filename);
+     return false;
+   }
+   std::string str;
+   while (std::getline(infile, str)) {
+     // skip the LIBRAY and EXPORTS lines (if any)
+     if ((str.compare(0,7,"LIBRARY") == 0) ||
+         (str.compare(0,7,"EXPORTS") == 0)) {
+       continue;
+     }
+     // remove leading tabs & spaces
+     str.erase(0, str.find_first_not_of(" \t"));
+     std::size_t found = str.find(" \t DATA");
+     if (found != std::string::npos) {
+       str.erase (found, std::string::npos);
+       this->DataSymbols.insert(str);
+     } else {
+       this->Symbols.insert(str);
+     }
+   }
+   infile.close();
+   return true;
 }
 
 void bindexplib::WriteFile(FILE* file)
