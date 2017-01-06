cf3365a424f669a08d5e4b83f5fd739122b1fe3f
Iyyappa Murugandi <iyyappam@microsoft.com>
2017-01-06 15:45:47 -0800

    Follow up commit for merging with latest CMake version of kitware.

--- Tests/QtAutogen/rcc_empty.cpp
+++
@@ -1,9 +0,0 @@
-
-extern int qInitResources_rcc_empty_resource();
-
-int main(int, char**)
-{
-  // Fails to link if the symbol is not present.
-  qInitResources_rcc_empty_resource();
-  return 0;
-}
