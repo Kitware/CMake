4dde0bc8ef1d953b8e4b13a2e465d756b5274933
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2017-01-28 17:10:02 +0100

    VS: Fix WINDOWS_EXPORT_ALL_SYMBOLS for external objects
    Teach Visual Studio generators to include external object files in the
    list of objects whose symbols are to be exported.  The Makefile and
    Ninja generators already did this.

--- Source/cmGlobalVisualStudioGenerator.cxx
+++ Source/cmGlobalVisualStudioGenerator.cxx
@@ -856,6 +856,14 @@ void cmGlobalVisualStudioGenerator::AddSymbolExportCommand(
     std::string objFile = obj_dir + map_it->second;
     objs.push_back(objFile);
   }
+  std::vector<cmSourceFile const*> externalObjectSources;
+  gt->GetExternalObjects(externalObjectSources, configName);
+  for (std::vector<cmSourceFile const*>::const_iterator it =
+         externalObjectSources.begin();
+       it != externalObjectSources.end(); ++it) {
+    objs.push_back((*it)->GetFullPath());
+  }
+
   gt->UseObjectLibraries(objs, configName);
   for (std::vector<std::string>::iterator it = objs.begin(); it != objs.end();
        ++it) {
