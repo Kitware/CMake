37d47b4375fd8c15f90bd1b03f6a2c1a0ac58636
Brad King <brad.king@kitware.com>
2017-02-07 14:25:49 -0500

    Tests: Fix `file://` URLs given to curl
    Since upstream curl commit curl-7_52_0~131 (URL-parser: for
    file://[host]/ URLs, the [host] must be localhost, 2016-11-11) we can no
    longer use URLs of the form `file://c:/...` on Windows.  These worked
    only accidentally before.  Use `file:///c:/...` instead.

--- Tests/RunCMake/file/DOWNLOAD-hash-mismatch.cmake
+++ Tests/RunCMake/file/DOWNLOAD-hash-mismatch.cmake
@@ -1,5 +1,5 @@
 file(DOWNLOAD
-  "file://${CMAKE_CURRENT_SOURCE_DIR}/DOWNLOAD-hash-mismatch.txt"
+  "file:///${CMAKE_CURRENT_SOURCE_DIR}/DOWNLOAD-hash-mismatch.txt"
   ${CMAKE_CURRENT_BINARY_DIR}/hash-mismatch.txt
   EXPECTED_HASH SHA1=0123456789abcdef0123456789abcdef01234567
   STATUS status
