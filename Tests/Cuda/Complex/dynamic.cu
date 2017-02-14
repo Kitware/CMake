c4a6135039a417087b478c031849337316022263
Brad King <brad.king@kitware.com>
2017-02-14 10:16:32 -0500

    Tests: Run clang-format on CUDA code

--- Tests/Cuda/Complex/dynamic.cu
+++ Tests/Cuda/Complex/dynamic.cu
@@ -1,7 +1,7 @@
 
-#include <string>
 #include <cuda.h>
 #include <iostream>
+#include <string>
 
 #ifdef _WIN32
 #define EXPORT __declspec(dllexport)
@@ -16,18 +16,15 @@ EXPORT int __host__ cuda_dynamic_host_func(int x)
   return dynamic_base_func(x);
 }
 
-static
-__global__
-void DetermineIfValidCudaDevice()
+static __global__ void DetermineIfValidCudaDevice()
 {
 }
 
 EXPORT void cuda_dynamic_lib_func()
 {
-  DetermineIfValidCudaDevice <<<1,1>>> ();
+  DetermineIfValidCudaDevice<<<1, 1>>>();
   cudaError_t err = cudaGetLastError();
-  if(err == cudaSuccess)
-    {
+  if (err == cudaSuccess) {
     std::cerr << cudaGetErrorString(err) << std::endl;
-    }
+  }
 }
--- Tests/Cuda/Complex/mixed.cu
+++ Tests/Cuda/Complex/mixed.cu
@@ -17,9 +17,7 @@ result_type_dynamic __device__ file2_func(int x);
 
 IMPORT void __host__ cuda_dynamic_lib_func();
 
-static
-__global__
-void mixed_kernel(result_type& r, int x)
+static __global__ void mixed_kernel(result_type& r, int x)
 {
   r = file1_func(x);
   result_type_dynamic rd = file2_func(x);
@@ -30,6 +28,6 @@ EXPORT int mixed_launch_kernel(int x)
   cuda_dynamic_lib_func();
 
   result_type r;
-  mixed_kernel <<<1,1>>> (r,x);
+  mixed_kernel<<<1, 1>>>(r, x);
   return r.sum;
 }
