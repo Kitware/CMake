a3ff237a5c221be52bbd2cf7b526fd19b5202a35
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-02-17 22:43:43 +0100

    ParserHelper: Move macros to bottom of files
    Since the class name is used in the macros, the iwyu tool gets confused
    wheter it needs a forward declaration or not.
    
    While editing the files, make sure structs have no typedef.  Also,
    remove confusing comments about Java.

--- Source/cmCommandArgumentParserHelper.h
+++ Source/cmCommandArgumentParserHelper.h
@@ -8,27 +8,15 @@
 #include <string>
 #include <vector>
 
-#define YYSTYPE cmCommandArgumentParserHelper::ParserType
-#define YYSTYPE_IS_DECLARED
-#define YY_EXTRA_TYPE cmCommandArgumentParserHelper*
-#define YY_DECL                                                               \
-  int cmCommandArgument_yylex(YYSTYPE* yylvalp, yyscan_t yyscanner)
-
-/** \class cmCommandArgumentParserHelper
- * \brief Helper class for parsing java source files
- *
- * Finds dependencies for java file and list of outputs
- */
-
 class cmMakefile;
 
 class cmCommandArgumentParserHelper
 {
 public:
-  typedef struct
+  struct ParserType
   {
     char* str;
-  } ParserType;
+  };
 
   cmCommandArgumentParserHelper();
   ~cmCommandArgumentParserHelper();
@@ -99,5 +87,11 @@ private:
   bool ReplaceAtSyntax;
   bool RemoveEmpty;
 };
+
+#define YYSTYPE cmCommandArgumentParserHelper::ParserType
+#define YYSTYPE_IS_DECLARED
+#define YY_EXTRA_TYPE cmCommandArgumentParserHelper*
+#define YY_DECL                                                               \
+  int cmCommandArgument_yylex(YYSTYPE* yylvalp, yyscan_t yyscanner)
 
 #endif
