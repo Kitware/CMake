d58d28a94142bfd290ea8053a60927138ac98d4d
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-02-17 22:43:43 +0100

    ParserHelper: Move macros to bottom of files
    Since the class name is used in the macros, the iwyu tool gets confused
    wheter it needs a forward declaration or not.
    
    While editing the files, make sure structs have no typedef.  Also,
    remove confusing comments about Java.

--- Source/cmExprParserHelper.h
+++ Source/cmExprParserHelper.h
@@ -8,23 +8,13 @@
 #include <string>
 #include <vector>
 
-#define YYSTYPE cmExprParserHelper::ParserType
-#define YYSTYPE_IS_DECLARED
-#define YY_EXTRA_TYPE cmExprParserHelper*
-#define YY_DECL int cmExpr_yylex(YYSTYPE* yylvalp, yyscan_t yyscanner)
-
-/** \class cmExprParserHelper
- * \brief Helper class for parsing java source files
- *
- * Finds dependencies for java file and list of outputs
- */
 class cmExprParserHelper
 {
 public:
-  typedef struct
+  struct ParserType
   {
     int Number;
-  } ParserType;
+  };
 
   cmExprParserHelper();
   ~cmExprParserHelper();
@@ -56,5 +46,10 @@ private:
   long FileLine;
   std::string ErrorString;
 };
+
+#define YYSTYPE cmExprParserHelper::ParserType
+#define YYSTYPE_IS_DECLARED
+#define YY_EXTRA_TYPE cmExprParserHelper*
+#define YY_DECL int cmExpr_yylex(YYSTYPE* yylvalp, yyscan_t yyscanner)
 
 #endif
