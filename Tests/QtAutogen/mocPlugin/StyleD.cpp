8a3114147f430b09ccb788de5f60279afb2bfc26
Sebastian Holtermann <sebholt@xwmw.org>
2017-02-21 11:27:02 +0100

    Autogen: Don't use .moc include in Q_PLUGIN_METADATA test

--- Tests/QtAutogen/mocPlugin/StyleD.cpp
+++ Tests/QtAutogen/mocPlugin/StyleD.cpp
@@ -1,17 +1,6 @@
 #include "StyleD.hpp"
 
-class StyleD : public QStylePlugin
-{
-  Q_OBJECT
-  // Json file in global sub director
-  Q_PLUGIN_METADATA(IID "org.styles.D" FILE "sub/StyleD.json")
-public:
-  QStyle* create(const QString& key);
-};
-
 QStyle* StyleD::create(const QString& key)
 {
   return 0;
 }
-
-#include "StyleD.moc"
