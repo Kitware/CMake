94e3f82cf114d12b190d1999a72838018eb171c9
Sebastian Holtermann <sebholt@xwmw.org>
2017-02-22 21:08:36 +0100

    Autogen: Add AUTOMOC_DEPEND_FILTERS test

--- Tests/QtAutogen/mocPlugin/StyleC.hpp
+++ Tests/QtAutogen/mocPlugin/StyleC.hpp
@@ -1,6 +1,7 @@
 #ifndef STYLEC_HPP
 #define STYLEC_HPP
 
+#include "StyleCommon.hpp"
 #include <QStylePlugin>
 
 class StyleC : public QStylePlugin
@@ -8,6 +9,7 @@ class StyleC : public QStylePlugin
   Q_OBJECT
   // Json file in global root directory
   Q_PLUGIN_METADATA(IID "org.styles.C" FILE "StyleC.json")
+  A_CUSTOM_MACRO(SomeArg, "StyleC_Custom.json", AnotherArg)
 public:
   QStyle* create(const QString& key);
 };
