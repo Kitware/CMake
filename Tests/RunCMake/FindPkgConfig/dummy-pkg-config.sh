8d71fa92838f9fd3d444963c6ea6ea0d7be39cf3
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2017-02-27 17:15:49 +0100

    FindPkgConfig: use new version checking "library >= version" syntax
    Instead of the deprecated --atleast-version one.

--- Tests/RunCMake/FindPkgConfig/dummy-pkg-config.sh
+++ Tests/RunCMake/FindPkgConfig/dummy-pkg-config.sh
@@ -4,18 +4,20 @@
 # to the --exists argument with the PKG_CONFIG_PATH environment variable
 # and returns 1 if they are different.
 
-case $1 in
-  --version)
-    echo "0.0-cmake-dummy"
-    ;;
-  --exists)
-    shift
-    eval last=\${$#}
-    echo "Expected: ${last}"
-    echo "Found:    ${PKG_CONFIG_PATH}"
-    [ "${last}" = "${PKG_CONFIG_PATH}" ] || exit 1
-    ;;
-  *)
-    exit 255
-    ;;
-esac
+while [ $# -gt 0 ]; do
+  case $1 in
+    --version)
+      echo "0.0-cmake-dummy"
+      exit 0
+      ;;
+    --exists)
+      shift
+      eval last=\${$#}
+      echo "Expected: ${last}"
+      echo "Found:    ${PKG_CONFIG_PATH}"
+      [ "${last}" = "${PKG_CONFIG_PATH}" ] && exit 0 || exit 1
+      ;;
+  esac
+  shift
+done
+exit 255
