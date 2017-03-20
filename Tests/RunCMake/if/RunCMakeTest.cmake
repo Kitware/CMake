edac95b955afdea2d129b32b029bf845dc29cb71
Gregor Jasny <gjasny@googlemail.com>
2017-03-20 21:49:59 +0100

    cmIfCommand: Reject duplicate else() and misplaced elseif()
    Closes: #14335

--- Tests/RunCMake/if/RunCMakeTest.cmake
+++ Tests/RunCMake/if/RunCMakeTest.cmake
@@ -3,7 +3,11 @@ include(RunCMake)
 run_cmake(InvalidArgument1)
 run_cmake(IsDirectory)
 run_cmake(IsDirectoryLong)
+run_cmake(duplicate-deep-else)
+run_cmake(duplicate-else)
+run_cmake(duplicate-else-after-elseif)
 run_cmake(elseif-message)
+run_cmake(misplaced-elseif)
 
 run_cmake(MatchesSelf)
 
