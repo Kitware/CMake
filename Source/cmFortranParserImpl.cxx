ab5f4e81f305e48234546583a2c8c4334cea78f0
Daniel Pfeifer <daniel@pfeifer-mail.de>
2017-03-24 23:59:53 +0100

    cmFortranParserImpl: remove unnecessary include

--- Source/cmFortranParserImpl.cxx
+++ Source/cmFortranParserImpl.cxx
@@ -1,7 +1,6 @@
 /* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
    file Copyright.txt or https://cmake.org/licensing for details.  */
 #include "cmFortranParser.h"
-#include "cmFortranLexer.h"
 #include "cmSystemTools.h"
 
 #include <assert.h>
