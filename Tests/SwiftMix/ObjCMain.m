77139e320c8ec7f92e1298cc57fea7276faceb12
Gregor Jasny <gjasny@googlemail.com>
2017-03-29 15:32:01 +0200

    Swift: Simplify mixed test case to make it version agnostic
    Issue: #16742

--- Tests/SwiftMix/ObjCMain.m
+++ Tests/SwiftMix/ObjCMain.m
@@ -1,10 +1,4 @@
 #import "SwiftMix-Swift.h"
 int ObjCMain(int argc, char const* const argv[]) {
-  if ([SwiftMainClass respondsToSelector:@selector(SwiftMain:argv:)]) {
-    return [SwiftMainClass SwiftMain:argc argv:argv];
-  }
-  if ([SwiftMainClass respondsToSelector:@selector(SwiftMainWithArgc:argv:)]) {
-    return [SwiftMainClass SwiftMainWithArgc:argc argv:argv];
-  }
-  return -1;
+  return [SwiftMainClass SwiftMain];
 }
--- Tests/SwiftMix/SwiftMain.swift
+++ Tests/SwiftMix/SwiftMain.swift
@@ -1,19 +1,8 @@
 import Foundation
 
 @objc class SwiftMainClass : NSObject {
-  class func SwiftMain(argc:Int, argv:UnsafePointer<UnsafePointer<CChar>>) -> Int32 {
-    dump("argc: \(argc)")
-#if swift(>=3.0)
-    for i in 0 ..< argc {
-      let argi = String(cString:argv[i]);
-      dump("arg[\(i)]: \(argi)");
-    }
-#else
-    for (var i = 0; i < argc; ++i) {
-      let argi = String.fromCString(argv[i])
-      dump("arg[\(i)]: \(argi)");
-    }
-#endif
+  class func SwiftMain() -> Int32 {
+    dump("Hello World!");
     return 0;
   }
 }
