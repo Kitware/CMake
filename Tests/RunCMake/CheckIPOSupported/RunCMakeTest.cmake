87199ea66b171708736e85989bf9b0af89a805c1
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-04-02 21:41:33 +0800

    CheckIPOSupported: Add Fortran support

--- Tests/RunCMake/CheckIPOSupported/RunCMakeTest.cmake
+++ Tests/RunCMake/CheckIPOSupported/RunCMakeTest.cmake
@@ -2,7 +2,6 @@ include(RunCMake)
 
 run_cmake(unparsed-arguments)
 run_cmake(user-lang-unknown)
-run_cmake(user-lang-fortran)
 run_cmake(default-lang-none)
 run_cmake(not-supported-by-cmake)
 run_cmake(not-supported-by-compiler)
