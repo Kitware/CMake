Kitware/CMake

commit b888104e44b48628eecfb56e8b74704c04b44f5e

FindOpenCL: add AMDAPPSDKROOT into paths for OpenCL library
This path was only looked up for windows.  The AMD SDK also exists for
Linux, and we may want to use the OpenCL library for there.

Modules/FindOpenCL.cmake
@@ -117,7 +117,11 @@ if(WIN32)
   endif()
 else()
   find_library(OpenCL_LIBRARY
-    NAMES OpenCL)
+    NAMES OpenCL
+      ENV AMDAPPSDKROOT
+    PATH_SUFFIXES
+      lib/x86_64
+      lib/x64)
 endif()
 
 set(OpenCL_LIBRARIES ${OpenCL_LIBRARY})

commit b259fe84ab5797076e56155d9198fc17afc32ba7

FindOpenCL: Add missing PATHS keyword
The change in commit b888104e44 (FindOpenCL: add AMDAPPSDKROOT into
paths for OpenCL library, 2017-05-03) accidentally left out the `PATHS`
keyword.  Without it properly installed AMD APP SDK (OpenCL) cannot be
detected.  Tested on Ubuntu 16.04.

Modules/FindOpenCL.cmake
@@ -118,6 +118,7 @@ if(WIN32)
 else()
   find_library(OpenCL_LIBRARY
     NAMES OpenCL
+    PATHS
       ENV AMDAPPSDKROOT
     PATH_SUFFIXES
       lib/x86_64