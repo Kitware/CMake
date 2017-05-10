9b112a848a2063c81fe758984351d765e712a2bc
Chuck Atkins <chuck.atkins@kitware.com>
2017-05-10 16:39:53 -0400

    Compilers: Port to use default cmake_record_lang_compile_features macros

--- Modules/Compiler/MSVC-CXX.cmake
+++ Modules/Compiler/MSVC-CXX.cmake
@@ -1,6 +1,8 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
 
+include(Compiler/CMakeCommonCompilerMacros)
+
 if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 16.0)
   # MSVC has no specific options to set language standards, but set them as
   # empty strings anyways so the feature test infrastructure can at least check
@@ -17,15 +19,3 @@ if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 16.0)
   # There is no meaningful default for this
   set(CMAKE_CXX_STANDARD_DEFAULT "")
 endif()
-
-macro(cmake_record_cxx_compile_features)
-  if (NOT CMAKE_CXX_COMPILER_VERSION VERSION_LESS 16.0)
-    list(APPEND CMAKE_CXX_COMPILE_FEATURES
-      cxx_std_98
-      cxx_std_11
-      cxx_std_14
-      cxx_std_17
-      )
-    _record_compiler_features(CXX "" CMAKE_CXX_COMPILE_FEATURES)
-  endif()
-endmacro()
