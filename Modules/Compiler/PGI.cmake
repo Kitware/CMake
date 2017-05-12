a94ae96e6e1b8ef4e169c7c23cf95e0894e648cb
Christian Pfeiffer <cpfeiffer@live.de>
2017-05-12 19:07:35 +0200

    Windows-PGI: Adapt default compiler flags
    PGI demands -Bdynamic (/MD equivalent) for linking together dynamic
    libraries, so we should make it our default mirroring the settings of
    e.g. Visual C++ and Intel C++.

--- Modules/Compiler/PGI.cmake
+++ Modules/Compiler/PGI.cmake
@@ -18,8 +18,16 @@ macro(__compiler_pgi lang)
   string(APPEND CMAKE_${lang}_FLAGS_INIT " ")
   string(APPEND CMAKE_${lang}_FLAGS_DEBUG_INIT " -g -O0")
   string(APPEND CMAKE_${lang}_FLAGS_MINSIZEREL_INIT " -O2 -s")
-  string(APPEND CMAKE_${lang}_FLAGS_RELEASE_INIT " -fast -O3 -Mipa=fast")
+  string(APPEND CMAKE_${lang}_FLAGS_RELEASE_INIT " -fast -O3")
+  # -Mipa was dropped with PGI 16.3 from Windows versions
+  if(NOT CMAKE_HOST_WIN32 OR CMAKE_${lang}_COMPILER_VERSION VERSION_LESS 16.3)
+    string(APPEND CMAKE_${lang}_FLAGS_RELEASE_INIT " -Mipa=fast")
+  endif()
   string(APPEND CMAKE_${lang}_FLAGS_RELWITHDEBINFO_INIT " -O2 -gopt")
+
+  if(CMAKE_HOST_WIN32)
+    string(APPEND CMAKE_${lang}_FLAGS_INIT " -Bdynamic")
+  endif()
 
   # Preprocessing and assembly rules.
   set(CMAKE_${lang}_CREATE_PREPROCESSED_SOURCE "<CMAKE_${lang}_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -E <SOURCE> > <PREPROCESSED_SOURCE>")
