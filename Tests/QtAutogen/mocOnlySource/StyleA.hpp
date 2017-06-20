50b3837c6420865f46911f7359040dcb65fc112f
Sebastian Holtermann <sebholt@xwmw.org>
2017-06-20 10:41:57 +0200

    Autogen: Q_OBJECT behind brace test

---
+++ Tests/QtAutogen/mocOnlySource/StyleA.hpp
@@ -0,0 +1,15 @@
+#ifndef STYLEA_HPP
+#define STYLEA_HPP
+
+#include <QObject>
+
+/// Q_OBJECT on a single new line
+///
+class StyleA : public QObject
+{
+  Q_OBJECT
+public:
+  StyleA();
+};
+
+#endif
