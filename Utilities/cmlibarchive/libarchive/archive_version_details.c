c80d8cb293155ef9be76079c90cb159990ecb43f
Brad King <brad.king@kitware.com>
2017-07-20 12:51:15 -0400

    libarchive: Fix inclusion of zlib, bzlib, and lzma for build within CMake
    Update a new source file imported from libarchive upstream to include
    the headers for compression libraries using the CMake wrappers.

--- Utilities/cmlibarchive/libarchive/archive_version_details.c
+++ Utilities/cmlibarchive/libarchive/archive_version_details.c
@@ -34,13 +34,13 @@ __FBSDID("$FreeBSD: head/lib/libarchive/archive_util.c 201098 2009-12-28 02:58:1
 #include <string.h>
 #endif
 #ifdef HAVE_ZLIB_H
-#include <zlib.h>
+#include <cm_zlib.h>
 #endif
 #ifdef HAVE_LZMA_H
-#include <lzma.h>
+#include <cm_lzma.h>
 #endif
 #ifdef HAVE_BZLIB_H
-#include <bzlib.h>
+#include <cm_bzlib.h>
 #endif
 #ifdef HAVE_LZ4_H
 #include <lz4.h>
