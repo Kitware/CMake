6e2193a26dd3e73af41a86f672d84144beb5a3c9
Brad King <brad.king@kitware.com>
2017-11-06 07:50:08 -0500

    Tests: Avoid hanging on check for Fortran
    Use a timeout when running the check for Fortran in case the tools hang.

--- Tests/CheckFortran.cmake
+++ Tests/CheckFortran.cmake
@@ -26,6 +26,7 @@ file(WRITE \"\${CMAKE_CURRENT_BINARY_DIR}/result.cmake\"
                                -A "${CMAKE_GENERATOR_PLATFORM}"
                                -T "${CMAKE_GENERATOR_TOOLSET}"
                                ${_D_CMAKE_GENERATOR_INSTANCE}
+    TIMEOUT 60
     OUTPUT_VARIABLE output
     ERROR_VARIABLE output
     RESULT_VARIABLE result
