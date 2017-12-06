8b2736c71ca6eb8e62a69f57ad07e4e19a95c936
Brad King <brad.king@kitware.com>
2017-12-06 11:23:58 -0500

    server: Revert "Report backtraces in codemodel response"
    The backtrace information is very repetitive and hugely increases the
    size of the codemodel object.  We need to remove it until an alternative
    representation can be developed.  Revert commit v3.10.0-rc1~393^2
    (server: Report backtraces in codemodel response, 2017-06-20), except
    for the protocol version number (because it indicates other new things).
    
    Unfortunately this is incompatible with clients that expect the
    "crossReferences" field in targets.  However, the regression in memory
    usage is quite serious, especially on large projects, and therefore
    breaks even older clients that do not use backtraces.  Since the
    "crossReferences" field was only provided by one release (3.10.0), it is
    simplest to revert it outright for 3.10.1.
    
    Fixes: #17502

--- Source/cmServerDictionary.h
+++ Source/cmServerDictionary.h
@@ -89,11 +89,6 @@ static const std::string kWARN_UNUSED_KEY = "warnUnused";
 static const std::string kWATCHED_DIRECTORIES_KEY = "watchedDirectories";
 static const std::string kWATCHED_FILES_KEY = "watchedFiles";
 
-static const std::string kTARGET_CROSS_REFERENCES_KEY = "crossReferences";
-static const std::string kLINE_NUMBER_KEY = "line";
-static const std::string kBACKTRACE_KEY = "backtrace";
-static const std::string kRELATED_STATEMENTS_KEY = "relatedStatements";
-
 static const std::string kSTART_MAGIC = "[== \"CMake Server\" ==[";
 static const std::string kEND_MAGIC = "]== \"CMake Server\" ==]";
 
