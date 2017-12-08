6273b8354902e1af124d6f9b4a0ba23cf5e8a9b9
Sebastian Holtermann <sebholt@xwmw.org>
2017-12-08 17:38:13 +0100

    Autogen: Tests: Separate uicOnly test

--- Tests/QtAutogen/CommonTests.cmake
+++ Tests/QtAutogen/CommonTests.cmake
@@ -1,3 +1,6 @@
 # Autogen tests common for Qt4 and Qt5
 ADD_AUTOGEN_TEST(MocOnly mocOnly)
 ADD_AUTOGEN_TEST(MocOptions mocOptions)
+if(QT_TEST_ALLOW_QT_MACROS)
+  ADD_AUTOGEN_TEST(UicOnly uicOnly)
+endif()
