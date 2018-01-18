ff91a5d54b5458eaebab6be814624925a02eccd7
Sebastian Holtermann <sebholt@xwmw.org>
2018-01-18 10:31:49 +0100

    Autogen: Tests: Extend AUTOMOC include patterns test

--- Tests/QtAutogen/MocOnly/main.cpp
+++ Tests/QtAutogen/MocOnly/main.cpp
@@ -1,3 +1,5 @@
+#include "IncA.hpp"
+#include "IncB.hpp"
 #include "StyleA.hpp"
 #include "StyleB.hpp"
 
@@ -5,5 +7,8 @@ int main(int argv, char** args)
 {
   StyleA styleA;
   StyleB styleB;
+  IncA incA;
+  IncB incB;
+
   return 0;
 }
