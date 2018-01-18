b4e6911bf51c05001c1b3cbcc04587afa6ba4577
Sebastian Holtermann <sebholt@xwmw.org>
2018-01-18 11:13:41 +0100

    Autogen: Tests: Extend AUTOUIC include patterns test

--- Tests/QtAutogen/CommonTests.cmake
+++ Tests/QtAutogen/CommonTests.cmake
@@ -1,9 +1,7 @@
 # Autogen tests common for Qt4 and Qt5
 ADD_AUTOGEN_TEST(MocOnly mocOnly)
 ADD_AUTOGEN_TEST(MocOptions mocOptions)
-if(QT_TEST_ALLOW_QT_MACROS)
-  ADD_AUTOGEN_TEST(UicOnly uicOnly)
-endif()
+ADD_AUTOGEN_TEST(UicOnly uicOnly)
 ADD_AUTOGEN_TEST(RccOnly rccOnly)
 ADD_AUTOGEN_TEST(RccEmpty rccEmpty)
 ADD_AUTOGEN_TEST(RccOffMocLibrary)
