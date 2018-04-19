b1f95e5b1477794c0ee3a56a4ea016a34c3e3d5c
Brad King <brad.king@kitware.com>
2018-04-19 11:26:06 -0400

    Fortran: Extend submodule test with great-grandchild
    Name the module using CamelCase to test lower-case file name conversion.
    Also add coverage of existing "sibling" module.

--- Tests/FortranModules/Submodules/parent.f90
+++ Tests/FortranModules/Submodules/parent.f90
@@ -7,9 +7,14 @@ module parent
     module function child_function() result(child_stuff)
       logical :: child_stuff
     end function
+    module function sibling_function() result(sibling_stuff)
+      logical :: sibling_stuff
+    end function
 
     ! Test Fortran 2008 "module subroutine" syntax
     module subroutine grandchild_subroutine()
+    end subroutine
+    module subroutine GreatGrandChild_subroutine()
     end subroutine
 
   end interface
