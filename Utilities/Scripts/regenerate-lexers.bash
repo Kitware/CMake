3d382a494fd8d4aad1a4206559c316138441098d
Daniel Franke <franke@edf-online.de>
2018-05-09 10:11:57 +0200

    regenerate-lexers.bash: Add option to check timestamps

--- Utilities/Scripts/regenerate-lexers.bash
+++ Utilities/Scripts/regenerate-lexers.bash
@@ -2,6 +2,11 @@
 
 set -e
 
+forced=1
+if [[ "${1}" = "make" ]]; then
+    forced=0
+fi
+
 pushd "${BASH_SOURCE%/*}/../../Source/LexerParser" > /dev/null
 
 for lexer in            \
@@ -14,11 +19,15 @@ do
     h_file=cm${lexer}Lexer.h
     in_file=cm${lexer}Lexer.in.l
 
+    if [[ (${in_file} -nt ${cxx_file}) || (${in_file} -nt ${h_file}) || (${forced} -gt 0) ]]; then
     echo "Generating Lexer ${lexer}"
-    flex --nounistd -DFLEXINT_H --noline --header-file=${h_file} -o${cxx_file} ${in_file}
-    sed -i 's/\s*$//'                       ${h_file} ${cxx_file}   # remove trailing whitespaces
-    sed -i '${/^$/d;}'                      ${h_file} ${cxx_file}   # remove blank line at the end
-    sed -i '1i#include "cmStandardLexer.h"' ${cxx_file}             # add cmStandardLexer.h include
+        flex --nounistd -DFLEXINT_H --noline --header-file=${h_file} -o${cxx_file} ${in_file}
+        sed -i 's/\s*$//'                       ${h_file} ${cxx_file}   # remove trailing whitespaces
+        sed -i '${/^$/d;}'                      ${h_file} ${cxx_file}   # remove blank line at the end
+        sed -i '1i#include "cmStandardLexer.h"' ${cxx_file}             # add cmStandardLexer.h include
+    else
+        echo "Skipped generating Lexer ${lexer}"
+    fi
 done
 
 
@@ -28,11 +37,15 @@ do
     c_file=cm${lexer}Lexer.c
     in_file=cm${lexer}Lexer.in.l
 
-    echo "Generating Lexer ${lexer}"
-    flex --nounistd -DFLEXINT_H --noline -o${c_file} ${in_file}
-    sed -i 's/\s*$//'                       ${c_file}   # remove trailing whitespaces
-    sed -i '${/^$/d;}'                      ${c_file}   # remove blank line at the end
-    sed -i '1i#include "cmStandardLexer.h"' ${c_file}   # add cmStandardLexer.h include
+    if [[ (${in_file} -nt ${c_file}) || (${forced} -gt 0) ]]; then
+        echo "Generating Lexer ${lexer}"
+        flex --nounistd -DFLEXINT_H --noline -o${c_file} ${in_file}
+        sed -i 's/\s*$//'                       ${c_file}   # remove trailing whitespaces
+        sed -i '${/^$/d;}'                      ${c_file}   # remove blank line at the end
+        sed -i '1i#include "cmStandardLexer.h"' ${c_file}   # add cmStandardLexer.h include
+    else
+        echo "Skipped generating Lexer ${lexer}"
+    fi
 
 done
 
