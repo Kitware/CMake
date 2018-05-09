5cd9f5c9656660bbe1995ab308f7d322226536a5
Daniel Franke <franke@edf-online.de>
2018-05-09 10:11:57 +0200

    regenerate-lexers.bash: Factor file names into variables

--- Utilities/Scripts/regenerate-lexers.bash
+++ Utilities/Scripts/regenerate-lexers.bash
@@ -10,23 +10,29 @@ for lexer in            \
     Expr                \
     Fortran
 do
+    cxx_file=cm${lexer}Lexer.cxx
+    h_file=cm${lexer}Lexer.h
+    in_file=cm${lexer}Lexer.in.l
+
     echo "Generating Lexer ${lexer}"
-    flex --nounistd -DFLEXINT_H --noline --header-file=cm${lexer}Lexer.h -ocm${lexer}Lexer.cxx cm${lexer}Lexer.in.l
-    sed -i 's/\s*$//'                       cm${lexer}Lexer.h cm${lexer}Lexer.cxx   # remove trailing whitespaces
-    sed -i '${/^$/d;}'                      cm${lexer}Lexer.h cm${lexer}Lexer.cxx   # remove blank line at the end
-    sed -i '1i#include "cmStandardLexer.h"' cm${lexer}Lexer.cxx                     # add cmStandardLexer.h include
+    flex --nounistd -DFLEXINT_H --noline --header-file=${h_file} -o${cxx_file} ${in_file}
+    sed -i 's/\s*$//'                       ${h_file} ${cxx_file}   # remove trailing whitespaces
+    sed -i '${/^$/d;}'                      ${h_file} ${cxx_file}   # remove blank line at the end
+    sed -i '1i#include "cmStandardLexer.h"' ${cxx_file}             # add cmStandardLexer.h include
 done
 
 
 # these lexers (at the moment only the ListFileLexer) are compiled as C and do not generate a header
 for lexer in ListFile
 do
+    c_file=cm${lexer}Lexer.c
+    in_file=cm${lexer}Lexer.in.l
 
     echo "Generating Lexer ${lexer}"
-    flex --nounistd -DFLEXINT_H --noline -ocm${lexer}Lexer.c cm${lexer}Lexer.in.l
-    sed -i 's/\s*$//'                       cm${lexer}Lexer.c   # remove trailing whitespaces
-    sed -i '${/^$/d;}'                      cm${lexer}Lexer.c   # remove blank line at the end
-    sed -i '1i#include "cmStandardLexer.h"' cm${lexer}Lexer.c   # add cmStandardLexer.h include
+    flex --nounistd -DFLEXINT_H --noline -o${c_file} ${in_file}
+    sed -i 's/\s*$//'                       ${c_file}   # remove trailing whitespaces
+    sed -i '${/^$/d;}'                      ${c_file}   # remove blank line at the end
+    sed -i '1i#include "cmStandardLexer.h"' ${c_file}   # add cmStandardLexer.h include
 
 done
 
