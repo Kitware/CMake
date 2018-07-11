1638ffa287e5032eecb1d8eaa1051a29379f6017
Kyle Edwards <kyle.edwards@kitware.com>
2018-07-11 14:32:00 -0400

    Tests: Add tests for install(<TARGETS|FILES> ... OPTIONAL)

--- Tests/RunCMake/install/RunCMakeTest.cmake
+++ Tests/RunCMake/install/RunCMakeTest.cmake
@@ -73,6 +73,8 @@ if(NOT RunCMake_GENERATOR STREQUAL "Xcode" OR NOT "$ENV{CMAKE_OSX_ARCHITECTURES}
 endif()
 
 run_install_test(TARGETS-InstallFromSubDir)
+run_install_test(TARGETS-OPTIONAL)
+run_install_test(FILES-OPTIONAL)
 
 set(run_install_test_components 1)
 run_install_test(FILES-EXCLUDE_FROM_ALL)
