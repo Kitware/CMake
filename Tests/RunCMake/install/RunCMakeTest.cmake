4108ab4be2307ef161467de46e4c35e8b086ce02
Kyle Edwards <kyle.edwards@kitware.com>
2018-07-12 10:04:37 -0400

    Tests: Add test for install(DIRECTORY) with PATTERN and REGEX

--- Tests/RunCMake/install/RunCMakeTest.cmake
+++ Tests/RunCMake/install/RunCMakeTest.cmake
@@ -85,6 +85,7 @@ run_install_test(Deprecated)
 run_install_test(PRE_POST_INSTALL_SCRIPT)
 run_install_test(SCRIPT)
 run_install_test(TARGETS-CONFIGURATIONS)
+run_install_test(DIRECTORY-PATTERN)
 
 set(run_install_test_components 1)
 run_install_test(FILES-EXCLUDE_FROM_ALL)
