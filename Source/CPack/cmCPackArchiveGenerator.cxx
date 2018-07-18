22fbf39d9ba968a4827cb1580dbbe11e4a8a0619
Sylvain Joubert <joubert.sy@gmail.com>
2018-07-18 17:15:52 +0200

    CPack: Fix log typos (spaces in the wrong places)

--- Source/CPack/cmCPackArchiveGenerator.cxx
+++ Source/CPack/cmCPackArchiveGenerator.cxx
@@ -106,15 +106,15 @@ int cmCPackArchiveGenerator::addOneComponentToArchive(
   gf.Open((filename).c_str(), false, true);                                   \
   if (!GenerateHeader(&gf)) {                                                 \
     cmCPackLogger(cmCPackLog::LOG_ERROR,                                      \
-                  "Problem to generate Header for archive < "                 \
+                  "Problem to generate Header for archive <"                  \
                     << (filename) << ">." << std::endl);                      \
     return 0;                                                                 \
   }                                                                           \
   cmArchiveWrite archive(gf, this->Compress, this->ArchiveFormat);            \
   if (!(archive)) {                                                           \
     cmCPackLogger(cmCPackLog::LOG_ERROR,                                      \
-                  "Problem to create archive < "                              \
-                    << (filename) << ">. ERROR =" << (archive).GetError()     \
+                  "Problem to create archive <"                               \
+                    << (filename) << ">, ERROR = " << (archive).GetError()    \
                     << std::endl);                                            \
     return 0;                                                                 \
   }
@@ -262,9 +262,9 @@ int cmCPackArchiveGenerator::PackageFiles()
     archive.Add(rp, 0, nullptr, false);
     if (!archive) {
       cmCPackLogger(cmCPackLog::LOG_ERROR,
-                    "Problem while adding file< "
+                    "Problem while adding file <"
                       << file << "> to archive <" << packageFileNames[0]
-                      << "> .ERROR =" << archive.GetError() << std::endl);
+                      << ">, ERROR = " << archive.GetError() << std::endl);
       return 0;
     }
   }
