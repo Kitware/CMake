7a7e94d6f7b74eeb165cab39f3aeeaba690160ef
David Faure <faure@kde.org>
2018-08-30 19:52:19 +0200

    CPack (DEB/RPM): add test for per-component description/summmary.

--- Tests/RunCMake/CPack/tests/PER_COMPONENT_FIELDS/test.cmake
+++ Tests/RunCMake/CPack/tests/PER_COMPONENT_FIELDS/test.cmake
@@ -11,6 +11,13 @@ if(GENERATOR_TYPE STREQUAL "DEB" OR GENERATOR_TYPE STREQUAL "RPM")
   set(CPACK_${GENERATOR_TYPE}${generator_type_suffix_}_PACKAGE_${group_} "default")
   set(CPACK_${GENERATOR_TYPE}${generator_type_suffix_}_PKG_2_PACKAGE_NAME "second")
   set(CPACK_${GENERATOR_TYPE}${generator_type_suffix_}_PKG_2_PACKAGE_${group_} "second_group")
+
+  set(CPACK_${GENERATOR_TYPE}${generator_type_suffix_}_PACKAGE_SUMMARY "Global summary") # not used for DEB
+  set(CPACK_${GENERATOR_TYPE}${generator_type_suffix_}_PKG_2_PACKAGE_SUMMARY "Summary for pkg_2") # not used for DEB
+
+  set(CPACK_COMPONENT_PKG_1_DESCRIPTION "Description for pkg_1")
+  set(CPACK_COMPONENT_PKG_2_DESCRIPTION "Description for pkg_2")
+  set(CPACK_COMPONENT_PKG_3_DESCRIPTION "Description for pkg_3")
 endif()
 
 install(FILES CMakeLists.txt DESTINATION foo COMPONENT pkg_1)
