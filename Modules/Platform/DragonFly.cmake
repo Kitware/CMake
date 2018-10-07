95bd6317bc199135775a2f19ee7890d3e6991e3d
Peter Wu <peter@lekensteyn.nl>
2018-10-07 22:06:11 +0200

    RPATH: Record support for $ORIGIN on various *BSD
    All of NetBSD, FreeBSD, OpenBSD and DragonFly BSD support `$ORIGIN`,
    but the last two require `-z origin` as documented at
    
        https://lekensteyn.nl/rpath.html
    
    The `-z origin` option causes a flag bit to be set and has no effect if
    the `RPATH` does not contain expandable tokens.

--- Modules/Platform/DragonFly.cmake
+++ Modules/Platform/DragonFly.cmake
@@ -3,3 +3,7 @@
 # see http://archive.netbsd.se/?ml=dfbsd-users&a=2007-07&m=4678361
 
 include(Platform/FreeBSD)
+
+# DragonFly BSD requires -z origin to enable $ORIGIN expansion in RPATH.
+# This is not required for FreeBSD since 10.2-RELEASE.
+set(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "-Wl,-z,origin,-rpath,")
