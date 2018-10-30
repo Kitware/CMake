b71a9598a408a134bd7d003b6586b345f2b961fe
Sebastian Holtermann <sebholt@xwmw.org>
2018-10-30 11:21:12 +0100

    Autogen: Add test for AUTOGEN_ORIGIN_DEPENDS=OFF

---
+++ Tests/QtAutogen/AutogenOriginDependsOff/b_qt.cpp
@@ -0,0 +1,28 @@
+
+#include "b_qt.hpp"
+#include <b_mc.hpp>
+
+namespace b_qt {
+
+/// @brief A source local QObject based class
+class Source_QObject : public QObject
+{
+  Q_OBJECT
+public:
+  Source_QObject() {}
+  ~Source_QObject() {}
+
+  std::string str;
+};
+
+std::string mocs_compilation()
+{
+  // Create and destroy QObject based classes
+  Header_QObject header_obj;
+  Source_QObject source_obj;
+
+  return std::string(b_mc::mocs_compilation());
+}
+}
+
+#include "b_qt.moc"
