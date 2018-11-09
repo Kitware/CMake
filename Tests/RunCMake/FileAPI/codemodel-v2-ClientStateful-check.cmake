ea0a0601682af00706cf57f6d3e27d4f0895ecbb
Kyle Edwards <kyle.edwards@kitware.com>
2018-11-09 13:54:41 -0500

    fileapi: Add test for codemodel v2

--- Tests/RunCMake/FileAPI/codemodel-v2-ClientStateful-check.cmake
+++ Tests/RunCMake/FileAPI/codemodel-v2-ClientStateful-check.cmake
@@ -3,8 +3,9 @@ set(expect
   query/client-foo
   query/client-foo/query.json
   reply
-  reply/codemodel-v2-[0-9a-f]+.json
-  reply/index-[0-9.T-]+.json
+  reply/codemodel-v2-[0-9a-f]+\\.json
+  reply/index-[0-9.T-]+\\.json
+  .*
   )
 check_api("^${expect}$")
 
