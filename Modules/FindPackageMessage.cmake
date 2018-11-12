89b5f4fcc7b8d3942fd92409ac57ddc3e7328560
Joachim Wuttke (l) <j.wuttke@fz-juelich.de>
2018-11-12 14:35:44 +0100

    Help: Correct macro -> function in FindPackageMessage.
    Correct in the text, and change capitalization accordingly.

--- Modules/FindPackageMessage.cmake
+++ Modules/FindPackageMessage.cmake
@@ -5,14 +5,14 @@
 FindPackageMessage
 ------------------
 
+.. code-block:: cmake
 
+  find_package_message(<name> "message for user" "find result details")
 
-FIND_PACKAGE_MESSAGE(<name> "message for user" "find result details")
-
-This macro is intended to be used in FindXXX.cmake modules files.  It
-will print a message once for each unique find result.  This is useful
-for telling the user where a package was found.  The first argument
-specifies the name (XXX) of the package.  The second argument
+This function is intended to be used in FindXXX.cmake modules files.
+It will print a message once for each unique find result.  This is
+useful for telling the user where a package was found.  The first
+argument specifies the name (XXX) of the package.  The second argument
 specifies the message to display.  The third argument lists details
 about the find result so that if they change the message will be
 displayed again.  The macro also obeys the QUIET argument to the
@@ -20,17 +20,17 @@ find_package command.
 
 Example:
 
-::
+.. code-block:: cmake
 
   if(X11_FOUND)
-    FIND_PACKAGE_MESSAGE(X11 "Found X11: ${X11_X11_LIB}"
+    find_package_message(X11 "Found X11: ${X11_X11_LIB}"
       "[${X11_X11_LIB}][${X11_INCLUDE_DIR}]")
   else()
    ...
   endif()
 #]=======================================================================]
 
-function(FIND_PACKAGE_MESSAGE pkg msg details)
+function(find_package_message pkg msg details)
   # Avoid printing a message repeatedly for the same find result.
   if(NOT ${pkg}_FIND_QUIETLY)
     string(REPLACE "\n" "" details "${details}")
