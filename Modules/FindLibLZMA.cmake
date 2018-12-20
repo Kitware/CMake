feafc7f3fed89d1a77ea7e3602b8e6eb57fec9d9
Marc Chevrier <marc.chevrier@gmail.com>
2018-12-20 15:26:51 +0100

    Help: FindLibLZMA: update formatting.

--- Modules/FindLibLZMA.cmake
+++ Modules/FindLibLZMA.cmake
@@ -5,34 +5,40 @@
 FindLibLZMA
 -----------
 
-Find LibLZMA
-
-Find LibLZMA headers and library
+Find LZMA compression algorithm headers and library.
 
 
-IMPORTED Targets
+Imported Targets
 ^^^^^^^^^^^^^^^^
 
 This module defines :prop_tgt:`IMPORTED` target ``LibLZMA::LibLZMA``, if
-LibLZMA has been found.
+liblzma has been found.
 
 Result variables
 ^^^^^^^^^^^^^^^^
 
 This module will set the following variables in your project:
 
-::
-
-  LIBLZMA_FOUND             - True if liblzma is found.
-  LIBLZMA_INCLUDE_DIRS      - Directory where liblzma headers are located.
-  LIBLZMA_LIBRARIES         - Lzma libraries to link against.
-  LIBLZMA_HAS_AUTO_DECODER  - True if lzma_auto_decoder() is found (required).
-  LIBLZMA_HAS_EASY_ENCODER  - True if lzma_easy_encoder() is found (required).
-  LIBLZMA_HAS_LZMA_PRESET   - True if lzma_lzma_preset() is found (required).
-  LIBLZMA_VERSION_MAJOR     - The major version of lzma
-  LIBLZMA_VERSION_MINOR     - The minor version of lzma
-  LIBLZMA_VERSION_PATCH     - The patch version of lzma
-  LIBLZMA_VERSION_STRING    - version number as a string (ex: "5.0.3")
+``LIBLZMA_FOUND``
+  True if liblzma headers and library were found.
+``LIBLZMA_INCLUDE_DIRS``
+  Directory where liblzma headers are located.
+``LIBLZMA_LIBRARIES``
+  Lzma libraries to link against.
+``LIBLZMA_HAS_AUTO_DECODER``
+  True if lzma_auto_decoder() is found (required).
+``LIBLZMA_HAS_EASY_ENCODER``
+  True if lzma_easy_encoder() is found (required).
+``LIBLZMA_HAS_LZMA_PRESET``
+  True if lzma_lzma_preset() is found (required).
+``LIBLZMA_VERSION_MAJOR``
+  The major version of lzma
+``LIBLZMA_VERSION_MINOR``
+  The minor version of lzma
+``LIBLZMA_VERSION_PATCH``
+  The patch version of lzma
+``LIBLZMA_VERSION_STRING``
+  version number as a string (ex: "5.0.3")
 #]=======================================================================]
 
 find_path(LIBLZMA_INCLUDE_DIR lzma.h )
