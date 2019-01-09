5a5a1d90f01d6364426be5f08244e9acf0526734
Joachim Wuttke (o) <j.wuttke@fz-juelich.de>
2019-01-09 11:01:46 +0100

    Help: FindThreads not needed with modern C++.

--- Modules/FindThreads.cmake
+++ Modules/FindThreads.cmake
@@ -29,9 +29,12 @@ caller can set
 
   THREADS_PREFER_PTHREAD_FLAG
 
-Please note that the compiler flag can only be used with the imported
+The compiler flag can only be used with the imported
 target. Use of both the imported target as well as this switch is highly
 recommended for new code.
+
+This module is not needed for C++11 and later if threading is done using
+``std::thread`` from the standard library.
 #]=======================================================================]
 
 include (CheckLibraryExists)
