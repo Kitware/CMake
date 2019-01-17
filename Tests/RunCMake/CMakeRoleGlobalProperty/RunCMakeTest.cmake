264bdac185f85e0508982df1b6efd7c3b8a1d1a4
Kyle Edwards <kyle.edwards@kitware.com>
2019-01-17 12:02:11 -0500

    CMAKE_ROLE: Fix value for ctest --build-and-test
    --build-and-test builds a CMake project, so CMAKE_ROLE should be
    PROJECT. Fix this and add a test case.

--- Tests/RunCMake/CMakeRoleGlobalProperty/RunCMakeTest.cmake
+++ Tests/RunCMake/CMakeRoleGlobalProperty/RunCMakeTest.cmake
@@ -5,3 +5,10 @@ run_cmake(Project)
 run_cmake_command(Script "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_LIST_DIR}/Script.cmake")
 run_cmake_command(FindPackage "${CMAKE_COMMAND}" --find-package -DNAME=DummyPackage -DCOMPILER_ID=GNU -DLANGUAGE=CXX -DMODE=EXIST "-DCMAKE_MODULE_PATH=${CMAKE_CURRENT_LIST_DIR}")
 run_ctest(CTest)
+run_cmake_command(BuildAndTest "${CMAKE_CTEST_COMMAND}"
+  --build-and-test
+      "${RunCMake_SOURCE_DIR}/BuildAndTest"
+      "${RunCMake_BINARY_DIR}/BuildAndTest-build"
+  --build-project CMakeRoleGlobalPropertyBuildAndTest
+  --build-generator "${RunCMake_GENERATOR}"
+  )
