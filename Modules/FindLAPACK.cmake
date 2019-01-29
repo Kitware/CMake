b323407235a3671eb11c7a039ce1c2f134025c73
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-01-29 14:31:01 -0500

    Find{BLAS,LAPACK}: Update docs to use modern conventions

--- Modules/FindLAPACK.cmake
+++ Modules/FindLAPACK.cmake
@@ -14,35 +14,51 @@ The approach follows that taken for the autoconf macro file,
 acx_lapack.m4 (distributed at
 http://ac-archive.sourceforge.net/ac-archive/acx_lapack.html).
 
-This module sets the following variables:
+Input Variables
+^^^^^^^^^^^^^^^
 
-::
+The following variables may be set to influence this module's behavior:
 
-  LAPACK_FOUND - set to true if a library implementing the LAPACK interface
-    is found
-  LAPACK_LINKER_FLAGS - uncached list of required linker flags (excluding -l
-    and -L).
-  LAPACK_LIBRARIES - uncached list of libraries (using full path name) to
-    link against to use LAPACK
-  LAPACK95_LIBRARIES - uncached list of libraries (using full path name) to
-    link against to use LAPACK95
-  LAPACK95_FOUND - set to true if a library implementing the LAPACK f95
-    interface is found
-  BLA_STATIC  if set on this determines what kind of linkage we do (static)
-  BLA_VENDOR  if set checks only the specified vendor, if not set checks
-     all the possibilities
-  BLA_F95     if set on tries to find the f95 interfaces for BLAS/LAPACK
+``BLA_STATIC``
+  if ``ON`` use static linkage
 
-List of vendors (BLA_VENDOR) valid in this module:
+``BLA_VENDOR``
+  If set, checks only the specified vendor, if not set checks all the
+  possibilities.  List of vendors valid in this module:
 
-* Intel(mkl)
-* OpenBLAS
-* FLAME
-* ACML
-* Apple
-* NAS
-* Generic
+  * ``Intel10_32`` (intel mkl v10 32 bit)
+  * ``Intel10_64lp`` (intel mkl v10+ 64 bit, threaded code, lp64 model)
+  * ``Intel10_64lp_seq`` (intel mkl v10+ 64 bit, sequential code, lp64 model)
+  * ``Intel10_64ilp`` (intel mkl v10+ 64 bit, threaded code, ilp64 model)
+  * ``Intel10_64ilp_seq`` (intel mkl v10+ 64 bit, sequential code, ilp64 model)
+  * ``Intel`` (obsolete versions of mkl 32 and 64 bit)
+  * ``OpenBLAS``
+  * ``FLAME``
+  * ``ACML``
+  * ``Apple``
+  * ``NAS``
+  * ``Generic``
 
+``BLA_F95``
+  if ``ON`` tries to find BLAS95/LAPACK95
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module defines the following variables:
+
+``LAPACK_FOUND``
+  library implementing the LAPACK interface is found
+``LAPACK_LINKER_FLAGS``
+  uncached list of required linker flags (excluding -l and -L).
+``LAPACK_LIBRARIES``
+  uncached list of libraries (using full path name) to link against
+  to use LAPACK
+``LAPACK95_LIBRARIES``
+  uncached list of libraries (using full path name) to link against
+  to use LAPACK95
+``LAPACK95_FOUND``
+  library implementing the LAPACK95 interface is found
 #]=======================================================================]
 
 set(_lapack_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
