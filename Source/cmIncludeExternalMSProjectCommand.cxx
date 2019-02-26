Kitware/CMake

commit b99129d2d81a494055d40443e8e044c9db4ca807

ENH: some code cleanup

Source/cmIncludeExternalMSProjectCommand.cxx
@@ -52,7 +52,7 @@ bool cmIncludeExternalMSProjectCommand
     // Create a target instance for this utility.
     cmTarget target;
     target.SetType(cmTarget::UTILITY, utility_name.c_str());
-    target.SetInAll(true);
+    target.SetProperty("EXCLUDE_FROM_ALL","FALSE");
     target.SetMakefile(this->Makefile);
     std::vector<std::string> no_outputs;
     cmCustomCommandLines commandLines;

Source/cmAddExecutableCommand.cxx
@@ -31,7 +31,7 @@ bool cmAddExecutableCommand::InitialPass(std::vector<std::string> const& args)
   ++s;
   bool use_win32 = false;
   bool use_macbundle = false;
-  bool in_all = true;
+  bool excludeFromAll = false;
   while ( s != args.end() )
     {
     if (*s == "WIN32")
@@ -47,7 +47,7 @@ bool cmAddExecutableCommand::InitialPass(std::vector<std::string> const& args)
     else if(*s == "EXCLUDE_FROM_ALL")
       {
       ++s;
-      in_all = false;
+      excludeFromAll = true;
       }
     else
       {
@@ -64,7 +64,7 @@ bool cmAddExecutableCommand::InitialPass(std::vector<std::string> const& args)
 
   std::vector<std::string> srclists(s, args.end());
   cmTarget* tgt = this->Makefile->AddExecutable(exename.c_str(), srclists,
-                                                in_all);
+                                                excludeFromAll);
   if ( use_win32 )
     {
     tgt->SetProperty("WIN32_EXECUTABLE", "ON");

Source/cmAddLibraryCommand.cxx
@@ -28,7 +28,7 @@ bool cmAddLibraryCommand::InitialPass(std::vector<std::string> const& args)
   // otherwise it defaults to static library.
   int shared = 
     !cmSystemTools::IsOff(this->Makefile->GetDefinition("BUILD_SHARED_LIBS"));
-  bool in_all = true;
+  bool excludeFromAll = false;
   
   std::vector<std::string>::const_iterator s = args.begin();
 
@@ -60,7 +60,7 @@ bool cmAddLibraryCommand::InitialPass(std::vector<std::string> const& args)
     else if(*s == "EXCLUDE_FROM_ALL")
       {
       ++s;
-      in_all = false;
+      excludeFromAll = true;
       }
     else
       {
@@ -85,7 +85,7 @@ bool cmAddLibraryCommand::InitialPass(std::vector<std::string> const& args)
     }
 
   this->Makefile->AddLibrary(this->LibName.c_str(), shared, srclists,
-                             in_all);
+                             excludeFromAll);
   
   return true;
 }

Source/cmAddSubDirectoryCommand.cxx
@@ -30,7 +30,7 @@ bool cmAddSubDirectoryCommand::InitialPass
   std::string srcArg = args[0];
   std::string binArg;
   
-  bool intoplevel = true;
+  bool excludeFromAll = false;
 
   // process the rest of the arguments looking for optional args
   std::vector<std::string>::const_iterator i = args.begin();
@@ -39,7 +39,7 @@ bool cmAddSubDirectoryCommand::InitialPass
     {
     if(*i == "EXCLUDE_FROM_ALL")
       {
-      intoplevel = false;
+      excludeFromAll = true;
       continue;
       }
     else if (!binArg.size())
@@ -122,7 +122,7 @@ bool cmAddSubDirectoryCommand::InitialPass
 
   // Add the subdirectory using the computed full paths.
   this->Makefile->AddSubDirectory(srcPath.c_str(), binPath.c_str(),
-                                  intoplevel, false, true);
+                                  excludeFromAll, false, true);
 
   return true;
 }

Source/cmGlobalGenerator.cxx
@@ -1006,13 +1006,13 @@ bool cmGlobalGenerator::IsExcluded(cmLocalGenerator* root,
   cmLocalGenerator* cur = gen->GetParent();
   while(cur && cur != root)
     {
-    if(cur->GetExcludeAll())
+    if(cur->GetMakefile()->GetPropertyAsBool("EXCLUDE_FROM_ALL"))
       {
       return true;
       }
     cur = cur->GetParent();
     }
-  return gen->GetExcludeAll();
+  return gen->GetMakefile()->GetPropertyAsBool("EXCLUDE_FROM_ALL");
 }
 
 
@@ -1359,7 +1359,7 @@ cmTarget cmGlobalGenerator::CreateGlobalTarget(
   cmTarget target;
   target.GetProperties().SetCMakeInstance(this->CMakeInstance);
   target.SetType(cmTarget::GLOBAL_TARGET, name);
-  target.SetInAll(false);
+  target.SetProperty("EXCLUDE_FROM_ALL","TRUE");
 
   std::vector<std::string> no_outputs;
   std::vector<std::string> no_depends;

Source/cmGlobalUnixMakefileGenerator3.cxx
@@ -229,7 +229,7 @@ void cmGlobalUnixMakefileGenerator3::WriteMainMakefile2()
     cmLocalGenerator *lg3 = lg;
     while (lg3)
       {
-      if (lg3->GetExcludeAll())
+      if (lg3->GetMakefile()->GetPropertyAsBool("EXCLUDE_FROM_ALL"))
         {
         exclude = true;
         break;
@@ -478,7 +478,7 @@ cmGlobalUnixMakefileGenerator3
        (l->second.GetType() == cmTarget::UTILITY))
       {
       // Add this to the list of depends rules in this directory.
-      if((!check_all || l->second.IsInAll()) &&
+      if((!check_all || !l->second.GetPropertyAsBool("EXCLUDE_FROM_ALL")) &&
          (!check_relink || l->second.NeedRelinkBeforeInstall()))
         {
         std::string tname = lg->GetRelativeTargetDirectory(l->second);
@@ -790,7 +790,7 @@ cmGlobalUnixMakefileGenerator3
                           localName.c_str(), depends, commands, true);
         
         // add the all/all dependency
-        if (!exclude && t->second.IsInAll())
+        if (!exclude && !t->second.GetPropertyAsBool("EXCLUDE_FROM_ALL"))
           {
           depends.clear();
           depends.push_back(localName);
@@ -937,7 +937,7 @@ GetNumberOfProgressActionsInAll(cmLocalUnixMakefileGenerator3 *lg)
            (t->second.GetType() == cmTarget::MODULE_LIBRARY) ||
            (t->second.GetType() == cmTarget::UTILITY))
           {
-          if (t->second.IsInAll())
+          if (!t->second.GetPropertyAsBool("EXCLUDE_FROM_ALL"))
             {
             std::vector<int> &progFiles = lg3->ProgressFiles[t->first];
             result += static_cast<unsigned long>(progFiles.size());
@@ -966,7 +966,7 @@ GetNumberOfProgressActionsInAll(cmLocalUnixMakefileGenerator3 *lg)
            (l->second.GetType() == cmTarget::UTILITY))
           {
           // Add this to the list of depends rules in this directory.
-          if (l->second.IsInAll() && 
+          if (!l->second.GetPropertyAsBool("EXCLUDE_FROM_ALL") && 
               targets.find(&l->second) == targets.end())
             {
             std::deque<cmTarget *> activeTgts;

Source/cmGlobalVisualStudio6Generator.cxx
@@ -247,7 +247,7 @@ void cmGlobalVisualStudio6Generator
             for(cmTargets::iterator al = atgts.begin();
                 al != atgts.end(); ++al)
               {
-              if (al->second.IsInAll())
+              if (!al->second.GetPropertyAsBool("EXCLUDE_FROM_ALL"))
                 {
                 if (al->second.GetType() == cmTarget::UTILITY ||
                     al->second.GetType() == cmTarget::GLOBAL_TARGET)

Source/cmGlobalVisualStudio71Generator.cxx
@@ -109,7 +109,7 @@ void cmGlobalVisualStudio71Generator
             for(cmTargets::iterator al = atgts.begin();
                 al != atgts.end(); ++al)
               {
-              if (al->second.IsInAll())
+              if (!al->second.GetPropertyAsBool("EXCLUDE_FROM_ALL"))
                 {
                 if (al->second.GetType() == cmTarget::UTILITY ||
                     al->second.GetType() == cmTarget::GLOBAL_TARGET)

commit 3106cf4e3d15e0b6320ea3528079dd807f8bc06f

include_external_msproject: Restore support for EXCLUDE_FROM_ALL
In commit dc6888573d (Pass EXCLUDE_FROM_ALL from directory to targets,
2019-01-15, v3.14.0-rc1~83^2) all `AddNewTarget` call sites were updated
to copy the directory-level `EXCLUDE_FROM_ALL` into the target property
of the same name, except that the one for `include_external_msproject`
was incorrectly missed.  Add it now.

Furthermore, refactoring in commit b99129d2d8 (ENH: some code cleanup,
2007-03-12, v2.6.0~2020) accidentally set the `EXCLUDE_FROM_ALL` target
property of `include_external_msproject`-generated targets to `FALSE`
instead of simply leaving it unset.  This was not necessary but had no
effect until the above commit gave it a meaning.  Drop that.

Fixes: #18986

Source/cmIncludeExternalMSProjectCommand.cxx
@@ -85,10 +85,12 @@ bool cmIncludeExternalMSProjectCommand::InitialPass(
     // Create a target instance for this utility.
     cmTarget* target = this->Makefile->AddNewTarget(cmStateEnums::UTILITY,
                                                     utility_name.c_str());
+    if (this->Makefile->GetPropertyAsBool("EXCLUDE_FROM_ALL")) {
+      target->SetProperty("EXCLUDE_FROM_ALL", "TRUE");
+    }
 
     target->SetProperty("GENERATOR_FILE_NAME", utility_name.c_str());
     target->SetProperty("EXTERNAL_MSPROJECT", path.c_str());
-    target->SetProperty("EXCLUDE_FROM_ALL", "FALSE");
 
     if (!customType.empty())
       target->SetProperty("VS_PROJECT_TYPE", customType.c_str());