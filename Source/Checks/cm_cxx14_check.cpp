e17deb7ad4ac83207c9e34d397f3ee054b3d09dd
Mathieu Garaud <mathieu.garaud@gmail.com>
2019-02-27 15:47:41 +0100

    Extend C++17/C++14 feature checks to cover more standard library APIs
    Make sure `std::cbegin`, `std::cend`, and `std::size` work in C++17 or
    C++14 mode before choosing the corresponding standard level for
    compiling CMake itself.  This helps in cases that the compiler is using
    a standard library too old to support the full standard level chosen.

--- Source/Checks/cm_cxx14_check.cpp
+++ Source/Checks/cm_cxx14_check.cpp
@@ -1,8 +1,15 @@
 #include <cstdio>
+#include <iterator>
 #include <memory>
 
 int main()
 {
+  int a[] = { 0, 1, 2 };
+  auto ai = std::cbegin(a);
+
+  int b[] = { 2, 1, 0 };
+  auto bi = std::cend(b);
+
   std::unique_ptr<int> u(new int(0));
-  return *u;
+  return *u + *ai + *(bi - 1);
 }
--- Source/Checks/cm_cxx17_check.cpp
+++ Source/Checks/cm_cxx17_check.cpp
@@ -1,9 +1,18 @@
 #include <cstdio>
+#include <iterator>
 #include <memory>
 #include <unordered_map>
 
 int main()
 {
+  int a[] = { 0, 1, 2 };
+  auto ai = std::cbegin(a);
+
+  int b[] = { 2, 1, 0 };
+  auto bi = std::cend(b);
+
+  auto ci = std::size(a);
+
   std::unique_ptr<int> u(new int(0));
-  return *u;
+  return *u + *ai + *(bi - 1) + (3 - static_cast<int>(ci));
 }
