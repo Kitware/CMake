Kitware/CMake

commit 2c9f35789d4955bf77436091d1535ed8761a15d2

VS: Decide project type by linker lang as fallback
Fixes: #16738

Source/cmGlobalVisualStudioGenerator.cxx
@@ -729,12 +729,26 @@ bool cmGlobalVisualStudioGenerator::TargetIsFortranOnly(
       return false;
     }
   }
+  // If there's only one source language, Fortran has to be used
+  // in order for the sources to compile.
+  // Note: Via linker propagation, LINKER_LANGUAGE could become CXX in
+  // this situation and mismatch from the actual language of the linker.
   gt->GetLanguages(languages, "");
   if (languages.size() == 1) {
     if (*languages.begin() == "Fortran") {
       return true;
     }
   }
+
+  // In the case of mixed object files or sources mixed with objects,
+  // decide the language based on the value of LINKER_LANGUAGE.
+  // This will not make it possible to mix source files of different
+  // languages, but object libraries will be linked together in the
+  // same fashion as other generators do.
+  if (gt->GetLinkerLanguage("") == "Fortran") {
+    return true;
+  }
+
   return false;
 }
 

commit 0b82f56ac69f2140cc05e7bdd4564ebf87ea6c6f

VS: Fix Fortran target type selection with RC sources
The Intel Fortran `.vfproj` files do support both Fortran and the
Windows Resource compiler (`.rc)` files.  Prior to CMake 3.9 we did not
support that, but commit 2c9f35789d (VS: Decide project type by linker
lang as fallback, 2017-03-30, v3.9.0-rc1~340^2) accidentally enabled it.
It was then broken by commit d3d2c3cd49 (VS: Fix Fortran target type
selection when linking C++ targets, 2019-02-04, v3.14.0-rc1~13^2).

Restore support for Fortran+RC in VS projects and add a test case.

Fixes: #19002

Source/cmGlobalVisualStudioGenerator.cxx
@@ -834,6 +834,10 @@ bool cmGlobalVisualStudioGenerator::TargetIsFortranOnly(
   if (linkLang && *linkLang) {
     languages.insert(linkLang);
   }
+
+  // Intel Fortran .vfproj files do support the resource compiler.
+  languages.erase("RC");
+
   return languages.size() == 1 && *languages.begin() == "Fortran";
 }
 

Tests/FortranOnly/CMakeLists.txt
@@ -12,6 +12,12 @@ add_executable(FortranOnly1 testf.f)
 set_property(TARGET FortranOnly1 PROPERTY OUTPUT_NAME FortranOnly)
 target_link_libraries(FortranOnly1 FortranOnlylib)
 
+# Test that Fortran+RC work together.
+# FIXME: Add .rc in more cases.
+if(CMAKE_GENERATOR MATCHES "Visual Studio")
+  set(test_rc testRC.rc)
+endif()
+
 # create a custom command that runs FortranOnly1 and puts
 # the output into the file testfhello.txt
 add_custom_command(OUTPUT ${FortranOnly_BINARY_DIR}/testfhello.txt
@@ -20,7 +26,7 @@ add_custom_command(OUTPUT ${FortranOnly_BINARY_DIR}/testfhello.txt
 # create a second executable FortranOnly2 that has
 # testfhello.txt has an source file so that it will
 # run the above custom command.
-add_executable(FortranOnly2 testfhello.txt testf.f)
+add_executable(FortranOnly2 testfhello.txt testf.f ${test_rc})
 target_link_libraries(FortranOnly2 FortranOnlylib)
 # create a custom target to check the content of testfhello.txt
 # by running the cmake script checktestf2.cmake