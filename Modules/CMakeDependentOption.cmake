d192caefff3a0a750c7ab86582b08b2b98142f08
Hong Xu <hong@topbug.net>
2019-08-02 13:30:39 -0700

    CMakeDependentOption: Document that 4th parameter follows if-syntax

--- Modules/CMakeDependentOption.cmake
+++ Modules/CMakeDependentOption.cmake
@@ -12,7 +12,7 @@ conditions are true.  When the option is not presented a default value
 is used, but any value set by the user is preserved for when the
 option is presented again.  Example invocation:
 
-::
+.. code-block:: cmake
 
   CMAKE_DEPENDENT_OPTION(USE_FOO "Use Foo" ON
                          "USE_BAR;NOT USE_ZOT" OFF)
@@ -21,7 +21,8 @@ If USE_BAR is true and USE_ZOT is false, this provides an option
 called USE_FOO that defaults to ON.  Otherwise, it sets USE_FOO to
 OFF.  If the status of USE_BAR or USE_ZOT ever changes, any value for
 the USE_FOO option is saved so that when the option is re-enabled it
-retains its old value.
+retains its old value.  Each element in the fourth parameter is
+evaluated as an if-condition, so :ref:`Condition Syntax` can be used.
 #]=======================================================================]
 
 macro(CMAKE_DEPENDENT_OPTION option doc default depends force)
