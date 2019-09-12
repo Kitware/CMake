b85407ae76951649907e8e5675fe17d65dfd5457
Regina Pfeifer <regina@mailbox.org>
2019-09-12 10:20:35 +0200

    cmInstallTargetsCommand: Port away from cmCommand
    Ref: #19499

--- Source/cmCommands.cxx
+++ Source/cmCommands.cxx
@@ -237,8 +237,7 @@ void GetProjectCommands(cmState* state)
                            cmIncludeRegularExpressionCommand);
   state->AddBuiltinCommand("install", cm::make_unique<cmInstallCommand>());
   state->AddBuiltinCommand("install_files", cmInstallFilesCommand);
-  state->AddBuiltinCommand("install_targets",
-                           cm::make_unique<cmInstallTargetsCommand>());
+  state->AddBuiltinCommand("install_targets", cmInstallTargetsCommand);
   state->AddBuiltinCommand("link_directories",
                            cm::make_unique<cmLinkDirectoriesCommand>());
   state->AddBuiltinCommand("project", cm::make_unique<cmProjectCommand>());
