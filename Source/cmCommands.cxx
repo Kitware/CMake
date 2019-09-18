fcfec154acea01518aa436d76f576d7cfea65fa2
Regina Pfeifer <regina@mailbox.org>
2019-09-18 17:34:23 +0200

    cmLinkDirectoriesCommand: Port away from cmCommand

--- Source/cmCommands.cxx
+++ Source/cmCommands.cxx
@@ -237,8 +237,7 @@ void GetProjectCommands(cmState* state)
   state->AddBuiltinCommand("install", cm::make_unique<cmInstallCommand>());
   state->AddBuiltinCommand("install_files", cmInstallFilesCommand);
   state->AddBuiltinCommand("install_targets", cmInstallTargetsCommand);
-  state->AddBuiltinCommand("link_directories",
-                           cm::make_unique<cmLinkDirectoriesCommand>());
+  state->AddBuiltinCommand("link_directories", cmLinkDirectoriesCommand);
   state->AddBuiltinCommand("project", cm::make_unique<cmProjectCommand>());
   state->AddBuiltinCommand("set_source_files_properties",
                            cmSetSourceFilesPropertiesCommand);
