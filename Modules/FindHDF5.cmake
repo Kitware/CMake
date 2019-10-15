08f4b5dcf9243754a3b6f88fc61d0c99578b08f6
Willem Deconinck <willem.deconinck@ecmwf.int>
2019-10-15 10:37:12 +0000

    FindHDF5: Append hdf5 to hdf5_hl to avoid undefined references

--- Modules/FindHDF5.cmake
+++ Modules/FindHDF5.cmake
@@ -761,7 +761,7 @@ mark_as_advanced( HDF5_DIFF_EXECUTABLE )
 if( NOT HDF5_FOUND )
     # seed the initial lists of libraries to find with items we know we need
     set(HDF5_C_LIBRARY_NAMES          hdf5)
-    set(HDF5_C_HL_LIBRARY_NAMES       hdf5_hl)
+    set(HDF5_C_HL_LIBRARY_NAMES       hdf5_hl ${HDF5_C_LIBRARY_NAMES} )
 
     set(HDF5_CXX_LIBRARY_NAMES        hdf5_cpp    ${HDF5_C_LIBRARY_NAMES})
     set(HDF5_CXX_HL_LIBRARY_NAMES     hdf5_hl_cpp ${HDF5_C_HL_LIBRARY_NAMES} ${HDF5_CXX_LIBRARY_NAMES})
