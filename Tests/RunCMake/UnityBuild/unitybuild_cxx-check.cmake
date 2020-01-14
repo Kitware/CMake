274a6f3699c1ad7b099bbabf49618f23122d84f0
Cristian Adam <cristian.adam@gmail.com>
2020-01-14 18:48:08 +0100

    Unity Build: include language in generated source file name
    Fixes: #20206

--- Tests/RunCMake/UnityBuild/unitybuild_cxx-check.cmake
+++ Tests/RunCMake/UnityBuild/unitybuild_cxx-check.cmake
@@ -1,4 +1,4 @@
-set(unitybuild_cxx "${RunCMake_TEST_BINARY_DIR}/CMakeFiles/tgt.dir/Unity/unity_0.cxx")
+set(unitybuild_cxx "${RunCMake_TEST_BINARY_DIR}/CMakeFiles/tgt.dir/Unity/unity_0_cxx.cxx")
 if(NOT EXISTS "${unitybuild_cxx}")
   set(RunCMake_TEST_FAILED "Generated unity source files ${unitybuild_cxx} does not exist.")
   return()
