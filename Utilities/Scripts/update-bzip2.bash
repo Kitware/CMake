39f2a9b286bb5346df3a17026aa8b4a1e327caa2
Brad King <brad.king@kitware.com>
2020-02-24 12:11:30 -0500

    bzip2: Update import script to get bzip2 1.0.8

--- Utilities/Scripts/update-bzip2.bash
+++ Utilities/Scripts/update-bzip2.bash
@@ -8,7 +8,7 @@ readonly name="bzip2"
 readonly ownership="bzip2 upstream <kwrobot@kitware.com>"
 readonly subtree="Utilities/cmbzip2"
 readonly repo="https://sourceware.org/git/bzip2.git"
-readonly tag="bzip2-1.0.5"
+readonly tag="bzip2-1.0.8"
 readonly shortlog=false
 readonly paths="
   LICENSE
