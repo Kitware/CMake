0001339a6f92afcd0e49a262b3d2aae4a1ce8d46
Alexander Stein <alexander.stein@mailbox.org>
2020-03-06 22:29:07 +0100

    GoogleTest: Add test case for XML_OUTPUT_DIR

--- Tests/RunCMake/GoogleTest/RunCMakeTest.cmake
+++ Tests/RunCMake/GoogleTest/RunCMakeTest.cmake
@@ -78,4 +78,32 @@ function(run_GoogleTest)
   )
 endfunction()
 
+function(run_GoogleTestXML)
+  # Use a single build tree for a few tests without cleaning.
+  set(RunCMake_TEST_BINARY_DIR ${RunCMake_BINARY_DIR}/GoogleTestXML-build)
+  set(RunCMake_TEST_NO_CLEAN 1)
+  if(NOT RunCMake_GENERATOR_IS_MULTI_CONFIG)
+    set(RunCMake_TEST_OPTIONS -DCMAKE_BUILD_TYPE=Debug)
+  endif()
+  file(REMOVE_RECURSE "${RunCMake_TEST_BINARY_DIR}")
+  file(MAKE_DIRECTORY "${RunCMake_TEST_BINARY_DIR}")
+
+  run_cmake(GoogleTestXML)
+
+  run_cmake_command(GoogleTestXML-discovery
+  ${CMAKE_COMMAND}
+  --build .
+  --config Debug
+  --target xml_output
+  )
+
+  run_cmake_command(GoogleTestXML-result
+  ${CMAKE_CTEST_COMMAND}
+  -C Debug
+  -R GoogleTestXML
+  --no-label-summary
+  )
+endfunction()
+
 run_GoogleTest()
+run_GoogleTestXML()
