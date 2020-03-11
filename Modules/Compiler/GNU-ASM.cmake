5932f0be4f36f97fcd3ab61a559379a2a1291371
Brad King <brad.king@kitware.com>
2020-03-11 11:45:46 -0400

    ASM: Fix depfile flags for GNU 'as' tool
    The GNU `as --help` shows `--MD <file>` as an option to generate depfiles
    as needed by Ninja.  There is no `-MT <target>` flag but fortunately the
    generated files automatically account for the `-o <obj>` flag.
    
    Issue: #20426

--- Modules/Compiler/GNU-ASM.cmake
+++ Modules/Compiler/GNU-ASM.cmake
@@ -4,3 +4,7 @@ include(Compiler/GNU)
 set(CMAKE_ASM_SOURCE_FILE_EXTENSIONS s;S;asm)
 
 __compiler_gnu(ASM)
+
+if(CMAKE_ASM${ASM_DIALECT}_COMPILER_ID_VENDOR_MATCH STREQUAL "GNU assembler")
+  set(CMAKE_DEPFILE_FLAGS_ASM${ASM_DIALECT} "--MD <DEPFILE>")
+endif()
