0251ddadab687dc3ad62c39381201fcd1f7c534f
Brad King <brad.king@kitware.com>
2020-03-25 07:14:42 -0400

    MinGW: Drop unnecessary windres selection code with Unix Makefiles
    Since commit 957c2aac7f (RC: Simplify selection of resource compiler
    based on C/C++ toolchain, 2015-05-07, v3.3.0-rc1~93^2~5) we select
    windres as the RC compiler on MinGW via `CMAKE_RC_COMPILER_INIT`.  Drop
    the special case from commit be9afbf453 (Find mingw's windres also when
    Unix Makefiles are used, 2012-08-27, v2.8.10~152^2).

--- Modules/Platform/Windows-GNU.cmake
+++ Modules/Platform/Windows-GNU.cmake
@@ -47,10 +47,6 @@ if("${_help}" MATCHES "GNU ld .* 2\\.1[1-6]")
   set(__WINDOWS_GNU_LD_RESPONSE 0)
 endif()
 
-if(NOT CMAKE_GENERATOR_RC AND CMAKE_GENERATOR MATCHES "Unix Makefiles")
-  set(CMAKE_GENERATOR_RC windres)
-endif()
-
 macro(__windows_compiler_gnu lang)
 
   if(MSYS OR MINGW)
