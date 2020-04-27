dd62ee376e4cd3018906f7a913a88d2352b637bc
Brad King <brad.king@kitware.com>
2020-04-27 08:29:52 -0400

    Tutorial: Restore MakeTable.cxx in step 6
    In commit c754a3d4b7 (Tutorial: Remove MakeTable.cxx from Steps 5 and 6,
    2020-04-23) it was incorrect to remove the file from step 6.  The
    instructions for that step show the addition of a reference to it from
    the `CMakeLists.txt` file.  Each step shows the addition of content to
    lead to the next step, so removing the file from step 6 was an
    off-by-one error.
    
    Issue: #20618

---
+++ Help/guide/tutorial/Step6/MathFunctions/MakeTable.cxx
@@ -0,0 +1,25 @@
+// A simple program that builds a sqrt table
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  // make sure we have enough arguments
+  if (argc < 2) {
+    return 1;
+  }
+
+  std::ofstream fout(argv[1], std::ios_base::out);
+  const bool fileOpen = fout.is_open();
+  if (fileOpen) {
+    fout << "double sqrtTable[] = {" << std::endl;
+    for (int i = 0; i < 10; ++i) {
+      fout << sqrt(static_cast<double>(i)) << "," << std::endl;
+    }
+    // close the table with a zero
+    fout << "0};" << std::endl;
+    fout.close();
+  }
+  return fileOpen ? 0 : 1; // return 0 if wrote the file
+}
