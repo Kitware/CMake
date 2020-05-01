4adfd2a3aa328b42b87a75c7178c7e8214e8d3c6
Jiri Malak <malak.jiri@gmail.com>
2020-05-01 16:14:33 +0200

    OpenWatcom: Add cross-compilation support for Windows
    Add system header files directories for cross-compilation

--- Modules/Platform/Windows-OpenWatcom.cmake
+++ Modules/Platform/Windows-OpenWatcom.cmake
@@ -4,6 +4,8 @@
 # This module is shared by multiple languages; use include blocker.
 include_guard()
 
+set(CMAKE_BUILD_TYPE_INIT Debug)
+
 set(CMAKE_CREATE_WIN32_EXE "system nt_win" )
 set(CMAKE_CREATE_CONSOLE_EXE "system nt" )
 string(APPEND CMAKE_SHARED_LINKER_FLAGS_INIT " system nt_dll")
@@ -14,10 +16,17 @@ set(CMAKE_SHARED_LIBRARY_C_FLAGS "-bd") # ... while this is a space separated st
 
 set(CMAKE_RC_COMPILER "rc" )
 
-set(CMAKE_BUILD_TYPE_INIT Debug)
-
 # single/multi-threaded                 /-bm
 # static/DLL run-time libraries         /-br
 # default is setup for multi-threaded + DLL run-time libraries
 string(APPEND CMAKE_C_FLAGS_INIT " -bt=nt -dWIN32 -br -bm")
 string(APPEND CMAKE_CXX_FLAGS_INIT " -bt=nt -xs -dWIN32 -br -bm")
+
+if(CMAKE_CROSSCOMPILING)
+  if(NOT CMAKE_C_STANDARD_INCLUDE_DIRECTORIES)
+    set(CMAKE_C_STANDARD_INCLUDE_DIRECTORIES $ENV{WATCOM}/h $ENV{WATCOM}/h/nt)
+  endif()
+  if(NOT CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES)
+    set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES $ENV{WATCOM}/h $ENV{WATCOM}/h/nt)
+  endif()
+endif()
