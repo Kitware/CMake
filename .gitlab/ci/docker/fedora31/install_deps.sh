d78bf2f229d6e22ab2ad76fe8256532d009b70df
Ben Boeckel <ben.boeckel@kitware.com>
2020-05-05 11:17:17 -0400

    ci: install sphinx into the CI image

--- .gitlab/ci/docker/fedora31/install_deps.sh
+++ .gitlab/ci/docker/fedora31/install_deps.sh
@@ -10,4 +10,10 @@ dnf install --setopt=install_weak_deps=False -y \
     gcc-c++ \
     git-core
 
+# Install documentation tools.
+dnf install --setopt=install_weak_deps=False -y \
+    python3-sphinx \
+    texinfo \
+    qt5-qttools-devel
+
 dnf clean all
