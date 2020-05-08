a1b1fc611bc8aeb519b79b3e1b86ce4e4dbc55e9
Ben Boeckel <ben.boeckel@kitware.com>
2020-05-08 10:15:27 -0400

    gitlab-ci: add Linux makefiles and ninja builders

--- .gitlab/ci/configure_common.cmake
+++ .gitlab/ci/configure_common.cmake
@@ -1,3 +1,9 @@
 set(CTEST_USE_LAUNCHERS "ON" CACHE STRING "")
 
+# We run the install right after the build. Avoid rerunning it when installing.
+set(CMAKE_SKIP_INSTALL_ALL_DEPENDENCY "ON" CACHE STRING "")
+# Install CMake under the build tree.
+set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/install" CACHE PATH "")
+set(CMake_TEST_INSTALL "OFF" CACHE BOOL "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_sccache.cmake")
