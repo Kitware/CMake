f00ab86f1f77cba7554a2524439bb5d1a07c55ab
Ben Boeckel <ben.boeckel@kitware.com>
2020-05-26 09:16:22 -0400

    ci: use BOOL for boolean settings

--- .gitlab/ci/configure_common.cmake
+++ .gitlab/ci/configure_common.cmake
@@ -1,7 +1,7 @@
-set(CTEST_USE_LAUNCHERS "ON" CACHE STRING "")
+set(CTEST_USE_LAUNCHERS "ON" CACHE BOOL "")
 
 # We run the install right after the build. Avoid rerunning it when installing.
-set(CMAKE_SKIP_INSTALL_ALL_DEPENDENCY "ON" CACHE STRING "")
+set(CMAKE_SKIP_INSTALL_ALL_DEPENDENCY "ON" CACHE BOOL "")
 # Install CMake under the build tree.
 set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/install" CACHE PATH "")
 set(CMake_TEST_INSTALL "OFF" CACHE BOOL "")
