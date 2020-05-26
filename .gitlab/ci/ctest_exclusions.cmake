9812e87ee44b758f82f4612950ffa03df934e90a
Ben Boeckel <ben.boeckel@kitware.com>
2020-05-26 09:19:31 -0400

    gitlab-ci: add a test of the VS generator based off of the ninja build

--- .gitlab/ci/ctest_exclusions.cmake
+++ .gitlab/ci/ctest_exclusions.cmake
@@ -1,6 +1,15 @@
 set(test_exclusions
 )
 
+if (CTEST_CMAKE_GENERATOR MATCHES "Visual Studio")
+  list(APPEND test_exclusions
+    # This test takes around 5 minutes with Visual Studio.
+    # https://gitlab.kitware.com/cmake/cmake/-/issues/20733
+    "^ExternalProjectUpdate$"
+    # This test is a dependency of the above and is only required for it.
+    "^ExternalProjectUpdateSetup$")
+endif ()
+
 string(REPLACE ";" "|" test_exclusions "${test_exclusions}")
 if (test_exclusions)
   set(test_exclusions "(${test_exclusions})")
