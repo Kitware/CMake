b33283a282da4afbf75b66390012b9ac73d13444
Brad King <brad.king@kitware.com>
2020-07-03 08:22:30 -0400

    Tests: Cover using RTF for DMG SLAs

--- Tests/RunCMake/CPack/tests/DMG_SLA/VerifyResult.cmake
+++ Tests/RunCMake/CPack/tests/DMG_SLA/VerifyResult.cmake
@@ -4,7 +4,7 @@ if(NOT res EQUAL 0)
   string(REPLACE "\n" "\n  " err "  ${err}")
   message(FATAL_ERROR "Running 'hdiutil udifderez -xml' on\n  ${dmg}\nfailed with:\n${err}")
 endif()
-foreach(key "LPic" "STR#" "TEXT")
+foreach(key "LPic" "STR#" "TEXT" "RTF ")
   if(NOT out MATCHES "<key>${key}</key>")
     string(REPLACE "\n" "\n  " out "  ${out}")
     message(FATAL_ERROR "error: running 'hdiutil udifderez -xml' on\n  ${dmg}\ndid not show '${key}' key:\n${out}")
@@ -19,9 +19,9 @@ foreach(line
     # STR# German first and last base64 lines
     "\tAAkGR2VybWFuC0FremVwdGllcmVuCEFibGVobmVuB0RydWNrZW4M\n"
     "\tYXVzZ2V3wopobHQgaXN0Lg==\n"
-    # TEXT English first and last base64 lines
-    "\tTElDRU5TRQ0tLS0tLS0tDVRoaXMgaXMgYW4gaW5zdGFsbGVyIGNy\n"
-    "\tTm8gbGljZW5zZSBwcm92aWRlZC4NDQ==\n"
+    # RTF English first and last base64 lines
+    "\te1xydGYxXGFuc2lcYW5zaWNwZzEyNTJcZGVmZjBcbm91aWNvbXBh\n"
+    "\tdmlkZWQuXHBhcg1ccGFyDX0NDQ==\n"
     # TEXT German first and last base64 lines
     "\tTElaRU5aDS0tLS0tLQ1EaWVzIGlzdCBlaW4gSW5zdGFsbGF0aW9u\n"
     "\tZ2ViZW4uDQ0=\n"
