4c939b0d39c21b0a7f30ce64d82e87f75b8babec
Brad King <brad.king@kitware.com>
2020-08-07 10:30:42 -0400

    Tests: Isolate RunCMake.FindPkgConfig from caller environment
    Avoid interference from `CMAKE_PREFIX_PATH` and friends if they
    happen to be set in the environment from which the tests are run.

--- Tests/RunCMake/FindPkgConfig/RunCMakeTest.cmake
+++ Tests/RunCMake/FindPkgConfig/RunCMakeTest.cmake
@@ -1,5 +1,10 @@
 include(RunCMake)
 
+# Isolate test cases from caller's environment.
+set(ENV{CMAKE_PREFIX_PATH} "")
+set(ENV{CMAKE_APPBUNDLE_PATH} "")
+set(ENV{CMAKE_FRAMEWORK_PATH} "")
+
 run_cmake(FindPkgConfig_NO_PKGCONFIG_PATH)
 run_cmake(FindPkgConfig_PKGCONFIG_PATH)
 run_cmake(FindPkgConfig_PKGCONFIG_PATH_NO_CMAKE_PATH)
