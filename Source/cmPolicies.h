359c500a2466ffc2507b81c0089bce18fd5debbb
Raul Tambre <raul@tambre.ee>
2020-08-08 13:17:05 +0300

    cmTarget: Raise error if imported target location is not set
    Previously we would synthesize <TARGET_NAME>-NOTFOUND as the location. This
    would then end up on the link line and cause build failures.
    Policy CMP0110 is added to control this behaviour.
    
    Fixes #19080, #19943.

--- Source/cmPolicies.h
+++ Source/cmPolicies.h
@@ -326,7 +326,11 @@ class cmMakefile;
          19, 0, cmPolicies::WARN)                                             \
   SELECT(POLICY, CMP0110,                                                     \
          "add_test() supports arbitrary characters in test names.", 3, 19, 0, \
-         cmPolicies::WARN)
+         cmPolicies::WARN)                                                    \
+  SELECT(POLICY, CMP0111,                                                     \
+         "An imported target with a missing location fails during "           \
+         "generation.",                                                       \
+         3, 19, 0, cmPolicies::WARN)
 
 #define CM_SELECT_ID(F, A1, A2, A3, A4, A5, A6) F(A1)
 #define CM_FOR_EACH_POLICY_ID(POLICY)                                         \
