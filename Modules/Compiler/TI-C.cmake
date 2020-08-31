0b4582f229db9fc2b6932c518ab816edd7709406
Josef Angstenberger <code@jtxa.de>
2020-09-01 01:17:18 +0200

    Compiler/TI: Fix definition of std options
    - The compile option variable has to be defined as list.
    
    - The compiler works in relaxed ANSI mode by default.
      Strict ANSI has to be explicitly set additionally to the
      language standard.

--- Modules/Compiler/TI-C.cmake
+++ Modules/Compiler/TI-C.cmake
@@ -1,8 +1,8 @@
 include(Compiler/TI)
 __compiler_ti(C)
 
-set(CMAKE_C90_STANDARD_COMPILE_OPTION "--c89")
-set(CMAKE_C90_EXTENSION_COMPILE_OPTION "--c89 --relaxed_ansi")
+set(CMAKE_C90_STANDARD_COMPILE_OPTION "--c89" "--strict_ansi")
+set(CMAKE_C90_EXTENSION_COMPILE_OPTION "--c89" "--relaxed_ansi")
 
-set(CMAKE_C99_STANDARD_COMPILE_OPTION "--c99")
-set(CMAKE_C99_EXTENSION_COMPILE_OPTION "--c99 --relaxed_ansi")
+set(CMAKE_C99_STANDARD_COMPILE_OPTION "--c99" "--strict_ansi")
+set(CMAKE_C99_EXTENSION_COMPILE_OPTION "--c99" "--relaxed_ansi")
