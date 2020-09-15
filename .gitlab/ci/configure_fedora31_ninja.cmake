1dcc5698289a49359c70f022d673cacc42b82239
Kyle Edwards <kyle.edwards@kitware.com>
2020-09-15 13:40:48 -0400

    gitlab-ci: Add GUI testing to each OS

--- .gitlab/ci/configure_fedora31_ninja.cmake
+++ .gitlab/ci/configure_fedora31_ninja.cmake
@@ -1,2 +1,3 @@
 set(CMake_TEST_ISPC "ON" CACHE STRING "")
+set(CMake_TEST_GUI "ON" CACHE BOOL "")
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora31_common.cmake")
--- .gitlab/ci/configure_windows_vs2019_x64_ninja.cmake
+++ .gitlab/ci/configure_windows_vs2019_x64_ninja.cmake
@@ -1,3 +1,4 @@
 set(CMake_TEST_WIX_NO_VERIFY "ON" CACHE BOOL "")
+set(CMake_TEST_GUI "ON" CACHE BOOL "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_common.cmake")
