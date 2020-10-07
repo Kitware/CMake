0556bd2f8a0cb4df6b2aaf157dc598358eef4c83
Brad King <brad.king@kitware.com>
2020-10-07 06:11:00 -0400

    gitlab-ci: use ctest_start GROUP option instead of deprecated TRACK

--- .gitlab/ci/ctest_configure.cmake
+++ .gitlab/ci/ctest_configure.cmake
@@ -6,7 +6,7 @@ set(cmake_args
   -C "${CMAKE_CURRENT_LIST_DIR}/configure_$ENV{CMAKE_CONFIGURATION}.cmake")
 
 # Create an entry in CDash.
-ctest_start(Experimental TRACK "${ctest_track}")
+ctest_start(Experimental GROUP "${ctest_group}")
 
 # Gather update information.
 find_package(Git)
