2208db114c97f6cd88306507ca917008b8e37d47
Kyle Edwards <kyle.edwards@kitware.com>
2020-10-14 15:11:40 -0400

    Help: Show multiple ways of doing cacheVariables
    This demonstrates both the simple string and the complex object,
    and ensures that they get automated testing.

--- Tests/RunCMake/CMakePresets/DocumentationExample.cmake
+++ Tests/RunCMake/CMakePresets/DocumentationExample.cmake
@@ -1,3 +1,4 @@
 include(${CMAKE_CURRENT_LIST_DIR}/TestVariable.cmake)
 
-test_variable(MY_CACHE_VARIABLE "BOOL" "OFF")
+test_variable(FIRST_CACHE_VARIABLE "BOOL" "OFF")
+test_variable(SECOND_CACHE_VARIABLE "UNINITIALIZED" "ON")
