b7f0327dcd4032c6a118825adcfdc8dab6295e7f
Brad King <brad.king@kitware.com>
2020-12-09 10:28:03 -0500

    Tests: Cover macOS host architecture selection on Apple Silicon hosts
    Add test cases verifying that `CMAKE_APPLE_SILICON_PROCESSOR` set as
    either a cache or environment variable causes that to be selected as the
    host architecture.  Also verify that sources compile using whatever is
    selected as the host architecture, even when the explicit setting is not
    used.
    
    Issue: #21554

---
+++ Tests/RunCMake/AppleSilicon/arm64-common.cmake
@@ -0,0 +1,10 @@
+enable_language(C)
+
+if(NOT CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm64")
+  message(FATAL_ERROR "CMAKE_HOST_SYSTEM_PROCESSOR is '${CMAKE_HOST_SYSTEM_PROCESSOR}', not 'arm64'")
+endif()
+if(NOT CMAKE_OSX_ARCHITECTURES STREQUAL "")
+  message(FATAL_ERROR "CMAKE_OSX_ARCHITECTURES is '${CMAKE_OSX_ARCHITECTURES}', not empty ''")
+endif()
+
+add_library(arm64 arm64.c)
