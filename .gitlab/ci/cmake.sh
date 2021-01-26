eefa0ed0fa6b7f520d77b85829951cd16e81a0b0
Brad King <brad.king@kitware.com>
2021-01-26 17:11:19 -0500

    ci: update to use CMake 3.19.3

--- .gitlab/ci/cmake.sh
+++ .gitlab/ci/cmake.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-readonly version="3.19.0"
+readonly version="3.19.3"
 
-case "$( uname -s )" in
-    Linux)
+case "$(uname -s)-$(uname -m)" in
+    Linux-x86_64)
         shatool="sha256sum"
-        sha256sum="5446cdee900e906e46162a5a7be9b4542bb5e886041cf8cffeda62aae2696ccf"
-        platform="Linux"
+        sha256sum="c18b65697e9679e5c88dccede08c323cd3d3730648e59048047bba82097e0ffc"
+        platform="Linux-x86_64"
         ;;
-    Darwin)
+    Darwin-*)
         shatool="shasum -a 256"
-        sha256sum="315eb5500753f797075b6ea43189420e97b0b9f32c8fc87ec94ba1d80b72eb7f"
-        platform="Darwin"
+        sha256sum="a6b79ad05f89241a05797510e650354d74ff72cc988981cdd1eb2b3b2bda66ac"
+        platform="macos-universal"
         ;;
     *)
-        echo "Unrecognized platform $( uname -s )"
+        echo "Unrecognized platform $(uname -s)-$(uname -m)"
         exit 1
         ;;
 esac
@@ -24,7 +24,7 @@ readonly shatool
 readonly sha256sum
 readonly platform
 
-readonly filename="cmake-$version-$platform-x86_64"
+readonly filename="cmake-$version-$platform"
 readonly tarball="$filename.tar.gz"
 
 cd .gitlab
