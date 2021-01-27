a601ed8e0e17cf004a8f2f7c290568afaf694642
Brad King <brad.king@kitware.com>
2021-01-27 08:36:27 -0500

    ci: download ninja on aarch64

--- .gitlab/ci/ninja.sh
+++ .gitlab/ci/ninja.sh
@@ -11,6 +11,13 @@ case "$(uname -s)-$(uname -m)" in
         sha256sum="763464859c7ef2ea3a0a10f4df40d2025d3bb9438fcb1228404640410c0ec22d"
         filename="ninja-linux"
         ;;
+    Linux-aarch64)
+        shatool="sha256sum"
+        sha256sum="c0c29925fd7f0f24981b3b9d18353c7111c9af59eb6e6b0ffc0c4914cdc7999c"
+        # Use binary built by adjacent 'docker/ninja/centos7-aarch64.bash' script.
+        baseurl="https://cmake.org/files/dependencies"
+        filename="ninja-$version-1-linux-aarch64"
+        ;;
     Darwin-*)
         shatool="shasum -a 256"
         sha256sum="6fa359f491fac7e5185273c6421a000eea6a2f0febf0ac03ac900bd4d80ed2a5"
