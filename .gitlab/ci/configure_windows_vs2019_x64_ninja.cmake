15f0da341fb79298316dc42d04a4c81a300b0f3e
Brad King <brad.king@kitware.com>
2021-02-18 08:30:17 -0500

    ci: Enable IPO and FindOpenGL tests on Windows+MSVC+Ninja
    These are expected to work under a normal VS installation.

--- .gitlab/ci/configure_windows_vs2019_x64_ninja.cmake
+++ .gitlab/ci/configure_windows_vs2019_x64_ninja.cmake
@@ -1,4 +1,7 @@
 set(CMake_TEST_WIX_NO_VERIFY "ON" CACHE BOOL "")
 set(CMake_TEST_GUI "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenGL "ON" CACHE BOOL "")
+set(CMake_TEST_IPO_WORKS_C "ON" CACHE BOOL "")
+set(CMake_TEST_IPO_WORKS_CXX "ON" CACHE BOOL "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_common.cmake")
