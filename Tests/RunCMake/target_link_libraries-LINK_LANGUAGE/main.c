b722eea9253acd9e33921779175fc615d064577a
Marc Chevrier <marc.chevrier@gmail.com>
2021-02-22 11:45:11 +0100

    Genex: LINK_LANGUAGE respects LINKER_LANGUAGE property
    If target property LINKER_LANGUAGE is set, LINK_LANGUAGE generator
    expression evaluation must be always successful.
    
    This fix can be helpful to elaborate a solution for issue #21818.

--- Tests/RunCMake/target_link_libraries-LINK_LANGUAGE/main.c
+++ Tests/RunCMake/target_link_libraries-LINK_LANGUAGE/main.c
@@ -1,12 +1,20 @@
 
-#if defined(_WIN32)
+#if defined(C_USE_CXX)
+void func_c_cxx();
+#else
+#  if defined(_WIN32)
 __declspec(dllimport)
-#endif
+#  endif
   void func_c();
+#endif
 
 int main()
 {
+#if defined(C_USE_CXX)
+  func_c_cxx();
+#else
   func_c();
+#endif
 
   return 0;
 }
