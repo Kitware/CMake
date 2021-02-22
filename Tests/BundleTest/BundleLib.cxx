8b22d9b3c305c63c706048704d4211b504f46457
Brad King <brad.king@kitware.com>
2021-02-22 12:20:52 -0500

    Tests: Remove explicit no-signing marks from BundleTest
    These were added by commit e29a92f525 (Xcode: Make BundleTests
    compatible with Xcode 11, 2019-09-01, v3.16.0-rc1~158^2).  The
    real problem is that the `.app` is constructed with text files
    under the `.app/Contents/MacOS/` folder.  Move those, and drop
    the special no-signing configuration.

--- Tests/BundleTest/BundleLib.cxx
+++ Tests/BundleTest/BundleLib.cxx
@@ -52,8 +52,8 @@ int foo(char* exec)
   (void)br;
 
   int res1 = findBundleFile(exec, "Resources/randomResourceFile.plist");
-  int res2 = findBundleFile(exec, "MacOS/SomeRandomFile.txt");
-  int res3 = findBundleFile(exec, "MacOS/README.rst");
+  int res2 = findBundleFile(exec, "Other/SomeRandomFile.txt");
+  int res3 = findBundleFile(exec, "Other/README.rst");
   if (!res1 || !res2 || !res3) {
     return 1;
   }
