741b85b42b4c8d826634b3f968d7de21df5f6c85
Nils Gladitz <nilsgladitz@gmail.com>
2021-04-16 18:00:46 +0200

    liblzma: Revise update script to get version 5.2.5
    Keep files related to multi threaded stream encoding.

--- Utilities/Scripts/update-liblzma.bash
+++ Utilities/Scripts/update-liblzma.bash
@@ -8,12 +8,17 @@ readonly name="liblzma"
 readonly ownership="liblzma upstream <xz-devel@tukaani.org>"
 readonly subtree="Utilities/cmliblzma"
 readonly repo="https://git.tukaani.org/xz.git"
-readonly tag="v5.2.4"
+readonly tag="v5.2.5"
 readonly shortlog=false
 readonly paths="
   COPYING
   src/common/common_w32res.rc
+  src/common/mythread.h
   src/common/sysdefs.h
+  src/common/tuklib_common.h
+  src/common/tuklib_config.h
+  src/common/tuklib_cpucores.c
+  src/common/tuklib_cpucores.h
   src/common/tuklib_integer.h
   src/liblzma/
 "
