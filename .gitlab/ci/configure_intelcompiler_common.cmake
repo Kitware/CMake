051c3a04498f89483edc43b468ae25748a7032a4
Brad King <brad.king@kitware.com>
2021-04-21 14:36:13 -0400

    Find{BLAS,LAPACK}: Add test case covering BLA_STATIC with Intel MKL

--- .gitlab/ci/configure_intelcompiler_common.cmake
+++ .gitlab/ci/configure_intelcompiler_common.cmake
@@ -1,4 +1,6 @@
 set(CMake_TEST_FindBLAS "Intel10_64lp" CACHE STRING "")
+set(CMake_TEST_FindBLAS_STATIC "Intel10_64lp" CACHE STRING "")
 set(CMake_TEST_FindLAPACK "Intel10_64lp" CACHE STRING "")
+set(CMake_TEST_FindLAPACK_STATIC "Intel10_64lp" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_external_test.cmake")
