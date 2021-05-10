89478e643f42bfdf0ace9f83416536bea9f23fab
Brad King <brad.king@kitware.com>
2021-05-10 10:44:24 -0400

    gitlab-ci: update to Fedora 34 base images

--- .gitlab/ci/configure_fedora34_ninja.cmake
+++ .gitlab/ci/configure_fedora34_ninja.cmake
@@ -4,4 +4,4 @@ set(CMake_TEST_GUI "ON" CACHE BOOL "")
 # Cover compilation with C++11 only and not higher standards.
 set(CMAKE_CXX_STANDARD "11" CACHE STRING "")
 
-include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora33_common.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora34_common.cmake")
