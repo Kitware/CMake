521cfc38a3faa9239015ceb8281800aa14a215cb
Robert Maynard <rmaynard@nvidia.com>
2021-05-18 11:30:01 -0400

    NVHPC: Support explicit language flags

--- Modules/Compiler/NVHPC-C.cmake
+++ Modules/Compiler/NVHPC-C.cmake
@@ -1,3 +1,7 @@
 include(Compiler/PGI-C)
 include(Compiler/NVHPC)
+
+# Needed so that we support `LANGUAGE` property correctly
+set(CMAKE_C_COMPILE_OPTIONS_EXPLICIT_LANGUAGE -x c)
+
 __compiler_nvhpc(C)
--- Modules/Compiler/NVHPC-CXX.cmake
+++ Modules/Compiler/NVHPC-CXX.cmake
@@ -1,3 +1,7 @@
 include(Compiler/PGI-CXX)
 include(Compiler/NVHPC)
+
+# Needed so that we support `LANGUAGE` property correctly
+set(CMAKE_CXX_COMPILE_OPTIONS_EXPLICIT_LANGUAGE -x c++)
+
 __compiler_nvhpc(CXX)
