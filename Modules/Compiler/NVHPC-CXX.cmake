364f6af1d753bacf7a007033b30ebf2d6e15fe24
Robert Maynard <rmaynard@nvidia.com>
2021-05-18 11:30:36 -0400

    NVHPC: Support Ninja dependency scanning
    Fixes: #22168

--- Modules/Compiler/NVHPC-CXX.cmake
+++ Modules/Compiler/NVHPC-CXX.cmake
@@ -4,4 +4,9 @@ include(Compiler/NVHPC)
 # Needed so that we support `LANGUAGE` property correctly
 set(CMAKE_CXX_COMPILE_OPTIONS_EXPLICIT_LANGUAGE -x c++)
 
+# Required since as of NVHPC 21.03 the `-MD` flag implicitly
+# implies `-E` and therefore compilation and dependency generation
+# can't occur in the same invocation
+set(CMAKE_CXX_DEPENDS_EXTRA_COMMANDS "<CMAKE_CXX_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -x c++ -M <SOURCE> -MT <OBJECT> -MD<DEP_FILE>")
+
 __compiler_nvhpc(CXX)
