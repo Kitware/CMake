8f68bcad8fafddc82b73b69f21e122ece8603a8f
Brad King <brad.king@kitware.com>
2021-06-03 10:40:56 -0400

    Tests: Add cases verifying flag ordering rules

--- Tests/RunCMake/target_compile_options/RunCMakeTest.cmake
+++ Tests/RunCMake/target_compile_options/RunCMakeTest.cmake
@@ -19,3 +19,16 @@ if (CMAKE_C_COMPILER_ID MATCHES "GNU|Clang")
   run_cmake_target(CMP0101-BEFORE_keyword OLD CMP0101_OLD)
   run_cmake_target(CMP0101-BEFORE_keyword NEW CMP0101_NEW)
 endif()
+
+function(run_Order)
+  run_cmake_with_options(Order)
+  set(RunCMake_TEST_NO_CLEAN 1)
+  set(RunCMake_TEST_BINARY_DIR ${RunCMake_BINARY_DIR}/Order-build)
+  set(RunCMake_TEST_OUTPUT_MERGE 1)
+  run_cmake_command(Order-build ${CMAKE_COMMAND} --build . --verbose --config Custom)
+endfunction()
+if(RunCMake_GENERATOR MATCHES "Ninja|Make" AND
+    CMAKE_C_COMPILER_ID MATCHES "^(GNU|Clang|AppleClang)$" AND
+    NOT CMAKE_C_SIMULATE_ID STREQUAL "MSVC")
+  run_Order()
+endif()
---
+++ Tests/RunCMake/target_compile_options/order.c
@@ -0,0 +1,7 @@
+#ifndef __OPTIMIZE__
+#  error "Optimizations not enabled!"
+#endif
+int main(void)
+{
+  return 0;
+}
