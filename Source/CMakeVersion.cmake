11695f5985e9cbd7a5206908fe6d2ea7e77865e9
Brad King <brad.king@kitware.com>
2021-06-17 13:18:30 -0400

    Begin post-3.21 development

--- Source/CMakeVersion.cmake
+++ Source/CMakeVersion.cmake
@@ -1,8 +1,8 @@
 # CMake version number components.
 set(CMake_VERSION_MAJOR 3)
 set(CMake_VERSION_MINOR 21)
-set(CMake_VERSION_PATCH 0)
-set(CMake_VERSION_RC 0)
+set(CMake_VERSION_PATCH 20210617)
+#set(CMake_VERSION_RC 0)
 set(CMake_VERSION_IS_DIRTY 0)
 
 # Start with the full version number used in tags.  It has no dev info.
