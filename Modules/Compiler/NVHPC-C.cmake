7ce718376cb6853793a75ee9a6e22ecff04e75fe
Robert Maynard <rmaynard@nvidia.com>
2021-07-28 13:47:19 -0400

    NVHPC: Support 21.07 change to '-MD' where it behaves like gcc

--- Modules/Compiler/NVHPC-C.cmake
+++ Modules/Compiler/NVHPC-C.cmake
@@ -4,9 +4,4 @@ include(Compiler/NVHPC)
 # Needed so that we support `LANGUAGE` property correctly
 set(CMAKE_C_COMPILE_OPTIONS_EXPLICIT_LANGUAGE -x c)
 
-# Required since as of NVHPC 21.03 the `-MD` flag implicitly
-# implies `-E` and therefore compilation and dependency generation
-# can't occur in the same invocation
-set(CMAKE_C_DEPENDS_EXTRA_COMMANDS "<CMAKE_C_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -x c -M <SOURCE> -MT <OBJECT> -MD<DEP_FILE>")
-
 __compiler_nvhpc(C)
