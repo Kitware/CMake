f5dbc27c27a09a3d62d619e9859b3b8c3cb7699a
Robert Maynard <rmaynard@nvidia.com>
2021-08-05 09:11:40 -0400

    NVHPC-CXX: Add support for C++20

--- Modules/Compiler/NVHPC-CXX.cmake
+++ Modules/Compiler/NVHPC-CXX.cmake
@@ -4,4 +4,9 @@ include(Compiler/NVHPC)
 # Needed so that we support `LANGUAGE` property correctly
 set(CMAKE_CXX_COMPILE_OPTIONS_EXPLICIT_LANGUAGE -x c++)
 
+if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 20.11)
+  set(CMAKE_CXX20_STANDARD_COMPILE_OPTION  -std=c++20)
+  set(CMAKE_CXX20_EXTENSION_COMPILE_OPTION -std=gnu++20)
+endif()
+
 __compiler_nvhpc(CXX)
