a2b782bbc113eab4c2d64d7f40fa6e829f6937b2
Luigi Fiorentini <lfiorentini@topcon.com>
2021-09-14 20:44:44 +0200

    VS: Map /Y- flag to not use precompiled headers with VS 2008

--- Source/cmGlobalVisualStudio7Generator.cxx
+++ Source/cmGlobalVisualStudio7Generator.cxx
@@ -37,6 +37,7 @@ static cmVS7FlagTable cmVS7ExtraFlagTable[] = {
     cmVS7FlagTable::UserValueIgnored | cmVS7FlagTable::Continue },
   { "PrecompiledHeaderThrough", "Yu", "Precompiled Header Name", "",
     cmVS7FlagTable::UserValueRequired },
+  { "UsePrecompiledHeader", "Y-", "Don't use precompiled header", "0", 0 },
   { "WholeProgramOptimization", "LTCG", "WholeProgramOptimization", "true",
     0 },
 
