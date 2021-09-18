0b4a56e64b52ac050e42192c635bd59527cd98b7
Johnny Jazeix <jazeix@gmail.com>
2021-09-18 18:52:28 +0200

    cmake: -E cat stops when an empty file is encountered
    Fixes: #22652

--- Tests/RunCMake/CommandLine/RunCMakeTest.cmake
+++ Tests/RunCMake/CommandLine/RunCMakeTest.cmake
@@ -647,9 +647,10 @@ run_cmake_command(E_cat_directory
 
 file(WRITE "${out}/first_file.txt" "first file to append\n")
 file(WRITE "${out}/second_file.txt" "second file to append\n")
+file(WRITE "${out}/empty_file.txt" "")
 file(WRITE "${out}/unicode_file.txt" "àéùç - 한국어") # Korean in Korean
 run_cmake_command(E_cat_good_cat
-  ${CMAKE_COMMAND} -E cat "${out}/first_file.txt" "${out}/second_file.txt" "${out}/unicode_file.txt")
+  ${CMAKE_COMMAND} -E cat "${out}/first_file.txt" "${out}/second_file.txt" "${out}/empty_file.txt" "${out}/unicode_file.txt")
 unset(out)
 
 run_cmake_command(E_cat_good_binary_cat
