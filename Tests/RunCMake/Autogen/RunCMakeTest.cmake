354c1f5628bdc104c9b9cd0436dd4cfa585124ec
Ben Boeckel <ben.boeckel@kitware.com>
2021-09-23 17:05:03 -0400

    Tests/RunCMake/Autogen: test CMP0111 behavior
    On Windows, imported shared libraries which only have an
    `IMPORTED_IMPLIB` set fail if they are depended upon by a target using
    automoc. Add a test for the behavior of depending upon all imported
    target types from an automoc-using target.

--- Tests/RunCMake/Autogen/RunCMakeTest.cmake
+++ Tests/RunCMake/Autogen/RunCMakeTest.cmake
@@ -5,4 +5,8 @@ if (with_qt5)
   run_cmake(QtInFunction)
   run_cmake(QtInFunctionNested)
   run_cmake(QtInFunctionProperty)
+
+  run_cmake(CMP0111-imported-target-full)
+  run_cmake(CMP0111-imported-target-libname)
+  run_cmake(CMP0111-imported-target-implib-only)
 endif ()
