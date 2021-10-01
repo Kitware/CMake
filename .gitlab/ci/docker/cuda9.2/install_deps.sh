5329366527f5e03cdafe4f7ad55482d513bf2fd7
Brad King <brad.king@kitware.com>
2021-10-01 10:21:39 -0400

    ci: update cuda9.2 base image after DST Root CA X3 expiry

--- .gitlab/ci/docker/cuda9.2/install_deps.sh
+++ .gitlab/ci/docker/cuda9.2/install_deps.sh
@@ -4,6 +4,12 @@ set -e
 
 apt-get update
 
+# Update base packages.
+apt-get install -y \
+    libgnutls30 \
+    libssl1.0.0 \
+    openssl
+
 # Install development tools.
 apt-get install -y \
     g++ \
