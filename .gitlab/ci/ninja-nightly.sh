b5a59251cbd3cfdc00ffeb6d6d64af12a540c702
Brad King <brad.king@kitware.com>
2021-10-06 13:23:18 -0400

    gitlab-ci: Try upstream ninja development branch in some nightly jobs

---
+++ .gitlab/ci/ninja-nightly.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+cd .gitlab
+
+git clone https://github.com/ninja-build/ninja.git ninja-src
+cmake -S ninja-src -B ninja-src/build -DCMAKE_BUILD_TYPE=Release
+cmake --build ninja-src/build --parallel --target ninja
+mv ninja-src/build/ninja .
+rm -rf ninja-src
