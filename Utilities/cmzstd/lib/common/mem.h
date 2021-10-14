e4e4c59180fca93da5713c076c2dd00a199554ac
Brad King <brad.king@kitware.com>
2021-10-14 10:35:52 -0400

    zstd: Backport fix for SIGBUS on armv6 from zstd 1.5.0
    Backport zstd commit `28d0120b` (Avoid SIGBUS on armv6, 2021-05-11,
    `v1.5.0^2~11^2`) to our bundled copy of zstd 1.4.5.
    
    Fixes: #22754

--- Utilities/cmzstd/lib/common/mem.h
+++ Utilities/cmzstd/lib/common/mem.h
@@ -173,9 +173,7 @@ void __asan_unpoison_memory_region(void const volatile *addr, size_t size);
  * Prefer these methods in priority order (0 > 1 > 2)
  */
 #ifndef MEM_FORCE_MEMORY_ACCESS   /* can be defined externally, on command line for example */
-#  if defined(__GNUC__) && ( defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || defined(__ARM_ARCH_6ZK__) || defined(__ARM_ARCH_6T2__) )
-#    define MEM_FORCE_MEMORY_ACCESS 2
-#  elif defined(__INTEL_COMPILER) || defined(__GNUC__) || defined(__ICCARM__)
+#  if defined(__INTEL_COMPILER) || defined(__GNUC__) || defined(__ICCARM__)
 #    define MEM_FORCE_MEMORY_ACCESS 1
 #  endif
 #endif
