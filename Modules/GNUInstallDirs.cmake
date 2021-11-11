c07bba27302960fc2f35b6a9e00eab8b32ca9a49
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-11-11 17:30:45 +0300

    GNUInstallDirs: Update CMAKE_INSTALL_MANDIR for FreeBSD
    FreeBSD has switched from PREFIX/man ro PREFIX/share/man
    see https://cgit.freebsd.org/ports/tree/CHANGES entry 20200115.
    
    Fixes: #22883

--- Modules/GNUInstallDirs.cmake
+++ Modules/GNUInstallDirs.cmake
@@ -330,7 +330,7 @@ else()
     "Info documentation (DATAROOTDIR/info)")
 endif()
 
-if(CMAKE_SYSTEM_NAME MATCHES "^(([^k].*)?BSD|DragonFly)$")
+if(CMAKE_SYSTEM_NAME MATCHES "^(([^k].*)?BSD|DragonFly)$" AND NOT CMAKE_SYSTEM_NAME MATCHES "^(FreeBSD)$")
   _GNUInstallDirs_cache_path_fallback(CMAKE_INSTALL_MANDIR "man"
     "Man documentation (man)")
 else()
