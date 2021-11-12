94ae6b78725ba9e563d593c90a3f3e900c92a52e
Brad King <brad.king@kitware.com>
2021-11-12 11:07:21 -0500

    ci: Enable FindODBC and FindOpenMP tests on Windows VS builds
    The packages comes with the Windows SDK and/or the compiler.

--- .gitlab/ci/configure_windows_vs_common.cmake
+++ .gitlab/ci/configure_windows_vs_common.cmake
@@ -1,3 +1,8 @@
 set(CMake_TEST_WIX_NO_VERIFY "ON" CACHE BOOL "")
+set(CMake_TEST_FindODBC "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenMP "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenMP_C "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenMP_CXX "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenMP_Fortran "OFF" CACHE BOOL "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_external_test.cmake")
