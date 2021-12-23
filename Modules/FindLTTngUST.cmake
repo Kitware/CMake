4ce5765a8605cf4aa0fbb01f77ae2bf764452ccd
Norbert Lange <nolange79@gmail.com>
2021-12-23 16:16:48 +0100

    FindLTTngUST: Fix version component extraction
    The regex was greedy and slurped away all but the last digit,
    so 2.13.1 was for ex. detected as 2.3.1.
    Fix this by expecting preceding whitespace.

--- Modules/FindLTTngUST.cmake
+++ Modules/FindLTTngUST.cmake
@@ -63,11 +63,11 @@ if(LTTNGUST_INCLUDE_DIRS AND LTTNGUST_LIBRARIES)
          REGEX "^[\t ]*#define[\t ]+LTTNG_UST_MINOR_VERSION[\t ]+[0-9]+[\t ]*$")
     file(STRINGS "${lttngust_version_file}" lttngust_version_patch_string
          REGEX "^[\t ]*#define[\t ]+LTTNG_UST_PATCHLEVEL_VERSION[\t ]+[0-9]+[\t ]*$")
-    string(REGEX REPLACE ".*([0-9]+).*" "\\1"
+    string(REGEX REPLACE ".*[\t ]+([0-9]+).*" "\\1"
            lttngust_v_major "${lttngust_version_major_string}")
-    string(REGEX REPLACE ".*([0-9]+).*" "\\1"
+    string(REGEX REPLACE ".*[\t ]+([0-9]+).*" "\\1"
            lttngust_v_minor "${lttngust_version_minor_string}")
-    string(REGEX REPLACE ".*([0-9]+).*" "\\1"
+    string(REGEX REPLACE ".*[\t ]+([0-9]+).*" "\\1"
            lttngust_v_patch "${lttngust_version_patch_string}")
     set(LTTNGUST_VERSION_STRING
         "${lttngust_v_major}.${lttngust_v_minor}.${lttngust_v_patch}")
