9d621ceba115a409ca58e8c59fba365f55fd6d26
Brad King <brad.king@kitware.com>
2022-02-03 06:42:06 -0500

    Tests: Run MFC test only when explicitly enabled
    Previously we used a complicated heuristic to decide whether or not to
    run the MFC test, but it sometimes decided incorrectly to run the test.
    Since that was first written, we have developed a convention for other
    tests to enable them via undocumented cache entries that are added only
    on machines known to meet the tests' requirements.  Do that for MFC.

--- .gitlab/ci/configure_windows_vs_common.cmake
+++ .gitlab/ci/configure_windows_vs_common.cmake
@@ -5,5 +5,6 @@ set(CMake_TEST_FindOpenMP_C "ON" CACHE BOOL "")
 set(CMake_TEST_FindOpenMP_CXX "ON" CACHE BOOL "")
 set(CMake_TEST_FindOpenMP_Fortran "OFF" CACHE BOOL "")
 set(CMake_TEST_Java OFF CACHE BOOL "")
+set(CMake_TEST_MFC "ON" CACHE BOOL "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_external_test.cmake")
