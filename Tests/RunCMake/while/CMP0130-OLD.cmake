30313aa72166ef06cde8c64ab1e582f8a242502e
Brad King <brad.king@kitware.com>
2022-03-04 13:20:50 -0500

    while: diagnose errors during condition evaluation
    Add a policy to diagnose condition errors in a compatible way.
    
    Fixes: #23296

---
+++ Tests/RunCMake/while/CMP0130-OLD.cmake
@@ -0,0 +1,2 @@
+cmake_policy(SET CMP0130 OLD)
+include(CMP0130-common.cmake)
--- Tests/RunCMake/while/CMP0130-common.cmake
+++ Tests/RunCMake/while/CMP0130-common.cmake
@@ -3,5 +3,4 @@ while(${paren})
   message(STATUS "Condition incorrectly true")
   break()
 endwhile()
-# FIXME(#23296): The above condition error is tolerated for compatibility.
 message(STATUS "Code incorrectly accepted")
