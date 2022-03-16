78d0613695c76609f5889660e1bc51cbbd4480d7
Brad King <brad.king@kitware.com>
2022-03-16 13:49:03 -0400

    ci: Drop p4 binary checksum because the download URL is not stable
    In commit f9526f39a1 (ci: Add bzr and p4 to Debian and Fedora base
    images, 2022-02-21) we checked the SHA-256 hash of the Perforce binary.
    However, content at the download URL has changed in just the last few
    weeks, so we cannot consider it stable.

--- .gitlab/ci/docker/debian10/install_deps.sh
+++ .gitlab/ci/docker/debian10/install_deps.sh
@@ -98,9 +98,7 @@ rm ironpython_2.7.10.deb ironpython.sha256sum
 
 # Perforce
 curl -L -O https://www.perforce.com/downloads/perforce/r21.2/bin.linux26x86_64/helix-core-server.tgz
-echo '72620c55e9389705582506d6f3388005fb4f674888a00a12a51edc2ae37823b3  helix-core-server.tgz' > helix.sha256sum
-sha256sum --check helix.sha256sum
 tar -C /usr/local/bin -xvzf helix-core-server.tgz -- p4 p4d
-rm helix-core-server.tgz helix.sha256sum
+rm helix-core-server.tgz
 
 apt-get clean
