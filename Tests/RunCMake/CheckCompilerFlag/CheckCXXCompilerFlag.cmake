9dd585d12c54f6d0768aea6526ea627f81f436ad
Brad King <brad.king@kitware.com>
2022-04-07 15:00:38 -0400

    CheckCompilerFlag: Fix regression in locale environment preservation
    Fix a typo from commit 660e0d80ae (internal/CheckCompilerFlag: rely on
    common configuration, 2022-01-12, v3.23.0-rc1~124^2~1) that caused
    locale environment variables to not be restored after they are set
    during the check.

--- Tests/RunCMake/CheckCompilerFlag/CheckCXXCompilerFlag.cmake
+++ Tests/RunCMake/CheckCompilerFlag/CheckCXXCompilerFlag.cmake
@@ -4,6 +4,10 @@ include(CheckCompilerFlag)
 
 set(CXX 1) # test that this is tolerated
 
+# test that the check uses an isolated locale
+set(_env_LC_ALL "${LC_ALL}")
+set(ENV{LC_ALL} "BAD")
+
 check_compiler_flag(CXX "-_this_is_not_a_flag_" SHOULD_FAIL)
 if(SHOULD_FAIL)
   message(SEND_ERROR "invalid CXX compile flag didn't fail.")
@@ -15,3 +19,8 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|LCC|Clang" AND NOT "x${CMAKE_CXX_SIMULATE_
     message(SEND_ERROR "${CMAKE_CXX_COMPILER_ID} compiler flag '-x c++' check failed")
   endif()
 endif()
+
+if(NOT "$ENV{LC_ALL}" STREQUAL "BAD")
+  message(SEND_ERROR "ENV{LC_ALL} was not preserved by check_compiler_flag")
+endif()
+set(ENV{LC_ALL} ${_env_LC_ALL})
