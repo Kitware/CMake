Kitware/CMake

commit 761b6e24666884f26fd229b83dce884ba02c190d

CPackIFW: Avoid colliding names for icons / logos
Fixes: #16515

Source/CPack/IFW/cmCPackIFWInstaller.cxx
@@ -371,7 +371,9 @@ void cmCPackIFWInstaller::GenerateInstallerFile()
 
   // Logo
   if (!this->Logo.empty()) {
-    std::string name = cmSystemTools::GetFilenameName(this->Logo);
+    std::string srcName = cmSystemTools::GetFilenameName(this->Logo);
+    std::string suffix = cmSystemTools::GetFilenameLastExtension(srcName);
+    std::string name = "cm_logo." + suffix;
     std::string path = this->Directory + "/config/" + name;
     cmsys::SystemTools::CopyFileIfDifferent(this->Logo, path);
     xout.Element("Logo", name);
@@ -405,19 +407,25 @@ void cmCPackIFWInstaller::GenerateInstallerFile()
   if (!this->IsVersionLess("1.4")) {
     // ApplicationIcon
     if (!this->InstallerApplicationIcon.empty()) {
-      std::string name =
+      std::string srcName =
         cmSystemTools::GetFilenameName(this->InstallerApplicationIcon);
+      std::string suffix = cmSystemTools::GetFilenameLastExtension(srcName);
+      std::string name = "cm_appicon." + suffix;
       std::string path = this->Directory + "/config/" + name;
-      name = cmSystemTools::GetFilenameWithoutExtension(name);
       cmsys::SystemTools::CopyFileIfDifferent(this->InstallerApplicationIcon,
                                               path);
+      // The actual file is looked up by attaching a '.icns' (macOS),
+      // '.ico' (Windows). No functionality on Unix.
+      name = cmSystemTools::GetFilenameWithoutExtension(name);
       xout.Element("InstallerApplicationIcon", name);
     }
 
     // WindowIcon
     if (!this->InstallerWindowIcon.empty()) {
-      std::string name =
+      std::string srcName =
         cmSystemTools::GetFilenameName(this->InstallerWindowIcon);
+      std::string suffix = cmSystemTools::GetFilenameLastExtension(srcName);
+      std::string name = "cm_winicon." + suffix;
       std::string path = this->Directory + "/config/" + name;
       cmsys::SystemTools::CopyFileIfDifferent(this->InstallerWindowIcon, path);
       xout.Element("InstallerWindowIcon", name);

commit 331c5d458e09d0e94c8102139e2b81b69b83dd7d

CPackIFW: Fix regression in icon file names
Changes in commit 761b6e2466 (CPackIFW: Avoid colliding names for icons
/ logos, 2021-10-08, v3.23.0-rc1~546^2) accidentally left an extra `.`
in computed file names.  Remove it.

Fixes: #23423

Source/CPack/IFW/cmCPackIFWInstaller.cxx
@@ -424,7 +424,7 @@ void cmCPackIFWInstaller::GenerateInstallerFile()
   if (!this->Logo.empty()) {
     std::string srcName = cmSystemTools::GetFilenameName(this->Logo);
     std::string suffix = cmSystemTools::GetFilenameLastExtension(srcName);
-    std::string name = "cm_logo." + suffix;
+    std::string name = "cm_logo" + suffix;
     std::string path = this->Directory + "/config/" + name;
     cmsys::SystemTools::CopyFileIfDifferent(this->Logo, path);
     xout.Element("Logo", name);
@@ -461,7 +461,7 @@ void cmCPackIFWInstaller::GenerateInstallerFile()
       std::string srcName =
         cmSystemTools::GetFilenameName(this->InstallerApplicationIcon);
       std::string suffix = cmSystemTools::GetFilenameLastExtension(srcName);
-      std::string name = "cm_appicon." + suffix;
+      std::string name = "cm_appicon" + suffix;
       std::string path = this->Directory + "/config/" + name;
       cmsys::SystemTools::CopyFileIfDifferent(this->InstallerApplicationIcon,
                                               path);
@@ -476,7 +476,7 @@ void cmCPackIFWInstaller::GenerateInstallerFile()
       std::string srcName =
         cmSystemTools::GetFilenameName(this->InstallerWindowIcon);
       std::string suffix = cmSystemTools::GetFilenameLastExtension(srcName);
-      std::string name = "cm_winicon." + suffix;
+      std::string name = "cm_winicon" + suffix;
       std::string path = this->Directory + "/config/" + name;
       cmsys::SystemTools::CopyFileIfDifferent(this->InstallerWindowIcon, path);
       xout.Element("InstallerWindowIcon", name);