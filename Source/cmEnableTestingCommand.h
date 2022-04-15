64ea1a272c87fda1de948d69130c7872ff01c39e
Ben Boeckel <ben.boeckel@kitware.com>
2022-04-15 15:47:10 -0400

    messages: remove screamake from comments and errors

--- Source/cmEnableTestingCommand.h
+++ Source/cmEnableTestingCommand.h
@@ -14,10 +14,10 @@ class cmExecutionStatus;
  *
  * Produce the output testfile. This produces a file in the build directory
  * called CMakeTestfile with a syntax similar to CMakeLists.txt.  It contains
- * the SUBDIRS() and ADD_TEST() commands from the source CMakeLists.txt
+ * the subdirs() and add_test() commands from the source CMakeLists.txt
  * file with CMake variables expanded.  Only the subdirs and tests
  * within the valid control structures are replicated in Testfile
- * (i.e. SUBDIRS() and ADD_TEST() commands within IF() commands that are
+ * (i.e. subdirs() and add_test() commands within IF() commands that are
  * not entered by CMake are not replicated in Testfile).
  * Note that CTest expects to find this file in the build directory root;
  * therefore, this command should be in the source directory root too.
