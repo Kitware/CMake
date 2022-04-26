2c140ac7b1eafb61a9e74ac8da7f13b69e473507
Brad King <brad.king@kitware.com>
2022-04-26 09:25:41 -0400

    ci: Avoid finding Python via Windows Registry
    Since commit 313ea361b0 (CMake: Use FindPython instead of
    FindPythonInterp to build CMake itself, 2022-04-21), CI jobs on some
    Windows hosts have been finding Python via the Windows Registry.
    Explicitly disable the registry search so that the CI jobs find the
    Python that we download and put in the PATH inside the job.

--- .gitlab/ci/configure_windows_common.cmake
+++ .gitlab/ci/configure_windows_common.cmake
@@ -2,5 +2,6 @@ set(BUILD_QtDialog ON CACHE BOOL "")
 set(BUILD_CursesDialog ON CACHE BOOL "")
 set(CMAKE_PREFIX_PATH "$ENV{CI_PROJECT_DIR}/.gitlab/qt" CACHE STRING "")
 set(CMake_TEST_Java OFF CACHE BOOL "")
+set(Python_FIND_REGISTRY NEVER CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
