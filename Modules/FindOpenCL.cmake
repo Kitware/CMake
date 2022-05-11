587d1942b56b22628d5f03aeb41819efe21b68a4
Dmitry Kushpet <kushpet@gmail.com>
2022-05-11 13:07:51 -0400

    FindOpenCL: Add support for version 3.0

--- Modules/FindOpenCL.cmake
+++ Modules/FindOpenCL.cmake
@@ -45,7 +45,7 @@ function(_FIND_OPENCL_VERSION)
   set(CMAKE_REQUIRED_QUIET ${OpenCL_FIND_QUIETLY})
 
   CMAKE_PUSH_CHECK_STATE()
-  foreach(VERSION "2_2" "2_1" "2_0" "1_2" "1_1" "1_0")
+  foreach(VERSION "3_0" "2_2" "2_1" "2_0" "1_2" "1_1" "1_0")
     set(CMAKE_REQUIRED_INCLUDES "${OpenCL_INCLUDE_DIR}")
 
     if(APPLE)
