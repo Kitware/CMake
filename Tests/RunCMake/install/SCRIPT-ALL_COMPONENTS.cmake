b0c8e31b54ca7ab2647e9301a12a73c23f887acb
Nils Gladitz <nilsgladitz@gmail.com>
2022-05-14 10:28:56 +0200

    install: Don't ignore EXCLUDE_FROM_ALL when used with ALL_COMPONENTS
    Fixes: #23494

--- Tests/RunCMake/install/SCRIPT-ALL_COMPONENTS.cmake
+++ Tests/RunCMake/install/SCRIPT-ALL_COMPONENTS.cmake
@@ -1,5 +1,16 @@
+
 install(
   SCRIPT "${CMAKE_CURRENT_SOURCE_DIR}/install_script.cmake"
+  ALL_COMPONENTS
+)
+
+install(
   CODE "write_empty_file(empty2.txt)"
   ALL_COMPONENTS
-  )
+  EXCLUDE_FROM_ALL
+)
+
+install(
+  CODE "write_empty_file(empty3.txt)"
+  ALL_COMPONENTS
+)
