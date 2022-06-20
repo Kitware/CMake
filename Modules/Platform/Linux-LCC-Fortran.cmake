57da7a4925fe687f8a66e03034271f33936a7bc6
Igor Molchanov <akemi_homura@kurisa.ch>
2022-06-20 23:07:28 +0300

    LCC: link with -lgfortran instead of -llfortran since 1.26.03
    Since LCC 1.26.03, compiler developers decided to rename
    liblfortran to libgfortran (internal reference: mcstbug#131633),
    and despite it's stated that "-llfortran will be automatically
    treated as -lgfortran", it actually does not work (and there's
    even no symlinks like liblfortran.* -> libgfortran.*); so we
    have to explicitly choose which library we have to link in.
    
    Fixes: #23646

--- Modules/Platform/Linux-LCC-Fortran.cmake
+++ Modules/Platform/Linux-LCC-Fortran.cmake
@@ -1,3 +1,7 @@
 include(Platform/Linux-LCC)
 __linux_compiler_lcc(Fortran)
-set(CMAKE_SHARED_LIBRARY_LINK_Fortran_FLAGS "-llfortran")
+if (CMAKE_Fortran_COMPILER_VERSION VERSION_LESS "1.26.03")
+  set(CMAKE_SHARED_LIBRARY_LINK_Fortran_FLAGS "-llfortran")
+else()
+  set(CMAKE_SHARED_LIBRARY_LINK_Fortran_FLAGS "-lgfortran")
+endif()
