3e1ace7f4633acb5d169bd334f05aa82f65bcfaf
Ben Boeckel <ben.boeckel@kitware.com>
2022-06-21 14:17:28 -0400

    ci: add git to the CI image
    This allows the CDash submission to know what commit is being submitted
    and show up in MR pipeline status links made by our workflow processes.

--- .gitlab/ci/docker/gcc_cxx_modules/install_deps.sh
+++ .gitlab/ci/docker/gcc_cxx_modules/install_deps.sh
@@ -3,5 +3,5 @@
 set -e
 
 dnf install -y --setopt=install_weak_deps=False \
-    gcc-c++ mpfr-devel libmpc-devel isl-devel flex bison file findutils diffutils
+    gcc-c++ mpfr-devel libmpc-devel isl-devel flex bison file findutils diffutils git-core
 dnf clean all
