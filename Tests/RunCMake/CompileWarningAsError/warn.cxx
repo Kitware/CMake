2e9ac1d2726c4f260efd6b455242ec5d65391e12
Robert Maynard <rmaynard@nvidia.com>
2022-06-28 09:49:36 -0400

    Tests: Refactor warn on error tests to support multiple languages

--- Tests/RunCMake/CompileWarningAsError/warn.cxx
+++ Tests/RunCMake/CompileWarningAsError/warn.cxx
@@ -1,17 +1,1 @@
-static void unused_function();
-
-#ifdef __SUNPRO_CC
-struct A
-{
-  virtual ~A() throw();
-};
-struct B : public A
-{
-  virtual ~B() throw(int);
-};
-#endif
-
-int main(int unused_argument, char* [])
-{
-  return 1;
-}
+#include "warn.c"
