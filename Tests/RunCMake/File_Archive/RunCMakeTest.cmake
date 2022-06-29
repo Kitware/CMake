dfff470d156b70b71a2349b51dee4da4275a75bd
Brad King <brad.king@kitware.com>
2022-06-29 16:26:33 -0400

    file(ARCHIVE*): Add test for keyword arguments with missing values

--- Tests/RunCMake/File_Archive/RunCMakeTest.cmake
+++ Tests/RunCMake/File_Archive/RunCMakeTest.cmake
@@ -13,6 +13,9 @@ run_cmake(zip)
 # Extracting only selected files or directories
 run_cmake(zip-filtered)
 
+run_cmake(create-missing-args)
+run_cmake(extract-missing-args)
+
 run_cmake(unsupported-format)
 run_cmake(zip-with-bad-compression)
 run_cmake(7zip-with-bad-compression)
