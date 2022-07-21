14a0e750cb33f09faf01134c420483d358d0fae8
Robert Maynard <rmaynard@nvidia.com>
2022-07-21 09:52:28 -0400

    cmake: In -P mode ignore flags like `--version` after `--`
    Fixes: #21031

--- Source/cmakemain.cxx
+++ Source/cmakemain.cxx
@@ -207,7 +207,7 @@ int do_cmake(int ac, char const* const* av)
 #ifndef CMAKE_BOOTSTRAP
   cmDocumentation doc;
   doc.addCMakeStandardDocSections();
-  if (doc.CheckOptions(ac, av)) {
+  if (doc.CheckOptions(ac, av, "--")) {
     // Construct and print requested documentation.
     cmake hcm(cmake::RoleInternal, cmState::Unknown);
     hcm.SetHomeDirectory("");
