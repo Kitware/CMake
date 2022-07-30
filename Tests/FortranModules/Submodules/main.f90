219a9b1e14084d16284c909ea055b3329750c909
Vitaly Mogulian <vitaly.mogulian@gmail.com>
2022-07-30 21:07:14 +0400

    Fortran: Fix suprious dependencies with submodules
    In commit 695f0d0d3a (cmFortranParser: Parse keywords as lexical tokens,
    2016-09-05, v3.7.0-rc1~150^2) we created keyword-specific variants of
    the original `USE WORD other EOSTMT` production, such as
    `MODULE WORD other EOSTMT` and `INTERFACE WORD other EOSTMT`.  The same
    pattern was used by more keyword-specific productions in commit b5ac8b8aa7
    (Fortran: Add support for submodule syntax in dependency scanning,
    2016-09-05, v3.7.0-rc1~73^2~1).
    
    The postfix part (`other`) of several keyword-specific productions is
    not needed to match Fortran syntax.  See the Fortran 2018 standard,
    para.4.1.4/1 on p.28, para.14.2.1/2 on pp.293-294.  The postfix is
    needed only for a case of operator 'use':
    
        use <module-name> [, only : <list-of-vars>]
    
    The unnecessary postfix matching from the keyword-specific productions
    such as module, submodule, and interface declarations can cause spurious
    module dependencies to be detected, so remove it.
    
    Extend the test suite with examples covering the previously-broken
    cases.
    
    Fixes: #18427

--- Tests/FortranModules/Submodules/main.f90
+++ Tests/FortranModules/Submodules/main.f90
@@ -1,7 +1,10 @@
 program main
   use parent, only : child_function,grandchild_subroutine
   use parent, only : sibling_function,GreatGrandChild_subroutine
+  ! Using a module without postfix
+  use obfuscated_parent
   implicit none
-  if (child_function()) call grandchild_subroutine
-  if (sibling_function()) call GreatGrandChild_subroutine
+  if (child_function())     call grandchild_subroutine
+  if (sibling_function())   call GreatGrandChild_subroutine
+  if (child_function_obf()) call grandchild_subroutine_obf
 end program
