3e2094fb9b35af4c262959117d203644d8f396ca
Michael Hirsch <scivision@users.noreply.gitlab.kitware.com>
2022-08-02 17:20:07 -0400

    FindLua: Improve documentation formatting
    Since commit cb811d11ce (Help: Improve description of modules,
    2019-04-12, v3.15.0-rc1~210^2) we've had two `::` prompts for
    the preformatted block listing the result variables.  Convert the
    block to a definition list.

--- Modules/FindLua.cmake
+++ Modules/FindLua.cmake
@@ -10,19 +10,22 @@ Locate Lua library.
 .. versionadded:: 3.18
   Support for Lua 5.4.
 
-This module defines::
+This module defines:
 
-::
-
-  LUA_FOUND          - if false, do not try to link to Lua
-  LUA_LIBRARIES      - both lua and lualib
-  LUA_INCLUDE_DIR    - where to find lua.h
-  LUA_VERSION_STRING - the version of Lua found
-  LUA_VERSION_MAJOR  - the major version of Lua
-  LUA_VERSION_MINOR  - the minor version of Lua
-  LUA_VERSION_PATCH  - the patch version of Lua
-
-
+``LUA_FOUND``
+  if false, do not try to link to Lua
+``LUA_LIBRARIES``
+  both lua and lualib
+``LUA_INCLUDE_DIR``
+  where to find lua.h
+``LUA_VERSION_STRING``
+  the version of Lua found
+``LUA_VERSION_MAJOR``
+  the major version of Lua
+``LUA_VERSION_MINOR``
+  the minor version of Lua
+``LUA_VERSION_PATCH``
+  the patch version of Lua
 
 Note that the expected include convention is
 
