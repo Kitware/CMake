81fd0d6e265985213ec3d0c1db4b8a6f314481ea
Maik Nijhuis <maik.nijhuis@triopsys.nl>
2022-08-04 16:09:20 +0200

    ExternalProject: Fix install on BUILD_ALWAYS+BUILD_BYPRODUCTS with Ninja
    The `BUILD_BYPRODUCTS` option causes the ExternalProject's `build` step
    in `build.ninja` to have `restat = 1`, so its "always out-of-date"
    status caused by `BUILD_ALWAYS` does not propagate to the `install`
    step.  Mark the latter step as explicitly always out-of-date too.
    
    Fixes: #23820

--- Tests/RunCMake/ExternalProject/BUILD_ALWAYS.cmake
+++ Tests/RunCMake/ExternalProject/BUILD_ALWAYS.cmake
@@ -12,6 +12,9 @@ ExternalProject_Add(always
   DOWNLOAD_COMMAND ""
   CONFIGURE_COMMAND "${CMAKE_COMMAND}" -P ${CMAKE_CURRENT_BINARY_DIR}/always-configure.cmake
   BUILD_COMMAND "${CMAKE_COMMAND}" -P ${CMAKE_CURRENT_BINARY_DIR}/always-build.cmake
+        COMMAND "${CMAKE_COMMAND}" -E copy_if_different ${CMAKE_CURRENT_LIST_FILE}
+                   "${CMAKE_CURRENT_BINARY_DIR}/byproduct.txt"
+  BUILD_BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/byproduct.txt"
   BUILD_ALWAYS 1
   INSTALL_COMMAND "${CMAKE_COMMAND}" -P ${CMAKE_CURRENT_BINARY_DIR}/always-install.cmake
   )
