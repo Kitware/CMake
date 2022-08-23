95a3d91e5ce2388b5c0e6ea4e4e3663477975a96
Alex Turbov <i.zaufi@gmail.com>
2022-08-23 20:06:48 +0400

    cmakemain.cxx: Remove always true preprocessor `#if` block

--- Source/cmakemain.cxx
+++ Source/cmakemain.cxx
@@ -113,14 +113,12 @@ const char* cmDocumentationOptions[][2] = {
   { "--compile-no-warning-as-error",
     "Ignore COMPILE_WARNING_AS_ERROR property and "
     "CMAKE_COMPILE_WARNING_AS_ERROR variable." },
-#  if !defined(CMAKE_BOOTSTRAP)
   { "--profiling-format=<fmt>",
     "Output data for profiling CMake scripts. Supported formats: "
     "google-trace" },
   { "--profiling-output=<file>",
     "Select an output path for the profiling data enabled through "
     "--profiling-format." },
-#  endif
   { nullptr, nullptr }
 };
 
