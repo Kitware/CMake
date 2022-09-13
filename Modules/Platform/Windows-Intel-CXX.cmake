90874a542e9d64d0a49326951defb2ed899bcbff
Marc Chevrier <marc.chevrier@gmail.com>
2022-09-13 13:36:33 +0200

    Intel classic compilers on Windows: use msvc style dependencies
    Intel classic compilers generate erroneous dependencies when gcc style
    is used with paths containing spaces. So, fall back to the old way to
    generate dependencies by using option /showIncludes.
    
    Fixes: #23948

--- Modules/Platform/Windows-Intel-CXX.cmake
+++ Modules/Platform/Windows-Intel-CXX.cmake
@@ -20,9 +20,8 @@ if((NOT DEFINED CMAKE_DEPENDS_USE_COMPILER OR CMAKE_DEPENDS_USE_COMPILER)
  set(CMAKE_CXX_DEPENDS_USE_COMPILER TRUE)
 endif()
 
-if("${CMAKE_SOURCE_DIR}${CMAKE_BINARY_DIR}" MATCHES " ")
-  # The Intel compiler does not properly escape spaces in a depfile.
-  # Fall back to msvc depfile format.
-  set(CMAKE_DEPFILE_FLAGS_CXX "/showIncludes")
-  set(CMAKE_CXX_DEPFILE_FORMAT msvc)
-endif()
+# The Intel compiler does not properly escape spaces in a depfile which can
+# occur in source and binary cmake paths as well as external include paths.
+# Until Intel fixes this bug, fall back unconditionally to msvc depfile format.
+set(CMAKE_DEPFILE_FLAGS_CXX "/showIncludes")
+set(CMAKE_CXX_DEPFILE_FORMAT msvc)
