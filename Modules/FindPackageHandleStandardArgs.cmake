Kitware/CMake

commit 8f50f135aefa3f287a87c80cc8edb3deee8ee198

FPHSA: Improve error message when VERSION_VAR is empty or has been unset()
Previously, when `VERSION_VAR` is set but empty, FPHSA emitted a
confusing error message, saying that the package was found but of an
unsuitable version "".  Fix the error message to state that the package
was not found.

Fixes: #23807

Modules/FindPackageHandleStandardArgs.cmake
@@ -535,9 +535,10 @@ function(FIND_PACKAGE_HANDLE_STANDARD_ARGS _NAME _FIRST_ARG)
   set(VERSION_MSG "")
   set(VERSION_OK TRUE)
 
-  # check with DEFINED here as the requested or found version may be "0"
+  # check that the version variable is not empty to avoid emitting a misleading
+  # message (i.e. `Found unsuitable version ""`)
   if (DEFINED ${_NAME}_FIND_VERSION)
-    if(DEFINED ${FPHSA_VERSION_VAR})
+    if(NOT "${${FPHSA_VERSION_VAR}}" STREQUAL "")
       set(_FOUND_VERSION ${${FPHSA_VERSION_VAR}})
       if (FPHSA_HANDLE_VERSION_RANGE)
         set (FPCV_HANDLE_VERSION_RANGE HANDLE_VERSION_RANGE)
@@ -547,6 +548,7 @@ function(FIND_PACKAGE_HANDLE_STANDARD_ARGS _NAME _FIRST_ARG)
       find_package_check_version ("${_FOUND_VERSION}" VERSION_OK RESULT_MESSAGE_VARIABLE VERSION_MSG
         ${FPCV_HANDLE_VERSION_RANGE})
     else()
+      set(VERSION_OK FALSE)
       # if the package was not found, but a version was given, add that to the output:
       if(${_NAME}_FIND_VERSION_EXACT)
         set(VERSION_MSG "(Required is exact version \"${${_NAME}_FIND_VERSION}\")")

Tests/RunCMake/FPHSA/RunCMakeTest.cmake
@@ -38,8 +38,11 @@ run_cmake(exact_1.2.3.5)
 unset(RunCMake_DEFAULT_stderr)
 
 # check if searching for a version 0 works
-list(APPEND RunCMake_TEST_OPTIONS "-DCMAKE_MODULE_PATH=${CMAKE_CURRENT_LIST_DIR}" "-DPseudo_VERSION=0")
+set(RunCMake_TEST_OPTIONS "-DCMAKE_MODULE_PATH=${CMAKE_CURRENT_LIST_DIR}" "-DPseudo_VERSION=0")
 run_cmake(exact_0_matching)
+
+set(RunCMake_TEST_OPTIONS "-DCMAKE_MODULE_PATH=${CMAKE_CURRENT_LIST_DIR}" "-DPseudo_VERSION=")
+run_cmake(empty_version)
 
 # check custom error message
 set(RunCMake_TEST_OPTIONS "-DCMAKE_MODULE_PATH=${CMAKE_CURRENT_LIST_DIR}" "-DCustomMessage_VERSION=1.2.3.4")

Tests/RunCMake/FPHSA/empty_version.cmake
@@ -1,0 +1,1 @@
+find_package(Pseudo 1 REQUIRED)

commit 07f0a78874e0cf8013654ff4bac5e7192a324609

FPHSA: Fix regression when VERSION_VAR is missing
If a package is found but FPHSA is called by the find module without a
`VERSION_VAR`, and the `find_package` call specifies a version, we have
previously accepted the package as found.  This was accidentally
regressed by commit 8f50f135ae (FPHSA: Improve error message when
VERSION_VAR is empty or has been unset(), 2022-08-01).  Restore it and
add a test case.

Modules/FindPackageHandleStandardArgs.cmake
@@ -538,17 +538,21 @@ function(FIND_PACKAGE_HANDLE_STANDARD_ARGS _NAME _FIRST_ARG)
   # check that the version variable is not empty to avoid emitting a misleading
   # message (i.e. `Found unsuitable version ""`)
   if (DEFINED ${_NAME}_FIND_VERSION)
-    if(NOT "${${FPHSA_VERSION_VAR}}" STREQUAL "")
-      set(_FOUND_VERSION ${${FPHSA_VERSION_VAR}})
-      if (FPHSA_HANDLE_VERSION_RANGE)
-        set (FPCV_HANDLE_VERSION_RANGE HANDLE_VERSION_RANGE)
+    if(DEFINED ${FPHSA_VERSION_VAR})
+      if(NOT "${${FPHSA_VERSION_VAR}}" STREQUAL "")
+        set(_FOUND_VERSION ${${FPHSA_VERSION_VAR}})
+        if (FPHSA_HANDLE_VERSION_RANGE)
+          set (FPCV_HANDLE_VERSION_RANGE HANDLE_VERSION_RANGE)
+        else()
+          set(FPCV_HANDLE_VERSION_RANGE NO_AUTHOR_WARNING_VERSION_RANGE)
+        endif()
+        find_package_check_version ("${_FOUND_VERSION}" VERSION_OK RESULT_MESSAGE_VARIABLE VERSION_MSG
+          ${FPCV_HANDLE_VERSION_RANGE})
       else()
-        set(FPCV_HANDLE_VERSION_RANGE NO_AUTHOR_WARNING_VERSION_RANGE)
+        set(VERSION_OK FALSE)
       endif()
-      find_package_check_version ("${_FOUND_VERSION}" VERSION_OK RESULT_MESSAGE_VARIABLE VERSION_MSG
-        ${FPCV_HANDLE_VERSION_RANGE})
-    else()
-      set(VERSION_OK FALSE)
+    endif()
+    if("${${FPHSA_VERSION_VAR}}" STREQUAL "")
       # if the package was not found, but a version was given, add that to the output:
       if(${_NAME}_FIND_VERSION_EXACT)
         set(VERSION_MSG "(Required is exact version \"${${_NAME}_FIND_VERSION}\")")

Tests/RunCMake/FPHSA/RunCMakeTest.cmake
@@ -44,6 +44,9 @@ run_cmake(exact_0_matching)
 set(RunCMake_TEST_OPTIONS "-DCMAKE_MODULE_PATH=${CMAKE_CURRENT_LIST_DIR}" "-DPseudo_VERSION=")
 run_cmake(empty_version)
 
+set(RunCMake_TEST_OPTIONS "-DCMAKE_MODULE_PATH=${CMAKE_CURRENT_LIST_DIR}")
+run_cmake(exact_1_no_version_var)
+
 # check custom error message
 set(RunCMake_TEST_OPTIONS "-DCMAKE_MODULE_PATH=${CMAKE_CURRENT_LIST_DIR}" "-DCustomMessage_VERSION=1.2.3.4")
 run_cmake(custom_message_1)

Tests/RunCMake/FPHSA/exact_1_no_version_var.cmake
@@ -1,0 +1,1 @@
+find_package(Pseudo 1 REQUIRED)