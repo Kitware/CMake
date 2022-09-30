0a2093d0c5d988903d9c771e9152c9912aa5fccc
Kyle Edwards <kyle.edwards@kitware.com>
2022-09-30 12:45:01 -0400

    clang-tidy module: add test framework

--- .gitlab/ci/env_fedora36_tidy.sh
+++ .gitlab/ci/env_fedora36_tidy.sh
@@ -1,3 +1,6 @@
-cmake -S Utilities/ClangTidyModule -B Utilities/ClangTidyModule/build
+cmake \
+  -S Utilities/ClangTidyModule \
+  -B Utilities/ClangTidyModule/build \
+  -DRUN_TESTS=ON
 cmake --build Utilities/ClangTidyModule/build
 ctest --test-dir Utilities/ClangTidyModule/build
