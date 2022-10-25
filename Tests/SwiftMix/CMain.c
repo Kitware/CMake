c0dd3dd2c13d8b7621bc86b6ea93e9188ab5dba7
Brad King <brad.king@kitware.com>
2022-10-25 11:56:34 -0400

    Xcode: Evaluate Swift compile definitions separately
    Xcode has a separate setting for Swift compile definitions, so we can
    compute a dedicated value for it.  Therefore we can:
    
    * Support the COMPILE_LANGUAGE generator expression for Swift-specific
      filters.
    
    * Avoid passing the `=value` part of definitions, which Swift does
      not support.
    
    This revises commit 5cb625eb2f (Xcode: Pass compile definitions to
    Swift, 2022-06-19, v3.25.0-rc1~493^2) and reverts commit 12c6fec6b4
    (Xcode: Drop CMAKE_INTDIR= definition in Swift targets, 2022-09-30,
    v3.25.0-rc1~60^2~2), as the latter is no longer needed.
    
    Fixes: #24086

--- Tests/SwiftMix/CMain.c
+++ Tests/SwiftMix/CMain.c
@@ -1,3 +1,16 @@
+#if !defined(FOO)
+#  error "FOO not defined"
+#endif
+#if BAR != 3
+#  error "FOO not defined to 3"
+#endif
+#if CCOND != 2
+#  error "CCOND not defined to 2"
+#endif
+#if defined(SWIFTCOND)
+#  error "SWIFTCOND defined"
+#endif
+
 extern int ObjCMain(void);
 int main(void)
 {
