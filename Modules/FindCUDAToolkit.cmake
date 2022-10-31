301815eb6243c7598c246d06524cff51f7e90576
Robert Maynard <rmaynard@nvidia.com>
2022-10-31 11:48:09 -0400

    FindCUDAToolkit: Add nvptxcompiler_static dependency on Threads::Threads

--- Modules/FindCUDAToolkit.cmake
+++ Modules/FindCUDAToolkit.cmake
@@ -366,7 +366,7 @@ This is a static library only.
 
 Targets Created:
 
-- ``CUDA::nvptxcompiler`` starting in CUDA 11.1
+- ``CUDA::nvptxcompiler_static`` starting in CUDA 11.1
 
 .. _`cuda_toolkit_nvRTC`:
 
@@ -1059,6 +1059,7 @@ if(CUDAToolkit_FOUND)
   _CUDAToolkit_find_and_add_import_lib(nvrtc DEPS cuda_driver)
   if(CUDAToolkit_VERSION VERSION_GREATER_EQUAL 11.1.0)
     _CUDAToolkit_find_and_add_import_lib(nvptxcompiler_static DEPS cuda_driver)
+    target_link_libraries(CUDA::nvptxcompiler_static INTERFACE Threads::Threads)
   endif()
 
   _CUDAToolkit_find_and_add_import_lib(nvml ALT nvidia-ml nvml)
