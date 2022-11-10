bf2e4a2e857c1e9c9caf73bdf05cc1efc111c196
Brad King <brad.king@kitware.com>
2022-11-10 09:22:11 -0500

    ci: Factor out helper to load ninja into environment on Windows

--- .gitlab/ci/env_windows_vs2022_x64_ninja_multi.ps1
+++ .gitlab/ci/env_windows_vs2022_x64_ninja_multi.ps1
@@ -2,9 +2,6 @@ if ("$env:CMAKE_CI_NIGHTLY" -eq "true") {
   . ".gitlab/ci/ispc-env.ps1"
 }
 
-$pwdpath = $pwd.Path
-& "$pwsh" -File ".gitlab/ci/ninja.ps1"
-Set-Item -Force -Path "env:PATH" -Value "$pwdpath\.gitlab;$env:PATH"
-ninja --version
+. .gitlab/ci/ninja-env.ps1
 
 Invoke-Expression -Command .gitlab/ci/vcvarsall.ps1
