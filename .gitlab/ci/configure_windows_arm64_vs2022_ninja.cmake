09c1b1db078022096d9b671ebd5ad6bb7a4b52fd
Brad King <brad.king@kitware.com>
2022-11-11 12:03:23 -0500

    gitlab-ci: Add windows-arm64 native jobs for Ninja and VS
    Add Windows ARM64 native CI jobs to build and test with Ninja,
    and to test with VS.
    
    Issue: #23624

---
+++ .gitlab/ci/configure_windows_arm64_vs2022_ninja.cmake
@@ -0,0 +1,7 @@
+# Qt host tools are not yet available natively on windows-arm64.
+set(CMake_TEST_GUI "OFF" CACHE BOOL "")
+set(BUILD_QtDialog "OFF" CACHE BOOL "")
+set(CMAKE_PREFIX_PATH "" CACHE STRING "")
+
+include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_msvc_cxx_modules_common.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_vs_common_ninja.cmake")
