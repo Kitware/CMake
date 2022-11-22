61075d2d7b0a77f53bc9a0d4c6644ab338f27886
Marc Chevrier <marc.chevrier@gmail.com>
2022-11-22 16:32:59 +0100

    XCode: ensure LINK_LIBRARY genex is usable with XCODE_LINK_BUILD_PHASE_MODE
    Fixes: #24176

--- Tests/RunCMake/XcodeProject/LinkBinariesBuildPhase_KNOWN_LOCATION-check.cmake
+++ Tests/RunCMake/XcodeProject/LinkBinariesBuildPhase_KNOWN_LOCATION-check.cmake
@@ -4,10 +4,17 @@ include(${RunCMake_TEST_BINARY_DIR}/foundLibs.cmake)
 # obj2    --> Embeds func3.o in the link flags, but obj2 is part of the path
 # ${libz} --> This is for imported2
 
-foreach(mainTarget IN ITEMS app1 shared1 module1 sharedFramework1)
+foreach(mainTarget IN ITEMS app1 app2 shared1 shared3 module1 sharedFramework1)
   checkFlags(OTHER_LDFLAGS ${mainTarget}
     "obj2"
     "static2;shared2;staticFramework2;sharedFramework2;${libz};${libresolv};CoreFoundation;sharedFrameworkExt;staticFrameworkExt"
+  )
+endforeach()
+
+foreach(mainTarget IN ITEMS app3 shared4)
+  checkFlags(OTHER_LDFLAGS ${mainTarget}
+    "obj2;shared2;sharedFramework2;sharedFrameworkExt"
+    "static2;staticFramework2;${libz};${libresolv};CoreFoundation;staticFrameworkExt"
   )
 endforeach()
 
