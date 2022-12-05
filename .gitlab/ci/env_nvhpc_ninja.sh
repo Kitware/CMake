7d2979d6eb8ee27a59ca64e897308f87146d2cb6
Brad King <brad.king@kitware.com>
2022-12-05 13:17:39 -0500

    gitlab-ci: Select CUDA version explicitly in NVHPC job
    The job added by commit 16a5c36795 (gitlab-ci: add nightly job testing
    NVHPC with Ninja on Linux, 2022-11-21) started failing after CI hosts
    were updated from NVIDIA driver version 520 to version 525.  Somehow
    this causes the NVHPC multi-cuda `nvcc` wrapper to select CUDA 10.2
    instead of 11.7 as with older driver versions.
    
    As a workaround, add the preferred version of `nvcc` to the PATH.
    
    Issue: #24225

--- .gitlab/ci/env_nvhpc_ninja.sh
+++ .gitlab/ci/env_nvhpc_ninja.sh
@@ -3,3 +3,7 @@ export CXX=nvc++
 export FC=nvfortran
 export CUDACXX=nvcc
 export CUDAHOSTCXX=nvc++
+# FIXME(#24225): /opt/nvidia/hpc_sdk/Linux_x86_64/22.9/compilers/bin/nvcc
+# selects a CUDA version 10.2 with host driver versions > 520.
+# Manually select the preferred nvcc version.
+export PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/22.9/cuda/11.7/bin:$PATH
