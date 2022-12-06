c445dd27979322b5ac6d144c7307554bb9228317
Quentin Berthet <qberthet@siradel.com>
2022-12-06 15:27:04 +0100

    VS: Add Xaml and Resx files to .Net SDK style projects
    Improve parity with classic MSBuild projects.
    
    Fixes: #23415

--- Tests/RunCMake/VsDotnetSdk/RunCMakeTest.cmake
+++ Tests/RunCMake/VsDotnetSdk/RunCMakeTest.cmake
@@ -6,6 +6,7 @@ run_cmake(VsDotnetSdkCustomCommandsSource)
 run_cmake(VsDotnetSdkStartupObject)
 run_cmake(VsDotnetSdkDefines)
 run_cmake(DotnetSdkVariables)
+run_cmake(VsDotnetSdkXamlFiles)
 
 function(run_VsDotnetSdk)
   set(RunCMake_TEST_BINARY_DIR ${RunCMake_BINARY_DIR}/VsDotnetSdk-build)
