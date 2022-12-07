8b9c9e7e27ecb1414498c668d7c83352fcaf7e39
Brad King <brad.king@kitware.com>
2022-12-07 08:21:39 -0500

    Tests: Teach RunCMake.FindBoost to avoid finding an external Boost
    The `ConfigModeNotFound` case does not expect to find a Boost.
    Avoid accidentally finding one on the host.

--- Tests/RunCMake/FindBoost/CommonNotFound.cmake
+++ Tests/RunCMake/FindBoost/CommonNotFound.cmake
@@ -1,2 +1,6 @@
+set(CMAKE_FIND_USE_CMAKE_PATH OFF)
+set(CMAKE_FIND_USE_CMAKE_ENVIRONMENT_PATH OFF)
+set(CMAKE_FIND_USE_CMAKE_SYSTEM_PATH OFF)
+set(CMAKE_FIND_USE_SYSTEM_ENVIRONMENT_PATH OFF)
 # Make sure to use the module mode signature here to not bypass FindBoost
 find_package(Boost 1.80 COMPONENTS timer foobar)
