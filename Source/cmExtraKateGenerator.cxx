8802b81b0da0f74addac291f30eaa58adff0bd69
Alexander Neundorf <neundorf@kde.org>
2023-02-13 22:34:53 +0100

    Kate: don't include files in the CMakeFiles/ dir in the project
    These files are generated by cmake, they shouldn't be in the project.

--- Source/cmExtraKateGenerator.cxx
+++ Source/cmExtraKateGenerator.cxx
@@ -282,9 +282,8 @@ std::string cmExtraKateGenerator::GenerateFilesString(
     cmMakefile* makefile = lgen->GetMakefile();
     const std::vector<std::string>& listFiles = makefile->GetListFiles();
     for (std::string const& listFile : listFiles) {
-      tmp = listFile;
-      {
-        files.insert(tmp);
+      if (listFile.find("/CMakeFiles/") == std::string::npos) {
+        files.insert(listFile);
       }
     }
 
