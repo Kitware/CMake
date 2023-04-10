f52847e9539ae38e4e3a31d8894e31991714d734
Garrett Campbell <gcampbell@microsoft.com>
2023-04-10 13:48:06 -0400

    typo fix

--- Source/LexerParser/cmListFileLexer.c
+++ Source/LexerParser/cmListFileLexer.c
@@ -2725,7 +2725,7 @@ static cmListFileLexer_BOM cmListFileLexer_ReadBOM(FILE* f)
       if (fread(b, 1, 2, f) == 2 && b[0] == 0 && b[1] == 0) {
         return cmListFileLexer_BOM_UTF32LE;
       }
-	  if (fgetpos(f, &p) != ) {
+	  if (fgetpos(f, &p) != 0) {
 		return cmListFileLexer_BOM_Broken;
 	  }
       if (fsetpos(f, &p) != 0) {
