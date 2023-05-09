c78be340ba110366f59ccfd4dc140e5ac31191d9
Brad King <brad.king@kitware.com>
2023-05-09 09:04:56 -0400

    Tests: Teach RunCMake to ignore Intel Fortran remark 10440
    The Intel Fortran compiler may remark:
    
        ifx: remark #10440: Note that use of a debug option without any
        optimization-level option will turnoff most compiler optimizations
        similar to use of '-O0'
    
    Teach RunCMake to drop such incidental lines before matching against
    expected output.
    
    This extends commit b2d030e8fe (Tests: Teach RunCMake to ignore Intel
    -Rdebug-disables-optimization remarks, 2022-06-14, v3.26.0-rc1~395^2)
    to cover the `ifx` Fortran compiler too.

--- Tests/RunCMake/RunCMake.cmake
+++ Tests/RunCMake/RunCMake.cmake
@@ -168,6 +168,7 @@ function(run_cmake test)
     "|[a-z]+\\([0-9]+\\) malloc:"
     "|clang[^:]*: warning: the object size sanitizer has no effect at -O0, but is explicitly enabled:"
     "|icp?x: remark: Note that use of .-g. without any optimization-level option will turn off most compiler optimizations"
+    "|ifx: remark #10440: Note that use of a debug option without any optimization-level option will turnoff most compiler optimizations"
     "|lld-link: warning: procedure symbol record for .* refers to PDB item index [0-9A-Fa-fx]+ which is not a valid function ID record"
     "|Error kstat returned"
     "|Hit xcodebuild bug"
