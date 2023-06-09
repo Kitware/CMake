Kitware/CMake

commit e08ba229ee3107bcb814ca45bc9c72a48abf1ba2

CMake code rely on cmList class for CMake lists management (part. 1)

Source/cmTestGenerator.cxx
@@ -13,6 +13,7 @@
 
 #include "cmGeneratorExpression.h"
 #include "cmGeneratorTarget.h"
+#include "cmList.h"
 #include "cmListFileCache.h"
 #include "cmLocalGenerator.h"
 #include "cmMakefile.h"
@@ -147,16 +148,15 @@ void cmTestGenerator::GenerateScriptForConfig(std::ostream& os,
   }
 
   // Evaluate command line arguments
-  std::vector<std::string> argv =
-    this->EvaluateCommandLineArguments(this->Test->GetCommand(), ge, config);
-
-  // Expand arguments if COMMAND_EXPAND_LISTS is set
-  if (this->Test->GetCommandExpandLists()) {
-    argv = cmExpandedLists(argv.begin(), argv.end());
-    // Expanding lists on an empty command may have left it empty
-    if (argv.empty()) {
-      argv.emplace_back();
-    }
+  cmList argv{
+    this->EvaluateCommandLineArguments(this->Test->GetCommand(), ge, config),
+    // Expand arguments if COMMAND_EXPAND_LISTS is set
+    this->Test->GetCommandExpandLists() ? cmList::ExpandElements::Yes
+                                        : cmList::ExpandElements::No
+  };
+  // Expanding lists on an empty command may have left it empty
+  if (argv.empty()) {
+    argv.emplace_back();
   }
 
   // Check whether the command executable is a target whose name is to
@@ -170,7 +170,7 @@ void cmTestGenerator::GenerateScriptForConfig(std::ostream& os,
     // Prepend with the emulator when cross compiling if required.
     cmValue emulator = target->GetProperty("CROSSCOMPILING_EMULATOR");
     if (cmNonempty(emulator)) {
-      std::vector<std::string> emulatorWithArgs = cmExpandedList(*emulator);
+      cmList emulatorWithArgs{ *emulator };
       std::string emulatorExe(emulatorWithArgs[0]);
       cmSystemTools::ConvertToUnixSlashes(emulatorExe);
       os << cmOutputConverter::EscapeForCMake(emulatorExe) << " ";

commit 1df3287bf6a49d410b81be670218259afc3474d4

add_test: Restore support for empty test arguments
This was regressed by refactoring in commit e08ba229ee (CMake code rely
on cmList class for CMake lists management (part. 1), 2023-04-14,
v3.27.0-rc1~174^2).  Fix it and add a test case.

Fixes: #24986

Source/cmTestGenerator.cxx
@@ -152,7 +152,8 @@ void cmTestGenerator::GenerateScriptForConfig(std::ostream& os,
     this->EvaluateCommandLineArguments(this->Test->GetCommand(), ge, config),
     // Expand arguments if COMMAND_EXPAND_LISTS is set
     this->Test->GetCommandExpandLists() ? cmList::ExpandElements::Yes
-                                        : cmList::ExpandElements::No
+                                        : cmList::ExpandElements::No,
+    cmList::EmptyElements::Yes
   };
   // Expanding lists on an empty command may have left it empty
   if (argv.empty()) {

Tests/RunCMake/add_test/CheckEmptyArgument.cmake
@@ -1,0 +1,11 @@
+if (NOT CMAKE_ARGV4 STREQUAL "A")
+  message(FATAL_ERROR "wrong parsing of arguments")
+endif()
+
+if (NOT CMAKE_ARGV5 STREQUAL "")
+  message(FATAL_ERROR "Empty argument was dropped")
+endif()
+
+if (NOT CMAKE_ARGV6 STREQUAL "B")
+  message(FATAL_ERROR "wrong parsing of arguments")
+endif()

Tests/RunCMake/add_test/EmptyArgument.cmake
@@ -1,0 +1,5 @@
+enable_testing()
+add_test(
+  NAME "EmptyArgument"
+  COMMAND "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_LIST_DIR}/CheckEmptyArgument.cmake" -- "A" "" "B"
+)

Tests/RunCMake/add_test/RunCMakeTest.cmake
@@ -33,3 +33,11 @@ foreach(case IN LISTS cases)
   run_case(OLD-${case})
   run_case(NEW-${case})
 endforeach()
+
+block()
+  set(RunCMake_TEST_BINARY_DIR ${RunCMake_BINARY_DIR}/EmptyArgument-build)
+  run_cmake(EmptyArgument)
+  # Run ctest on the generated CTestTestfile.cmake.
+  set(RunCMake_TEST_NO_CLEAN 1)
+  run_cmake_command(EmptyArgument-ctest ${CMAKE_CTEST_COMMAND} -C Debug)
+endblock()