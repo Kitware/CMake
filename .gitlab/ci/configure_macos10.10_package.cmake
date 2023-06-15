7f6cebb1cdf68366f31de6c2c6e9c7e13a2f1eb9
Brad King <brad.king@kitware.com>
2023-06-15 09:13:19 -0400

    ci: Split macOS 10.10+ and 10.13+ package language standard selection
    Prepare to use a different language standard level for each package.

--- .gitlab/ci/configure_macos10.10_package.cmake
+++ .gitlab/ci/configure_macos10.10_package.cmake
@@ -1,4 +1,6 @@
 set(CPACK_SYSTEM_NAME "macos10.10-universal" CACHE STRING "")
 set(CMAKE_OSX_DEPLOYMENT_TARGET "10.10" CACHE STRING "")
+set(CMAKE_C_STANDARD "11" CACHE STRING "")
+set(CMAKE_CXX_STANDARD "14" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_macos_package_common.cmake")
