d58253d155b43b8af1d0b64412145625907e3d9e
Ben Boeckel <ben.boeckel@kitware.com>
2023-07-27 18:23:46 -0400

    clang-tidy: fix `performance-faster-string-find` lints

--- Source/CPack/WiX/cmCPackWIXGenerator.cxx
+++ Source/CPack/WiX/cmCPackWIXGenerator.cxx
@@ -1050,7 +1050,7 @@ std::string cmCPackWIXGenerator::GetRightmostExtension(
 {
   std::string extension;
 
-  std::string::size_type i = filename.rfind(".");
+  std::string::size_type i = filename.rfind('.');
   if (i != std::string::npos) {
     extension = filename.substr(i);
   }
