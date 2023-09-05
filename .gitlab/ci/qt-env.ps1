67abc25e6716a75814fbc66e38786e498387e6c5
Brad King <brad.king@kitware.com>
2023-09-05 15:22:37 -0400

    ci: Factor out location of cmake in Qt download script

--- .gitlab/ci/qt-env.ps1
+++ .gitlab/ci/qt-env.ps1
@@ -1,6 +1,9 @@
+if ($cmake -eq $null) {
+    throw ('$cmake powershell variable not set ')
+}
 if ("$env:PROCESSOR_ARCHITECTURE" -eq "AMD64") {
     $pwdpath = $pwd.Path
-    cmake -P .gitlab/ci/download_qt.cmake
+    & $cmake -P .gitlab/ci/download_qt.cmake
     Set-Item -Force -Path "env:PATH" -Value "$pwdpath\.gitlab\qt\bin;$env:PATH"
     qmake -v
 } elseif ("$env:PROCESSOR_ARCHITECTURE" -eq "ARM64") {
