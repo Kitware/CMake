7abb6d14ca7a2c88627263ad1f9e720b99c0ae0d
權少 <ljqican@163.com>
2023-09-08 13:06:33 -0400

    FindLua{50,51}: Fix preformatted documentation blocks
    Avoid duplicate `::`.

--- Modules/FindLua50.cmake
+++ Modules/FindLua50.cmake
@@ -5,28 +5,19 @@
 FindLua50
 ---------
 
-
-
 Locate Lua library.
-This module defines::
 
-::
+This module defines::
 
   LUA50_FOUND, if false, do not try to link to Lua
   LUA_LIBRARIES, both lua and lualib
   LUA_INCLUDE_DIR, where to find lua.h and lualib.h (and probably lauxlib.h)
 
-
-
-Note that the expected include convention is
-
-::
+Note that the expected include convention is::
 
   #include "lua.h"
 
-and not
-
-::
+and not::
 
   #include <lua/lua.h>
 
