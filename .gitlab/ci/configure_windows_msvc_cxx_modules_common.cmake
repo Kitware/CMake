5eb7bd641a909925c6341819bc7900300776924f
Ben Boeckel <ben.boeckel@kitware.com>
2023-09-21 17:47:24 -0400

    Tests/RunCMake/CXXModules: remove rules file requirement
    Only Clang needs to disable extensions in order to work; other
    implementations work as-is.

--- .gitlab/ci/configure_windows_msvc_cxx_modules_common.cmake
+++ .gitlab/ci/configure_windows_msvc_cxx_modules_common.cmake
@@ -1,2 +1,1 @@
 set(CMake_TEST_MODULE_COMPILATION "named,compile_commands,collation,partitions,internal_partitions,shared,export_bmi,install_bmi,bmionly" CACHE STRING "")
-set(CMake_TEST_MODULE_COMPILATION_RULES "${CMAKE_CURRENT_LIST_DIR}/cxx_modules_rules_msvc.cmake" CACHE STRING "")
