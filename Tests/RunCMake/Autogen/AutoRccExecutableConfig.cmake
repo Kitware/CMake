c3f0825d3c0876829d066493a3fc0f0a516fb0f2
Orkun Tokdemir <ilhanorkuntokdemir@gmail.com>
2023-09-25 15:40:54 +0200

    Tests/RunCMake/Autogen: Factor out test setup

--- Tests/RunCMake/Autogen/AutoRccExecutableConfig.cmake
+++ Tests/RunCMake/Autogen/AutoRccExecutableConfig.cmake
@@ -1,16 +1,8 @@
-enable_language(CXX)
-
-set(CMAKE_CXX_STANDARD 11)
-find_package(Qt${with_qt_version} REQUIRED COMPONENTS Core Widgets Gui)
+include(RccExample.cmake)
 
 if(NOT TARGET Qt${with_qt_version}::rcc)
   message(FATAL_ERROR "Qt${with_qt_version}::rcc not found")
 endif()
-
-add_library(dummy STATIC example.cpp data.qrc)
-target_link_libraries(dummy Qt${with_qt_version}::Core
-                            Qt${with_qt_version}::Widgets
-                            Qt${with_qt_version}::Gui)
 
 get_target_property(rcc_location Qt${with_qt_version}::rcc IMPORTED_LOCATION)
 set_target_properties(dummy PROPERTIES AUTORCC_OPTIONS "EXE_PATH=${rcc_location}")
@@ -20,5 +12,4 @@ add_executable(myrcc $<$<CONFIG:Debug>:exe_debug.cpp>
                      $<$<CONFIG:RelWithDebInfo>:exe_relwithdebinfo.cpp>
 )
 
-set_target_properties(dummy PROPERTIES AUTORCC_EXECUTABLE $<TARGET_FILE:myrcc>
-                                       AUTORCC ON)
+set_target_properties(dummy PROPERTIES AUTORCC_EXECUTABLE $<TARGET_FILE:myrcc>)
