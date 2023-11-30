8bd78627dd3d4d2b6f83a4b688ad97cc71840857
Brad King <brad.king@kitware.com>
2023-11-30 13:13:15 -0500

    CMake 3.28.0-rc6

--- Source/CMakeVersion.cmake
+++ Source/CMakeVersion.cmake
@@ -2,7 +2,7 @@
 set(CMake_VERSION_MAJOR 3)
 set(CMake_VERSION_MINOR 28)
 set(CMake_VERSION_PATCH 0)
-set(CMake_VERSION_RC 5)
+set(CMake_VERSION_RC 6)
 set(CMake_VERSION_IS_DIRTY 0)
 
 # Start with the full version number used in tags.  It has no dev info.
