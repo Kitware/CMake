bd31a35899bec834e941fb145bf700d43751ff5d
Brad King <brad.king@kitware.com>
2023-12-12 09:52:34 -0500

    Tests: Add unit tests for internal cmake_determine_linker_id helper

--- Tests/RunCMake/ParseImplicitLinkInfo/RunCMakeTest.cmake
+++ Tests/RunCMake/ParseImplicitLinkInfo/RunCMakeTest.cmake
@@ -6,6 +6,10 @@ run_cmake(Inspect)
 set(info "${RunCMake_BINARY_DIR}/Inspect-build/info.cmake")
 include("${info}")
 
+if(CMAKE_HOST_UNIX)
+  run_cmake_script(DetermineLinkerId)
+endif()
+
 if(INFO_CMAKE_C_IMPLICIT_LINK_DIRECTORIES MATCHES ";")
   run_cmake_with_options(ExcludeDirs "-Dinfo=${RunCMake_BINARY_DIR}/Inspect-build/info.cmake")
 endif()
---
+++ Tests/RunCMake/ParseImplicitLinkInfo/ld-v/debian12-ld.lld.bash
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo 'Debian LLD 14.0.6 (compatible with GNU linkers)'
