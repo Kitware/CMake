459d1cc095f216170b9e3e0f28b58e58954607ad
Brad King <brad.king@kitware.com>
2023-12-13 10:38:01 -0500

    Tests: Verify that linker tool is detected and identified where expected

--- Tests/RunCMake/ParseImplicitLinkInfo/Inspect.cmake
+++ Tests/RunCMake/ParseImplicitLinkInfo/Inspect.cmake
@@ -2,10 +2,17 @@ enable_language(C)
 
 set(info "")
 foreach(var
+    CMAKE_SYSTEM_NAME
+    CMAKE_C_COMPILER
+    CMAKE_C_COMPILER_ID
+    CMAKE_C_COMPILER_VERSION
+    CMAKE_C_COMPILER_LINKER
+    CMAKE_C_COMPILER_LINKER_ID
+    CMAKE_C_COMPILER_LINKER_VERSION
     CMAKE_C_IMPLICIT_LINK_DIRECTORIES
     )
   if(DEFINED ${var})
-    string(APPEND info "set(INFO_${var} \"${${var}}\")\n")
+    string(APPEND info "set(${var} \"${${var}}\")\n")
   endif()
 endforeach()
 
