2b5447d4a8a35f58e97a42270202a5dad3277dc4
Brad King <brad.king@kitware.com>
2024-01-26 14:46:21 -0500

    Tests: Shorten RunCMake.CrosscompilingEmulator executable names
    Also switch to pure C.

--- Tests/RunCMake/CrosscompilingEmulator/AddTest.cmake
+++ Tests/RunCMake/CrosscompilingEmulator/AddTest.cmake
@@ -1,26 +1,28 @@
-set(CMAKE_CROSSCOMPILING 1)
+enable_language(C)
 enable_testing()
+set(CMAKE_CROSSCOMPILING 1)
+
 add_test(NAME DoesNotUseEmulator
   COMMAND ${CMAKE_COMMAND} -E echo "Hi")
 
-add_executable(generated_exe simple_src_exiterror.cxx)
+add_executable(exe main.c)
 
 add_test(NAME UsesEmulator
-  COMMAND generated_exe)
+  COMMAND exe)
 
 add_test(NAME DoesNotUseEmulatorWithGenex
-  COMMAND $<TARGET_FILE:generated_exe>)
+  COMMAND $<TARGET_FILE:exe>)
 
 add_subdirectory(AddTest)
 
 add_test(NAME UsesEmulatorWithExecTargetFromSubdirAddedWithoutGenex
-  COMMAND generated_exe_in_subdir_added_to_test_without_genex)
+  COMMAND subdir_exe_no_genex)
 
 add_test(NAME DoesNotUseEmulatorWithExecTargetFromSubdirAddedWithGenex
-  COMMAND $<TARGET_FILE:generated_exe_in_subdir_added_to_test_with_genex>)
+  COMMAND $<TARGET_FILE:subdir_exe_with_genex>)
 
-add_executable(generated_exe_test_launcher simple_src_exiterror.cxx)
-set_property(TARGET generated_exe_test_launcher PROPERTY TEST_LAUNCHER "pseudo_test_launcher")
+add_executable(exe_test_launcher main.c)
+set_property(TARGET exe_test_launcher PROPERTY TEST_LAUNCHER "pseudo_test_launcher")
 
 add_test(NAME UsesTestLauncherAndEmulator
-  COMMAND generated_exe_test_launcher)
+  COMMAND exe_test_launcher)
