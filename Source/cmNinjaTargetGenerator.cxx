9a45c9fbd55798f9636778504562cd03e293da2e
Ben Boeckel <ben.boeckel@kitware.com>
2024-02-13 12:16:59 -0500

    cmNinjaTargetGenerator: use `emplace_back` for scanning deps

--- Source/cmNinjaTargetGenerator.cxx
+++ Source/cmNinjaTargetGenerator.cxx
@@ -1190,7 +1190,7 @@ void cmNinjaTargetGenerator::WriteObjectBuildStatements(
 
     cmNinjaBuild build(this->LanguageDyndepRule(language, config));
     build.Outputs.push_back(this->GetDyndepFilePath(language, config));
-    build.ImplicitOuts.push_back(
+    build.ImplicitOuts.emplace_back(
       cmStrCat(this->Makefile->GetCurrentBinaryDirectory(), '/',
                this->LocalGenerator->GetTargetDirectory(this->GeneratorTarget),
                this->GetGlobalGenerator()->ConfigDirectory(config), '/',
@@ -1209,10 +1209,12 @@ void cmNinjaTargetGenerator::WriteObjectBuildStatements(
     auto const linked_directories =
       this->GetLinkedTargetDirectories(language, config);
     for (std::string const& l : linked_directories.Direct) {
-      build.ImplicitDeps.push_back(cmStrCat(l, '/', language, "Modules.json"));
+      build.ImplicitDeps.emplace_back(
+        cmStrCat(l, '/', language, "Modules.json"));
     }
     for (std::string const& l : linked_directories.Forward) {
-      build.ImplicitDeps.push_back(cmStrCat(l, '/', language, "Modules.json"));
+      build.ImplicitDeps.emplace_back(
+        cmStrCat(l, '/', language, "Modules.json"));
     }
 
     this->GetGlobalGenerator()->WriteBuild(this->GetImplFileStream(fileConfig),
