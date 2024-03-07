ac5aa7f1bffecb7bc9abc53a0ce541c248b8d33c
Brad King <brad.king@kitware.com>
2024-03-07 16:43:36 -0500

    ci: Explicitly specify WiX architecture in packaging jobs

--- .gitlab/ci/configure_windows_x86_64_package.cmake
+++ .gitlab/ci/configure_windows_x86_64_package.cmake
@@ -1,6 +1,9 @@
 # CPack package file name component for this platform.
 set(CPACK_SYSTEM_NAME "windows-x86_64" CACHE STRING "")
 
+# Tell WiX to package for this architecture.
+set(CPACK_WIX_ARCHITECTURE "x64" CACHE STRING "")
+
 # Use APIs from at most Windows 7
 set(CMAKE_C_FLAGS "-D_WIN32_WINNT=0x601 -DNTDDI_VERSION=0x06010000" CACHE STRING "")
 set(CMAKE_CXX_FLAGS "-GR -EHsc -D_WIN32_WINNT=0x601 -DNTDDI_VERSION=0x06010000" CACHE STRING "")
