c1616c98bf6a9a08591c34724dfde9c0277fcff6
Brad King <brad.king@kitware.com>
2024-03-07 15:06:27 -0500

    ci: Move job settings away from unrelated comment

--- .gitlab/ci/configure_windows_arm64_vs2022_ninja.cmake
+++ .gitlab/ci/configure_windows_arm64_vs2022_ninja.cmake
@@ -1,9 +1,10 @@
 # Qt host tools are not yet available natively on windows-arm64.
 set(CMake_TEST_GUI "OFF" CACHE BOOL "")
-set(CMake_TEST_TLS_VERIFY_URL "https://gitlab.kitware.com" CACHE STRING "")
-set(CMake_TEST_TLS_VERSION "1.2" CACHE STRING "")
 set(BUILD_QtDialog "OFF" CACHE BOOL "")
 set(CMAKE_PREFIX_PATH "" CACHE STRING "")
+
+set(CMake_TEST_TLS_VERIFY_URL "https://gitlab.kitware.com" CACHE STRING "")
+set(CMake_TEST_TLS_VERSION "1.2" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_msvc_cxx_modules_common.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_vs_common_ninja.cmake")
