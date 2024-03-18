8959ad9db1e88527ad3d1b42347785a4b6fa6c65
Brad King <brad.king@kitware.com>
2024-03-18 10:51:22 -0400

    Tests/RunCMake/CMakePackage: Fix Apple architecture selection argument order
    Call `generate_apple_architecture_selection_file` with matching order in
    `SINGLE_ARCHITECTURES` and `SINGLE_ARCHITECTURE_INCLUDE_FILES`.

--- Tests/RunCMake/CMakePackage/apple-export-ios-simulator-common.cmake
+++ Tests/RunCMake/CMakePackage/apple-export-ios-simulator-common.cmake
@@ -1,11 +1,14 @@
 include(apple-export-common.cmake)
 
 if(IOS_SIMULATOR_SELECT_ARCHS)
+  set(IOS_SIMULATOR_SELECT_FILES "${IOS_SIMULATOR_SELECT_ARCHS}")
+  list(TRANSFORM IOS_SIMULATOR_SELECT_FILES PREPEND "lib/ios-simulator-")
+  list(TRANSFORM IOS_SIMULATOR_SELECT_FILES APPEND "/cmake/mylib/mylib-targets.cmake")
   generate_apple_architecture_selection_file(mylib-select-arch-install.cmake
     INSTALL_DESTINATION lib/ios-simulator/cmake/mylib
     INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}
     SINGLE_ARCHITECTURES "${IOS_SIMULATOR_SELECT_ARCHS}"
-    SINGLE_ARCHITECTURE_INCLUDE_FILES "lib/ios-simulator-arm64/cmake/mylib/mylib-targets.cmake;lib/ios-simulator-x86_64/cmake/mylib/mylib-targets.cmake"
+    SINGLE_ARCHITECTURE_INCLUDE_FILES "${IOS_SIMULATOR_SELECT_FILES}"
     UNIVERSAL_ARCHITECTURES "${IOS_SIMULATOR_SELECT_ARCHS}"
     UNIVERSAL_INCLUDE_FILE "lib/ios-simulator/cmake/mylib/mylib-targets.cmake"
     )
