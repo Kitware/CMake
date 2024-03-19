eb403bb7b42ddb59c8621997400c37787d753708
Brad King <brad.king@kitware.com>
2024-03-19 19:00:02 -0400

    Tests: Pass path to WiX 3 into RunCMake.CPack_WIX test
    Avoid requiring its location to be in the PATH environment variable.

--- .gitlab/ci/configure_windows_arm64_vs2022_ninja.cmake
+++ .gitlab/ci/configure_windows_arm64_vs2022_ninja.cmake
@@ -3,9 +3,9 @@ set(CMake_TEST_GUI "OFF" CACHE BOOL "")
 set(BUILD_QtDialog "OFF" CACHE BOOL "")
 set(CMAKE_PREFIX_PATH "" CACHE STRING "")
 
-set(CMake_TEST_CPACK_WIX "ON" CACHE BOOL "")
 set(CMake_TEST_TLS_VERIFY_URL "https://gitlab.kitware.com" CACHE STRING "")
 set(CMake_TEST_TLS_VERSION "1.2" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_msvc_cxx_modules_common.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_wix_common.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_vs_common_ninja.cmake")
--- Tests/RunCMake/CPack_WIX/RunCMakeTest.cmake
+++ Tests/RunCMake/CPack_WIX/RunCMakeTest.cmake
@@ -2,4 +2,6 @@ include(RunCPack)
 
 set(RunCPack_GENERATORS WIX)
 
+set(ENV{PATH} "${CMake_TEST_CPACK_WIX3};$ENV{PATH}")
+
 run_cpack(AppWiX BUILD GLOB *.msi VERIFY powershell -ExecutionPolicy Bypass -File ${CMAKE_CURRENT_LIST_DIR}/print-msi.ps1)
