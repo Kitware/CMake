e7f5ad8df2f8dda35fea51b7c01f068497c16940
Brad King <brad.king@kitware.com>
2024-03-19 19:07:40 -0400

    Tests: Prepare RunCMake.CPack_WIX to cover multiple WiX versions

--- Tests/RunCMake/CPack_WIX/RunCMakeTest.cmake
+++ Tests/RunCMake/CPack_WIX/RunCMakeTest.cmake
@@ -1,7 +1,16 @@
 include(RunCPack)
 
+set(env_PATH "$ENV{PATH}")
+
 set(RunCPack_GENERATORS WIX)
+set(RunCPack_GLOB *.msi)
+set(RunCPack_VERIFY powershell -ExecutionPolicy Bypass -File ${CMAKE_CURRENT_LIST_DIR}/print-msi.ps1)
 
-set(ENV{PATH} "${CMake_TEST_CPACK_WIX3};$ENV{PATH}")
+function(run_cpack_wix v)
+  run_cpack(${v}-AppWiX SAMPLE AppWiX BUILD)
+endfunction()
 
-run_cpack(AppWiX BUILD GLOB *.msi VERIFY powershell -ExecutionPolicy Bypass -File ${CMAKE_CURRENT_LIST_DIR}/print-msi.ps1)
+if(CMake_TEST_CPACK_WIX3)
+  set(ENV{PATH} "${CMake_TEST_CPACK_WIX3};${env_PATH}")
+  run_cpack_wix(3)
+endif()
