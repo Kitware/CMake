713994426427154100b0f457838dfc671b1d832f
Brad King <brad.king@kitware.com>
2024-03-29 12:48:11 -0400

    ctest: Fall back to CMake environment variable for TLS server verification
    Use `ENV{CMAKE_TLS_VERIFY}` if `CTEST_TLS_VERIFY` or `CMAKE_TLS_VERIFY`
    is not set.
    
    Issue: #23608

--- Help/variable/CTEST_TLS_VERIFY.rst
+++ Help/variable/CTEST_TLS_VERIFY.rst
@@ -9,5 +9,5 @@ before including the :module:`CTest` module.  The value is a boolean
 indicating whether to  verify the server certificate when submitting
 to a dashboard via ``https://`` URLs.
 
-If ``CTEST_TLS_VERIFY`` is not set, :variable:`CMAKE_TLS_VERIFY` is
-used instead.
+If ``CTEST_TLS_VERIFY`` is not set, the :variable:`CMAKE_TLS_VERIFY` variable
+or :envvar:`CMAKE_TLS_VERIFY` environment variable is used instead.
--- Tests/RunCMake/CTestCommandLine/RunCMakeTest.cmake
+++ Tests/RunCMake/CTestCommandLine/RunCMakeTest.cmake
@@ -491,6 +491,7 @@ run_ctest(check-configuration-type)
 function(run_FailDrop case)
   set(RunCMake_TEST_BINARY_DIR ${RunCMake_BINARY_DIR}/FailDrop-${case}-build)
   run_cmake_with_options(FailDrop-${case} ${ARGN})
+  unset(ENV{CMAKE_TLS_VERIFY}) # Test that env variable is saved in ctest config file.
   unset(ENV{CMAKE_TLS_VERSION}) # Test that env variable is saved in ctest config file.
   set(RunCMake_TEST_NO_CLEAN 1)
   run_cmake_command(FailDrop-${case}-ctest
@@ -504,8 +505,14 @@ run_FailDrop(TLSVersion-1.1-env)
 unset(ENV{CMAKE_TLS_VERSION})
 run_FailDrop(TLSVerify-ON -DCTEST_TLS_VERIFY=ON)
 run_FailDrop(TLSVerify-ON-cmake -DCMAKE_TLS_VERIFY=ON) # Test fallback to CMake variable.
+set(ENV{CMAKE_TLS_VERIFY} 1) # Test fallback to env variable.
+run_FailDrop(TLSVerify-ON-env)
+unset(ENV{CMAKE_TLS_VERIFY})
 run_FailDrop(TLSVerify-OFF -DCTEST_TLS_VERIFY=OFF)
 run_FailDrop(TLSVerify-OFF-cmake -DCMAKE_TLS_VERIFY=OFF) # Test fallback to CMake variable.
+set(ENV{CMAKE_TLS_VERIFY} 0) # Test fallback to env variable.
+run_FailDrop(TLSVerify-OFF-env)
+unset(ENV{CMAKE_TLS_VERIFY})
 
 run_cmake_command(EmptyDirCoverage-ctest
   ${CMAKE_CTEST_COMMAND} -C Debug -M Experimental -T Coverage
