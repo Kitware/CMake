fa57e523605a2db4b031ebec7bcd19050767cfd2
Brad King <brad.king@kitware.com>
2024-04-05 15:12:17 -0400

    ci: add cuda12.2 base image

---
+++ .gitlab/ci/docker/cuda12.2/install_deps.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+apt-get update
+
+# Install dependency without interaction.
+env DEBIAN_FRONTEND=noninteractive \
+    TZ=America/New_York \
+  apt-get install -y \
+    tzdata
+
+# Install development tools.
+apt-get install -y \
+    g++ \
+    clang-18 \
+    curl \
+    git
+
+apt-get clean
