b84b0fdd89d9eef543503f8ca6d09a5bb4d162c6
Brad King <brad.king@kitware.com>
2024-04-23 14:19:38 -0400

    ci: Enable FindOpenMP tests in LLVM/Clang jobs on Fedora

--- .gitlab/ci/configure_fedora40_common_clang.cmake
+++ .gitlab/ci/configure_fedora40_common_clang.cmake
@@ -2,4 +2,9 @@ set(CMAKE_Fortran_COMPILER "/usr/bin/flang-new" CACHE FILEPATH "")
 set(CMAKE_Fortran_COMPILER_ID "LLVMFlang" CACHE STRING "")
 set(CMAKE_Fortran_COMPILER_SUPPORTS_F90 "1" CACHE BOOL "")
 
+set(CMake_TEST_FindOpenMP_C "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenMP_CXX "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenMP_Fortran "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenMP "ON" CACHE BOOL "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_external_test.cmake")
