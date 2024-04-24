b05f94fa10d4bf6b80afaff8c4bb0857b67c717e
Brad King <brad.king@kitware.com>
2024-04-24 12:02:19 -0400

    ci: Enable FindOpenACC tests in LLVM/Clang jobs on Fedora
    Test commit 30962830dc (FindOpenACC: Add support for the Clang Compiler,
    2024-02-01, v3.29.0-rc1~37^2) now that we have Clang 18.

--- .gitlab/ci/configure_fedora40_common_clang.cmake
+++ .gitlab/ci/configure_fedora40_common_clang.cmake
@@ -2,6 +2,10 @@ set(CMAKE_Fortran_COMPILER "/usr/bin/flang-new" CACHE FILEPATH "")
 set(CMAKE_Fortran_COMPILER_ID "LLVMFlang" CACHE STRING "")
 set(CMAKE_Fortran_COMPILER_SUPPORTS_F90 "1" CACHE BOOL "")
 
+set(CMake_TEST_FindOpenACC "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenACC_C "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenACC_CXX "ON" CACHE BOOL "")
+set(CMake_TEST_FindOpenACC_Fortran "OFF" CACHE BOOL "") # flang-new fails producing LLVM IR
 set(CMake_TEST_FindOpenMP_C "ON" CACHE BOOL "")
 set(CMake_TEST_FindOpenMP_CXX "ON" CACHE BOOL "")
 set(CMake_TEST_FindOpenMP_Fortran "ON" CACHE BOOL "")
