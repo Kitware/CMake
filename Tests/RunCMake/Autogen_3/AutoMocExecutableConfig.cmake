6ee5c431d5a7217b12268cc6ccc1ac62625abf4f
Orkun Tokdemir <ilhanorkuntokdemir@gmail.com>
2024-06-21 15:41:44 +0200

    Autogen: Separate RunCMake.AutogenQtX tests
    The RunCMake.AutogenQtX tests occasionally time out so separate
    `RunCMake.AutogenQtX` tests.
    
    Example: https://open.cdash.org/tests/1583599782

--- Tests/RunCMake/Autogen_3/AutoMocExecutableConfig.cmake
+++ Tests/RunCMake/Autogen_3/AutoMocExecutableConfig.cmake
@@ -7,9 +7,9 @@ endif()
 get_target_property(moc_location Qt${with_qt_version}::moc IMPORTED_LOCATION)
 set_target_properties(dummy PROPERTIES AUTOMOC_MOC_OPTIONS "EXE_PATH=${moc_location}")
 
-add_executable(mymoc $<$<CONFIG:Debug>:exe_debug.cpp>
-                     $<$<CONFIG:Release>:exe_release.cpp>
-                     $<$<CONFIG:RelWithDebInfo>:exe_relwithdebinfo.cpp>
+add_executable(mymoc $<$<CONFIG:Debug>:../Autogen_common/exe_debug.cpp>
+                     $<$<CONFIG:Release>:../Autogen_common/exe_release.cpp>
+                     $<$<CONFIG:RelWithDebInfo>:../Autogen_common/exe_relwithdebinfo.cpp>
 )
 
 set_target_properties(dummy PROPERTIES AUTOMOC_EXECUTABLE $<TARGET_FILE:mymoc>)
--- Tests/RunCMake/Autogen_4/AutoUicExecutableConfig.cmake
+++ Tests/RunCMake/Autogen_4/AutoUicExecutableConfig.cmake
@@ -7,9 +7,9 @@ endif()
 get_target_property(uic_location Qt${with_qt_version}::uic IMPORTED_LOCATION)
 set_target_properties(dummy PROPERTIES AUTOUIC_OPTIONS "EXE_PATH=${uic_location}")
 
-add_executable(myuic $<$<CONFIG:Debug>:exe_debug.cpp>
-                     $<$<CONFIG:Release>:exe_release.cpp>
-                     $<$<CONFIG:RelWithDebInfo>:exe_relwithdebinfo.cpp>
+add_executable(myuic $<$<CONFIG:Debug>:../Autogen_common/exe_debug.cpp>
+                     $<$<CONFIG:Release>:../Autogen_common/exe_release.cpp>
+                     $<$<CONFIG:RelWithDebInfo>:../Autogen_common/exe_relwithdebinfo.cpp>
 )
 
 set_target_properties(dummy PROPERTIES AUTOUIC_EXECUTABLE $<TARGET_FILE:myuic>)
