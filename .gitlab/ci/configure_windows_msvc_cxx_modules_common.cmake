23cbeb5035910b65b758783d8750ecf99779ec5d
Ben Boeckel <ben.boeckel@kitware.com>
2024-08-08 10:47:53 -0400

    ci: enable `build_database` CXXModules tests

--- .gitlab/ci/configure_windows_msvc_cxx_modules_common.cmake
+++ .gitlab/ci/configure_windows_msvc_cxx_modules_common.cmake
@@ -1,1 +1,1 @@
-set(CMake_TEST_MODULE_COMPILATION "named,compile_commands,collation,partitions,internal_partitions,shared,export_bmi,install_bmi,bmionly,import_std23" CACHE STRING "")
+set(CMake_TEST_MODULE_COMPILATION "named,compile_commands,collation,partitions,internal_partitions,shared,export_bmi,install_bmi,bmionly,import_std23,build_database" CACHE STRING "")
