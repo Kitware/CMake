24fbfa915cab35b47425d97cf299d355fd6cf547
Brad King <brad.king@kitware.com>
2024-09-12 09:18:27 -0400

    Tests/RunCMake/BuildDepends: Simplify LinkDepends case

--- Tests/RunCMake/BuildDepends/LinkDepends.cmake
+++ Tests/RunCMake/BuildDepends/LinkDepends.cmake
@@ -1,19 +1,17 @@
-
 enable_language(C)
 
 include("${CMAKE_BINARY_DIR}/../LinkDependsExternalLibrary-build/ExternalLibrary-debug.cmake")
 cmake_path(GET EXTERNAL_LIBRARY PARENT_PATH EXTERNAL_DIR)
 
-add_library(LinkDependsLib SHARED "${CMAKE_CURRENT_BINARY_DIR}/lib_depends.c")
+add_library(LinkDependsLib SHARED LinkDependsLib.c)
 target_link_directories(LinkDependsLib PRIVATE "${EXTERNAL_DIR}")
 target_link_libraries(LinkDependsLib PRIVATE External)
 
-add_executable(LinkDependsExe "${CMAKE_CURRENT_BINARY_DIR}/exe_depends.c")
+add_executable(LinkDependsExe LinkDependsExe.c)
 target_link_directories(LinkDependsExe PRIVATE "${EXTERNAL_DIR}")
 target_link_libraries(LinkDependsExe PRIVATE External)
 
-
-file(GENERATE  OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/check-$<LOWER_CASE:$<CONFIG>>.cmake"
+file(GENERATE OUTPUT "${CMAKE_BINARY_DIR}/check-$<LOWER_CASE:$<CONFIG>>.cmake"
   CONTENT "
 set(check_pairs
   \"$<TARGET_FILE:LinkDependsLib>|${EXTERNAL_LIBRARY}\"
