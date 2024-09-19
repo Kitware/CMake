01dde28daba7d31008ac3bc2ad8cf91d2ea44dbe
Brad King <brad.king@kitware.com>
2024-09-19 10:30:05 -0400

    ci: curl with Secure Transport on macOS does not support TLS 1.3
    curl 8.10 now rejects TLS 1.3 since the Secure Transport backend
    does not support it.

--- .gitlab/ci/configure_macos_arm64_curl.cmake
+++ .gitlab/ci/configure_macos_arm64_curl.cmake
@@ -3,10 +3,7 @@ set(CMAKE_USE_SYSTEM_CURL "OFF" CACHE BOOL "")
 
 set(CMake_TEST_TLS_VERIFY_URL "https://gitlab.kitware.com" CACHE STRING "")
 set(CMake_TEST_TLS_VERIFY_URL_BAD "https://badtls-expired.kitware.com" CACHE STRING "")
-
-# Test that our vendored curl accepts CURL_SSLVERSION_TLSv1_3.  It is passed
-# through to Secure Transport, but macOS does not actually enforce it.
-set(CMake_TEST_TLS_VERSION "1.3" CACHE STRING "")
+set(CMake_TEST_TLS_VERSION "1.2" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_macos_common.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/configure_common.cmake")
