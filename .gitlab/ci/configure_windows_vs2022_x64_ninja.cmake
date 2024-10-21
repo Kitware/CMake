19d81b357c30e951778dc6461c52a336d5b40b0f
Brad King <brad.king@kitware.com>
2024-10-21 16:53:54 -0400

    ci: Host our own URL to test expected TLS_VERSION failure
    Avoid unnecessary load on an external resource.

--- .gitlab/ci/configure_windows_vs2022_x64_ninja.cmake
+++ .gitlab/ci/configure_windows_vs2022_x64_ninja.cmake
@@ -12,7 +12,7 @@ endif()
 set(CMake_TEST_TLS_VERIFY_URL "https://gitlab.kitware.com" CACHE STRING "")
 set(CMake_TEST_TLS_VERIFY_URL_BAD "https://badtls-expired.kitware.com" CACHE STRING "")
 set(CMake_TEST_TLS_VERSION "1.2" CACHE STRING "")
-set(CMake_TEST_TLS_VERSION_URL_BAD "https://tls-v1-1.badssl.com:1011" CACHE STRING "")
+set(CMake_TEST_TLS_VERSION_URL_BAD "https://badtls-v1-1.kitware.com:8011" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_msvc_cxx_modules_common.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_wix_common.cmake")
