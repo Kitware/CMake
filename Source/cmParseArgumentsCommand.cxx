Kitware/CMake

commit ceeea4e511d658b249615e5c98231f070aaedeb9

cmake_parse_arguments: Set variable if empty string given after keyword
If a single-value keyword is followed by an empty string, the
command unsets the variable for that keyword instead of setting
it to the empty string. This is inconsistent and unexpected. Add
policy CMP0174 which ensures the variable for a single-value
keyword is always set when any value is given, not just for a
non-empty value.

The new CMP0174 policy only affects the PARSE_ARGV form of
cmake_parse_arguments. The older form silently drops all empty
string arguments before processing the argument list.

Fixes: #25972

Source/cmParseArgumentsCommand.cxx
@@ -6,6 +6,7 @@
 #include <set>
 #include <utility>
 
+#include <cm/optional>
 #include <cm/string_view>
 
 #include "cmArgumentParser.h"
@@ -14,6 +15,7 @@
 #include "cmList.h"
 #include "cmMakefile.h"
 #include "cmMessageType.h"
+#include "cmPolicies.h"
 #include "cmRange.h"
 #include "cmStringAlgorithms.h"
 #include "cmSystemTools.h"
@@ -41,7 +43,7 @@ std::string JoinList(std::vector<std::string> const& arg, bool escape)
 }
 
 using options_map = std::map<std::string, bool>;
-using single_map = std::map<std::string, std::string>;
+using single_map = std::map<std::string, cm::optional<std::string>>;
 using multi_map =
   std::map<std::string, ArgumentParser::NonEmpty<std::vector<std::string>>>;
 using options_set = std::set<cm::string_view>;
@@ -82,12 +84,32 @@ static void PassParsedArguments(
                            iter.second ? "TRUE" : "FALSE");
   }
 
+  const cmPolicies::PolicyStatus cmp0174 =
+    makefile.GetPolicyStatus(cmPolicies::CMP0174);
   for (auto const& iter : singleValArgs) {
-    if (!iter.second.empty()) {
-      makefile.AddDefinition(cmStrCat(prefix, iter.first), iter.second);
-    } else {
-      makefile.RemoveDefinition(prefix + iter.first);
+    if (iter.second.has_value()) {
+      // So far, we only know that the keyword was given, not whether a value
+      // was provided after the keyword
+      if (keywordsMissingValues.find(iter.first) ==
+          keywordsMissingValues.end()) {
+        // A (possibly empty) value was given
+        if (cmp0174 == cmPolicies::NEW || !iter.second->empty()) {
+          makefile.AddDefinition(cmStrCat(prefix, iter.first), *iter.second);
+          continue;
+        }
+        if (cmp0174 == cmPolicies::WARN) {
+          makefile.IssueMessage(
+            MessageType::AUTHOR_WARNING,
+            cmStrCat("An empty string was given as the value after the ",
+                     iter.first,
+                     " keyword. Policy CMP0174 is not set, so "
+                     "cmake_parse_arguments() will unset the ",
+                     prefix, iter.first,
+                     " variable rather than setting it to an empty string."));
+        }
+      }
     }
+    makefile.RemoveDefinition(prefix + iter.first);
   }
 
   for (auto const& iter : multiValArgs) {

Tests/RunCMake/cmake_parse_arguments/CornerCasesArgvN.cmake
@@ -51,3 +51,48 @@ function(test4)
   TEST(upref_UNPARSED_ARGUMENTS foo "bar;none")
 endfunction()
 test4(MULTI foo bar\\ none)
+
+# Single-value keyword with empty string as value
+message(NOTICE "Testing CMP0174 = NEW")
+block(SCOPE_FOR POLICIES)
+  cmake_policy(SET CMP0174 NEW)
+  function(test_cmp0174_new_missing)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
+    TEST(arg_KEYWORDS_MISSING_VALUES "P2")
+    TEST(arg_P1 "")
+    TEST(arg_P2 "UNDEFINED")
+  endfunction()
+  test_cmp0174_new_missing(P1 "" P2)
+
+  function(test_cmp0174_new_no_missing)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
+    TEST(arg_KEYWORDS_MISSING_VALUES "UNDEFINED")
+    TEST(arg_P1 "")
+    TEST(arg_P2 "UNDEFINED")
+  endfunction()
+  test_cmp0174_new_no_missing(P1 "")
+endblock()
+
+message(NOTICE "Testing CMP0174 = OLD")
+block(SCOPE_FOR POLICIES)
+  cmake_policy(SET CMP0174 OLD)
+  function(test_cmp0174_old)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
+    TEST(arg_KEYWORDS_MISSING_VALUES "P2")
+    TEST(arg_P1 "UNDEFINED")
+    TEST(arg_P2 "UNDEFINED")
+  endfunction()
+  test_cmp0174_old(P1 "" P2)
+endblock()
+
+message(NOTICE "Testing CMP0174 = WARN")
+block(SCOPE_FOR POLICIES)
+  cmake_policy(VERSION 3.30)  # WARN
+  function(test_cmp0174_warn)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
+    TEST(arg_KEYWORDS_MISSING_VALUES "P2")
+    TEST(arg_P1 "UNDEFINED")
+    TEST(arg_P2 "UNDEFINED")
+  endfunction()
+  test_cmp0174_warn(P1 "" P2)
+endblock()

Source/cmPolicies.h
@@ -533,7 +533,11 @@ class cmMakefile;
          "the CPack WIX Generator.",                                          \
          3, 31, 0, cmPolicies::WARN)                                          \
   SELECT(POLICY, CMP0173, "The CMakeFindFrameworks module is removed.", 3,    \
-         31, 0, cmPolicies::WARN)
+         31, 0, cmPolicies::WARN)                                             \
+  SELECT(POLICY, CMP0174,                                                     \
+         "cmake_parse_arguments(PARSE_ARGV) defines a variable for an empty " \
+         "string after a single-value keyword.",                              \
+         3, 31, 0, cmPolicies::WARN)
 
 #define CM_SELECT_ID(F, A1, A2, A3, A4, A5, A6) F(A1)
 #define CM_FOR_EACH_POLICY_ID(POLICY)                                         \

Tests/RunCMake/cmake_parse_arguments/test_utils.cmake
@@ -13,6 +13,8 @@ function(TEST variable)
       if(DEFINED ${variable})
         message(FATAL_ERROR "'${variable}' shall be undefined but has value '${${variable}}'")
       endif()
+    elseif(NOT DEFINED ${variable})
+        message(FATAL_ERROR "'${variable}' should be defined but is not")
     elseif("${expected}" STREQUAL "FALSE")
       if(NOT ${variable} STREQUAL "FALSE")
         message(FATAL_ERROR "'${variable}' shall be FALSE")
@@ -23,7 +25,7 @@ function(TEST variable)
       endif()
     else()
       if(NOT ${variable} STREQUAL "${expected}")
-        message(FATAL_ERROR "'${variable}' shall be '${expected}'")
+        message(FATAL_ERROR "'${variable}' shall be '${expected}' but has value '${${variable}}'")
       endif()
     endif()
   endif()

commit c8567acc326dc72a7f9f8371bd6f4836eeab7c28

cmake_parse_arguments: Restore capture of value after repeated keyword
When a single-value keyword is repeated, and the first instance is
missing a value, it prevents the value from the second instance from
being stored in a variable. This was a regression introduced by
commit ceeea4e511 (cmake_parse_arguments: Set variable if empty string
given after keyword, 2024-08-18). That change also didn't create a
variable if the keyword was given but without a value. The purpose
of the change was to always define a variable if a keyword was given.
Lastly, that change didn't protect the CMP0174 logic to make it only
apply to the PARSE_ARGV form.

The first two of the above problems are fixed here by tracking the
keywords given instead of checking which keywords were missing
values. The third problem is also fixed here, being tightly coupled
to the same logic as the first two problems.

Fixes: #26397

Source/cmParseArgumentsCommand.cxx
@@ -6,7 +6,6 @@
 #include <set>
 #include <utility>
 
-#include <cm/optional>
 #include <cm/string_view>
 
 #include "cmArgumentParser.h"
@@ -43,7 +42,7 @@ std::string JoinList(std::vector<std::string> const& arg, bool escape)
 }
 
 using options_map = std::map<std::string, bool>;
-using single_map = std::map<std::string, cm::optional<std::string>>;
+using single_map = std::map<std::string, std::string>;
 using multi_map =
   std::map<std::string, ArgumentParser::NonEmpty<std::vector<std::string>>>;
 using options_set = std::set<cm::string_view>;
@@ -76,7 +75,7 @@ struct UserArgumentParser : public cmArgumentParser<void>
 static void PassParsedArguments(
   const std::string& prefix, cmMakefile& makefile, const options_map& options,
   const single_map& singleValArgs, const multi_map& multiValArgs,
-  const std::vector<std::string>& unparsed,
+  const std::vector<std::string>& unparsed, const options_set& keywordsSeen,
   const options_set& keywordsMissingValues, bool parseFromArgV)
 {
   for (auto const& iter : options) {
@@ -87,29 +86,26 @@ static void PassParsedArguments(
   const cmPolicies::PolicyStatus cmp0174 =
     makefile.GetPolicyStatus(cmPolicies::CMP0174);
   for (auto const& iter : singleValArgs) {
-    if (iter.second.has_value()) {
-      // So far, we only know that the keyword was given, not whether a value
-      // was provided after the keyword
-      if (keywordsMissingValues.find(iter.first) ==
-          keywordsMissingValues.end()) {
-        // A (possibly empty) value was given
-        if (cmp0174 == cmPolicies::NEW || !iter.second->empty()) {
-          makefile.AddDefinition(cmStrCat(prefix, iter.first), *iter.second);
-          continue;
-        }
-        if (cmp0174 == cmPolicies::WARN) {
-          makefile.IssueMessage(
-            MessageType::AUTHOR_WARNING,
-            cmStrCat("An empty string was given as the value after the ",
-                     iter.first,
-                     " keyword. Policy CMP0174 is not set, so "
-                     "cmake_parse_arguments() will unset the ",
-                     prefix, iter.first,
-                     " variable rather than setting it to an empty string."));
-        }
+    if (keywordsSeen.find(iter.first) == keywordsSeen.end()) {
+      makefile.RemoveDefinition(cmStrCat(prefix, iter.first));
+    } else if ((parseFromArgV && cmp0174 == cmPolicies::NEW) ||
+               !iter.second.empty()) {
+      makefile.AddDefinition(cmStrCat(prefix, iter.first), iter.second);
+    } else {
+      // The OLD policy behavior doesn't define a variable for an empty or
+      // missing value, and we can't differentiate between those two cases.
+      if (parseFromArgV && (cmp0174 == cmPolicies::WARN)) {
+        makefile.IssueMessage(
+          MessageType::AUTHOR_WARNING,
+          cmStrCat("The ", iter.first,
+                   " keyword was followed by an empty string or no value at "
+                   "all. Policy CMP0174 is not set, so "
+                   "cmake_parse_arguments() will unset the ",
+                   prefix, iter.first,
+                   " variable rather than setting it to an empty string."));
       }
+      makefile.RemoveDefinition(cmStrCat(prefix, iter.first));
     }
-    makefile.RemoveDefinition(prefix + iter.first);
   }
 
   for (auto const& iter : multiValArgs) {
@@ -237,6 +233,14 @@ bool cmParseArgumentsCommand(std::vector<std::string> const& args,
     }
   }
 
+  std::vector<cm::string_view> keywordsSeen;
+  parser.BindParsedKeywords(keywordsSeen);
+
+  // For single-value keywords, only the last instance matters, since it
+  // determines the value stored. But if a keyword is repeated, it will be
+  // added to this vector if _any_ instance is missing a value. If one of the
+  // earlier instances is missing a value but the last one isn't, its presence
+  // in this vector will be misleading.
   std::vector<cm::string_view> keywordsMissingValues;
   parser.BindKeywordsMissingValue(keywordsMissingValues);
 
@@ -244,7 +248,7 @@ bool cmParseArgumentsCommand(std::vector<std::string> const& args,
 
   PassParsedArguments(
     prefix, status.GetMakefile(), options, singleValArgs, multiValArgs,
-    unparsed,
+    unparsed, options_set(keywordsSeen.begin(), keywordsSeen.end()),
     options_set(keywordsMissingValues.begin(), keywordsMissingValues.end()),
     parseFromArgV);
 

Tests/RunCMake/cmake_parse_arguments/CornerCasesArgvN.cmake
@@ -60,7 +60,7 @@ block(SCOPE_FOR POLICIES)
     cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
     TEST(arg_KEYWORDS_MISSING_VALUES "P2")
     TEST(arg_P1 "")
-    TEST(arg_P2 "UNDEFINED")
+    TEST(arg_P2 "")
   endfunction()
   test_cmp0174_new_missing(P1 "" P2)
 
@@ -71,28 +71,56 @@ block(SCOPE_FOR POLICIES)
     TEST(arg_P2 "UNDEFINED")
   endfunction()
   test_cmp0174_new_no_missing(P1 "")
+
+  # For repeated keywords, the keyword will be reported as missing a value if
+  # any one of them is missing a value.
+  function(test_cmp0174_new_repeated_arg)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
+    TEST(arg_KEYWORDS_MISSING_VALUES "P1")
+    TEST(arg_P1 "abc")
+    TEST(arg_P2 "UNDEFINED")
+  endfunction()
+  test_cmp0174_new_repeated_arg(P1 P1 abc)
 endblock()
 
 message(NOTICE "Testing CMP0174 = OLD")
 block(SCOPE_FOR POLICIES)
   cmake_policy(SET CMP0174 OLD)
   function(test_cmp0174_old)
-    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2;P3" "")
     TEST(arg_KEYWORDS_MISSING_VALUES "P2")
     TEST(arg_P1 "UNDEFINED")
     TEST(arg_P2 "UNDEFINED")
+    TEST(arg_P3 "UNDEFINED")
   endfunction()
   test_cmp0174_old(P1 "" P2)
+
+  function(test_cmp0174_old_repeated_arg)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
+    TEST(arg_KEYWORDS_MISSING_VALUES "P1")
+    TEST(arg_P1 "abc")
+    TEST(arg_P2 "UNDEFINED")
+  endfunction()
+  test_cmp0174_old_repeated_arg(P1 P1 abc)
 endblock()
 
 message(NOTICE "Testing CMP0174 = WARN")
 block(SCOPE_FOR POLICIES)
   cmake_policy(VERSION 3.30)  # WARN
   function(test_cmp0174_warn)
-    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2;P3" "")
     TEST(arg_KEYWORDS_MISSING_VALUES "P2")
     TEST(arg_P1 "UNDEFINED")
     TEST(arg_P2 "UNDEFINED")
+    TEST(arg_P3 "UNDEFINED")
   endfunction()
   test_cmp0174_warn(P1 "" P2)
+
+  function(test_cmp0174_warn_repeated_arg)
+    cmake_parse_arguments(PARSE_ARGV 0 arg "" "P1;P2" "")
+    TEST(arg_KEYWORDS_MISSING_VALUES "P1")
+    TEST(arg_P1 "abc")
+    TEST(arg_P2 "UNDEFINED")
+  endfunction()
+  test_cmp0174_warn_repeated_arg(P1 P1 abc)
 endblock()