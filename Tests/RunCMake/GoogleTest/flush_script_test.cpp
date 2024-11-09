9a24c1e8022de70eab6046adbaa7e9427af75a9e
Craig Scott <craig.scott@crascit.com>
2024-11-09 13:34:07 +1100

    GoogleTest: Clear script content buffer on flush and flush less often
    There's no need to check if flushing is needed after every command
    is added to the variable holding the script content. It is enough to only
    check once per test name. This simplifies the flushing logic, removing
    the need for a separate flush_script() command. Previously, we were
    not clearing the flushed script contents in all cases, but this is now
    rigorously enforced at the one location where flushing is performed.
    
    Also simplify the flushing of the list of test names, since that too doesn't
    need a separate command. It is simpler and safer to handle that
    directly inline where the one call to flush_tests_buffer() was
    previously being made.
    
    Fixes: #26431

--- Tests/RunCMake/GoogleTest/flush_script_test.cpp
+++ Tests/RunCMake/GoogleTest/flush_script_test.cpp
@@ -10,10 +10,11 @@ int main(int argc, char** argv)
   if (argc > 1 && std::string(argv[1]) == "--gtest_list_tests") {
     std::cout << "flush_script_test.\n";
     const size_t flushThreshold = 50000;
-    const size_t testCaseNum = 4;
-    std::string testName(flushThreshold / (testCaseNum - 1), 'T');
-    for (size_t i = 0; i < testCaseNum; ++i)
-      std::cout << "  " << testName.c_str() << "\n";
+    const size_t flushAfter = 4;
+    const size_t testCaseNum = 3 * flushAfter;
+    std::string testName(flushThreshold / flushAfter, 'T');
+    for (size_t i = 1; i <= testCaseNum; ++i)
+      std::cout << "  t" << i << testName.c_str() << "\n";
   }
   return 0;
 }
