3fad553ea5a972c90932c62eac15e587594e0e62
Marc Chevrier <marc.chevrier@gmail.com>
2024-11-20 10:29:47 +0100

    FindPython: enhance Python3Embedded test
    Exclude too older compilers and avoid deprecated functions.
    
    Fixes: #26459

--- Tests/FindPython/display_time.c
+++ Tests/FindPython/display_time.c
@@ -9,27 +9,32 @@
 void display_time(void)
 {
 #if defined(PYTHON3)
-  wchar_t* program = Py_DecodeLocale("display_time", NULL);
-  if (program == NULL) {
-    fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
-    exit(1);
+  PyConfig config;
+  PyStatus status;
+
+  PyConfig_InitPythonConfig(&config);
+  status =
+    PyConfig_SetBytesString(&config, &config.program_name, "display_time");
+  if (PyStatus_Exception(status)) {
+    Py_ExitStatusException(status);
   }
+
   char* cmd = "from time import time,ctime\n"
               "print('Today is', ctime(time()))\n";
 #else
   char* program = "display_time";
   char* cmd = "from time import time,ctime\n"
               "print 'Today is', ctime(time())\n";
-#endif
 
   Py_SetProgramName(program); /* optional but recommended */
+#endif
   Py_Initialize();
   PyRun_SimpleString(cmd);
 #if defined(PYTHON3)
   if (Py_FinalizeEx() < 0) {
     exit(120);
   }
-  PyMem_RawFree(program);
+  PyConfig_Clear(&config);
 #else
   Py_Finalize();
 #endif
