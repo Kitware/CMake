482b7c358b94c1051b5d520d4410692d8c3b0baa
Brad King <brad.king@kitware.com>
2025-01-23 14:40:40 -0500

    ci: Add rbenv to Debian and Fedora base images

--- .gitlab/ci/docker/fedora41/install_deps.sh
+++ .gitlab/ci/docker/fedora41/install_deps.sh
@@ -17,6 +17,10 @@ done
 # Remove some other packages tests
 find /usr/lib64/python3.13/site-packages/breezy -type d -a -name tests -exec rm -rf {} +
 
+# Ruby rbenv
+rbenv install 3.1.2
+rbenv global 3.1.2
+
 # Perforce
 curl -L https://www.perforce.com/downloads/perforce/r21.2/bin.linux26x86_64/helix-core-server.tgz -o - \
   | tar -C /usr/local/bin -xvzf - -- p4 p4d
