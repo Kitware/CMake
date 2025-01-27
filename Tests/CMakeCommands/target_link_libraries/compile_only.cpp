c87004fdc37477c255d9271b3d0c52575018af37
Robert Maynard <rmaynard@nvidia.com>
2025-01-27 17:20:11 -0500

    COMPILE_ONLY: Genex now stops object libraries from linking
    Previously we didn't properly exclude $<COMPILE_ONLY:OBJECT lib>
    from the right hand side of  `target_link_libraries`.
    
    Fix the update that commit 73337cb383 (LINK_LIBRARIES: Evaluate
    separately for linking and usage requirements, 2022-03-22,
    v3.24.0-rc1~404^2~2) made to `AddObjectEntries`.
    
    Fixes: #26642

--- Tests/CMakeCommands/target_link_libraries/compile_only.cpp
+++ Tests/CMakeCommands/target_link_libraries/compile_only.cpp
@@ -3,6 +3,13 @@
 #  error "Missing propagated define"
 #endif
 
+#ifdef HAVE_FUNCTION
+int non_duplicate_function()
+{
+  return 42;
+}
+#endif
+
 // Solaris needs non-empty content so ensure
 // we have at least one symbol
 int Solaris_requires_a_symbol_here = 0;
