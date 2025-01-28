fda055c2605420a2a23d1a2022a338a5d2de5861
Daniel Pfeifer <daniel@pfeifer-mail.de>
2025-01-28 09:12:28 +0100

    cmCTestConfigureHandler: Move class into cmCTestConfigureCommand.cxx
    Place declaration and definitions into places where following
    refactoring will cause minimal deltas.

--- Source/CTest/cmCTestConfigureCommand.cxx
+++ Source/CTest/cmCTestConfigureCommand.cxx
@@ -2,8 +2,10 @@
    file Copyright.txt or https://cmake.org/licensing for details.  */
 #include "cmCTestConfigureCommand.h"
 
+#include <chrono>
 #include <cstring>
 #include <sstream>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -12,16 +14,29 @@
 
 #include "cmArgumentParser.h"
 #include "cmCTest.h"
-#include "cmCTestConfigureHandler.h"
 #include "cmCTestGenericHandler.h"
+#include "cmDuration.h"
 #include "cmExecutionStatus.h"
+#include "cmGeneratedFileStream.h"
 #include "cmGlobalGenerator.h"
 #include "cmList.h"
 #include "cmMakefile.h"
 #include "cmStringAlgorithms.h"
 #include "cmSystemTools.h"
 #include "cmValue.h"
+#include "cmXMLWriter.h"
 #include "cmake.h"
+
+class cmCTestConfigureHandler : public cmCTestGenericHandler
+{
+public:
+  cmCTestConfigureHandler(cmCTest* ctest)
+    : cmCTestGenericHandler(ctest)
+  {
+  }
+
+  int ProcessHandler() override;
+};
 
 std::unique_ptr<cmCTestGenericHandler>
 cmCTestConfigureCommand::InitializeHandler(HandlerArguments& arguments,
@@ -155,6 +170,88 @@ cmCTestConfigureCommand::InitializeHandler(HandlerArguments& arguments,
   auto handler = cm::make_unique<cmCTestConfigureHandler>(this->CTest);
   handler->SetQuiet(args.Quiet);
   return std::unique_ptr<cmCTestGenericHandler>(std::move(handler));
+}
+
+int cmCTestConfigureHandler::ProcessHandler()
+{
+  cmCTestOptionalLog(this->CTest, HANDLER_OUTPUT,
+                     "Configure project" << std::endl, this->Quiet);
+  std::string cCommand =
+    this->CTest->GetCTestConfiguration("ConfigureCommand");
+  if (cCommand.empty()) {
+    cmCTestLog(this->CTest, ERROR_MESSAGE,
+               "Cannot find ConfigureCommand key in the DartConfiguration.tcl"
+                 << std::endl);
+    return -1;
+  }
+
+  std::string buildDirectory =
+    this->CTest->GetCTestConfiguration("BuildDirectory");
+  if (buildDirectory.empty()) {
+    cmCTestLog(this->CTest, ERROR_MESSAGE,
+               "Cannot find BuildDirectory  key in the DartConfiguration.tcl"
+                 << std::endl);
+    return -1;
+  }
+
+  auto elapsed_time_start = std::chrono::steady_clock::now();
+  std::string output;
+  int retVal = 0;
+  bool res = false;
+  if (!this->CTest->GetShowOnly()) {
+    cmGeneratedFileStream os;
+    if (!this->StartResultingXML(cmCTest::PartConfigure, "Configure", os)) {
+      cmCTestLog(this->CTest, ERROR_MESSAGE,
+                 "Cannot open configure file" << std::endl);
+      return 1;
+    }
+    std::string start_time = this->CTest->CurrentTime();
+    auto start_time_time = std::chrono::system_clock::now();
+
+    cmGeneratedFileStream ofs;
+    this->StartLogFile("Configure", ofs);
+    cmCTestOptionalLog(this->CTest, HANDLER_VERBOSE_OUTPUT,
+                       "Configure with command: " << cCommand << std::endl,
+                       this->Quiet);
+    res = this->CTest->RunMakeCommand(cCommand, output, &retVal,
+                                      buildDirectory.c_str(),
+                                      cmDuration::zero(), ofs);
+
+    if (ofs) {
+      ofs.close();
+    }
+
+    if (os) {
+      cmXMLWriter xml(os);
+      this->CTest->StartXML(xml, this->CMake, this->AppendXML);
+      this->CTest->GenerateSubprojectsOutput(xml);
+      xml.StartElement("Configure");
+      xml.Element("StartDateTime", start_time);
+      xml.Element("StartConfigureTime", start_time_time);
+      xml.Element("ConfigureCommand", cCommand);
+      cmCTestOptionalLog(this->CTest, DEBUG, "End" << std::endl, this->Quiet);
+      xml.Element("Log", output);
+      xml.Element("ConfigureStatus", retVal);
+      xml.Element("EndDateTime", this->CTest->CurrentTime());
+      xml.Element("EndConfigureTime", std::chrono::system_clock::now());
+      xml.Element("ElapsedMinutes",
+                  std::chrono::duration_cast<std::chrono::minutes>(
+                    std::chrono::steady_clock::now() - elapsed_time_start)
+                    .count());
+      xml.EndElement(); // Configure
+      this->CTest->EndXML(xml);
+    }
+  } else {
+    cmCTestOptionalLog(this->CTest, DEBUG,
+                       "Configure with command: " << cCommand << std::endl,
+                       this->Quiet);
+  }
+  if (!res || retVal) {
+    cmCTestLog(this->CTest, ERROR_MESSAGE,
+               "Error(s) when configuring the project" << std::endl);
+    return -1;
+  }
+  return 0;
 }
 
 bool cmCTestConfigureCommand::InitialPass(std::vector<std::string> const& args,
