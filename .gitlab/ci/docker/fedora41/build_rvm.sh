264c90240352399b52a8279d6a97ff9977aaa8a1
Alex Turbov <i.zaufi@gmail.com>
2025-01-29 19:03:06 +0400

    ci(fedora41): split install prerequisites and build RVM into separate phases
    It improves parallelization and speeds up the build by reusing cached layers.

--- .gitlab/ci/docker/fedora41/build_rvm.sh
+++ .gitlab/ci/docker/fedora41/build_rvm.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-dnf install -y $(grep '^[^#]\+$' /root/rvm_packages.lst)
-
 gpg2 --keyserver hkps://keyserver.ubuntu.com \
      --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 \
                  7D2BAF1CF37B13E2069D6956105BD0E739499BDB
