fa08bd803d529f40b6267de5dd7760d5fccc17c9
Alex Turbov <i.zaufi@gmail.com>
2025-01-29 23:58:35 +0400

    ci(fedora41): refactor tests removal from Python packages

--- .gitlab/ci/docker/fedora41/install_deps.sh
+++ .gitlab/ci/docker/fedora41/install_deps.sh
@@ -4,10 +4,7 @@ set -e
 
 dnf install -y $(grep '^[^#]\+$' /root/deps_packages.lst)
 
-# Remove tests for numpy
+# Remove tests for Python packages
 for v in 3.13; do
-    find /usr/lib64/python${v}/site-packages/numpy -type d -a -name tests -exec rm -rf {} +
+    find /usr/lib64/python${v}/site-packages -type d -a -name tests -exec rm -rf {} +
 done
-
-# Remove some other packages tests
-find /usr/lib64/python3.13/site-packages/breezy -type d -a -name tests -exec rm -rf {} +
