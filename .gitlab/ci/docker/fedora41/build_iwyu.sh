59383f650977b73bf3764d173d19b20039f0454d
Alex Turbov <i.zaufi@gmail.com>
2025-01-29 18:52:54 +0400

    ci(fedora41): use cache for IWYU build

--- .gitlab/ci/docker/fedora41/build_iwyu.sh
+++ .gitlab/ci/docker/fedora41/build_iwyu.sh
@@ -2,16 +2,24 @@
 
 set -e
 
-cd /root
-git clone "https://github.com/include-what-you-use/include-what-you-use.git"
-cd include-what-you-use
+cd
+if [ -d include-what-you-use/.git ]; then
+    cd include-what-you-use
+    git pull
+else
+    git clone https://github.com/include-what-you-use/include-what-you-use.git
+    cd include-what-you-use
+fi
+
 readonly llvm_full_version="$( clang --version | head -n1 | cut -d' ' -f3 )"
 readonly llvm_version="$( echo "$llvm_full_version" | cut -d. -f-1 )"
 git checkout "clang_$llvm_version"
-mkdir build
+
+mkdir -p build
 cd build
 
 cmake -GNinja \
+    --fresh \
     -DCMAKE_BUILD_TYPE=Release \
     -DIWYU_RESOURCE_RELATIVE_TO=clang \
     -DIWYU_RESOURCE_DIR=../lib/clang/"$llvm_version" \
@@ -22,4 +30,4 @@ cmake --build . --parallel
 
 DESTDIR=~/iwyu-destdir cmake --install .
 
-tar -C /root/iwyu-destdir -cf /root/iwyu.tar .
+tar -C ~/iwyu-destdir -cf ~/iwyu.tar .
