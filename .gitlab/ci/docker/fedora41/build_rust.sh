0e5159656ae5de34d34b384cab4c8e37de171528
Alex Turbov <i.zaufi@gmail.com>
2025-01-29 19:13:18 +0400

    ci(fedora41): split install prerequisites and build for Rust packages

--- .gitlab/ci/docker/fedora41/build_rust.sh
+++ .gitlab/ci/docker/fedora41/build_rust.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-dnf install -y $(grep '^[^#]\+$' /root/rust_packages.lst)
-
 typos_version=1.29.4
 cargo install --root /usr/local --version "$typos_version" typos-cli
+
+strip /usr/local/bin/typos
 
 tar -C /usr/local -cf /root/rust.tar bin/typos
