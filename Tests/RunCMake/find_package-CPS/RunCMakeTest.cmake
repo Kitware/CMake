5ae259602700cac4e001987ccb72454d24753414
Matthew Woehlke <matthew.woehlke@kitware.com>
2025-02-06 14:42:12 -0500

    Tests: Simplify and improve RunCMake/find_package-CPS tests
    Refactor common logic out of individual RunCMake/find_package-CPS tests.
    Move enabling the experimental gate to the RunCMakeTest.cmake, add a
    separate test to verify the experimental warning, and suppress it
    everywhere else. This will greatly simplify writing non-failure tests.

--- Tests/RunCMake/find_package-CPS/RunCMakeTest.cmake
+++ Tests/RunCMake/find_package-CPS/RunCMakeTest.cmake
@@ -1,5 +1,13 @@
 include(RunCMake)
 
+run_cmake(ExperimentalWarning)
+
+# Enable experimental feature and suppress warnings
+set(RunCMake_TEST_OPTIONS
+  -Wno-dev
+  "-DCMAKE_EXPERIMENTAL_FIND_CPS_PACKAGES:STRING=e82e467b-f997-4464-8ace-b00808fff261"
+  )
+
 run_cmake(MissingTransitiveDependency)
 run_cmake(MissingComponent)
 run_cmake(MissingComponentDependency)
