5afbc739dc91ffeaca2ff2dfe47c873f583f5cb5
Brad King <brad.king@kitware.com>
2025-02-21 11:31:26 -0500

    ci: Specify Qt version separately for each Fedora build
    Currently all builds use Qt 5.  Prepare to switch some to Qt 6.

--- .gitlab/ci/configure_fedora41_makefiles_symlinked.cmake
+++ .gitlab/ci/configure_fedora41_makefiles_symlinked.cmake
@@ -1,2 +1,3 @@
+set(CMake_QT_MAJOR_VERSION "5" CACHE STRING "")
 include("${CMAKE_CURRENT_LIST_DIR}/configure_symlinked_common.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora41_common.cmake")
--- .gitlab/ci/configure_fedora41_tidy.cmake
+++ .gitlab/ci/configure_fedora41_tidy.cmake
@@ -2,5 +2,6 @@ set(CMake_RUN_CLANG_TIDY ON CACHE BOOL "")
 set(CMake_USE_CLANG_TIDY_MODULE ON CACHE BOOL "")
 set(CMake_CLANG_TIDY_MODULE "$ENV{CI_PROJECT_DIR}/Utilities/ClangTidyModule/build/libcmake-clang-tidy-module.so" CACHE FILEPATH "")
 set(CMake_CLANG_TIDY_EXPORT_FIXES_DIR "$ENV{CI_PROJECT_DIR}/.gitlab/clang-tidy-fixes" CACHE PATH "")
+set(CMake_QT_MAJOR_VERSION "5" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora41_common.cmake")
