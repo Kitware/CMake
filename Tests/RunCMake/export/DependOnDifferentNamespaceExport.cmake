26691689a3cad9adfa5f269de27cc2b8a511118f
Matthew Woehlke <matthew.woehlke@kitware.com>
2025-02-24 14:54:50 -0500

    export: Allow compatible build-tree exports
    Modify how cmExportBuildFileGenerator::FindExportInfo collects target
    export information to properly record the export sets which export a
    target. (The old behavior treated every invocation as a separate set,
    which is reasonable for export(TARGETS) but does not allow referencing a
    target exported multiple times via export(EXPORT). This reflects recent
    changes to allow this for installed exports.)

--- Tests/RunCMake/export/DependOnDifferentNamespaceExport.cmake
+++ Tests/RunCMake/export/DependOnDifferentNamespaceExport.cmake
@@ -1,7 +1,9 @@
 add_library(doubleexported INTERFACE)
 install(TARGETS doubleexported EXPORT exportset)
-export(EXPORT exportset FILE "${CMAKE_CURRENT_BINARY_DIR}/export1.cmake")
-export(EXPORT exportset FILE "${CMAKE_CURRENT_BINARY_DIR}/export2.cmake")
+export(EXPORT exportset
+  FILE "${CMAKE_CURRENT_BINARY_DIR}/export1.cmake")
+export(EXPORT exportset NAMESPACE test::
+  FILE "${CMAKE_CURRENT_BINARY_DIR}/export2.cmake")
 add_library(exported INTERFACE)
 target_link_libraries(exported INTERFACE doubleexported)
 export(TARGETS exported FILE "${CMAKE_CURRENT_BINARY_DIR}/exports.cmake")
