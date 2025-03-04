1a716aac0f5ecb0422859228ea3d05999b3c3c4d
gweberask <g.weber@askgroup.it>
2025-03-04 17:00:20 +0100

    ADSP: bugfix for assembler option
    change '-proc='  into '-proc '  because of error message from ADSP assembler. C Compiler and Linker is useing the same option...

--- Modules/Compiler/ADSP.cmake
+++ Modules/Compiler/ADSP.cmake
@@ -10,7 +10,7 @@ macro(__compiler_adsp lang)
 
   set(CMAKE_${lang}_LINK_MODE DRIVER)
 
-  set(_CMAKE_${lang}_ADSP_FLAGS "-proc=${CMAKE_ADSP_PROCESSOR}")
+  set(_CMAKE_${lang}_ADSP_FLAGS "-proc ${CMAKE_ADSP_PROCESSOR}")
 
   set(CMAKE_DEPFILE_FLAGS_${lang} "-MD -Mo <DEP_FILE>")
 
