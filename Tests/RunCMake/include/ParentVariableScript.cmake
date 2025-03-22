0b85f8f137716fd415e2ede7d1ac0167d4d5f3e2
Benjamin Buch <benni.buch@gmail.com>
2025-03-22 19:46:51 +0100

    Tests: Add cases for CMAKE_PARENT_LIST_FILE with include() and find_package()
    Issue: #25026
    Co-authored-by: Brad King <brad.king@kitware.com>

---
+++ Tests/RunCMake/include/ParentVariableScript.cmake
@@ -0,0 +1,6 @@
+if(DEFINED CMAKE_PARENT_LIST_FILE)
+  message(SEND_ERROR "`CMAKE_PARENT_LIST_FILE` is not expected to be set here")
+endif()
+message(STATUS "ParentVariableScript.cmake: '${CMAKE_PARENT_LIST_FILE}'")
+include("${CMAKE_CURRENT_LIST_DIR}/ParentVariableScript/include1.cmake")
+message(STATUS "ParentVariableScript.cmake: '${CMAKE_PARENT_LIST_FILE}'")
---
+++ Tests/RunCMake/include/ParentVariableScript/include1.cmake
@@ -0,0 +1,3 @@
+message(STATUS "ParentVariableScript/include1\.cmake: '${CMAKE_PARENT_LIST_FILE}'")
+include("${CMAKE_CURRENT_LIST_DIR}/include2.cmake")
+message(STATUS "ParentVariableScript/include1\.cmake: '${CMAKE_PARENT_LIST_FILE}'")
