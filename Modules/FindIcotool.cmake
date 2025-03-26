dd6b3a1d9ef62c9201df42db2ec553ce927c1040
Peter Kokot <peterkokot@gmail.com>
2025-03-27 00:58:49 +0100

    FindIcotool: Update documentation

--- Modules/FindIcotool.cmake
+++ Modules/FindIcotool.cmake
@@ -5,16 +5,43 @@
 FindIcotool
 -----------
 
-Find icotool
+Finds ``icotool``, command-line program for converting and creating Win32 icon
+and cursor files.
 
-This module looks for icotool. Convert and create Win32 icon and cursor files.
-This module defines the following values:
+Result Variables
+^^^^^^^^^^^^^^^^
 
-::
+This module defines the following variables:
 
-  ICOTOOL_EXECUTABLE: the full path to the icotool tool.
-  ICOTOOL_FOUND: True if icotool has been found.
-  ICOTOOL_VERSION_STRING: the version of icotool found.
+``Icotool_FOUND``
+  True if ``icotool`` has been found.  For backward compatibility, the
+  ``ICOTOOL_FOUND`` variable is also set to the same value.
+``ICOTOOL_VERSION_STRING``
+  The version of ``icotool`` found.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``ICOTOOL_EXECUTABLE``
+  The full path to the ``icotool`` tool.
+
+Examples
+^^^^^^^^
+
+Finding ``icotool`` and executing it in a process to create ``.ico`` icon from
+the source ``.png`` image located in the current source directory:
+
+.. code-block:: cmake
+
+  find_package(Icotool)
+  if(Icotool_FOUND)
+    execute_process(
+      COMMAND
+        ${ICOTOOL_EXECUTABLE} -c -o ${CMAKE_CURRENT_BINARY_DIR}/img.ico img.png
+    )
+  endif()
 #]=======================================================================]
 
 find_program(ICOTOOL_EXECUTABLE
