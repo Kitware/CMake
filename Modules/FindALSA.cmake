d5225ed7c787cd4c65eb9f0caeb26fb409d27828
Peter Kokot <peterkokot@gmail.com>
2025-04-01 00:57:00 +0200

    FindALSA: Update documentation
    - Added examples section.
    - Synced descriptions.

--- Modules/FindALSA.cmake
+++ Modules/FindALSA.cmake
@@ -5,17 +5,18 @@
 FindALSA
 --------
 
-Find Advanced Linux Sound Architecture (ALSA)
-
-Find the alsa libraries (``asound``)
+Finds the Advanced Linux Sound Architecture (ALSA) library (``asound``).
 
 Imported Targets
 ^^^^^^^^^^^^^^^^
 
-.. versionadded:: 3.12
+This module provides the following :ref:`Imported Targets`:
 
-This module defines :prop_tgt:`IMPORTED` target ``ALSA::ALSA``, if
-ALSA has been found.
+``ALSA::ALSA``
+  .. versionadded:: 3.12
+
+  Target encapsulating the ALSA library usage requirements.  This target is
+  available only if ALSA is found.
 
 Result Variables
 ^^^^^^^^^^^^^^^^
@@ -23,24 +24,34 @@ Result Variables
 This module defines the following variables:
 
 ``ALSA_FOUND``
-  True if ALSA_INCLUDE_DIR & ALSA_LIBRARY are found
+  Boolean indicating whether the ALSA library is found.
 
 ``ALSA_LIBRARIES``
-  List of libraries when using ALSA.
+  List of libraries needed for linking to use ALSA library.
 
 ``ALSA_INCLUDE_DIRS``
-  Where to find the ALSA headers.
+  Include directories containing headers needed to use ALSA library.
 
-Cache variables
+Cache Variables
 ^^^^^^^^^^^^^^^
 
 The following cache variables may also be set:
 
 ``ALSA_INCLUDE_DIR``
-  the ALSA include directory
+  The ALSA include directory.
 
 ``ALSA_LIBRARY``
-  the absolute path of the asound library
+  The absolute path of the asound library.
+
+Examples
+^^^^^^^^
+
+Finding the ALSA library and linking it to a project target:
+
+.. code-block:: cmake
+
+  find_package(ALSA)
+  target_link_libraries(project_target PRIVATE ALSA::ALSA)
 #]=======================================================================]
 
 cmake_policy(PUSH)
