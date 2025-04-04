9eeae17f32ee56cafc7882890b82b725c6d6a3f6
Peter Kokot <peterkokot@gmail.com>
2025-04-04 05:27:42 +0200

    FindQuickTime: Update documentation
    - Synced the module with other similar find modules.
    - Added examples section.
    - Added notice regarding QuickTime being deprecated and no longer
      supported by Apple.  The AVFoundation and AVKit frameworks are
      suggested instead, also according to the upstream documentation.

--- Modules/FindQuickTime.cmake
+++ Modules/FindQuickTime.cmake
@@ -5,18 +5,54 @@
 FindQuickTime
 -------------
 
-Locate QuickTime This module defines:
+Finds the QuickTime multimedia framework, which provides support for video,
+audio, and interactive media.
+
+.. note::
+
+  This module is for the QuickTime framework, which has been deprecated by Apple
+  and is no longer supported.  On Apple systems, use AVFoundation and AVKit
+  instead.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module defines the following variables:
+
+``QuickTime_FOUND``
+  Boolean indicating whether the QuickTime is found.  For backward
+  compatibility, the ``QUICKTIME_FOUND`` variable is also set to the same value.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
 
 ``QUICKTIME_LIBRARY``
+  The path to the QuickTime library.
 
-``QUICKTIME_FOUND``
-  if false, do not try to link to gdal
 ``QUICKTIME_INCLUDE_DIR``
-  where to find the headers
+  Directory containing QuickTime headers.
 
-``$QUICKTIME_DIR`` is an environment variable that would correspond to::
+Hints
+^^^^^
 
-  ./configure --prefix=$QUICKTIME_DIR
+This module accepts the following variables:
+
+``QUICKTIME_DIR``
+  Environment variable that can be set to help locate a QuickTime library
+  installed in a custom location.  It should point to the installation
+  destination that was used when configuring, building, and installing QuickTime
+  library: ``./configure --prefix=$QUICKTIME_DIR``.
+
+Examples
+^^^^^^^^
+
+Finding QuickTime library:
+
+.. code-block:: cmake
+
+  find_package(QuickTime)
 #]=======================================================================]
 
 find_path(QUICKTIME_INCLUDE_DIR QuickTime/QuickTime.h QuickTime.h
