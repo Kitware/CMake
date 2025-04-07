db0e2574cb2d3e4471f3cbf210870158dfd74f9e
Brad King <brad.king@kitware.com>
2025-04-07 19:27:55 -0400

    cmSystemTools: Restore FindProgram look-up of on-disk case on Windows
    KWSys's `FindProgram` no longer looks up the actual case on disk.  This
    behavior change was inherited from the change to `CollapseFullPath`.
    Extend commit 773b75e4ed (cmake: Explicitly look up on-disk case of
    input paths on Windows, 2024-10-23, v4.0.0-rc1~589^2) to cover this by
    wrapping `FindProgram` in a CMake-specific layer.
    
    Issue: #20214

--- Source/cmSystemTools.cxx
+++ Source/cmSystemTools.cxx
@@ -2992,6 +2992,16 @@ unsigned int cmSystemTools::RandomNumber()
   return static_cast<unsigned int>(gen());
 }
 
+std::string cmSystemTools::FindProgram(std::string const& name,
+                                       std::vector<std::string> const& path)
+{
+  std::string exe = cmsys::SystemTools::FindProgram(name, path);
+  if (!exe.empty()) {
+    exe = cmSystemTools::ToNormalizedPathOnDisk(std::move(exe));
+  }
+  return exe;
+}
+
 namespace {
 std::string InitLogicalWorkingDirectory()
 {
