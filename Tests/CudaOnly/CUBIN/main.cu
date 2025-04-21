3ff777de128ae36559eff2eaa95406b8b3885362
Robert Maynard <rmaynard@nvidia.com>
2025-04-21 15:21:17 -0400

    CUDA: Update CUDA tests to handle CUDA 13 deprecations

--- Tests/CudaOnly/CUBIN/main.cu
+++ Tests/CudaOnly/CUBIN/main.cu
@@ -36,7 +36,14 @@ int main()
   cuDeviceGet(&device, 0);
 
   CUcontext context;
+#if defined(__CUDACC_VER_MAJOR__) && __CUDACC_VER_MAJOR__ >= 13
+  CUctxCreateParams params;
+  params.execAffinityParams = nullptr;
+  params.numExecAffinityParams = 0;
+  cuCtxCreate(&context, &params, 0, device);
+#else
   cuCtxCreate(&context, 0, device);
+#endif
 
   CUmodule module;
   for (auto p : actual_paths) {
