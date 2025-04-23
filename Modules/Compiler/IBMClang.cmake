ccef69bde4f3e307f7f518101f2122858239e0d8
Brad King <brad.king@kitware.com>
2025-04-23 11:25:01 -0400

    AUTOMOC: Avoid compiler warnings while computing predefines
    LLVM/Clang 20, when invoked as `clang++ -E -c ...`, now warns:
    
        warning: argument unused during compilation: '-c'
    
    Drop the unnecessary `-c` flag.  Also add a `-w` flag to suppress
    warnings so their text is not parsed as predefines.

--- Modules/Compiler/IBMClang.cmake
+++ Modules/Compiler/IBMClang.cmake
@@ -70,7 +70,7 @@ macro(__compiler_ibmclang lang)
   )
 
   if("${lang}" STREQUAL "CXX")
-    list(APPEND CMAKE_${lang}_COMPILER_PREDEFINES_COMMAND "-dM" "-E" "-c" "${CMAKE_ROOT}/Modules/CMakeCXXCompilerABI.cpp")
+    list(APPEND CMAKE_${lang}_COMPILER_PREDEFINES_COMMAND "-w" "-dM" "-E" "${CMAKE_ROOT}/Modules/CMakeCXXCompilerABI.cpp")
   endif()
 
   set(CMAKE_PCH_EXTENSION .pch)
