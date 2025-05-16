607d9cf5613bd335c29a0ad9609eca4698283fa6
Tolga Mizrak <tolga.mizrak@schultesonline.de>
2025-05-16 08:01:45 +0200

    WINDOWS_EXPORT_ALL_SYMBOLS: Do not export C++ operators declared extern "C"
    The pattern `extern "C" { inline bool operator==(...) {} }` appears
    in at least one Windows SDK header, `winnt.h`.  Translation units that
    instantiate the inline symbol produce object files with a symbol named
    just `==`.  Avoid exporting such symbols because the linker will not
    recognize them.
    
    Fixes: #24999

--- Source/bindexplib.cxx
+++ Source/bindexplib.cxx
@@ -290,9 +290,11 @@ public:
               symbol.compare(0, 4, vectorPrefix)) {
             SectChar = this->SectionHeaders[pSymbolTable->SectionNumber - 1]
                          .Characteristics;
-            // skip symbols containing a dot or are from managed code
+            // Skip symbols containing a dot, are from managed code,
+            // or are C++ operators incorrectly declared extern "C".
             if (symbol.find('.') == std::string::npos &&
-                !SymbolIsFromManagedCode(symbol)) {
+                !SymbolIsFromManagedCode(symbol) &&
+                !SymbolIsOperatorExternC(symbol)) {
               // skip arm64ec thunk symbols
               if (this->SymbolArch != Arch::ARM64EC ||
                   (symbol.find("$ientry_thunk") == std::string::npos &&
@@ -335,6 +337,12 @@ private:
     return symbol == "__t2m" || symbol == "__m2mep" || symbol == "__mep" ||
       symbol.find("$$F") != std::string::npos ||
       symbol.find("$$J") != std::string::npos;
+  }
+
+  bool SymbolIsOperatorExternC(std::string const& symbol)
+  {
+    return symbol.find_first_not_of("=<>+-*/%,?|~!^&[]()") ==
+      std::string::npos;
   }
 
   std::set<std::string>& Symbols;
