9adf93d1125f27fb5423ce0ac9aad97d320d4168
Matthew Woehlke <matthew.woehlke@kitware.com>
2025-05-29 13:43:15 -0400

    Tests/InstallPackageInfo: Consolidate bad arguments tests
    Consolidate InstallPackageInfo's bad arguments tests into only two test
    cases that each test multiple invocations, rather than having a single
    test per invocation. This makes them match the similar ExportPackageInfo
    tests which were necessarily consolidated due to those testing many more
    variants (due to differences in the respective interfaces). This will
    simplify adding additional tests in the future.

--- Tests/RunCMake/InstallPackageInfo/BadArgs1.cmake
+++ Tests/RunCMake/InstallPackageInfo/BadArgs1.cmake
@@ -1,3 +1,4 @@
 add_library(foo INTERFACE)
 install(TARGETS foo EXPORT foo DESTINATION .)
 install(PACKAGE_INFO test EXPORT foo COMPAT_VERSION 1.0)
+install(PACKAGE_INFO test EXPORT foo VERSION_SCHEMA simple)
--- Tests/RunCMake/InstallPackageInfo/BadArgs3.cmake
+++
@@ -1,3 +0,0 @@
-add_library(foo INTERFACE)
-install(TARGETS foo EXPORT foo DESTINATION .)
-install(PACKAGE_INFO test EXPORT foo APPENDIX test VERSION 1.0)
