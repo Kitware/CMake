d89f1cbc162ffaf746f117ccb05b44b3ca277d4e
Brad King <brad.king@kitware.com>
2025-06-03 09:26:16 -0400

    ci: Enable Emscripten test on some Debian jobs in nightly pipeline

--- .gitlab/ci/configure_debian12_ninja_clang.cmake
+++ .gitlab/ci/configure_debian12_ninja_clang.cmake
@@ -1,6 +1,7 @@
 if (NOT "$ENV{CMAKE_CI_NIGHTLY}" STREQUAL "")
   set(CMake_TEST_IAR_TOOLCHAINS "/opt/iarsystems" CACHE PATH "")
   set(CMake_TEST_TICLANG_TOOLCHAINS "$ENV{CI_PROJECT_DIR}/.gitlab/ticlang" CACHE PATH "")
+  set(CMake_TEST_Emscripten_TOOLCHAINS "$ENV{CI_PROJECT_DIR}/.gitlab/emsdk/upstream/emscripten" CACHE PATH "")
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_external_test.cmake")
--- .gitlab/ci/env_debian12_ninja_clang.sh
+++ .gitlab/ci/env_debian12_ninja_clang.sh
@@ -1,6 +1,7 @@
 if test "$CMAKE_CI_NIGHTLY" = "true"; then
   source .gitlab/ci/iar-env.sh
   source .gitlab/ci/ticlang-env.sh
+  source .gitlab/ci/emsdk-env.sh
 fi
 
 export CC=/usr/bin/clang-15
