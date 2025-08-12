0e00166462f0373467024c911699fe4cec9c777c
Peter Kokot <peterkokot@gmail.com>
2025-08-12 07:17:58 +0200

    FindDevIL: Add DevIL_VERSION
    This module now provides a DevIL_VERSION result variable and supports
    the `<version>` argument in the find_package() call. Version range can
    be also specified.
    
    Fixes: #26858

--- Tests/FindDevIL/Test/main.c
+++ Tests/FindDevIL/Test/main.c
@@ -1,4 +1,6 @@
 #include <IL/il.h>
+#include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
@@ -6,5 +8,16 @@ int main(void)
   ilInit();
 
   ilShutDown();
-  return 0;
+
+  int version = IL_VERSION;
+  int major = version / 100;
+  int minor = version / 10 % 10;
+  int patch = version % 10;
+  char version_string[100];
+  snprintf(version_string, sizeof(version_string), "%d.%d.%d", major, minor,
+           patch);
+
+  printf("Found DevIL version %s, expected version %s\n", version_string,
+         CMAKE_EXPECTED_DEVIL_VERSION);
+  return strcmp(version_string, CMAKE_EXPECTED_DEVIL_VERSION);
 }
