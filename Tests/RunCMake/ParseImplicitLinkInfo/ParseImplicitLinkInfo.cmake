f2b2bac85d64851a4454db142aa5656d728a79b1
Brad King <brad.king@kitware.com>
2025-09-17 14:01:34 -0400

    Emscripten: Detect and identify linker invoked by the compiler driver

--- Tests/RunCMake/ParseImplicitLinkInfo/ParseImplicitLinkInfo.cmake
+++ Tests/RunCMake/ParseImplicitLinkInfo/ParseImplicitLinkInfo.cmake
@@ -20,6 +20,7 @@ set(targets
   darwin-C-AppleClang-8.0.0.8000042 darwin-CXX-AppleClang-8.0.0.8000042
     darwin_nostdinc-C-AppleClang-8.0.0.8000042
     darwin_nostdinc-CXX-AppleClang-8.0.0.8000042
+  emscripten-C-Clang-21.0.0 emscripten-CXX-Clang-21.0.0
   freebsd-C-Clang-3.3.0 freebsd-CXX-Clang-3.3.0 freebsd-Fortran-GNU-4.6.4
   hand-C-empty hand-CXX-empty
   hand-C-relative hand-CXX-relative
