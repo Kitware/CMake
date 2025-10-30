b7ad081f1abd0eec709e8efa7f4ab61f7571ec1e
Brad King <brad.king@kitware.com>
2025-10-30 17:15:35 -0400

    Tests: Fix RunCMake.CXXModules compilation with Clang 21
    Clang 21 now enforces placement of the module declaration:
    
        .../scan_properties/module.cxx:7:8: error: module
           declaration must occur at the start of the translation unit

--- Tests/RunCMake/CXXModules/examples/scan_properties/module.cxx
+++ Tests/RunCMake/CXXModules/examples/scan_properties/module.cxx
@@ -1,10 +1,10 @@
+export module M;
+
 #if SCANNING_CONTROL
 #  ifndef CMAKE_SCANNED_THIS_SOURCE
 #    error "This file should have been scanned"
 #  endif
 #endif
-
-export module M;
 
 export int from_module()
 {
