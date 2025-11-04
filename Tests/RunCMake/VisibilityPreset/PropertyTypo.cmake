7cc114601a134a8a2c17f5da5e436cbdabb06d6d
Matthew Woehlke <matthew.woehlke@kitware.com>
2025-11-04 12:45:30 -0500

    codespell: Prune ignore list
    Remove entries from the codespell ignore list that do not seem to be
    present any more. Also, remove a deliberate typo and instead add a local
    annotation to ignore it.
    
    This list is still uncomfortably long, but unfortunately current
    versions of codespell do not offer any granularity in between annotating
    individual uses (which is not practical in most cases) or globally
    ignoring words (which is overly broad and risks missing real errors).

--- Tests/RunCMake/VisibilityPreset/PropertyTypo.cmake
+++ Tests/RunCMake/VisibilityPreset/PropertyTypo.cmake
@@ -5,4 +5,4 @@ set(CMAKE_CXX_COMPILE_OPTIONS_VISIBILITY_INLINES_HIDDEN "-fvisibility-inlines-hi
 set(CMAKE_CXX_COMPILE_OPTIONS_VISIBILITY "-fvisibility=")
 
 add_library(visibility_preset SHARED lib.cpp)
-set_property(TARGET visibility_preset PROPERTY CXX_VISIBILITY_PRESET hiden)
+set_property(TARGET visibility_preset PROPERTY CXX_VISIBILITY_PRESET hiden) # codespell:ignore hiden
