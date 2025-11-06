a61ef50ea6f3e0f85c6ccd1dd6c181096e1cd208
Brad King <brad.king@kitware.com>
2025-11-06 14:09:20 -0500

    ci: Update Fedora 43 base image to get bzr and flang fixes
    * breezy: https://bugzilla.redhat.com/show_bug.cgi?id=2366194
    * flang: https://bugzilla.redhat.com/show_bug.cgi?id=2401176

--- .gitlab/ci/env_fedora43_common_clang.sh
+++ .gitlab/ci/env_fedora43_common_clang.sh
@@ -1,9 +1,3 @@
 export CC=/usr/bin/clang-21
 export CXX=/usr/bin/clang++-21
 export FC=/usr/bin/flang-21
-
-# FIXME(Fedora): Flang 21 packages do not make the runtime library findable.
-# https://github.com/llvm/llvm-project/issues/138340
-# https://bugzilla.redhat.com/show_bug.cgi?id=2401176
-# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1117534
-export LD_LIBRARY_PATH=/usr/lib/clang/21/lib/x86_64-redhat-linux-gnu
