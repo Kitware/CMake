48f091a166e583f6d1528d066d1e3f77fb5dffc1
Brad King <brad.king@kitware.com>
2025-11-11 16:23:54 -0500

    ci: Update debian12-ninja job to use Swift 6.2.1

--- .gitlab/ci/swift-env.sh
+++ .gitlab/ci/swift-env.sh
@@ -1,7 +1,7 @@
-curl -L -O "https://download.swift.org/swift-5.7.1-release/ubuntu1804/swift-5.7.1-RELEASE/swift-5.7.1-RELEASE-ubuntu18.04.tar.gz"
-echo '2b30f9efc969d9e96f0836d0871130dffb369822a3823ee6f3db44c29c1698e3  swift-5.7.1-RELEASE-ubuntu18.04.tar.gz' > swift.sha256sum
+curl -L -O "https://download.swift.org/swift-6.2.1-release/debian12/swift-6.2.1-RELEASE/swift-6.2.1-RELEASE-debian12.tar.gz"
+echo 'd6405e4fb7f092cbb9973a892ce8410837b4335f67d95bf8607baef1f69939e4  swift-6.2.1-RELEASE-debian12.tar.gz' > swift.sha256sum
 sha256sum --check swift.sha256sum
 mkdir /opt/swift
-tar xzf swift-5.7.1-RELEASE-ubuntu18.04.tar.gz -C /opt/swift --strip-components=2
-rm swift-5.7.1-RELEASE-ubuntu18.04.tar.gz swift.sha256sum
+tar xzf swift-6.2.1-RELEASE-debian12.tar.gz -C /opt/swift --strip-components=2
+rm swift-6.2.1-RELEASE-debian12.tar.gz swift.sha256sum
 export SWIFTC="/opt/swift/bin/swiftc"
