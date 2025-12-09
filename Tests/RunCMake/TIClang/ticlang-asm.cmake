003d728faa64d44881d331e183f08b4c49faec39
Brad King <brad.king@kitware.com>
2025-12-09 15:09:15 -0500

    Tests/RunCMake/TIClang: Remove unused link flags from test cases
    Typos in commit 9ba0a54554 (TIClang: Add support for TI Clang-based
    compilers, including tiarmclang, 2023-12-06, v3.29.0-rc1~4^2~1)
    left the flags unused.  In particular, `CMAKE_<LANG>_LINKER_FLAGS`
    does not exist.

--- Tests/RunCMake/TIClang/ticlang-asm.cmake
+++ Tests/RunCMake/TIClang/ticlang-asm.cmake
@@ -2,4 +2,3 @@ enable_language(ASM)
 
 add_executable(exec-asm)
 target_sources(exec-asm PRIVATE module.s)
-target_link_options(exec-asm PRIVATE ${LINKER_OPTS})
--- Tests/RunCMake/TIClang/ticlang-cxx.cmake
+++ Tests/RunCMake/TIClang/ticlang-cxx.cmake
@@ -2,4 +2,3 @@ enable_language(CXX)
 
 add_executable(exec-cxx)
 target_sources(exec-cxx PRIVATE module.cxx)
-target_link_options(exec-cxx PRIVATE ${LINKER_OPTS})
