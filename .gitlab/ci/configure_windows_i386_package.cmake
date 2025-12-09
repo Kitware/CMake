d54e15a5b87d5b3e63989fb8a2a8d7a89b009d4f
Brad King <brad.king@kitware.com>
2025-12-08 20:27:19 -0500

    ci: Require Windows 10 in official precompiled binaries
    See Windows SDK docs [1] on API selection via preprocessor definitions.
    
    [1] https://learn.microsoft.com/en-us/cpp/porting/modifying-winver-and-win32-winnt
    
    Closes: #27450

--- .gitlab/ci/configure_windows_i386_package.cmake
+++ .gitlab/ci/configure_windows_i386_package.cmake
@@ -4,10 +4,10 @@ set(CPACK_SYSTEM_NAME "windows-i386" CACHE STRING "")
 # Tell WiX to package for this architecture.
 set(CPACK_WIX_ARCHITECTURE "x86" CACHE STRING "")
 
-# Use APIs from at most Windows 7
-set(CMAKE_C_FLAGS "-D_WIN32_WINNT=0x601 -DNTDDI_VERSION=0x06010000" CACHE STRING "")
-set(CMAKE_CXX_FLAGS "-GR -EHsc -D_WIN32_WINNT=0x601 -DNTDDI_VERSION=0x06010000" CACHE STRING "")
-set(CMAKE_EXE_LINKER_FLAGS "-machine:x86 -subsystem:console,6.01" CACHE STRING "")
+# Use APIs from at most Windows 10
+set(CMAKE_C_FLAGS "-D_WIN32_WINNT=0x0A00 -DNTDDI_VERSION=0x0A000000" CACHE STRING "")
+set(CMAKE_CXX_FLAGS "-GR -EHsc -D_WIN32_WINNT=0x0A00 -DNTDDI_VERSION=0x0A000000" CACHE STRING "")
+set(CMAKE_EXE_LINKER_FLAGS "-machine:x86 -subsystem:console,6.02" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_package_common_x86.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/configure_windows_package_common.cmake")
---
+++ Help/release/dev/ci-package-windows-10.rst
@@ -0,0 +1,5 @@
+ci-package-windows-10
+---------------------
+
+* The precompiled Windows binaries provided on
+  `cmake.org <https://cmake.org/download/>`_ now require Windows 10 or higher.
