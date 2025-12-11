Kitware/CMake

commit 1b9e8f833f1169c793c7b36253203163c53b8764

Linux: Compile with 64-bit time_t even on 32-bit architectures
Extend commit 5b10f96793 (Linux: Compile with _FILE_OFFSET_BITS=64 on
32-bit Linux, 2020-09-23, v3.19.0-rc1~112^2) to cover `time_t` too,
by compiling with `_TIME_BITS=64`.

CompileFlags.cmake
@@ -69,7 +69,7 @@ endif()
 # Use 64-bit off_t on 32-bit Linux
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_SIZEOF_VOID_P EQUAL 4)
   # ensure 64bit offsets are used for filesystem accesses for 32bit compilation
-  add_compile_definitions(_FILE_OFFSET_BITS=64)
+  add_compile_definitions(_FILE_OFFSET_BITS=64 _TIME_BITS=64)
 endif()
 
 # Workaround for TOC Overflow on ppc64

Tests/RunCMake/string/RunCMakeTest.cmake
@@ -23,9 +23,6 @@ run_cmake(JoinNoVar)
 run_cmake(Timestamp)
 if(NOT CMAKE_SYSTEM_NAME STREQUAL "AIX" # FIXME: Needs 64-bit build
     AND NOT CMAKE_SYSTEM_NAME STREQUAL "SunOS" # FIXME: Needs 64-bit build
-    AND NOT (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND
-    CMAKE_SYSTEM_PROCESSOR MATCHES "^(hppa|parisc|parisc64|sparc|sparc64)$" # FIXME: 32-bit time_t?
-             )
     )
   run_cmake(Timestamp2038)
 endif()

commit cea7f7fc328955de5632f856bfc0474749b50219

Linux: Do not force 64-bit `time_t` on 32-bit archs with system libarchive
Commit 1b9e8f833f (Linux: Compile with 64-bit time_t even on 32-bit
architectures, 2025-06-26, v4.1.0-rc2~16^2) broke running with a
system-provided libarchive that uses a 32-bit `time_t`.  If CMake
is configured to build with a system libarchive, assume the user
has taken responsibility to match `time_t` sizes.

Fixes: #27448

CompileFlags.cmake
@@ -68,8 +68,14 @@ endif()
 
 # Use 64-bit off_t on 32-bit Linux
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux" AND CMAKE_SIZEOF_VOID_P EQUAL 4)
-  # ensure 64bit offsets are used for filesystem accesses for 32bit compilation
-  add_compile_definitions(_FILE_OFFSET_BITS=64 _TIME_BITS=64)
+  # Use 64-bit off_t even in 32-bit builds.
+  add_compile_definitions(_FILE_OFFSET_BITS=64)
+
+  # Use 64-bit time_t even in 32-bit builds.  Skip this with
+  # system libarchive because its ABI uses the default time_t.
+  if (NOT CMAKE_USE_SYSTEM_LIBARCHIVE)
+    add_compile_definitions(_TIME_BITS=64)
+  endif()
 endif()
 
 # Workaround for TOC Overflow on ppc64