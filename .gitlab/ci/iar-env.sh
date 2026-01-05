b554dfffd7e4eacce2b88f384bb548761ada4930
Brad King <brad.king@kitware.com>
2026-01-05 13:55:55 -0500

    ci: IAR arm 9.70.2, riscv 3.40.2, rl78 5.20.2, avr 8.10.3, rh850 3.20.1
    Run `.gitlab/ci/repackage/iar.ps1` with the installer and host the
    package archives ourselves.

--- .gitlab/ci/iar-env.sh
+++ .gitlab/ci/iar-env.sh
@@ -1,9 +1,9 @@
 files='
-bxarm-9.70.1.deb
-bxavr-8.10.2.deb
-bxrh850-3.10.2.deb
-bxriscv-3.40.1.deb
-bxrl78-5.20.1.deb
+bxarm-9.70.2.deb
+bxavr-8.10.3.deb
+bxrh850-3.20.1.deb
+bxriscv-3.40.2.deb
+bxrl78-5.20.2.deb
 bxrx-5.20.1.deb
 '
 for f in $files; do
@@ -12,11 +12,11 @@ for f in $files; do
 done
 
 echo '
-3b16748e560ab8fa3ffe6d6807186ac706134c78bc9db911112ab3ee67c7b997  bxarm-9.70.1.deb
-4a1065291952a23a8bfbbaa4eb36ca49b0af8653b8faab34ce955d9d48d64506  bxavr-8.10.2.deb
-b14085a0f21750c58168125d3cece2e3fcbd4c6495c652b5e65b6637bac0ac31  bxrh850-3.10.2.deb
-2e7de58a3aad43ef4199b811edd4dae9c4bff633376393f12fcb77ca27aba831  bxriscv-3.40.1.deb
-6a2b6163dd971635715f49cb072c853e5de55d2b0089f319a6a9f6db540af4bd  bxrl78-5.20.1.deb
+0a90ec54f097ebc24f7e12aa665b22cdaa78ea0a59792814392dce510d9fece1  bxarm-9.70.2.deb
+4fc428eee8617d365c526d67f9a30ed0873cbb916160a35ab7b50b4de0628dc0  bxavr-8.10.3.deb
+48b85860fc226aa8284b21ba3a49bfe018b12d61b5d0ab184e90840836583521  bxrh850-3.20.1.deb
+e701a208d4fac3fe52f685b785c1983383de48d556a77c5921f649d983c7339f  bxriscv-3.40.2.deb
+eb672e903d11f45adb591cf4a56e45187fe979c05e1c21d30ba7c1f6d95ea648  bxrl78-5.20.2.deb
 059667a53b6683b1b09b7842d71e784c04bc2376734421c3a628acdbc6bef9e3  bxrx-5.20.1.deb
 ' > bxdebs.sha256sum
 sha256sum --check bxdebs.sha256sum
