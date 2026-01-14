cc1f631e38cf22e2c69e16cce977f6c98c7599d9
Brad King <brad.king@kitware.com>
2026-01-13 21:16:57 -0500

    ci: Cover compilation with Clang on Fedora in every merge request
    Swap roles of Ninja+GNU job and Ninja+Clang job on Fedora.
    Other jobs already cover compilation with GNU on Fedora.

--- .gitlab/ci/configure_fedora43_ninja_valgrind.cmake
+++ .gitlab/ci/configure_fedora43_ninja_valgrind.cmake
@@ -3,4 +3,3 @@ set(CMake_TEST_Qt6 ON CACHE BOOL "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora43_valgrind.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora43_ninja.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/configure_external_test.cmake")
--- .gitlab/ci/env_fedora43_ninja_clang.sh
+++ .gitlab/ci/env_fedora43_ninja_clang.sh
@@ -1,1 +1,6 @@
+if test "$CMAKE_CI_NIGHTLY" = "true"; then
+  source .gitlab/ci/appimagetool-env.sh
+  source .gitlab/ci/ispc-env.sh
+fi
+
 . .gitlab/ci/env_fedora43_common_clang.sh
