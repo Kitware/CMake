5ccf55ffa71a1b7409d6501524bf34c2f4692d1f
Brad King <brad.king@kitware.com>
2026-01-13 21:19:20 -0500

    ci: Decouple Fedora+Clang common job settings from external testing

--- .gitlab/ci/configure_fedora43_common_clang.cmake
+++ .gitlab/ci/configure_fedora43_common_clang.cmake
@@ -13,5 +13,3 @@ set(CMake_TEST_FindOpenMP_C "ON" CACHE BOOL "")
 set(CMake_TEST_FindOpenMP_CXX "ON" CACHE BOOL "")
 set(CMake_TEST_FindOpenMP_Fortran "ON" CACHE BOOL "")
 set(CMake_TEST_FindOpenMP "ON" CACHE BOOL "")
-
-include("${CMAKE_CURRENT_LIST_DIR}/configure_external_test.cmake")
--- .gitlab/ci/configure_fedora43_ninja_clang.cmake
+++ .gitlab/ci/configure_fedora43_ninja_clang.cmake
@@ -1,3 +1,4 @@
 set(CMake_TEST_MODULE_COMPILATION "named,compile_commands,collation,partitions,internal_partitions,export_bmi,install_bmi,shared,bmionly,build_database,import_std23" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora43_common_clang.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_external_test.cmake")
