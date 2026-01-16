c238f174a962e3a3f7a5aa3f5d78ed398d35e86b
Robert Maynard <rmaynard@nvidia.com>
2026-01-16 13:35:39 -0500

    Tests: Remove invalid CUDA code from tests
    Multiple tests passed a host side stack address for writing on the GPU.
    Now we pass a GPU backed heap address.

--- Tests/CudaOnly/SeparateCompilation/file3.cu
+++ Tests/CudaOnly/SeparateCompilation/file3.cu
@@ -6,17 +6,24 @@
 result_type __device__ file1_func(int x);
 result_type_dynamic __device__ file2_func(int x);
 
-static __global__ void file3_kernel(result_type& r, int x)
+static __global__ void file3_kernel(result_type* r, int x)
 {
   // call static_func which is a method that is defined in the
   // static library that is always out of date
-  r = file1_func(x);
+  *r = file1_func(x);
   result_type_dynamic rd = file2_func(x);
 }
 
 result_type file3_launch_kernel(int x)
 {
-  result_type r;
+  result_type* r;
+  cudaMallocManaged(&r, sizeof(result_type));
+
   file3_kernel<<<1, 1>>>(r, x);
-  return r;
+  cudaDeviceSynchronize();
+
+  auto result = *r;
+  cudaFree(r);
+
+  return result;
 }
