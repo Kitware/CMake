aebf56325786c8344a11235c50f0b14a1174a2d5
Charlie Savage <cfis@savagexi.com>
2026-01-22 17:00:08 -0800

    Tests/RunCMake/FindRuby: Fix RVM tests
    Also improve error messages.

--- Tests/RunCMake/FindRuby/Rvm/RvmOnly.cmake
+++ Tests/RunCMake/FindRuby/Rvm/RvmOnly.cmake
@@ -4,35 +4,14 @@ set(CMAKE_FIND_LIBRARY_SUFFIXES "")
 set(Ruby_FIND_VIRTUALENV ONLY)
 set(Ruby_RBENV_EXECUTABLE "") # Suppress rbenv code path for this test.
 
-# Test: FindRuby.RvmOnly
-if (MY_RUBY_HOME)
-  # => Trying to find exactly system ruby using ONLY virtual environment should fail
-  find_package (Ruby ${SYSTEM_RUBY_VERSION} EXACT QUIET)
-  if(Ruby_FOUND)
-    message (FATAL_ERROR "Ruby unexpectedly found.")
-  endif()
-  # And should work to find the rvm version
-  find_package (Ruby ${RVM_RUBY_VERSION} EXACT QUIET)
-  if(Ruby_FOUND)
-    message (FATAL_ERROR "Ruby unexpectedly found.")
-  endif()
+# Trying to find the exact system ruby version using ONLY virtual environment should fail
+find_package (Ruby ${SYSTEM_RUBY_VERSION} EXACT QUIET)
+if (Ruby_FOUND)
+  message (FATAL_ERROR "Incorrectly found system Ruby: ${Ruby_EXECUTABLE}, ${MY_RUBY_HOME}")
 endif()
 
-
-# Test: FindRuby.UnsetRvmOnly
-if (NOT MY_RUBY_HOME)
-
-  # If ENV{MY_RUBY_HOME} isn't defined and Ruby_FIND_VIRTUALENV is set to ONLY
-  # then Ruby should not be found
-
-  find_package (Ruby ${RVM_RUBY_VERSION} EXACT QUIET)
-  if(Ruby_FOUND)
-    message (FATAL_ERROR "RVM Ruby unexpectedly found.")
-  endif()
-
-  # it should *not* find the system ruby
-  find_package (Ruby ${SYSTEM_RUBY_VERSION} EXACT QUIET)
-  if(Ruby_FOUND)
-    message (FATAL_ERROR "Ruby unexpectedly found.")
-  endif()
+# Finding RVM version should work
+find_package (Ruby ${RVM_RUBY_VERSION} EXACT QUIET)
+if (!Ruby_FOUND)
+  message (FATAL_ERROR "Failed to find RVM Ruby: ${MY_RUBY_HOME}")
 endif()
--- Tests/RunCMake/FindRuby/Rvm/UnsetRvmOnly.cmake
+++ Tests/RunCMake/FindRuby/Rvm/UnsetRvmOnly.cmake
@@ -4,34 +4,15 @@ set(CMAKE_FIND_LIBRARY_SUFFIXES "")
 set(Ruby_FIND_VIRTUALENV ONLY)
 set(Ruby_RBENV_EXECUTABLE "") # Suppress rbenv code path for this test.
 
-# Test: FindRuby.RvmOnly
-if (MY_RUBY_HOME)
-  # => Trying to find exactly system ruby using ONLY virtual environment should fail
-  find_package (Ruby ${SYSTEM_RUBY_VERSION} EXACT QUIET)
-  if(Ruby_FOUND)
-    message (FATAL_ERROR "Ruby unexpectedly found.")
-  endif()
-  # And should work to find the rvm version
-  find_package (Ruby ${RVM_RUBY_VERSION} EXACT QUIET)
-  if(Ruby_FOUND)
-    message (FATAL_ERROR "Ruby unexpectedly found.")
-  endif()
+# If ENV{MY_RUBY_HOME} isn't defined and Ruby_FIND_VIRTUALENV is set to ONLY
+# then Ruby should not be found
+find_package (Ruby ${RVM_RUBY_VERSION} EXACT QUIET)
+if (Ruby_FOUND)
+  message (FATAL_ERROR "Incorrectly found Ruby: ${Ruby_EXECUTABLE}")
 endif()
 
-
-# Test: FindRuby.UnsetRvmOnly
-if (NOT MY_RUBY_HOME)
-  # If ENV{MY_RUBY_HOME} isn't defined and Ruby_FIND_VIRTUALENV is set to ONLY
-  # then Ruby should not be found
-
-  find_package (Ruby ${RVM_RUBY_VERSION} EXACT QUIET)
-  if(Ruby_FOUND)
-    message (FATAL_ERROR "RVM Ruby unexpectedly found.")
-  endif()
-
-  # it should *not* find the system ruby
-  find_package (Ruby ${SYSTEM_RUBY_VERSION} EXACT QUIET)
-  if(Ruby_FOUND)
-    message (FATAL_ERROR "Ruby unexpectedly found.")
-  endif()
+# it should *not* find the system ruby
+find_package (Ruby ${SYSTEM_RUBY_VERSION} EXACT QUIET)
+if (Ruby_FOUND)
+  message (FATAL_ERROR "Incorrectly found Ruby: ${Ruby_EXECUTABLE}")
 endif()
