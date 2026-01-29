26cdd956ac6d442c027aa981d0c6b79874e82b66
Aliaksandr Averchanka <a.averchanka@axxonsoft.dev>
2026-01-29 09:17:15 +0300

    kwiml: provide common cm3p version header

---
+++ Utilities/cm3p/kwiml/version.h
@@ -0,0 +1,11 @@
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+   file LICENSE.rst or https://cmake.org/licensing for details.  */
+#pragma once
+
+/* Use the KWIML library configured for CMake.  */
+#include "cmThirdParty.h"
+#ifdef CMAKE_USE_SYSTEM_KWIML
+#  include <kwiml/version.h> // IWYU pragma: export
+#else
+#  include <KWIML/include/kwiml/version.h> // IWYU pragma: export
+#endif
