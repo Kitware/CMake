25c05b0bac7368d982461a64e2abab65069aef4c
Aliaksandr Averchanka <a.averchanka@axxonsoft.dev>
2026-01-29 09:17:15 +0300

    kwiml: Update script to get version header

--- Utilities/Scripts/update-kwiml.bash
+++ Utilities/Scripts/update-kwiml.bash
@@ -8,7 +8,7 @@ readonly name="KWIML"
 readonly ownership="KWIML Upstream <kwrobot@kitware.com>"
 readonly subtree="Utilities/KWIML"
 readonly repo="https://gitlab.kitware.com/utils/kwiml.git"
-readonly tag="master"
+readonly tag="master" # When updating, sync KWIML_VERSION below!
 readonly shortlog=true
 readonly exact_tree_match=false
 readonly paths="
@@ -16,6 +16,10 @@ readonly paths="
 
 extract_source () {
     git_archive
+    pushd "${extractdir}/${name}-reduced"
+    cp src/version.h.in include/kwiml/version.h
+    sed -i 's/@KWIML_VERSION@/1.0.0/;s/@KWIML_VERSION_DECIMAL@/1000000/' include/kwiml/version.h
+    popd
 }
 
 . "${BASH_SOURCE%/*}/update-third-party.bash"
