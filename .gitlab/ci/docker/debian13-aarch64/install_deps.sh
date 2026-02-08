3a68a0c75c65026baf5df1fcda51242989213fa7
Brad King <brad.king@kitware.com>
2026-02-08 15:20:06 -0500

    ci: Add locales to the Debian and Fedora base images
    Offer jobs a UTF-8 and a Latin-1 locale.

--- .gitlab/ci/docker/debian13-aarch64/install_deps.sh
+++ .gitlab/ci/docker/debian13-aarch64/install_deps.sh
@@ -3,3 +3,7 @@
 set -e
 
 apt-get install -y $(grep '^[^#]\+$' /root/deps_packages.lst)
+
+# Add locales.
+sed -i -E '/^# en_US[ .](ISO-8859-1|UTF-8)( |$)/ s/^# //' /etc/locale.gen
+dpkg-reconfigure --frontend=noninteractive locales
--- .gitlab/ci/docker/fedora43/install_deps.sh
+++ .gitlab/ci/docker/fedora43/install_deps.sh
@@ -4,6 +4,10 @@ set -e
 
 dnf install -y $(grep '^[^#]\+$' /root/deps_packages.lst)
 
+# Add locales.
+localedef --no-archive --inputfile=en_US --charmap=ISO-8859-1 en_US.ISO-8859-1
+localedef --no-archive --inputfile=en_US --charmap=UTF-8 en_US.UTF-8
+
 # Remove tests for Python packages
 for v in 3.14; do
     find /usr/lib64/python${v}/site-packages -type d -a -name tests -exec rm -rf {} +
