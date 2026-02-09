7c3104ae0c786297342901174ef565332df3c21b
Ole Hüser <167137-j0le@users.noreply.gitlab.kitware.com>
2026-02-09 17:53:00 +0100

    ci: Fix test environment covering `CMAKE_CXX_STDLIB_MODULES_JSON`
    In commit 85e536b5eb (gitlab-ci: test `CMAKE_CXX_STDLIB_MODULES_JSON` in
    CI, 2025-09-11, v4.2.0-rc1~182^2) we intended to move the file
    `libstdc++.modules.json` to a new folder, but source and destination
    paths for the `file(RENAME ...)` command were the same. They both were
    the original location of the file.  Fix the code to move the file to the
    directory created with `file(MAKE_DIRECTORY ...)`.
    
    Fixes: #27586

--- .gitlab/ci/configure_linux_gcc_cxx_modules_reloc_ninja.cmake
+++ .gitlab/ci/configure_linux_gcc_cxx_modules_reloc_ninja.cmake
@@ -2,7 +2,7 @@
 # `CMAKE_CXX_STDLIB_MODULES_JSON` is needed to use `import std`.
 set(gcc_prefix "/opt/gcc-importstd")
 set(CMake_TEST_CXX_STDLIB_MODULES_JSON
-  "${gcc_prefix}/lib64/libstdc++.modules.json"
+  "${gcc_prefix}/lib64.reloc/libstdc++.modules.json"
   CACHE FILEPATH "")
 file(MAKE_DIRECTORY
   "${gcc_prefix}/lib64.reloc")
