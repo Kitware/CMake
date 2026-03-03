41500ba575addd177aa7c45b0a6d244b6b5e9270
Alex Turbov <i.zaufi@gmail.com>
2026-03-03 21:46:59 +0400

    CPack/RPM: avoid `ls -la` process fork in tests
    Use `file(READ_SYMLINK)` instead.

--- Tests/RunCMake/CPack/tests/SYMLINKS/VerifyResult.cmake
+++ Tests/RunCMake/CPack/tests/SYMLINKS/VerifyResult.cmake
@@ -6,21 +6,18 @@ set(whitespaces "[\\t\\n\\r ]*")
 file(GLOB_RECURSE symlink_files RELATIVE "${bin_dir}" "${bin_dir}/*/symlink_*")
 
 foreach(check_symlink IN LISTS symlink_files)
-  get_filename_component(symlink_name "${check_symlink}" NAME)
-  execute_process(COMMAND ls -la "${check_symlink}"
-            WORKING_DIRECTORY "${bin_dir}"
-            OUTPUT_VARIABLE SYMLINK_POINT_
-            OUTPUT_STRIP_TRAILING_WHITESPACE)
+  cmake_path(GET check_symlink FILENAME symlink_name)
+  file(READ_SYMLINK "${check_symlink}" symlink_dst)
 
   if("${symlink_name}" STREQUAL "symlink_to_empty_dir")
-    string(REGEX MATCH "^.*${whitespaces}->${whitespaces}empty_dir$" check_symlink "${SYMLINK_POINT_}")
+    string(REGEX MATCH "^empty_dir$" check_symlink "${symlink_dst}")
   elseif("${symlink_name}" STREQUAL "symlink_to_non_empty_dir")
-    string(REGEX MATCH "^.*${whitespaces}->${whitespaces}non_empty_dir$" check_symlink "${SYMLINK_POINT_}")
+    string(REGEX MATCH "^non_empty_dir$" check_symlink "${symlink_dst}")
   else()
     message(FATAL_ERROR "error: unexpected rpm symbolic link '${check_symlink}'")
   endif()
 
   if(NOT check_symlink)
-    message(FATAL_ERROR "symlink points to unexpected location '${SYMLINK_POINT_}'")
+    message(FATAL_ERROR "symlink points to unexpected location '${symlink_dst}'")
   endif()
 endforeach()
