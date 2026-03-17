0db2ea97047ceb78f8764ca1196c8257da65c247
Arkadiusz Lachowicz <areklachowicz@gmail.com>
2026-03-17 16:11:12 +0100

    ExternalData: Add configurable link materialization mode
    Fixes: #27700

---
+++ Help/release/dev/ExternalData-link-mode.rst
@@ -0,0 +1,7 @@
+ExternalData-link-mode
+----------------------
+
+* The :module:`ExternalData` module gained an
+  :variable:`ExternalData_LINK_MODE` variable to select an ordered list
+  of file materialization modes for hard links, symbolic links, and
+  copies.
--- Tests/RunCMake/ExternalData/RunCMakeTest.cmake
+++ Tests/RunCMake/ExternalData/RunCMakeTest.cmake
@@ -1,5 +1,25 @@
 include(RunCMake)
 
+function(run_externaldata_local test)
+  run_cmake_command(${test}
+    ${CMAKE_COMMAND}
+    -DExternalData_ACTION=local
+    -DExternalData_OBJECT_STORES=${RunCMake_BINARY_DIR}/${test}-store
+    ${ARGN}
+    -Dfile=${RunCMake_BINARY_DIR}/${test}-output.txt
+    -Dname=${RunCMake_SOURCE_DIR}/CMakeLists.txt
+    -P ${RunCMake_SOURCE_DIR}/../../../Modules/ExternalData.cmake
+    )
+endfunction()
+
+function(run_externaldata_local_link_mode test link_mode)
+  string(REPLACE ";" "\\\\;" link_mode_arg "${link_mode}")
+  run_externaldata_local(${test}
+    "-DExternalData_LINK_MODE=${link_mode_arg}"
+    ${ARGN}
+    )
+endfunction()
+
 run_cmake(BadAlgoMap1)
 run_cmake(BadAlgoMap2)
 run_cmake(BadArguments)
@@ -8,6 +28,13 @@ run_cmake(BadCustom2)
 run_cmake(BadCustom3)
 run_cmake(BadCustom4)
 run_cmake(BadHashAlgo1)
+run_externaldata_local_link_mode(BadLinkMode "invalid;copy"
+  )
+run_externaldata_local_link_mode(BadLinkModeNoSymlinks "symlink;copy"
+  -DExternalData_NO_SYMLINKS=1
+  )
+run_externaldata_local_link_mode(GoodLinkModeList "copy;hardlink"
+  )
 run_cmake(BadOption1)
 run_cmake(BadOption2)
 run_cmake(BadRecurse1)
