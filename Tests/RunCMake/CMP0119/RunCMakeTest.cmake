d3c05d303722ad9e0dfa35bd99e903703b9e4383
Brad King <brad.king@kitware.com>
2026-03-25 13:54:46 -0400

    Tests/RunCMake/CMP0119: Update for OrangeC 7
    The compiler no longer supports arbitrary source extensions,
    even when the `-x $lang` flag is given.

--- Tests/RunCMake/CMP0119/RunCMakeTest.cmake
+++ Tests/RunCMake/CMP0119/RunCMakeTest.cmake
@@ -12,6 +12,8 @@ if(NOT RunCMake_GENERATOR MATCHES "Visual Studio|Xcode" AND
   run_CMP0119(WARN)
   run_CMP0119(OLD)
 endif()
-if((CMAKE_C_COMPILER_ID MATCHES "(GNU|LCC|Clang|MSVC|Borland|Embarcadero|Intel|TI|OrangeC)"))
+if((CMAKE_C_COMPILER_ID MATCHES "(GNU|LCC|Clang|MSVC|Borland|Embarcadero|Intel|TI)")
+  # FIXME(OrangeC#1137): OrangeC 7 no longer accepts custom file extensions with -x
+  OR (CMAKE_C_COMPILER_ID STREQUAL "OrangeC" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 7))
   run_CMP0119(NEW)
 endif()
