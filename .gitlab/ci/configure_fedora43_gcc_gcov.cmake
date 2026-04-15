a7ea25f7b628bd509290f0670e56e7013244c1e6
Brad King <brad.king@kitware.com>
2026-04-15 15:12:50 -0400

    ci: Simplify construction of compile and link flags for gcov job

--- .gitlab/ci/configure_fedora43_gcc_gcov.cmake
+++ .gitlab/ci/configure_fedora43_gcc_gcov.cmake
@@ -6,28 +6,30 @@ set(CMake_TEST_TLS_VERIFY_URL_BAD "https://badtls-expired.kitware.com" CACHE STR
 set(CMake_TEST_TLS_VERSION "1.3" CACHE STRING "")
 set(CMake_TEST_TLS_VERSION_URL_BAD "https://badtls-v1-1.kitware.com:8011" CACHE STRING "")
 
-string(CONCAT coverage_flags
-  "--coverage "
-  "-fprofile-abs-path "
+string(JOIN " " coverage_flags
+  "--coverage"
+  "-fprofile-abs-path"
   # These files are committed generated sources and contain relative path
   # `#line` directives. CI coverage then cannot find the source files reliably.
   # See related issue #20001.
-  "-fprofile-exclude-files=cmExprParser[.].*\\;cmFortranParser[.].* "
-  "-fdiagnostics-show-option "
-  "-Wall "
-  "-Wextra "
-  "-Wshadow "
-  "-Wpointer-arith "
-  "-Winvalid-pch "
-  "-Wcast-align "
-  "-Wdisabled-optimization "
-  "-Wwrite-strings "
-  "-fstack-protector-all "
-  "-Wconversion "
-  "-Wno-error=sign-conversion "
-  "-Wno-error=conversion ")
-string(CONCAT link_flags
-  "--coverage ")
+  "-fprofile-exclude-files=cmExprParser[.].*\\;cmFortranParser[.].*"
+  "-fdiagnostics-show-option"
+  "-Wall"
+  "-Wextra"
+  "-Wshadow"
+  "-Wpointer-arith"
+  "-Winvalid-pch"
+  "-Wcast-align"
+  "-Wdisabled-optimization"
+  "-Wwrite-strings"
+  "-fstack-protector-all"
+  "-Wconversion"
+  "-Wno-error=sign-conversion"
+  "-Wno-error=conversion"
+)
+string(JOIN " " link_flags
+  "--coverage"
+)
 
 set(CMAKE_C_FLAGS "${coverage_flags}" CACHE STRING "")
 set(CMAKE_CXX_FLAGS "${coverage_flags} -Woverloaded-virtual -Wstrict-null-sentinel" CACHE STRING "")
