b736acdc3b5749f135d5d1ef80cf949b58a3eec2
Tyler Yankee <tyler.yankee@kitware.com>
2026-06-12 15:04:24 -0400

    Tests/instrumentation: Check errors for query schema validation

--- Tests/RunCMake/Instrumentation/check-query-dir.cmake
+++ Tests/RunCMake/Instrumentation/check-query-dir.cmake
@@ -1,12 +1,19 @@
 include(${CMAKE_CURRENT_LIST_DIR}/verify-snippet.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/json.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/validate_schema.cmake)
-file(GLOB_RECURSE queries LIST_DIRECTORIES false ${v1}/query/*)
 
+file(GLOB_RECURSE queries LIST_DIRECTORIES false ${v1}/query/*)
 foreach(query ${queries})
   validate_schema(
     "${query}"
     "${CMAKE_CURRENT_LIST_DIR}/../../../Help/manual/instrumentation/query-v1-schema.json"
     "${schema_validate_result}"
   )
+  if (RunCMake_TEST_FAILED)
+    add_error("${RunCMake_TEST_FAILED}")
+  endif()
 endforeach()
+
+if (ERROR_MESSAGE)
+  message(FATAL_ERROR ${ERROR_MESSAGE})
+endif()
