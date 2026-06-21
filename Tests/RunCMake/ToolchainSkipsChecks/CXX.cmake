170248fbcbfa44884fa81cd0f1957350d43a16a0
Ben Boeckel <ben.boeckel@kitware.com>
2026-06-20 22:06:55 -0400

    CMakeDetermineCompilerABI: require more variables to skip checks
    These variables were missed in the addition of the feature.
    
    Fixes: #27894

--- Tests/RunCMake/ToolchainSkipsChecks/CXX.cmake
+++ Tests/RunCMake/ToolchainSkipsChecks/CXX.cmake
@@ -1,1 +1,15 @@
 enable_language(CXX)
+
+# See #27894
+if (NOT CMAKE_SIZEOF_VOID_P)
+  message(FATAL_ERROR
+    "`CMAKE_SIZEOF_VOID_P` not initialized from toolchain data")
+endif ()
+if (CMAKE_CXX_COMPILER_ABI AND NOT CMAKE_INTERNAL_PLATFORM_ABI)
+  message(FATAL_ERROR
+    "`CMAKE_INTERNAL_PLATFORM_ABI` not initialized from toolchain data")
+endif ()
+if (CMAKE_CXX_LIBRARY_ARCHITECTURE AND NOT CMAKE_LIBRARY_ARCHITECTURE)
+  message(FATAL_ERROR
+    "`CMAKE_LIBRARY_ARCHITECTURE` not initialized from toolchain data")
+endif ()
