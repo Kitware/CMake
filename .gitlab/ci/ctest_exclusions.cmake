7d408ff993c616d5c2448da8e73cc3ef8a757168
Brad King <brad.king@kitware.com>
2026-07-31 10:35:04 -0400

    ci: Drop outdated test exclusion from valgrind job
    This was missed in commit 93449833ff (RunCMake/CXXModulesCompile: split
    into 3 variants, 2026-05-21, v4.4.0-rc1~101^2~1).

--- .gitlab/ci/ctest_exclusions.cmake
+++ .gitlab/ci/ctest_exclusions.cmake
@@ -34,7 +34,6 @@ if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "_valgrind")
     "^RunCMake.Autogen_Qt6_1$"
     "^RunCMake.GoogleTest$"
     "^RunCMake.CXXModules$"
-    "^RunCMake.CXXModulesCompile$"
     "^RunCMake.CommandLine$"
 
     # Too spurious under Valgrind.
