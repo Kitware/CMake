Kitware/CMake

commit c7d2a172534de20ea56dedbe32c73e4e288d0a58

IAR: Add support for C-STAT static analysis
The IAR platform offers an integrated static analysis tool named
IAR C-STAT.

Closes: #26844

Source/cmCommonTargetGenerator.cxx
@@ -360,6 +360,7 @@ std::string cmCommonTargetGenerator::GenerateCodeCheckRules(
   std::string iwyu;
   std::string cpplint;
   std::string cppcheck;
+  std::string icstat;
 
   auto evaluateProp = [&](std::string const& prop) -> std::string {
     auto const value = this->GeneratorTarget->GetProperty(prop);
@@ -383,9 +384,12 @@ std::string cmCommonTargetGenerator::GenerateCodeCheckRules(
 
     std::string const cppcheck_prop = cmStrCat(lang, "_CPPCHECK");
     cppcheck = evaluateProp(cppcheck_prop);
+
+    std::string const icstat_prop = cmStrCat(lang, "_ICSTAT");
+    icstat = evaluateProp(icstat_prop);
   }
   if (cmNonempty(iwyu) || cmNonempty(tidy) || cmNonempty(cpplint) ||
-      cmNonempty(cppcheck)) {
+      cmNonempty(cppcheck) || cmNonempty(icstat)) {
     std::string code_check = cmakeCmd + " -E __run_co_compile";
     if (!compilerLauncher.empty()) {
       // In __run_co_compile case the launcher command is supplied
@@ -480,6 +484,24 @@ std::string cmCommonTargetGenerator::GenerateCodeCheckRules(
       code_check += " --cppcheck=";
       code_check +=
         this->GeneratorTarget->GetLocalGenerator()->EscapeForShell(cppcheck);
+    }
+    if (cmNonempty(icstat)) {
+      code_check += " --icstat=";
+      std::string checksParam{};
+      std::string dbParam{};
+      // Set default values for mandatory parameters
+      std::string checksFile{ "cstat_sel_checks.txt" };
+      std::string dbFile{ "cstat.db" };
+      // Populate the command line with C-STAT
+      // mandatory parameters unless specified
+      if (icstat.find("--checks=") == std::string::npos) {
+        checksParam = cmStrCat(";--checks=", checksFile);
+      }
+      if (icstat.find("--db=") == std::string::npos) {
+        dbParam = cmStrCat(";--db=", dbFile);
+      }
+      code_check += this->GeneratorTarget->GetLocalGenerator()->EscapeForShell(
+        cmStrCat(icstat, checksParam, dbParam));
     }
     if (cmNonempty(tidy) || (cmNonempty(cpplint)) || (cmNonempty(cppcheck))) {
       code_check += " --source=";

Source/cmcmd.cxx
@@ -513,6 +513,55 @@ int HandleCppCheck(std::string const& runCmd, std::string const& sourceFile,
   return ret;
 }
 
+int HandleIcstat(std::string const& runCmd, std::string const& sourceFile,
+                 std::vector<std::string> const& orig_cmd)
+{
+  // Construct the IAR C-STAT command line.
+  cmList icstat_cmd{ runCmd, cmList::EmptyElements::Yes };
+  std::string icstat_analyze{ "analyze" };
+  std::string icstat_dashdash{ "--" };
+  std::string stdOut;
+  std::string stdErr;
+  int ret;
+
+  icstat_cmd.push_back(icstat_analyze);
+  icstat_cmd.push_back(sourceFile);
+  icstat_cmd.push_back(icstat_dashdash);
+
+  for (auto const& cmd : orig_cmd) {
+    icstat_cmd.push_back(cmd);
+  }
+
+  // Create the default manifest ruleset file when not found
+  if (!cmSystemTools::FileExists("cstat_sel_checks.txt")) {
+    std::string ichecks_cmd = cmSystemTools::GetFilenamePath(orig_cmd[0]);
+    ichecks_cmd = cmStrCat(ichecks_cmd, "/ichecks --default stdchecks");
+    if (!cmSystemTools::RunSingleCommand(ichecks_cmd, &stdOut, &stdErr, &ret,
+                                         nullptr,
+                                         cmSystemTools::OUTPUT_NONE)) {
+      std::cerr << "Error generating default manifest file '" << ichecks_cmd
+                << "'. " << stdOut << '\n';
+      return 1;
+    }
+  }
+
+  // Run the IAR C-STAT command line. Capture its output.
+  if (!cmSystemTools::RunSingleCommand(icstat_cmd, &stdOut, &stdErr, &ret,
+                                       nullptr, cmSystemTools::OUTPUT_NONE)) {
+    std::cerr << "Error running '" << icstat_cmd[0] << "': " << stdOut << '\n';
+    return 1;
+  }
+  if (ret == 0) {
+    std::cerr << "Warning: C-STAT static analysis reported diagnostics:\n";
+  } else {
+    std::cerr << "Error: C-STAT static analysis reported failure:\n";
+  }
+  std::cerr << stdOut;
+  std::cerr << stdErr;
+
+  return ret;
+}
+
 using CoCompileHandler = int (*)(std::string const&, std::string const&,
                                  std::vector<std::string> const&);
 
@@ -523,10 +572,11 @@ struct CoCompiler
   bool NoOriginalCommand;
 };
 
-std::array<CoCompiler, 5> const CoCompilers = {
+std::array<CoCompiler, 6> const CoCompilers = {
   { // Table of options and handlers.
     { "--cppcheck=", HandleCppCheck, false },
     { "--cpplint=", HandleCppLint, false },
+    { "--icstat=", HandleIcstat, false },
     { "--iwyu=", HandleIWYU, false },
     { "--lwyu=", HandleLWYU, true },
     { "--tidy=", HandleTidy, false } }

Source/cmTarget.cxx
@@ -463,12 +463,14 @@ TargetProperty const StaticTargetProperties[] = {
   { "C_CLANG_TIDY_EXPORT_FIXES_DIR"_s, IC::CanCompileSources },
   { "C_CPPLINT"_s, IC::CanCompileSources },
   { "C_CPPCHECK"_s, IC::CanCompileSources },
+  { "C_ICSTAT"_s, IC::CanCompileSources },
   { "C_INCLUDE_WHAT_YOU_USE"_s, IC::CanCompileSources },
   // -- C++
   { "CXX_CLANG_TIDY"_s, IC::CanCompileSources },
   { "CXX_CLANG_TIDY_EXPORT_FIXES_DIR"_s, IC::CanCompileSources },
   { "CXX_CPPLINT"_s, IC::CanCompileSources },
   { "CXX_CPPCHECK"_s, IC::CanCompileSources },
+  { "CXX_ICSTAT"_s, IC::CanCompileSources },
   { "CXX_INCLUDE_WHAT_YOU_USE"_s, IC::CanCompileSources },
   // -- Objective C
   { "OBJC_CLANG_TIDY"_s, IC::CanCompileSources },
@@ -1783,6 +1785,7 @@ void cmTarget::CopyImportedCxxModulesProperties(cmTarget const* tgt)
     "CXX_CLANG_TIDY_EXPORT_FIXES_DIR",
     "CXX_CPPLINT",
     "CXX_CPPCHECK",
+    "CXX_ICSTAT",
     "CXX_INCLUDE_WHAT_YOU_USE",
 
     // Build graph properties

Tests/RunCMake/CMakeLists.txt
@@ -1361,7 +1361,11 @@ if(WIN32)
 endif()
 
 if(CMake_TEST_IAR_TOOLCHAINS)
-  add_RunCMake_test(IAR -DCMake_TEST_IAR_TOOLCHAINS=${CMake_TEST_IAR_TOOLCHAINS})
+  add_executable(pseudo_icstat pseudo_icstat.c)
+  add_RunCMake_test(IAR
+    -DCMake_TEST_IAR_TOOLCHAINS=${CMake_TEST_IAR_TOOLCHAINS}
+    -DPSEUDO_ICSTAT=$<TARGET_FILE:pseudo_icstat>
+  )
   set_property(TEST RunCMake.IAR APPEND PROPERTY LABELS "IAR")
 endif()
 if(CMake_TEST_TICLANG_TOOLCHAINS)

Tests/RunCMake/IAR/RunCMakeTest.cmake
@@ -10,8 +10,9 @@ endif()
 
 function(run_toolchain case)
   set(RunCMake_TEST_BINARY_DIR ${RunCMake_BINARY_DIR}/${case}-build)
-  run_cmake_with_options(${case} ${ARGN})
+  run_cmake_with_options(${case} "-DPSEUDO_ICSTAT=${PSEUDO_ICSTAT}" ${ARGN})
   set(RunCMake_TEST_NO_CLEAN 1)
+  set(RunCMake_TEST_OUTPUT_MERGE 1)
   run_cmake_command(${case}-build ${CMAKE_COMMAND} --build .)
 endfunction()
 
@@ -61,7 +62,19 @@ foreach(_iar_toolchain IN LISTS _iar_toolchains)
     -DCMAKE_EXE_LINKER_FLAGS=${LINK_OPTS}
   )
 
+  run_toolchain(iar-c-bad
+    -DCMAKE_SYSTEM_NAME=Generic
+    -DCMAKE_C_COMPILER=${_iar_toolchain}
+    -DCMAKE_EXE_LINKER_FLAGS=${LINK_OPTS}
+  )
+
   run_toolchain(iar-cxx
+    -DCMAKE_SYSTEM_NAME=Generic
+    -DCMAKE_CXX_COMPILER=${_iar_toolchain}
+    -DCMAKE_EXE_LINKER_FLAGS=${LINK_OPTS}
+  )
+
+  run_toolchain(iar-cxx-bad
     -DCMAKE_SYSTEM_NAME=Generic
     -DCMAKE_CXX_COMPILER=${_iar_toolchain}
     -DCMAKE_EXE_LINKER_FLAGS=${LINK_OPTS}

Tests/RunCMake/IAR/iar-c-bad.cmake
@@ -1,5 +1,5 @@
 enable_language(C)
-set(CMAKE_C_ICSTAT "${PSEUDO_ICSTAT}" -some -args)
+set(CMAKE_C_ICSTAT "${PSEUDO_ICSTAT}" -bad)
 add_executable(exec-c module.c)
 target_compile_options(exec-c PRIVATE -e)
 target_link_options(exec-c PRIVATE ${LINKER_OPTS})

Tests/RunCMake/IAR/iar-cxx-bad.cmake
@@ -1,5 +1,5 @@
 enable_language(CXX)
-set(CMAKE_CXX_ICSTAT "${PSEUDO_ICSTAT}" -some -args)
+set(CMAKE_CXX_ICSTAT "${PSEUDO_ICSTAT}" -bad)
 add_executable(exec-cxx module.cxx)
 target_compile_options(exec-cxx PRIVATE -e)
 target_link_options(exec-cxx PRIVATE ${LINKER_OPTS})

Tests/RunCMake/pseudo_icstat.c
@@ -1,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+  int i;
+  int result = 0;
+  for (i = 1; i < argc; ++i) {
+    if (strcmp(argv[i], "-bad") == 0) {
+      fprintf(stdout, "stdout from bad command line arg '-bad'\n");
+      fprintf(stderr, "stderr from bad command line arg '-bad'\n");
+      return 1;
+    }
+  }
+  fprintf(stderr,
+          "\"foo/bar.c\",2 Severity-High[SPC-uninit-var-some]:"
+          "Variable `i' may be uninitialized.\n\n");
+  fprintf(stderr,
+          "\"foo/bar.c\",2 Severity-Medium[MISRAC2012-Rule-8.2_a]:"
+          "`main' does not have a valid prototype.\n\n");
+  fprintf(stderr,
+          "\"foo/bar.c\",2 Severity-Low[MISRAC2012-Rule-21.6]:"
+          "Use of `stdio.h' is not compliant.\n\n");
+  fprintf(stderr,
+          "\"foo/bar.c\",2 Severity-Low[MISRAC2012-Rule-17.7]:"
+          "The return value of this call to `printf()' is discarded.\n\n");
+  return result;
+}

commit 57fea01732b4824e9c4da4ced691227d0ac42deb

IAR: Fix malformed C-STAT command line
There was an issue with a malformed command line introduced in
commit c7d2a172 (IAR: Add support for C-STAT static analysis,
2025-04-08, v4.1.0-rc1~385^2), where the source file was
incorrectly passed as a parameter when executing `icstat` in
HandleIcstat(). `icstat` does not take the source file as a
direct parameter.

Source/cmCommonTargetGenerator.cxx
@@ -607,21 +607,22 @@ std::string cmCommonTargetGenerator::GenerateCodeCheckRules(
     }
     if (cmNonempty(icstat)) {
       code_check += " --icstat=";
+      // Unless specified otherwise via CMAKE_<LANG>_ICSTAT,
+      // populate the icstat command line using default options
+      // for its mandatory parameters.
       std::string checksParam{};
-      std::string dbParam{};
-      // Set default values for mandatory parameters
-      std::string checksFile{ "cstat_sel_checks.txt" };
-      std::string dbFile{ "cstat.db" };
-      // Populate the command line with C-STAT
-      // mandatory parameters unless specified
       if (icstat.find("--checks=") == std::string::npos) {
+        std::string const checksFile{ "cstat_sel_checks.txt" };
         checksParam = cmStrCat(";--checks=", checksFile);
       }
+      std::string dbParam{};
       if (icstat.find("--db=") == std::string::npos) {
+        std::string const dbFile{ "cstat.db" };
         dbParam = cmStrCat(";--db=", dbFile);
       }
+      std::string analyzeCmd{ ";analyze" };
       code_check += this->GeneratorTarget->GetLocalGenerator()->EscapeForShell(
-        cmStrCat(icstat, checksParam, dbParam));
+        cmStrCat(icstat, checksParam, dbParam, analyzeCmd));
     }
     if (cmNonempty(tidy) || (cmNonempty(cpplint)) || (cmNonempty(cppcheck)) ||
         cmNonempty(pvs) || cmNonempty(icstat)) {

Source/cmcmd.cxx
@@ -623,18 +623,15 @@ int HandleIcstat(std::string const& runCmd, std::string const& sourceFile,
 {
   // Construct the IAR C-STAT command line.
   cmList icstat_cmd{ runCmd, cmList::EmptyElements::Yes };
-  std::string icstat_analyze{ "analyze" };
-  std::string icstat_dashdash{ "--" };
   std::string stdOut;
   std::string stdErr;
   int ret;
 
-  icstat_cmd.push_back(icstat_analyze);
-  icstat_cmd.push_back(sourceFile);
-  icstat_cmd.push_back(icstat_dashdash);
+  std::string const double_dash{ "--" };
+  icstat_cmd.push_back(double_dash);
 
   for (auto const& cmd : orig_cmd) {
-    icstat_cmd.push_back(cmd);
+    icstat_cmd.emplace_back(cmd);
   }
 
   // Create the default manifest ruleset file when not found