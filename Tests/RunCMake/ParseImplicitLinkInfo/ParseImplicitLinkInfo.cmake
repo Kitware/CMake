7716efa3c6a5044b53c2bb58c34ef49c88c6c910
Brad King <brad.king@kitware.com>
2026-08-31 14:08:39 -0400

    Tests/RunCMake/ParseImplicitLinkInfo: Run more cases on non-Windows hosts

--- Tests/RunCMake/ParseImplicitLinkInfo/ParseImplicitLinkInfo.cmake
+++ Tests/RunCMake/ParseImplicitLinkInfo/ParseImplicitLinkInfo.cmake
@@ -74,9 +74,6 @@ set(targets
 if(CMAKE_HOST_WIN32)
   # The KWSys actual-case cache breaks case sensitivity on Windows.
   list(FILTER targets EXCLUDE REGEX "-XL|-SunPro")
-else()
-  # Windows drive letters are not recognized as absolute on other platforms.
-  list(FILTER targets EXCLUDE REGEX "mingw")
 endif()
 
 include(${CMAKE_ROOT}/Modules/CMakeParseImplicitLinkInfo.cmake)
