314c720eafae72f3a330b748d583dd3a3e3f05c6
Tyler Yankee <tyler.yankee@kitware.com>
2026-09-10 08:46:50 -0400

    cpack: Fail-fast on unknown arguments

--- Source/CPack/cpack.cxx
+++ Source/CPack/cpack.cxx
@@ -253,13 +253,20 @@ int main(int argc, char const* const* argv)
   bool parsed = true;
   for (std::size_t i = 0; i < inputArgs.size(); i++) {
     auto const& arg = inputArgs[i];
+    bool matched = false;
     for (auto const& m : arguments) {
       if (m.matches(arg)) {
+        matched = true;
         if (!m.parse(arg, i, inputArgs, &cminst, &globalMF)) {
           parsed = false;
         }
         break;
       }
+    }
+    if (!matched && cmHasPrefix(arg, '-')) {
+      std::string error = cmStrCat("Unknown argument: ", arg);
+      cmCPack_Log(&log, cmCPackLog::LOG_ERROR, error << '\n');
+      parsed = false;
     }
   }
 
