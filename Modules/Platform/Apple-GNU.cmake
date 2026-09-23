Kitware/CMake

commit a1b5c0da877f78db3e5dfc8d9ec4f9dac21f7a5c

Modules: Tolerate CMAKE_EXECUTE_PROCESS_COMMAND_ERROR_IS_FATAL
Capture the `execute_process` command's `RESULT_VARIABLE`.

Issue: #27782

Modules/Platform/Apple-GNU.cmake
@@ -31,8 +31,9 @@ macro(cmake_gnu_set_sysroot_flag lang)
       COMMAND ${CMAKE_${lang}_COMPILER} "-v" "--help"
       OUTPUT_VARIABLE _gcc_help
       ERROR_VARIABLE _gcc_help
+      RESULT_VARIABLE _gcc_result
       )
-    if("${_gcc_help}" MATCHES "isysroot")
+    if(_gcc_result EQUAL 0 AND "${_gcc_help}" MATCHES "isysroot")
       message(CHECK_PASS "yes")
       set(CMAKE_${lang}_SYSROOT_FLAG "-isysroot")
     else()
@@ -51,8 +52,9 @@ macro(cmake_gnu_set_osx_deployment_target_flag lang)
       COMMAND ${CMAKE_${lang}_COMPILER} "-v" "--help"
       OUTPUT_VARIABLE _gcc_help
       ERROR_VARIABLE _gcc_help
+      RESULT_VARIABLE _gcc_result
       )
-    if("${_gcc_help}" MATCHES "macosx-version-min")
+    if(_gcc_result EQUAL 0 AND "${_gcc_help}" MATCHES "macosx-version-min")
       message(CHECK_PASS "yes")
       set(CMAKE_${lang}_OSX_DEPLOYMENT_TARGET_FLAG "-mmacosx-version-min=")
     else()

Modules/CMakeDetermineRustCompiler.cmake
@@ -30,9 +30,10 @@ if(RUSTC_FILENAME STREQUAL "rustup")
     COMMAND ${RUSTUP_PATH} which rustc
     OUTPUT_VARIABLE REAL_RUSTC
     OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
+    RESULT_VARIABLE _rust_result
+    )
 
-  if("${REAL_RUSTC}" STREQUAL "")
+  if(NOT _rust_result EQUAL 0 OR "${REAL_RUSTC}" STREQUAL "")
     message(FATAL_ERROR "Failed to find path to real rustc")
   endif()
 

Modules/CMakeDetermineSystem.cmake
@@ -15,19 +15,24 @@ if(CMAKE_HOST_UNIX)
       execute_process(COMMAND ${CMAKE_UNAME} -v
         OUTPUT_VARIABLE _CMAKE_HOST_SYSTEM_MAJOR_VERSION
         OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET)
+        ERROR_QUIET
+        RESULT_VARIABLE _uname_result)
       execute_process(COMMAND ${CMAKE_UNAME} -r
         OUTPUT_VARIABLE _CMAKE_HOST_SYSTEM_MINOR_VERSION
         OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET)
-      set(CMAKE_HOST_SYSTEM_VERSION "${_CMAKE_HOST_SYSTEM_MAJOR_VERSION}.${_CMAKE_HOST_SYSTEM_MINOR_VERSION}")
+        ERROR_QUIET
+        RESULT_VARIABLE _uname_result2)
+      if(_uname_result EQUAL 0 AND _uname_result2 EQUAL 0)
+        set(CMAKE_HOST_SYSTEM_VERSION "${_CMAKE_HOST_SYSTEM_MAJOR_VERSION}.${_CMAKE_HOST_SYSTEM_MINOR_VERSION}")
+      endif()
       unset(_CMAKE_HOST_SYSTEM_MAJOR_VERSION)
       unset(_CMAKE_HOST_SYSTEM_MINOR_VERSION)
     elseif(CMAKE_HOST_SYSTEM_NAME STREQUAL "Android")
       execute_process(COMMAND getprop ro.build.version.sdk
         OUTPUT_VARIABLE CMAKE_HOST_SYSTEM_VERSION
         OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET)
+        ERROR_QUIET
+        RESULT_VARIABLE _uname_result)
 
       if(NOT DEFINED CMAKE_SYSTEM_VERSION)
         set(_ANDROID_API_LEVEL_H $ENV{PREFIX}/include/android/api-level.h)
@@ -49,7 +54,8 @@ if(CMAKE_HOST_UNIX)
       execute_process(COMMAND ${CMAKE_UNAME} -r
         OUTPUT_VARIABLE CMAKE_HOST_SYSTEM_VERSION
         OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET)
+        ERROR_QUIET
+        RESULT_VARIABLE _uname_result)
     endif()
     if(CMAKE_HOST_SYSTEM_NAME MATCHES "Linux|CYGWIN.*|MSYS.*|^GNU$|Android")
       execute_process(COMMAND ${CMAKE_UNAME} -m

Modules/Compiler/ARMClang.cmake
@@ -25,9 +25,14 @@ endif()
 function(__armclang_set_processor_list lang out_var)
   execute_process(COMMAND "${CMAKE_${lang}_COMPILER}" --target=${CMAKE_${lang}_COMPILER_TARGET} -mcpu=list
     OUTPUT_VARIABLE processor_list
-    ERROR_VARIABLE processor_list)
-  string(REGEX MATCHALL "-mcpu=([^ \n]*)" processor_list "${processor_list}")
-  string(REGEX REPLACE "-mcpu=" "" processor_list "${processor_list}")
+    ERROR_VARIABLE processor_list
+    RESULT_VARIABLE _res
+    )
+  set(processor_list)
+  if(_res EQUAL 0)
+    string(REGEX MATCHALL "-mcpu=([^ \n]*)" processor_list "${processor_list}")
+    string(REGEX REPLACE "-mcpu=" "" processor_list "${processor_list}")
+  endif()
   set(${out_var} "${processor_list}" PARENT_SCOPE)
 endfunction()
 
@@ -45,9 +50,14 @@ endfunction()
 function(__armclang_set_arch_list lang out_var)
   execute_process(COMMAND "${CMAKE_${lang}_COMPILER}" --target=${CMAKE_${lang}_COMPILER_TARGET} -march=list
     OUTPUT_VARIABLE arch_list
-    ERROR_VARIABLE arch_list)
-  string(REGEX MATCHALL "-march=([^ \n]*)" arch_list "${arch_list}")
-  string(REGEX REPLACE "-march=" "" arch_list "${arch_list}")
+    ERROR_VARIABLE arch_list
+    RESULT_VARIABLE _res
+    )
+  set(arch_list)
+  if(_res EQUAL 0)
+    string(REGEX MATCHALL "-march=([^ \n]*)" arch_list "${arch_list}")
+    string(REGEX REPLACE "-march=" "" arch_list "${arch_list}")
+  endif()
   set(${out_var} "${arch_list}" PARENT_SCOPE)
 endfunction()
 
@@ -61,9 +71,14 @@ function(__armlink_set_cpu_list lang out_var)
 
   execute_process(COMMAND "${CMAKE_LINKER}" ${__linker_wrapper_flags} --cpu=list
     OUTPUT_VARIABLE cpu_list
-    ERROR_VARIABLE cpu_list)
-  string(REGEX MATCHALL "--cpu=([^ \n]*)" cpu_list "${cpu_list}")
-  string(REGEX REPLACE "--cpu=" "" cpu_list "${cpu_list}")
+    ERROR_VARIABLE cpu_list
+    RESULT_VARIABLE _res
+    )
+  set(cpu_list)
+  if(_res EQUAL 0)
+    string(REGEX MATCHALL "--cpu=([^ \n]*)" cpu_list "${cpu_list}")
+    string(REGEX REPLACE "--cpu=" "" cpu_list "${cpu_list}")
+  endif()
   set(${out_var} "${cpu_list}" PARENT_SCOPE)
 endfunction()
 

Modules/Compiler/NAG-Fortran.cmake
@@ -6,10 +6,11 @@ if(NOT CMAKE_Fortran_COMPILER_WORKS AND NOT CMAKE_Fortran_COMPILER_FORCED)
     COMMAND ${CMAKE_Fortran_COMPILER} dummy.o -dryrun
     OUTPUT_VARIABLE _dryrun
     ERROR_VARIABLE _dryrun
+    RESULT_VARIABLE _dryrun_result
     )
   # Match an object file.
   string(REGEX MATCH "/[^ ]*/[^ /][^ /]*\\.o" _nag_obj "${_dryrun}")
-  if(_nag_obj)
+  if(_nag_obj AND _dryrun_result EQUAL 0)
     # Parse object directory and convert to a regex.
     string(REGEX REPLACE "/[^/]*$" "" _nag_dir "${_nag_obj}")
     string(REGEX REPLACE "([][+.*()^])" "\\\\\\1" _nag_regex "${_nag_dir}")

Modules/Internal/CMakeDetermineLinkerId.cmake
@@ -31,7 +31,10 @@ function(cmake_determine_linker_id lang linker)
                     OUTPUT_STRIP_TRAILING_WHITESPACE
                     ERROR_STRIP_TRAILING_WHITESPACE
                     COMMAND_ERROR_IS_FATAL NONE
+                    RESULT_VARIABLE _res
     )
+    # We capture "_res" but don't check it because it's expected that
+    # linkers will exit with non-zero on flags they do not have.
 
     string(JOIN "\" \"" flags_string ${flags})
     string(REGEX REPLACE "\n\n.*" "" linker_desc_head "${linker_desc}")

Modules/Linker/GNU.cmake
@@ -24,11 +24,13 @@ function(__linker_gnu lang)
       # Ninja 1.10 or upper is required
       execute_process(COMMAND "${CMAKE_MAKE_PROGRAM}" --version
         OUTPUT_VARIABLE _ninja_version
-        ERROR_VARIABLE _ninja_version)
+        ERROR_VARIABLE _ninja_version
+        RESULT_VARIABLE _res
+        )
       if (_ninja_version MATCHES "[0-9]+(\\.[0-9]+)*")
         set (_ninja_version "${CMAKE_MATCH_0}")
       endif()
-      if (_ninja_version VERSION_LESS "1.10")
+      if (NOT _res EQUAL 0 OR _ninja_version VERSION_LESS "1.10")
         set(CMAKE_${lang}_LINKER_DEPFILE_SUPPORTED FALSE)
       endif()
     endif()
@@ -38,8 +40,9 @@ function(__linker_gnu lang)
       if (CMAKE_${lang}_COMPILER_LINKER AND CMAKE_${lang}_COMPILER_LINKER_ID MATCHES "GNU|LLD|MOLD")
         execute_process(COMMAND "${CMAKE_${lang}_COMPILER_LINKER}" --help
                         OUTPUT_VARIABLE _linker_capabilities
-                        ERROR_VARIABLE _linker_capabilities)
-        if(_linker_capabilities MATCHES "--dependency-file")
+                        ERROR_VARIABLE _linker_capabilities
+                        RESULT_VARIABLE _res)
+        if(_res EQUAL 0 AND _linker_capabilities MATCHES "--dependency-file")
           set(CMAKE_${lang}_LINKER_DEPFILE_SUPPORTED TRUE)
         else()
           set(CMAKE_${lang}_LINKER_DEPFILE_SUPPORTED FALSE)

Modules/Platform/Android/Determine-Compiler-Standalone.cmake
@@ -30,8 +30,9 @@ execute_process(
   OUTPUT_VARIABLE _gcc_version
   ERROR_VARIABLE _gcc_error
   OUTPUT_STRIP_TRAILING_WHITESPACE
+  RESULT_VARIABLE _gcc_result
   )
-if(_gcc_version MATCHES "^([0-9]+\\.[0-9]+)")
+if(_gcc_result EQUAL 0 AND _gcc_version MATCHES "^([0-9]+\\.[0-9]+)")
   set(_ANDROID_TOOL_C_TOOLCHAIN_VERSION "${CMAKE_MATCH_1}")
 else()
   message(FATAL_ERROR

commit cc6bcefc6a686a0caea5ea7c4258f3070c4e2983

Apple: Restore detection of platform-specific GNU Fortran flags
Detection of `-isysroot` was broken by commit a1b5c0da87 (Modules:
Tolerate CMAKE_EXECUTE_PROCESS_COMMAND_ERROR_IS_FATAL, 2026-05-05,
v4.4.0-rc1~209^2~1) because `gfortran -v --help` returns non-zero.

Fixes: #28111

Modules/Platform/Apple-GNU.cmake
@@ -31,9 +31,9 @@ macro(cmake_gnu_set_sysroot_flag lang)
       COMMAND ${CMAKE_${lang}_COMPILER} "-v" "--help"
       OUTPUT_VARIABLE _gcc_help
       ERROR_VARIABLE _gcc_help
-      RESULT_VARIABLE _gcc_result
+      RESULT_VARIABLE _gcc_result # may be non-zero
       )
-    if(_gcc_result EQUAL 0 AND "${_gcc_help}" MATCHES "isysroot")
+    if("${_gcc_help}" MATCHES "isysroot")
       message(CHECK_PASS "yes")
       set(CMAKE_${lang}_SYSROOT_FLAG "-isysroot")
     else()
@@ -52,9 +52,9 @@ macro(cmake_gnu_set_osx_deployment_target_flag lang)
       COMMAND ${CMAKE_${lang}_COMPILER} "-v" "--help"
       OUTPUT_VARIABLE _gcc_help
       ERROR_VARIABLE _gcc_help
-      RESULT_VARIABLE _gcc_result
+      RESULT_VARIABLE _gcc_result # may be non-zero
       )
-    if(_gcc_result EQUAL 0 AND "${_gcc_help}" MATCHES "macosx-version-min")
+    if("${_gcc_help}" MATCHES "macosx-version-min")
       message(CHECK_PASS "yes")
       set(CMAKE_${lang}_OSX_DEPLOYMENT_TARGET_FLAG "-mmacosx-version-min=")
     else()